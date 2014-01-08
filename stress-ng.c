62b37650d16b944233228f7155c545d1527bcf00
Colin Ian King <colin.king@canonical.com>
2014-01-08 19:28:01 +0000

    Update copyright year
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-ng.c
+++ stress-ng.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Canonical
+ * Copyright (C) 2013-2014 Canonical
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
