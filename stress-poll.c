2876761030352982c9e158d3562001cad2b690f7
Colin Ian King <colin.king@canonical.com>
2014-11-06 00:14:38 +0000

    Make poll and select act on multiple pipe fds
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-poll.c
+++ stress-poll.c
@@ -30,8 +30,37 @@
 #include <unistd.h>
 #include <poll.h>
 #include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
 
 #include "stress-ng.h"
+
+#define MAX_PIPES	(5)
+#define POLL_BUF	(4)
+
+static inline void pipe_read(int fd, int n)
+{
+	char buf[POLL_BUF];
+	ssize_t ret;
+
+	ret = read(fd, buf, sizeof(buf));
+	if (opt_flags & OPT_FLAGS_VERIFY) {
+		if (ret < 0) {
+			pr_fail(stderr, "pipe read error detected\n");
+			return;
+		}
+		if (ret > 0) {
+			ssize_t i;
+
+			for (i = 0; i < ret; i++) {
+				if (buf[i] != '0' + n) {
+					pr_fail(stderr, "pipe read error, expecting different data on pipe\n");
+					return;
+				}
+			}
+		}
+	}
+}
 
 /*
  *  stress_poll()
@@ -43,33 +72,119 @@ int stress_poll(
 	const uint64_t max_ops,
 	const char *name)
 {
+	int pipefds[MAX_PIPES][2];
+	int i;
+	pid_t pid;
+	int ret = EXIT_SUCCESS;
+
 	(void)instance;
-	(void)name;
 
-	do {
-		struct timeval tv;
-		int ret;
+	for (i = 0; i < MAX_PIPES; i++) {
+		if (pipe(pipefds[i]) < 0) {
+			pr_failed_dbg(name, "pipe");
+			while (--i >= 0) {
+				(void)close(pipefds[i][0]);
+				(void)close(pipefds[i][1]);
+			}
+			return EXIT_FAILURE;
+		}
+	}
 
-		ret = poll(NULL, 0, 0);
-		if ((opt_flags & OPT_FLAGS_VERIFY) &&
-                    (ret < 0) && (errno != EINTR)) {
-			pr_fail(stderr, "poll failed with error: %d (%s)\n",
-				errno, strerror(errno));
+	pid = fork();
+	if (pid < 0) {
+		pr_failed_dbg(name, "fork");
+		ret = EXIT_FAILURE;
+		goto tidy;
+	}
+	else if (pid == 0) {
+		/* Child writer */
+		for (i = 0; i < MAX_PIPES; i++)
+			(void)close(pipefds[i][0]);
+
+		for (;;) {
+			char buf[POLL_BUF];
+
+			memset(buf, '0' + i, sizeof(buf));
+
+			/* Write on a randomly chosen pipe */
+			i = (mwc() >> 8) % MAX_PIPES;
+			if (write(pipefds[i][1], buf, sizeof(buf)) < 0) {
+				pr_failed_dbg(name, "write");
+				goto abort;
+			}
+		}
+abort:
+		for (i = 0; i < MAX_PIPES; i++)
+			(void)close(pipefds[i][1]);
+		exit(EXIT_SUCCESS);
+	} else {
+		/* Parent read */
+
+		int maxfd = 0;
+		struct pollfd fds[MAX_PIPES];
+		fd_set rfds;
+
+		FD_ZERO(&rfds);
+		for (i = 0; i < MAX_PIPES; i++) {
+			fds[i].fd = pipefds[i][0];
+			fds[i].events = POLLIN;
+			fds[i].revents = 0;
+
+			FD_SET(pipefds[i][0], &rfds);
+			if (pipefds[i][0] > maxfd)
+				maxfd = pipefds[i][0];
 		}
 
-		tv.tv_sec = 0;
-		tv.tv_usec = 0;
-		ret = select(0, NULL, NULL, NULL, &tv);
-		if ((opt_flags & OPT_FLAGS_VERIFY) &&
-                    (ret < 0) && (errno != EINTR)) {
-			pr_fail(stderr, "select failed with error: %d (%s)\n",
-				errno, strerror(errno));
-		}
-		if (!opt_do_run)
-			break;
-		(void)sleep(0);
-		(*counter)++;
-	} while (opt_do_run && (!max_ops || *counter < max_ops));
+		do {
+			struct timeval tv;
+			int ret;
 
-	return EXIT_SUCCESS;
+			/* First, stress out poll */
+			ret = poll(fds, MAX_PIPES, mwc() & 15);
+			if ((opt_flags & OPT_FLAGS_VERIFY) &&
+			    (ret < 0) && (errno != EINTR)) {
+				pr_fail(stderr, "poll failed with error: %d (%s)\n",
+				errno, strerror(errno));
+			}
+			if (ret > 0) {
+				for (i = 0; i < MAX_PIPES; i++)
+					if (fds[i].revents == POLLIN)
+						pipe_read(fds[i].fd, i);
+			}
+
+			/* Second, stress out select */
+			tv.tv_sec = 0;
+			tv.tv_usec = mwc() & 1023;
+			ret = select(maxfd + 1, &rfds, NULL, NULL, &tv);
+			if ((opt_flags & OPT_FLAGS_VERIFY) &&
+			    (ret < 0) && (errno != EINTR)) {
+				pr_fail(stderr, "select failed with error: %d (%s)\n",
+					errno, strerror(errno));
+			}
+			if (ret > 0) {
+				for (i = 0; i < MAX_PIPES; i++) {
+					if (FD_ISSET(pipefds[i][0], &rfds))
+						pipe_read(pipefds[i][0], i);
+					FD_SET(pipefds[i][0], &rfds);
+				}
+			}
+			if (!opt_do_run)
+				break;
+
+			/* Third, stress zero sleep, this is like a select zero timeout */
+			(void)sleep(0);
+
+			(*counter)++;
+		} while (opt_do_run && (!max_ops || *counter < max_ops));
+
+		(void)kill(pid, SIGKILL);
+	}
+
+tidy:
+	for (i = 0; i < MAX_PIPES; i++) {
+		(void)close(pipefds[i][0]);
+		(void)close(pipefds[i][1]);
+	}
+
+	return ret;
 }
