b811f63fadac5c3713112a1dbd906cbc2143e8c0
Colin Ian King <colin.king@canonical.com>
2015-01-24 13:41:14 +0000

    Remove need for ms_flags if building for GNU HURD
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-mmap.c
+++ stress-mmap.c
@@ -120,8 +120,10 @@ int stress_mmap(
 #endif
 	const size_t sz = opt_mmap_bytes & ~(page_size - 1);
 	const size_t pages4k = sz / page_size;
+#if !defined(__gnu_hurd__)
 	const int ms_flags = (opt_flags & OPT_FLAGS_MMAP_ASYNC) ?
 		MS_ASYNC : MS_SYNC;
+#endif
 	const pid_t pid = getpid();
 	int fd = -1, flags = MAP_PRIVATE | MAP_ANONYMOUS;
 	char filename[PATH_MAX];
