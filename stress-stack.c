1b3706b46a4f0a5dbe0bdb3737b720327f630441
Colin Ian King <colin.king@canonical.com>
2015-02-01 14:57:20 +0000

    Add NetBSD support to stress-stack
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-stack.c
+++ stress-stack.c
@@ -30,7 +30,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
-#if !defined(__FreeBSD__) && !defined(__OpenBSD__)
+#if !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__NetBSD__)
 #include <alloca.h>
 #endif
 
