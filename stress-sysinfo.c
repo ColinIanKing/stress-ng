25ffcc56a766bca8f1e4e5b38feaff92e0611716
Colin Ian King <colin.king@canonical.com>
2015-04-22 12:08:03 +0100

    stress-sysinfo: make EACCES non-fatal error on stat
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-sysinfo.c
+++ stress-sysinfo.c
@@ -95,7 +95,7 @@ int stress_sysinfo(
 				continue;
 			}
 			if ((ret < 0) && (opt_flags & OPT_FLAGS_VERIFY)) {
-				if (errno != ENOSYS && errno != EOVERFLOW) {
+				if (errno != ENOSYS && errno != EOVERFLOW && errno != EACCES) {
 					pr_fail(stderr, "%s: statfs on %s failed: errno=%d (%s)\n",
 						name, mnts[i], errno, strerror(errno));
 				}
