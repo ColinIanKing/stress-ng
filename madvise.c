e5f738f2023c64a7f4d50a13d31048b5879263c5
Colin Ian King <colin.king@canonical.com>
2015-06-02 19:31:54 +0100

    Make madvise build on non-GNU/HURD systems
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- madvise.c
+++ madvise.c
@@ -33,7 +33,7 @@
 
 #include "stress-ng.h"
 
-#if _BSD_SOURCE
+#if !defined(__gnu_hurd__)
 static const int madvise_options[] = {
 #ifdef MADV_NORMAL
 	MADV_NORMAL,
@@ -90,9 +90,9 @@ static const int madvise_options[] = {
  */
 int madvise_random(void *addr, const size_t length)
 {
-#if _BSD_SOURCE
+#if !defined(__gnu_hurd__)
 	if (opt_flags & OPT_FLAGS_MMAP_MADVISE) {
-		int i = (mwc() >> 7) % SIZEOF_ARRAY(madvise_options);
+		int i = (mwc32() >> 7) % SIZEOF_ARRAY(madvise_options);
 
 		return madvise(addr, length, madvise_options[i]);
 	}
