ColinIanKing/stress-ng

commit 5d816c71b86732af6c9a86ffcb054e6304e6b10d

Add NUMA stressor
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-ng.h
@@ -724,6 +724,15 @@ typedef enum {
 #endif
 	STRESS_NICE,
 	STRESS_NULL,
+#if defined(__linux__) &&		\
+    defined(__NR_get_mempolicy) &&	\
+    defined(__NR_mbind) &&		\
+    defined(__NR_migrate_pages) &&	\
+    defined(__NR_move_pages) &&		\
+    defined(__NR_set_mempolicy)
+	__STRESS_NUMA,
+#define STRESS_NUMA __STRESS_NUMA
+#endif
 	STRESS_OPEN,
 	STRESS_PIPE,
 	STRESS_POLL,
@@ -830,7 +839,7 @@ typedef enum {
 #define STRESS_WAIT __STRESS_WAIT
 #endif
 	STRESS_WCS,
-#if defined(__linux__)
+#if 0 && defined(__linux__)
 	__STRESS_XATTR,
 #define STRESS_XATTR __STRESS_XATTR
 #endif
@@ -1129,6 +1138,11 @@ typedef enum {
 
 	OPT_NULL,
 	OPT_NULL_OPS,
+
+#if defined(STRESS_NUMA)
+	OPT_NUMA,
+	OPT_NUMA_OPS,
+#endif
 
 	OPT_OPEN_OPS,
 
@@ -1676,6 +1690,7 @@ STRESS(stress_mq);
 STRESS(stress_nice);
 STRESS(stress_noop);
 STRESS(stress_null);
+STRESS(stress_numa);
 STRESS(stress_open);
 STRESS(stress_pipe);
 STRESS(stress_poll);

Makefile
@@ -79,6 +79,7 @@ SRC =   stress-affinity.c \
 	stress-nice.c \
 	stress-noop.c \
 	stress-null.c \
+	stress-numa.c \
 	stress-open.c \
 	stress-pipe.c \
 	stress-poll.c \

stress-ng.1
@@ -989,6 +989,12 @@ start N workers writing to /dev/null.
 .B \-\-null\-ops N
 stop null stress workers after N /dev/null bogo write operations.
 .TP
+.B \-\-numa N
+start N workers that migrate stressors and a 4MB memory mapped buffer around all the available NUMA nodes.  This uses migrate_pages(2) to move the stressors and mbind(2) and move_pages(2) to move the pages of the mapped buffer. After each move, the buffer is written to to force activity over the bus which results cache misses.
+.TP
+.B \-\-numa\-ops N
+stop NUMA stress workers after N bogo NUMA operations.
+.TP
 .B \-o N, \-\-open N
 start N workers that perform open(2) and then close(2) operations on /dev/zero. The maximum opens at one time is system defined, so the test will run up to this maximum, or 65536 open file descriptors, which ever comes first.
 .TP

stress-ng.c
@@ -231,6 +231,9 @@ static const stress_t stressors[] = {
 #endif
 	STRESSOR(nice, NICE, CLASS_SCHEDULER | CLASS_OS),
 	STRESSOR(null, NULL, CLASS_DEV | CLASS_MEMORY | CLASS_OS),
+#if defined(STRESS_NUMA)
+	STRESSOR(numa, NUMA, CLASS_CPU | CLASS_MEMORY | CLASS_OS),
+#endif
 	STRESSOR(open, OPEN, CLASS_FILESYSTEM | CLASS_OS),
 	STRESSOR(pipe, PIPE, CLASS_PIPE_IO | CLASS_MEMORY | CLASS_OS),
 	STRESSOR(poll, POLL, CLASS_SCHEDULER | CLASS_OS),
@@ -549,6 +552,10 @@ static const struct option long_options[] = {
 	{ "no-madvise",	0,	0,	OPT_NO_MADVISE },
 	{ "null",	1,	0,	OPT_NULL },
 	{ "null-ops",	1,	0,	OPT_NULL_OPS },
+#if defined(STRESS_NUMA)
+	{ "numa",	1,	0,	OPT_NUMA },
+	{ "numa-ops",	1,	0,	OPT_NUMA_OPS },
+#endif
 	{ "open",	1,	0,	OPT_OPEN },
 	{ "open-ops",	1,	0,	OPT_OPEN_OPS },
 #if defined(STRESS_PAGE_IN)
@@ -982,6 +989,10 @@ static const help_t help_stressors[] = {
 	{ NULL,		"nice-ops N",		"stop when N nice bogo operations completed" },
 	{ NULL,		"null N",		"start N workers writing to /dev/null" },
 	{ NULL,		"null-ops N",		"stop when N /dev/null bogo write operations completed" },
+#if defined(STRESS_NUMA)
+	{ NULL,		"numa N",		"start N workers stressing NUMA interfaces" },
+	{ NULL,		"numa-ops N",		"stop when N NUMA bogo operations completed" },
+#endif
 	{ "o",		"open N",		"start N workers exercising open/close" },
 	{ NULL,		"open-ops N",		"stop when N open/close bogo operations completed" },
 	{ "p N",	"pipe N",		"start N workers exercising pipe I/O" },

stress-numa.c
@@ -1,0 +1,315 @@
+/*
+ * Copyright (C) 2013-2015 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This code is a complete clean re-write of the stress tool by
+ * Colin Ian King <colin.king@canonical.com> and attempts to be
+ * backwardly compatible with the stress tool by Amos Waterland
+ * <apw@rossby.metr.ou.edu> but has more stress tests and more
+ * functionality.
+ *
+ */
+#define _GNU_SOURCE
+
+#include "stress-ng.h"
+
+#if defined(STRESS_NUMA)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/syscall.h>
+
+#define NUMA_LONG_BITS		(sizeof(unsigned long) * 8)
+
+#define NUMA_GETBIT(a, i)      (a[i / NUMA_LONG_BITS] & (1 << (i & (NUMA_LONG_BITS - 1))))
+#define NUMA_CLRBIT(a, i)      (a[i / NUMA_LONG_BITS] &= ~(1 << (i & (NUMA_LONG_BITS - 1))))
+#define NUMA_SETBIT(a, i)      (a[i / NUMA_LONG_BITS] |= (1 << (i & (NUMA_LONG_BITS - 1))))
+
+#define MPOL_DEFAULT		(0)
+#define MPOL_PREFERRED		(1)
+#define MPOL_BIND		(2)
+#define MPOL_INTERLEAVE		(3)
+
+#define MPOL_F_NODE		(1 << 0)
+#define MPOL_F_ADDR		(1 << 1)
+#define MPOL_F_MEMS_ALLOWED	(1 << 2)
+
+#define MPOL_MF_STRICT		(1 << 0)
+#define MPOL_MF_MOVE		(1 << 1)
+#define MPOL_MF_MOVE_ALL	(1 << 2)
+
+#define SYS_NODE_PATH	"/sys/devices/system/node"
+#define MMAP_SZ			(4 * MB)
+
+typedef struct node {
+	uint32_t	node_id;
+	struct node	*next;
+} node_t;
+
+/*
+ *  Rather than have all the libnuma overhead, just
+ *  perform NUMA operations via the system call
+ *  interfaces.
+ */
+static int sys_get_mempolicy(
+	int *mode, unsigned long *nodemask,
+	unsigned long maxnode, unsigned long addr,
+	unsigned long flags)
+{
+	return syscall(__NR_get_mempolicy,
+		mode, nodemask, maxnode, addr, flags);
+}
+
+static int sys_set_mempolicy(
+	int mode,
+	unsigned long *nodemask,
+	unsigned long maxnode)
+{
+	return syscall(__NR_set_mempolicy,
+		mode, nodemask, maxnode);
+}
+
+static long sys_mbind(
+	void *addr, unsigned long len,
+	int mode, const unsigned long *nodemask,
+	unsigned long maxnode, unsigned flags)
+{
+	return syscall(__NR_mbind,
+		addr, len, mode, nodemask, maxnode, flags);
+}
+
+static long migrate_pages(
+	int pid, unsigned long maxnode,
+	const unsigned long *old_nodes,
+	const unsigned long *new_nodes)
+{
+	return syscall(__NR_migrate_pages,
+		pid, maxnode, old_nodes, new_nodes);
+}
+
+static long move_pages(
+	int pid, unsigned long count,
+	void **pages, const int *nodes,
+	int *status, int flags)
+{
+	return syscall(__NR_move_pages,
+		pid, count, pages, nodes,
+		status, flags);
+}
+
+/*
+ *  stress_numa_free_nodes()
+ *	free circular list of node info
+ */
+static void stress_numa_free_nodes(node_t *nodes)
+{
+	node_t *n = nodes;
+
+	while (n) {
+		node_t *next = n->next;
+
+		free(n);
+		n = next;
+
+		if (n == nodes)
+			break;
+	}
+}
+
+/*
+ *  stress_numa_get_nodes(void)
+ *	collect number of nodes, add them to a
+ *	circular linked list
+ */
+static int stress_numa_get_nodes(node_t **node_ptr)
+{
+	DIR *dir;
+	struct dirent *entry;
+	unsigned long n = 0;
+	node_t *tail = NULL;
+	*node_ptr = NULL;
+
+	dir = opendir(SYS_NODE_PATH);
+	if (!dir)
+		return -1;
+
+	while ((entry = readdir(dir)) != NULL) {
+		uint32_t node_id;
+		node_t *node;
+
+		if (strncmp(entry->d_name, "node", 4))
+			continue;
+		if (!isdigit(entry->d_name[4]))
+			continue;
+		if (sscanf(&entry->d_name[4], "%u10", &node_id) != 1)
+			continue;
+
+		node = calloc(1, sizeof(*node));
+		if (!node) {
+			break;
+		}
+		node->node_id = node_id;
+		node->next = *node_ptr;
+		*node_ptr = node;
+		if (!tail)
+			tail = node;
+		tail->next = node;
+		n++;
+	}
+	(void)closedir(dir);
+
+	return n;
+}
+
+/*
+ *  stress_numa()
+ *	stress the Linux NUMA interfaces
+ */
+int stress_numa(
+        uint64_t *const counter,
+        const uint32_t instance,
+        const uint64_t max_ops,
+        const char *name)
+{
+	unsigned long numa_nodes, max_nodes;
+	unsigned long nbits, lbits = 8 * sizeof(unsigned long);
+	uint8_t *buf;
+	const pid_t mypid = getpid();
+	const unsigned long page_sz = stress_get_pagesize();
+	const unsigned long num_pages = MMAP_SZ / page_sz;
+	node_t *n;
+	int rc = EXIT_FAILURE;
+
+	(void)instance;
+
+	numa_nodes = stress_numa_get_nodes(&n);
+	if (numa_nodes <= 1) {
+		pr_inf(stderr, "Multiple NUMA nodes not found, aborting test.\n");
+		goto numa_free;
+	}
+	nbits = (numa_nodes + lbits - 1) / lbits;
+	max_nodes = nbits * lbits;
+
+	/*
+	 *  We need a buffer to migrate around NUMA nodes
+	 */
+	buf = mmap(NULL, MMAP_SZ, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
+	if (buf == MAP_FAILED) {
+		pr_fail(stderr, "%s: mmap'd region of %zu bytes failed",
+			name, (size_t)MMAP_SZ);
+		goto numa_free;
+	}
+
+	do {
+		int j, mode, ret, status[num_pages], dest_nodes[num_pages];
+		unsigned long i, node_mask[lbits], old_node_mask[lbits];
+		void *pages[num_pages];
+		uint8_t *ptr;
+		node_t *n_tmp;
+
+		/*
+		 *  Fetch memory policy
+		 */
+		ret = sys_get_mempolicy(&mode, node_mask, max_nodes,
+			(unsigned long)buf, MPOL_F_ADDR);
+		if (ret < 0) {
+			pr_fail(stderr, "%s: get_mempolicy: errno=%d (%s)\n",
+				name, errno, strerror(errno));
+		}
+		if (!opt_do_run)
+			break;
+
+		memset(node_mask, 0, sizeof(node_mask));
+		NUMA_SETBIT(node_mask, n->node_id);
+		ret = sys_set_mempolicy(MPOL_PREFERRED, node_mask, max_nodes);
+		if (ret < 0) {
+			pr_fail(stderr, "%s: set_mempolicy: errno=%d (%s)\n",
+				name, errno, strerror(errno));
+		}
+		memset(buf, 0xff, MMAP_SZ);
+		if (!opt_do_run)
+			break;
+
+		/*
+		 *  mbind the buffer
+		 */
+		memset(node_mask, 0, sizeof(node_mask));
+		NUMA_SETBIT(node_mask, n->node_id);
+		ret = sys_mbind(buf, MMAP_SZ, MPOL_BIND, node_mask, max_nodes, MPOL_MF_STRICT);
+		if (ret < 0) {
+			pr_fail(stderr, "%s: mbind: errno=%d (%s)\n",
+				name, errno, strerror(errno));
+		}
+		memset(buf, 0xaa, MMAP_SZ);
+		if (!opt_do_run)
+			break;
+
+		/* Move to next node */
+		n = n->next;
+
+		/*
+		 *  Migrate all this processes pages to the current new node
+		 */
+		memset(old_node_mask, 0xff, sizeof(old_node_mask));
+		memset(node_mask, 0, sizeof(node_mask));
+		NUMA_SETBIT(node_mask, n->node_id);
+		ret = migrate_pages(mypid, max_nodes, old_node_mask, node_mask);
+		if (ret < 0) {
+			pr_fail(stderr, "%s: migrate_pages: errno=%d (%s)\n",
+				name, errno, strerror(errno));
+		}
+		if (!opt_do_run)
+			break;
+
+		n_tmp = n;
+		for (j = 0; j < 16; j++) {
+			/*
+			 *  Now move pages to lots of different numa nodes
+			 */
+			for (ptr = buf, i = 0; i < num_pages; i++, ptr += page_sz, n_tmp = n_tmp->next) {
+				pages[i] = ptr;
+				dest_nodes[i] = n_tmp->node_id;
+			}
+			memset(status, 0, sizeof(status));
+			ret = move_pages(mypid, num_pages, pages, dest_nodes, status, MPOL_MF_MOVE);
+			if (ret < 0) {
+				pr_fail(stderr, "%s: migrate_pages: errno=%d (%s)\n",
+					name, errno, strerror(errno));
+			}
+			memset(buf, j, MMAP_SZ);
+			if (!opt_do_run)
+				break;
+		}
+		(*counter)++;
+	} while (opt_do_run && (!max_ops || *counter < max_ops));
+
+	rc = EXIT_SUCCESS;
+	munmap(buf, MMAP_SZ);
+numa_free:
+	stress_numa_free_nodes(n);
+
+	return rc;
+}
+#endif

commit 1c8cc958d1804d8dcec282c2f6e3938d777e2872

Re-enable xattr tests
Commit 5d816c71b86732af6c9a86ffcb054e6304e6b10d accidentally
disabled the xttr tests

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-ng.h
@@ -851,7 +851,7 @@ typedef enum {
 #define STRESS_WAIT __STRESS_WAIT
 #endif
 	STRESS_WCS,
-#if 0 && defined(__linux__)
+#if defined(__linux__)
 	__STRESS_XATTR,
 #define STRESS_XATTR __STRESS_XATTR
 #endif