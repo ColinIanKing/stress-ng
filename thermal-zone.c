8bdce4b784ee973aa156e16f29fb66b18780bb96
Colin Ian King <colin.king@canonical.com>
2015-07-22 23:52:01 +0100

    thermal-zone: cast to double rather than float
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- thermal-zone.c
+++ thermal-zone.c
@@ -188,7 +188,7 @@ void tz_dump(
 			}
 
 			if (total) {
-				double temp = ((float)total / count) / 1000.0;
+				double temp = ((double)total / count) / 1000.0;
 				char *munged = munge_underscore((char *)stressors[i].name);
 
 				if (!dumped_heading) {
