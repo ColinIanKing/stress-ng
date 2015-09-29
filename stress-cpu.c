b1e8a8c81d9c15ed91a5d17655191ce5ae6534a8
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2015-09-29 09:31:19 +0100

    stress-cpu: check if floating-point complex supported by C lib (LP: #1500349)
    Luca Pizzamiglio noted:
    
    "I'm testing stress-ng on FreeBSD and I noted some compile problems, if
    an older compiler stack is used (normally it's a clang or gcc4.2.1
    problem).
    
    If floating point complex functions are missing (because the libm is not
    STDC_IEC_559_COMPLEX compliant), this patch avoid to compile
    stress_cpu_complex functions in a clean way, using the
    __STDC_IEC_559_COMPLEX__ preprocessor definition.
    
    The three functions stress_cpu_complex_double(),
    stress_cpu_complex_float() and stress_cpu_complex_long_double() are not
    compiled and executed."
    
    Signed-off-by: Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
    Reviewed-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-cpu.c
+++ stress-cpu.c
@@ -866,6 +866,7 @@ stress_cpu_fp(_Decimal128, decimal128, sinl, cosl)
 /* Append floating point literal specifier to literal value */
 #define FP(val, ltype)	val ## ltype
 
+#if defined(__STDC_IEC_559_COMPLEX__)
 /*
  *  Generic complex stressor macro
  */
@@ -891,6 +892,7 @@ static void HOT OPTIMIZE3 stress_cpu_ ## _name(const char *name)\
 stress_cpu_complex(complex float, f, complex_float, csinf, ccosf)
 stress_cpu_complex(complex double, , complex_double, csin, ccos)
 stress_cpu_complex(complex long double, l, complex_long_double, csinl, ccosl)
+#endif /* __STDC_IEC_559_COMPLEX__ */
 
 #define int_float_ops(_ftype, flt_a, flt_b, flt_c, flt_d,	\
 	_sin, _cos, int_a, int_b, _c1, _c2, _c3)		\
@@ -2058,9 +2060,11 @@ static stress_cpu_stressor_info_t cpu_methods[] = {
 	{ "ackermann",		stress_cpu_ackermann },
 	{ "bitops",		stress_cpu_bitops },
 	{ "callfunc",		stress_cpu_callfunc },
+#if defined(__STDC_IEC_559_COMPLEX__)
 	{ "cdouble",		stress_cpu_complex_double },
 	{ "cfloat",		stress_cpu_complex_float },
 	{ "clongdouble",	stress_cpu_complex_long_double },
+#endif /* __STDC_IEC_559_COMPLEX__ */
 	{ "correlate",		stress_cpu_correlate },
 	{ "crc16",		stress_cpu_crc16 },
 #if defined(STRESS_FLOAT_DECIMAL)
