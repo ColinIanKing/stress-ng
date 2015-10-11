1e899cd9e2a80e83ac282e7688295f4cda2ee3f2
Colin Ian King <colin.king@canonical.com>
2015-10-11 15:55:16 +0100

    stress-sysfs: make sysfs reader more stressful
    spawn multiple reader threads per sys entry to create racy
    and stressy reads
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-sysfs.c
+++ stress-sysfs.c
@@ -36,8 +36,19 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <pthread.h>
+#include <signal.h>
 
-#define SYS_BUF_SZ	(4096)
+#define SYS_BUF_SZ		(4096)
+#define MAX_READ_THREADS	(4)
+
+static volatile bool keep_running;
+static sigset_t set;
+
+typedef struct ctxt {
+	const char *name;
+	const char *path;
+} ctxt_t;
 
 /*
  *  stress_sys_read()
@@ -45,27 +56,30 @@
  */
 static inline void stress_sys_read(const char *name, const char *path)
 {
-	int fd, i;
+	int fd;
+	ssize_t i = 0;
 	char buffer[SYS_BUF_SZ];
 
 	if ((fd = open(path, O_RDONLY | O_NONBLOCK)) < 0)
 		return;
 
-	/* Limit to 4K * 4K of data to read per file */
-	for (i = 0; i < 4096; i++) {
-		ssize_t ret;
+	/*
+	 *  Multiple randomly sized reads
+	 */
+	while (i < (4096 * SYS_BUF_SZ)) {
+		ssize_t ret, sz = 1 + (mwc32() % sizeof(buffer));
 redo:
 		if (!opt_do_run)
 			break;
-
-		ret = read(fd, buffer, SYS_BUF_SZ);
+		ret = read(fd, buffer, sz);
 		if (ret < 0) {
-			if (errno == EINTR)
+			if ((errno == EAGAIN) || (errno == EINTR))
 				goto redo;
 			break;
 		}
-		if (ret < SYS_BUF_SZ)
+		if (ret < sz)
 			break;
+		i += sz;
 	}
 	(void)close(fd);
 
@@ -74,6 +88,77 @@ redo:
 	    (opt_flags & OPT_FLAGS_VERIFY)) {
 		pr_fail(stderr, "%s: R_OK access failed on %s which could be opened, errno=%d (%s)\n",
 			name, path, errno, strerror(errno));
+	}
+}
+
+/*
+ *  stress_sys_read_thread
+ *	keep exercising a sysfs entry until
+ *	controlling thread triggers an exit
+ */
+static void *stress_sys_read_thread(void *ctxt_ptr)
+{
+	static void *nowt = NULL;
+	uint8_t stack[SIGSTKSZ];
+        stack_t ss;
+	ctxt_t *ctxt = (ctxt_t *)ctxt_ptr;
+
+	/*
+	 *  Block all signals, let controlling thread
+	 *  handle these
+	 */
+	sigprocmask(SIG_BLOCK, &set, NULL);
+
+	/*
+	 *  According to POSIX.1 a thread should have
+	 *  a distinct alternative signal stack.
+	 *  However, we block signals in this thread
+	 *  so this is probably just totally unncessary.
+	 */
+	ss.ss_sp = (void *)stack;
+	ss.ss_size = SIGSTKSZ;
+	ss.ss_flags = 0;
+	if (sigaltstack(&ss, NULL) < 0) {
+		pr_failed_err("pthread", "sigaltstack");
+		return &nowt;
+	}
+	while (keep_running && opt_do_run)
+		stress_sys_read(ctxt->name, ctxt->path);
+
+	return &nowt;
+}
+
+/*
+ *  stress_proc_sys_threads()
+ *	create a bunch of threads to thrash read a sys entry
+ */
+static void stress_sys_read_threads(const char *name, const char *path)
+{
+	size_t i;
+	pthread_t pthreads[MAX_READ_THREADS];
+	int ret[MAX_READ_THREADS];
+	ctxt_t ctxt;
+
+	ctxt.name = name;
+	ctxt.path = path;
+
+	memset(ret, 0, sizeof(ret));
+
+	keep_running = true;
+
+	for (i = 0; i < MAX_READ_THREADS; i++) {
+		ret[i] = pthread_create(&pthreads[i], NULL, stress_sys_read_thread, &ctxt);
+	}
+	for (i = 0; i < 8; i++) {
+		if (!opt_do_run)
+			break;
+		stress_sys_read(name, path);
+	}
+	keep_running = false;
+
+	for (i = 0; i < MAX_READ_THREADS; i++) {
+		if (ret[i] == 0)
+			pthread_join(pthreads[i], NULL);
 	}
 }
 
@@ -122,7 +207,7 @@ static void stress_sys_dir(
 			if (sys_read) {
 				snprintf(filename, sizeof(filename),
 					"%s/%s", path, d->d_name);
-				stress_sys_read(name, filename);
+				stress_sys_read_threads(name, filename);
 			}
 			break;
 		default:
