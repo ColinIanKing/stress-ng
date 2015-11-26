b1051f773bef6ad968d27ecfe2cd0daa1f811626
Colin Ian King <colin.king@canonical.com>
2015-11-26 21:52:55 +0000

    stress-fallocate: add some more fallocate modes
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-fallocate.c
+++ stress-fallocate.c
@@ -60,7 +60,12 @@ static const int modes[] = {
 #if defined(FALLOC_FL_ZERO_RANGE)
 	FALLOC_FL_ZERO_RANGE,
 #endif
-	0
+#if defined(FALLOC_FL_COLLAPSE_RANGE)
+	FALLOC_FL_COLLAPSE_RANGE,
+#endif
+#if defined(FALLOC_FL_INSERT_RANGE)
+	FALLOC_FL_INSERT_RANGE,
+#endif
 };
 #endif
 
