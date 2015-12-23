e87b17f4403dc70187e53e7282e3aecc4e5a40db
Colin Ian King <colin.king@canonical.com>
2015-12-23 13:30:14 +0000

    Ensure test-appormor build test covers the aa API
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- test-apparmor.c
+++ test-apparmor.c
@@ -22,10 +22,35 @@
  * functionality.
  *
  */
-
+#include <unistd.h>
 #include <sys/apparmor.h>
 
 int main(void)
 {
-	return aa_is_enabled();
+	int ret;
+	aa_kernel_interface *kern_if;
+
+	ret = aa_is_enabled();
+	if (ret < 0)
+		return ret;
+
+	ret = aa_kernel_interface_new(&kern_if, NULL, NULL);
+	if (ret < 0)
+		return ret;
+
+	ret = aa_kernel_interface_load_policy(kern_if, NULL, 0);
+	if (ret < 0)
+		return ret;
+
+	ret = aa_kernel_interface_replace_policy(kern_if, NULL, 0);
+	if (ret < 0)
+		return ret;
+	
+	ret = aa_kernel_interface_remove_policy(kern_if, "dummy");
+	if (ret < 0)
+		return ret;
+
+	aa_kernel_interface_unref(kern_if);
+
+	return ret;
 }
