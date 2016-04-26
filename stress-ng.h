ColinIanKing/stress-ng

commit 8b9eb2900ab870c634d4343f2351e6a28b6ec5a0

Ensure all clone() calls are 16 byte aligned for aarch64 (LP: #1573117)
Turns out we get -EINVAL if the stack being passed to clone on aarch64
is not properly aligned.  Add a stack alignment helper and align all
stacks appropriately.

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-ng.h
@@ -2213,6 +2213,16 @@ static inline int exit_status(const int err)
 		EXIT_NO_RESOURCE : EXIT_FAILURE;
 }
 
+/*
+ *  Stack aligning for clone() system calls
+ *	align to nearest 16 bytes for aarch64 et al,
+ *	assumes we have enough slop to do this
+ */
+static inline void *align_stack(void *stack_top)
+{
+	return (void *)((uintptr_t)stack_top & ~(uintptr_t)0);
+
+}
 
 /* Memory tweaking */
 extern int madvise_random(void *addr, const size_t length);

stress-bind-mount.c
@@ -97,7 +97,9 @@ int stress_bind_mount(
 	context.counter = counter;
 
 	pid = clone(stress_bind_mount_child,
-		stack_top, CLONE_NEWUSER | CLONE_NEWNS | CLONE_NEWPID | CLONE_VM, &context, 0);
+		align_stack(stack_top),
+		CLONE_NEWUSER | CLONE_NEWNS | CLONE_NEWPID | CLONE_VM,
+		&context, 0);
 	if (pid < 0) {
 		int rc = exit_status(errno);
 

stress-clone.c
@@ -261,7 +261,8 @@ int stress_clone(
 			if (!clone_info)
 				break;
 			stack_top = clone_info->stack + stack_offset;
-			clone_info->pid = clone(clone_func, stack_top, flag, NULL);
+			clone_info->pid = clone(clone_func,
+				align_stack(stack_top), flag, NULL);
 			if (clone_info->pid == -1) {
 				/*
 				 * Reached max forks or error

stress-userfaultfd.c
@@ -236,7 +236,7 @@ int stress_userfaultfd(
 	 *  We need to clone the child and share the same VM address space
 	 *  as parent so we can perform the page fault handling
 	 */
-	pid = clone(stress_userfaultfd_child, stack_top, 
+	pid = clone(stress_userfaultfd_child, align_stack(stack_top),
 		SIGCHLD | CLONE_FILES | CLONE_FS | CLONE_SIGHAND | CLONE_VM, &c);
 	if (pid < 0) {
 		pr_err(stderr, "%s: fork failed, errno = %d (%s)\n",
@@ -265,7 +265,7 @@ int stress_userfaultfd(
 		if (msg.event != UFFD_EVENT_PAGEFAULT) {
 			pr_fail_err(name, "userfaultfd msg not pagefault event");
 			continue;
-		} 
+		}
 		/* We only expect a write fault */
 		if (!(msg.arg.pagefault.flags & UFFD_PAGEFAULT_FLAG_WRITE)) {
 			pr_fail_err(name, "userfaultfd msg not write pahge fault event");

stress-vm-rw.c
@@ -317,7 +317,8 @@ int stress_vm_rw(
 		return EXIT_FAILURE;
 	}
 
-	ctxt.pid = clone(stress_vm_child, stack_top, SIGCHLD | CLONE_VM, &ctxt);
+	ctxt.pid = clone(stress_vm_child, align_stack(stack_top),
+		SIGCHLD | CLONE_VM, &ctxt);
 	if (ctxt.pid < 0) {
 		(void)close(ctxt.pipe_wr[0]);
 		(void)close(ctxt.pipe_wr[1]);

commit d12036f3b0bb62b2bc9203d1ca4dae032d50092e

Fix alignment mask to ensure stacks are 16 byte aligned (LP: #1573117)
The original fix that was checked in had the incorrect mask, and was
hence doing nothing. Fix this by setting the mask correctly to ensure
the stack alignment has all zeros on the bottom 4 bits.

Fixes: 8b9eb2900ab870c634 (Ensure all clone() calls are 16 byte aligned for aarch64)
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-ng.h
@@ -2221,7 +2221,7 @@ static inline int exit_status(const int err)
  */
 static inline void *align_stack(void *stack_top)
 {
-	return (void *)((uintptr_t)stack_top & ~(uintptr_t)0);
+	return (void *)((uintptr_t)stack_top & ~(uintptr_t)0xf);
 
 }
 