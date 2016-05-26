271b5e97274c5444b2e3ef4370c3a660562130f3
Colin Ian King <colin.king@canonical.com>
2016-05-26 19:45:06 +0100

    stress-open: use different forms of open flag combinations
    Add more stress to open by using different open flags
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-open.c
+++ stress-open.c
@@ -34,6 +34,117 @@
 
 #include "stress-ng.h"
 
+typedef int (*open_func_t)(void);
+
+static int open_dev_zero_rd(void)
+{
+	int flags = 0;
+#if defined(O_ASYNC)
+	flags |= (mwc32() & O_ASYNC);
+#endif
+#if defined(O_CLOEXEC)
+	flags |= (mwc32() & O_CLOEXEC);
+#endif
+#if defined(O_LARGEFILE)
+	flags |= (mwc32() & O_LARGEFILE);
+#endif
+#if defined(O_NOFOLLOW)
+	flags |= (mwc32() & O_NOFOLLOW);
+#endif
+#if defined(O_NONBLOCK)
+	flags |= (mwc32() & O_NONBLOCK);
+#endif
+
+	return open("/dev/zero", O_RDONLY | flags);
+}
+
+static int open_dev_null_wr(void)
+{
+	int flags = 0;
+#if defined(O_ASYNC)
+	flags |= (mwc32() & O_ASYNC);
+#endif
+#if defined(O_CLOEXEC)
+	flags |= (mwc32() & O_CLOEXEC);
+#endif
+#if defined(O_LARGEFILE)
+	flags |= (mwc32() & O_LARGEFILE);
+#endif
+#if defined(O_NOFOLLOW)
+	flags |= (mwc32() & O_NOFOLLOW);
+#endif
+#if defined(O_NONBLOCK)
+	flags |= (mwc32() & O_NONBLOCK);
+#endif
+#if defined(O_DSYNC)
+	flags |= (mwc32() & O_DSYNC);
+#endif
+#if defined(O_SYNC)
+	flags |= (mwc32() & O_SYNC);
+#endif
+
+	return open("/dev/null", O_WRONLY | flags);
+}
+
+#if defined(O_TMPFILE)
+static int open_tmp_rdwr(void)
+{
+	int flags = 0;
+#if defined(O_TRUNC)
+	flags |= (mwc32() & O_TRUNC);
+#endif
+#if defined(O_APPEND)
+	flags |= (mwc32() & O_APPEND);
+#endif
+#if defined(O_NOATIME)
+	flags |= (mwc32() & O_NOATIME);
+#endif
+#if defined(O_DIRECT)
+	flags |= (mwc32() & O_DIRECT);
+#endif
+	return open("/tmp", O_TMPFILE | flags | O_RDWR, S_IRUSR | S_IWUSR);
+}
+#endif
+
+#if defined(O_TMPFILE) && defined(O_EXCL)
+static int open_tmp_rdwr_excl(void)
+{
+	return open("/tmp", O_TMPFILE | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+}
+#endif
+
+#if defined(O_DIRECTORY)
+static int open_dir(void)
+{
+	return open(".", O_DIRECTORY | O_RDONLY);
+}
+#endif
+
+#if defined(O_PATH)
+static int open_path(void)
+{
+	return open(".", O_DIRECTORY | O_PATH);
+}
+#endif
+
+
+static open_func_t open_funcs[] = {
+	open_dev_zero_rd,
+	open_dev_null_wr,
+#if defined(O_TMPFILE)
+	open_tmp_rdwr,
+#endif
+#if defined(O_TMPFILE) && defined(O_EXCL)
+	open_tmp_rdwr_excl,
+#endif
+#if defined(O_DIRECTORY)
+	open_dir,
+#endif
+#if defined(O_PATH)
+	open_path
+#endif
+};
+
 /*
  *  stress_open()
  *	stress system by rapid open/close calls
@@ -53,7 +164,10 @@ int stress_open(
 
 	do {
 		for (i = 0; i < max_fd; i++) {
-			fds[i] = open("/dev/zero", O_RDONLY);
+			int idx = mwc32() % SIZEOF_ARRAY(open_funcs);
+
+			fds[i] = open_funcs[idx]();
+
 			if (fds[i] < 0)
 				break;
 			if (!opt_do_run)
