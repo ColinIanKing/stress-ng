5d921a6dde9319358b436b7b350b309f49f184db
Colin Ian King <colin.king@canonical.com>
2016-11-17 21:29:53 +0000

    stress-aio-linux: use return as errno rather than errno
    the aio_* API is unlike most system calls, it returns
    the errno as the -ve return from the call. Fix the stressor
    to use this return correctly.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-aio-linux.c
+++ stress-aio-linux.c
@@ -177,6 +177,7 @@ int stress_aiol(
 		}
 		ret = io_submit(ctx, (long)opt_aio_linux_requests, cbs);
 		if (ret < 0) {
+			errno =- ret;
 			if (errno == EAGAIN)
 				continue;
 			pr_fail_err(name, "io_submit");
@@ -200,6 +201,7 @@ int stress_aiol(
 
 			ret = io_getevents(ctx, 1, n, events, timeout_ptr);
 			if (ret < 0) {
+				errno =- ret;
 				if ((errno == EINTR) && (opt_do_run))
 					continue;
 				pr_fail_err(name, "io_getevents");
