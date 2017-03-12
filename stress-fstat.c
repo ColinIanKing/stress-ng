0e4a59923caacab16fd189f54b622f97869a7921
Colin Ian King <colin.king@canonical.com>
2017-03-12 23:08:01 +0000

    stress-fstat: run multiple threads on fstat for more stress
    Try to break each device being fstat'd by exercising it with
    multiple threads.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-fstat.c
+++ stress-fstat.c
@@ -24,17 +24,37 @@
  */
 #include "stress-ng.h"
 
+#define MAX_FSTAT_THREADS	(4)
+#define FSTAT_LOOPS		(16)
+
 static const char *opt_fstat_dir = "/dev";
 static sigjmp_buf jmpbuf;
+static volatile bool keep_running;
+
+/* paths we should never stat */
+static const char *blacklist[] = {
+	"/dev/watchdog"
+};
+
+/* stat path information */
+typedef struct stat_info {
+	char		*path;		/* path to stat */
+	bool		ignore;		/* true to ignore this path */
+	bool		access;		/* false if we can't access path */
+	struct stat_info *next;		/* next stat_info in list */
+} stat_info_t;
+
+/* Thread context information */
+typedef struct ctxt {
+	const args_t 	*args;		/* Stressor args */
+	stat_info_t 	*si;		/* path stat information */
+	const uid_t	euid;		/* euid of process */
+} ctxt_t;
 
 void stress_set_fstat_dir(const char *optarg)
 {
 	opt_fstat_dir = optarg;
 }
-
-static const char *blacklist[] = {
-	"/dev/watchdog"
-};
 
 /*
  *  handle_fstat_sigalrm()
@@ -43,8 +63,8 @@ static const char *blacklist[] = {
 static void MLOCKED handle_fstat_sigalrm(int dummy)
 {
 	(void)dummy;
-	g_keep_stressing_flag = false;
 
+	g_keep_stressing_flag = false;
 	siglongjmp(jmpbuf, 1);
 }
 
@@ -63,26 +83,129 @@ static bool do_not_stat(const char *filename)
 	return false;
 }
 
+static void stress_fstat_helper(const ctxt_t *ctxt)
+{
+	int fd;
+	struct stat buf;
+	stat_info_t *si = ctxt->si;
+
+	if ((stat(si->path, &buf) < 0) && (errno != ENOMEM)) {
+		si->ignore = true;
+		return;
+	}
+	if ((lstat(si->path, &buf) < 0) && (errno != ENOMEM)) {
+		si->ignore = true;
+		return;
+	}
+	/*
+	 *  Opening /dev files such as /dev/urandom
+	 *  may block when running as root, so
+	 *  avoid this.
+	 */
+	if ((si->access) && (ctxt->euid)) {
+		fd = open(si->path, O_RDONLY | O_NONBLOCK);
+		if (fd < 0) {
+			si->access = false;
+			return;
+		}
+		if ((fstat(fd, &buf) < 0) && (errno != ENOMEM))
+			si->ignore = true;
+		(void)close(fd);
+	}
+}
+
+
+/*
+ *  stress_fstat_thread
+ *	keep exercising a file until
+ *	controlling thread triggers an exit
+ */
+static void *stress_fstat_thread(void *ctxt_ptr)
+{
+	static void *nowt = NULL;
+	static sigset_t set;
+	uint8_t stack[SIGSTKSZ + STACK_ALIGNMENT];
+	const ctxt_t *ctxt = (const ctxt_t *)ctxt_ptr;
+
+	/*
+	 *  Block all signals, let controlling thread
+	 *  handle these
+	 */
+	sigprocmask(SIG_BLOCK, &set, NULL);
+
+	/*
+	 *  According to POSIX.1 a thread should have
+	 *  a distinct alternative signal stack.
+	 *  However, we block signals in this thread
+	 *  so this is probably just totally unncessary.
+	 */
+	memset(stack, 0, sizeof(stack));
+	if (stress_sigaltstack(stack, SIGSTKSZ) < 0)
+		return &nowt;
+
+	while (keep_running && g_keep_stressing_flag) {
+		size_t i;
+
+		for (i = 0; i < FSTAT_LOOPS; i++)  {
+			if (!g_keep_stressing_flag)
+				break;
+			stress_fstat_helper(ctxt);
+		}
+	}
+
+	return &nowt;
+}
+
+/*
+ *  stress_fstat_threads()
+ *	create a bunch of threads to thrash a file
+ */
+static void stress_fstat_threads(const args_t *args, stat_info_t *si, const uid_t euid)
+{
+	size_t i;
+	pthread_t pthreads[MAX_FSTAT_THREADS];
+	int ret[MAX_FSTAT_THREADS];
+	ctxt_t ctxt = {
+		.args 	= args,
+		.si 	= si,
+		.euid	= euid
+	};
+
+	memset(ret, 0, sizeof(ret));
+	memset(pthreads, 0, sizeof(pthreads));
+
+	keep_running = true;
+
+	for (i = 0; i < MAX_FSTAT_THREADS; i++) {
+		ret[i] = pthread_create(&pthreads[i], NULL,
+				stress_fstat_thread, &ctxt);
+	}
+	for (i = 0; i < FSTAT_LOOPS; i++) {
+		if (!g_keep_stressing_flag)
+			break;
+		stress_fstat_helper(&ctxt);
+	}
+	keep_running = false;
+
+	for (i = 0; i < MAX_FSTAT_THREADS; i++) {
+		if (ret[i] == 0)
+			pthread_join(pthreads[i], NULL);
+	}
+}
+
 /*
  *  stress_fstat()
  *	stress system with fstat
  */
 int stress_fstat(const args_t *args)
 {
-	typedef struct dir_info {
-		char	*path;
-		bool	ignore;
-		bool	noaccess;
-		struct dir_info *next;
-	} dir_info_t;
-
-	DIR *dp;
-	dir_info_t *di;
-	static dir_info_t *dir_info;
+	stat_info_t *si;
+	static stat_info_t *stat_info;
 	struct dirent *d;
 	NOCLOBBER int ret = EXIT_FAILURE;
 	bool stat_some;
 	const uid_t euid = geteuid();
+	DIR *dp;
 
 	if (stress_sighandler(args->name, SIGALRM, handle_fstat_sigalrm, NULL) < 0)
 		return EXIT_FAILURE;
@@ -110,86 +233,48 @@ int stress_fstat(const args_t *args)
 		(void)snprintf(path, sizeof(path), "%s/%s", opt_fstat_dir, d->d_name);
 		if (do_not_stat(path))
 			continue;
-		if ((di = calloc(1, sizeof(*di))) == NULL) {
+		if ((si = calloc(1, sizeof(*si))) == NULL) {
 			pr_err("%s: out of memory\n", args->name);
 			(void)closedir(dp);
 			goto free_cache;
 		}
-		if ((di->path = strdup(path)) == NULL) {
+		if ((si->path = strdup(path)) == NULL) {
 			pr_err("%s: out of memory\n", args->name);
-			free(di);
+			free(si);
 			(void)closedir(dp);
 			goto free_cache;
 		}
-		di->ignore = false;
-		di->noaccess = false;
-		di->next = dir_info;
-		dir_info = di;
+		si->ignore = false;
+		si->access = true;
+		si->next = stat_info;
+		stat_info = si;
 	}
 	(void)closedir(dp);
 
 	do {
 		stat_some = false;
 
-		for (di = dir_info; g_keep_stressing_flag && di; di = di->next) {
-			int fd;
-			struct stat buf;
-
+		for (si = stat_info; g_keep_stressing_flag && si; si = si->next) {
 			if (!keep_stressing())
-				goto aborted;
-
-			if (di->ignore)
+				break;
+			if (si->ignore)
 				continue;
+			stress_fstat_threads(args, si, euid);
 
-			if ((stat(di->path, &buf) < 0) &&
-			    (errno != ENOMEM)) {
-				di->ignore = true;
-				continue;
-			}
-			if ((lstat(di->path, &buf) < 0) &&
-			    (errno != ENOMEM)) {
-				di->ignore = true;
-				continue;
-			}
-			if (di->noaccess)
-				continue;
-
-			/*
-			 *  Opening /dev files such as /dev/urandom
-			 *  may block when running as root, so
-			 *  avoid this.
-			 */
-			if (!euid) {
-				fd = open(di->path, O_RDONLY | O_NONBLOCK);
-				if (fd < 0) {
-					di->noaccess = true;
-					continue;
-				}
-
-				if ((fstat(fd, &buf) < 0) &&
-				    (errno != ENOMEM)) {
-					di->ignore = true;
-					(void)close(fd);
-					continue;
-				}
-
-				(void)close(fd);
-			}
 			stat_some = true;
 			inc_counter(args);
 		}
 	} while (stat_some && keep_stressing());
 
-aborted:
 	ret = EXIT_SUCCESS;
 free_cache:
 	/* Free cache */
-	for (di = dir_info; di; ) {
-		dir_info_t *next = di->next;
+	for (si = stat_info; si; ) {
+		stat_info_t *next = si->next;
 
-		free(di->path);
-		free(di);
-		di = next;
+		free(si->path);
+		free(si);
+		si = next;
 	}
 
 	return ret;
