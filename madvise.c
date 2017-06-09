eef799f70dff48254a8f0256876a2ad4c8753b98
Colin Ian King <colin.king@canonical.com>
2017-06-09 12:01:16 +0100

    madvise: fix build failure because of incorrect build config check
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- madvise.c
+++ madvise.c
@@ -91,7 +91,7 @@ static const int madvise_options[] = {
  */
 int madvise_random(void *addr, const size_t length)
 {
-#if !defined(__gnu_hurd__) && !defined(__minix__)
+#if defined(HAVE_MADVISE)
 	if (g_opt_flags & OPT_FLAGS_MMAP_MADVISE) {
 		int i = (mwc32() >> 7) % SIZEOF_ARRAY(madvise_options);
 
