2b028625ba7829f936171133d0c9cc305512a779
Colin Ian King <colin.king@canonical.com>
2017-07-07 14:17:44 +0100

    stress-tsearch: tsearch-size should not be a byte size value
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-tsearch.c
+++ stress-tsearch.c
@@ -33,7 +33,7 @@ void stress_set_tsearch_size(const char *opt)
 {
 	uint64_t tsearch_size;
 
-	tsearch_size = get_uint64_byte(opt);
+	tsearch_size = get_uint64(opt);
 	check_range("tsearch-size", tsearch_size,
 		MIN_TSEARCH_SIZE, MAX_TSEARCH_SIZE);
 	set_setting("tsearch-size", TYPE_ID_UINT64, &tsearch_size);
