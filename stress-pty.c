e5ac8f6c5ba25bb860876edc9ded589dbb194e7a
Colin Ian King <colin.king@canonical.com>
2017-07-07 14:07:10 +0100

    stress-pty: pty-max should not be a byte size value
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-pty.c
+++ stress-pty.c
@@ -45,7 +45,7 @@ void stress_set_pty_max(const char *opt)
 {
 	uint64_t pty_max;
 
-	pty_max = get_uint64_byte(opt);
+	pty_max = get_uint64(opt);
 	check_range("pty-max", pty_max,
 		MIN_PTYS, MAX_PTYS);
 	set_setting("pty-max", TYPE_ID_UINT64, &pty_max);
