f9f7aab05db4f1106f65da896ae51c8a27f5afc2
Colin Ian King <colin.king@canonical.com>
2017-07-07 13:58:27 +0100

    stress-exec: exec-max option should not be a byte size value
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-exec.c
+++ stress-exec.c
@@ -32,7 +32,7 @@ void stress_set_exec_max(const char *opt)
 {
 	uint64_t exec_max;
 
-	exec_max = get_uint64_byte(opt);
+	exec_max = get_uint64(opt);
 	check_range("exec-max", exec_max,
 		MIN_EXECS, MAX_EXECS);
 	set_setting("exec-max", TYPE_ID_INT64, &exec_max);
