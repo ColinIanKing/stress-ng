468777aebe65fb26ff96b141538d93c9690ce2d0
Colin Ian King <colin.king@canonical.com>
2017-07-07 14:10:17 +0100

    stress-sleep: sleep-max should not be a byte size value
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-sleep.c
+++ stress-sleep.c
@@ -42,7 +42,7 @@ void stress_set_sleep_max(const char *opt)
 {
 	uint64_t sleep_max;
 
-	sleep_max = get_uint64_byte(opt);
+	sleep_max = get_uint64(opt);
 	check_range("sleep-max", sleep_max,
 		MIN_SLEEP, MAX_SLEEP);
 	set_setting("sleep-max", TYPE_ID_UINT64, &sleep_max);
