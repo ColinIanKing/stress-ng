8f65c121b68d5630518ec6ba59e664115dbe5bef
Colin Ian King <colin.king@canonical.com>
2017-07-13 12:00:49 +0100

    stress-matrix: scale up default float values by 65535
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-matrix.c
+++ stress-matrix.c
@@ -454,7 +454,7 @@ static inline int stress_matrix_exercise(
 
 	matrix_ptr_t a = NULL, b = NULL, r = NULL;
 	register size_t i;
-	const matrix_type_t v = 1 / (matrix_type_t)((uint64_t)~0);
+	const matrix_type_t v = 65535 / (matrix_type_t)((uint64_t)~0);
 	int flags = MAP_PRIVATE | MAP_ANONYMOUS;
 #if defined(MAP_POPULATE)
 	flags |= MAP_POPULATE;
