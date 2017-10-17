d9883877b4480188b43d6c2c209df17fc4968428
Colin Ian King <colin.king@canonical.com>
2017-10-17 17:59:32 +0100

    stress-lsearch: use sizeof on size of object and not the type
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-lsearch.c
+++ stress-lsearch.c
@@ -67,11 +67,11 @@ int stress_lsearch(const args_t *args)
 	}
 	max = (size_t)lsearch_size;
 
-	if ((data = calloc(max, sizeof(int32_t))) == NULL) {
+	if ((data = calloc(max, sizeof(*data))) == NULL) {
 		pr_fail_dbg("malloc");
 		return EXIT_NO_RESOURCE;
 	}
-	if ((root = calloc(max, sizeof(int32_t))) == NULL) {
+	if ((root = calloc(max, sizeof(*data))) == NULL) {
 		free(data);
 		pr_fail_dbg("malloc");
 		return EXIT_NO_RESOURCE;
@@ -85,14 +85,14 @@ int stress_lsearch(const args_t *args)
 			void *ptr;
 
 			data[i] = ((mwc32() & 0xfff) << 20) ^ i;
-			ptr = lsearch(&data[i], root, &n, sizeof(int32_t), cmp);
+			ptr = lsearch(&data[i], root, &n, sizeof(*data), cmp);
 			(void)ptr;
 		}
 		/* Step #2, find */
 		for (i = 0; g_keep_stressing_flag && i < n; i++) {
 			int32_t *result;
 
-			result = lfind(&data[i], root, &n, sizeof(int32_t), cmp);
+			result = lfind(&data[i], root, &n, sizeof(*data), cmp);
 			if (g_opt_flags & OPT_FLAGS_VERIFY) {
 				if (result == NULL)
 					pr_fail("%s: element %zu could not be found\n", args->name, i);
