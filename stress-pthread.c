22573225d4cdd12af781e86dc29275e98aab2bee
Colin Ian King <colin.king@canonical.com>
2017-11-06 19:31:08 +0000

    stress-pthread: use shim_sched_yield instead if pthread_yield
    pthread_yield calls sched_yield on Linux, so use the abstraction
    shim to make it more portable.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-pthread.c
+++ stress-pthread.c
@@ -138,9 +138,7 @@ static void *stress_pthread_func(void *parg)
 			pr_fail_errno("pthread condition wait", ret);
 			break;
 		}
-#if !defined(__NetBSD__) && !defined(__sun__)
-		(void)pthread_yield();
-#endif
+		(void)shim_sched_yield();
 	}
 	ret = pthread_mutex_unlock(&mutex);
 	if (ret)
