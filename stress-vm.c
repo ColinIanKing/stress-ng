110d9e171028c65ebe5042dc9515ee9831b676d7
Colin Ian King <colin.king@canonical.com>
2017-12-27 13:23:38 +0000

    stress-vm: fix warning when building with PEDANTIC=1
    Clean up warning:
    stress-vm.c:95:20: warning: no previous declaration for X
    'keep_stressing_vm' [-Wmissing-declarations]
    bool HOT OPTIMIZE3 keep_stressing_vm(const args_t *args)
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-vm.c
+++ stress-vm.c
@@ -92,7 +92,7 @@ static const vm_madvise_info_t vm_madvise_info[] = {
  *  keep_stressing()
  *	returns true if we can keep on running a stressor
  */
-bool HOT OPTIMIZE3 keep_stressing_vm(const args_t *args)
+static bool HOT OPTIMIZE3 keep_stressing_vm(const args_t *args)
 {
 	return (LIKELY(g_keep_stressing_flag) &&
 	        LIKELY(!args->max_ops || ((*args->counter >> VM_BOGO_SHIFT) < args->max_ops)));
