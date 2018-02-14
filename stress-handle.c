1d7d089081ce107be29553b970641af2b763e523
Colin Ian King <colin.king@canonical.com>
2018-02-14 12:58:12 +0000

    stress-handle: catch any SIGKILL signals and report back
    We're seeing this stressor being SIGKILL'd on i386 systems, so add
    a SIGKILL catching layer to see if it's being OOM'd
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-handle.c
+++ stress-handle.c
@@ -99,73 +99,128 @@ static int get_mount_info(const args_t *args)
 int stress_handle(const args_t *args)
 {
 	int mounts;
+	pid_t pid;
 
 	if ((mounts = get_mount_info(args)) < 0) {
 		pr_fail("%s: failed to parse /proc/self/mountinfo\n", args->name);
 		return EXIT_FAILURE;
 	}
 
-	do {
-		struct file_handle *fhp, *tmp;
-		int mount_id, mount_fd, fd, i;
+again:
+	if (!g_keep_stressing_flag)
+		goto tidy;
+	pid = fork();
+	if (pid < 0) {
+		if (errno == EAGAIN)
+			goto again;
+		pr_err("%s: fork failed: errno=%d: (%s)\n",
+			args->name, errno, strerror(errno));
+	} else if (pid > 0) {
+		int status, ret;
 
-		if ((fhp = malloc(sizeof(*fhp))) == NULL)
-			continue;
-
-		fhp->handle_bytes = 0;
-		if ((name_to_handle_at(AT_FDCWD, FILENAME, fhp, &mount_id, 0) != -1) &&
-		    (errno != EOVERFLOW)) {
-			pr_fail_err("name_to_handle_at: failed to get file handle size");
-			free(fhp);
-			break;
-		}
-		tmp = realloc(fhp, sizeof(struct file_handle) + fhp->handle_bytes);
-		if (tmp == NULL) {
-			free(fhp);
-			continue;
-		}
-		fhp = tmp;
-		if (name_to_handle_at(AT_FDCWD, FILENAME, fhp, &mount_id, 0) < 0) {
-			pr_fail_err("name_to_handle_at: failed to get file handle");
-			free(fhp);
-			break;
-		}
-
-		mount_fd = -2;
-		for (i = 0; i < mounts; i++) {
-			if (mount_info[i].mount_id == mount_id) {
-				mount_fd = open(mount_info[i].mount_path, O_RDONLY);
-				break;
+		(void)setpgid(pid, g_pgrp);
+		/* Parent, wait for child */
+		ret = waitpid(pid, &status, 0);
+		if (ret < 0) {
+			if (errno != EINTR)
+				pr_dbg("%s: waitpid(): errno=%d (%s)\n",
+					args->name, errno, strerror(errno));
+			(void)kill(pid, SIGTERM);
+			(void)kill(pid, SIGKILL);
+			(void)waitpid(pid, &status, 0);
+		} else if (WIFSIGNALED(status)) {
+			pr_dbg("%s: child died: %s (instance %d)\n",
+				args->name, stress_strsignal(WTERMSIG(status)),
+				args->instance);
+			/* If we got killed by OOM killer, re-start */
+			if (WTERMSIG(status) == SIGKILL) {
+				if (g_opt_flags & OPT_FLAGS_OOMABLE) {
+					log_system_mem_info();
+					pr_dbg("%s: assuming killed by OOM "
+						"killer, bailing out "
+						"(instance %d)\n",
+						args->name, args->instance);
+					_exit(0);
+				} else {
+					log_system_mem_info();
+					pr_dbg("%s: assuming killed by OOM "
+						"killer, restarting again "
+						"(instance %d)\n",
+						args->name, args->instance);
+					goto again;
+				}
 			}
 		}
-		if (mount_fd == -2) {
-			pr_fail("%s: cannot find mount id %d\n", args->name, mount_id);
-			free(fhp);
-			break;
-		}
-		if (mount_fd < 0) {
-			pr_fail("%s: failed to open mount path '%s': errno=%d (%s)\n",
-				args->name, mount_info[i].mount_path, errno, strerror(errno));
-			free(fhp);
-			break;
-		}
-		if ((fd = open_by_handle_at(mount_fd, fhp, O_RDONLY)) < 0) {
-			/* We don't abort if EPERM occurs, that's not a test failure */
-			if (errno != EPERM) {
-				pr_fail("%s: open_by_handle_at: failed to open: errno=%d (%s)\n",
-					args->name, errno, strerror(errno));
-				(void)close(mount_fd);
+	} else if (pid == 0) {
+		(void)setpgid(0, g_pgrp);
+		stress_parent_died_alarm();
+
+		/* Make sure this is killable by OOM killer */
+		set_oom_adjustment(args->name, true);
+
+		do {
+			struct file_handle *fhp, *tmp;
+			int mount_id, mount_fd, fd, i;
+
+			if ((fhp = malloc(sizeof(*fhp))) == NULL)
+				continue;
+
+			fhp->handle_bytes = 0;
+			if ((name_to_handle_at(AT_FDCWD, FILENAME, fhp, &mount_id, 0) != -1) &&
+			    (errno != EOVERFLOW)) {
+				pr_fail_err("name_to_handle_at: failed to get file handle size");
 				free(fhp);
 				break;
 			}
-		} else {
-			(void)close(fd);
-		}
-		(void)close(mount_fd);
-		free(fhp);
-		inc_counter(args);
-	} while (keep_stressing());
+			tmp = realloc(fhp, sizeof(struct file_handle) + fhp->handle_bytes);
+			if (tmp == NULL) {
+				free(fhp);
+				continue;
+			}
+			fhp = tmp;
+			if (name_to_handle_at(AT_FDCWD, FILENAME, fhp, &mount_id, 0) < 0) {
+				pr_fail_err("name_to_handle_at: failed to get file handle");
+				free(fhp);
+				break;
+			}
 
+			mount_fd = -2;
+			for (i = 0; i < mounts; i++) {
+				if (mount_info[i].mount_id == mount_id) {
+					mount_fd = open(mount_info[i].mount_path, O_RDONLY);
+					break;
+				}
+			}
+			if (mount_fd == -2) {
+				pr_fail("%s: cannot find mount id %d\n", args->name, mount_id);
+				free(fhp);
+				break;
+			}
+			if (mount_fd < 0) {
+				pr_fail("%s: failed to open mount path '%s': errno=%d (%s)\n",
+					args->name, mount_info[i].mount_path, errno, strerror(errno));
+				free(fhp);
+				break;
+			}
+			if ((fd = open_by_handle_at(mount_fd, fhp, O_RDONLY)) < 0) {
+				/* We don't abort if EPERM occurs, that's not a test failure */
+				if (errno != EPERM) {
+					pr_fail("%s: open_by_handle_at: failed to open: errno=%d (%s)\n",
+						args->name, errno, strerror(errno));
+					(void)close(mount_fd);
+					free(fhp);
+					break;
+				}
+			} else {
+				(void)close(fd);
+			}
+			(void)close(mount_fd);
+			free(fhp);
+			inc_counter(args);
+		} while (keep_stressing());
+		_exit(EXIT_SUCCESS);
+	}
+tidy:
 	free_mount_info(mounts);
 
 	return EXIT_SUCCESS;
