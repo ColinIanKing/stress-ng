ColinIanKing/stress-ng

commit d01abd84f03f65e5303746eee7b318675c04827c

stress-enosys: wider sanity checks for fork/vfork/cloning (LP:#1753705)
We really don't want fork/vfork/clone system calls to be called
so add some more smarts to detect these before the call and also
before/after the system call. We can detect that a system call has
created a new child pid and then force an exit rather causing a
forkbomb run-away

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-enosys.c
@@ -47,13 +47,55 @@ static inline long syscall7(long number, long arg1, long arg2,
 			    long arg3, long arg4, long arg5,
 			    long arg6, long arg7)
 {
-	return syscall(number, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+	int ret;
+	pid_t pid = getpid();
+
+	ret = syscall(number, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+
+	if (getpid() != pid) {
+		/* Somehow we forked/cloned ourselves, so exit */
+		_exit(0);
+	}
+	return ret;
 }
 PRAGMA_POP
 
 static inline bool HOT OPTIMIZE3 syscall_find(long number)
 {
 	hash_syscall_t *h = hash_syscall_table[number % HASH_SYSCALL_SIZE];
+
+#if defined(SYS_clone)
+	if ((number & 0xffff) == SYS_clone)
+		return true;
+#endif
+#if defined(SYS_clone2)
+	if ((number & 0xffff) == SYS_clone2)
+		return true;
+#endif
+#if defined(__NR_clone)
+	if ((number & 0xffff) == __NR_clone)
+		return true;
+#endif
+#if defined(__NR_clone2)
+	if ((number & 0xffff) == __NR_clone2)
+		return true;
+#endif
+#if defined(SYS_fork)
+	if ((number & 0xffff) == SYS_fork)
+		return true;
+#endif
+#if defined(__NR_fork)
+	if ((number & 0xffff) == __NR_fork)
+		return true;
+#endif
+#if defined(SYS_vfork)
+	if ((number & 0xffff) == SYS_vfork)
+		return true;
+#endif
+#if defined(__NR_vfork)
+	if ((number & 0xffff) == __NR_vfork)
+		return true;
+#endif
 
 	while (h) {
 		if (h->number == number)
@@ -209,6 +251,9 @@ static const long skip_syscalls[] = {
 #endif
 #if defined(SYS_clone)
 	SYS_clone,
+#endif
+#if defined(SYS_clone2)
+	SYS_clone2,
 #endif
 #if defined(SYS_close)
 	SYS_close,
@@ -2942,7 +2987,7 @@ static inline int stress_do_syscall(const args_t *args, const long number)
 	/* Check if this is a known non-ENOSYS syscall */
 	if (syscall_find(number))
 		return rc;
-	if (!g_keep_stressing_flag)
+	if (!keep_stressing())
 		return 0;
 	pid = fork();
 	if (pid < 0) {
@@ -2986,19 +3031,27 @@ static inline int stress_do_syscall(const args_t *args, const long number)
 		/*
 		 *  Try various ENOSYS calls
 		 */
+		if (!keep_stressing())
+			_exit(EXIT_SUCCESS);
 		ret = syscall7(number, -1, -1, -1, -1, -1, -1, -1);
 		if ((ret < 0) && (errno != ENOSYS))
 			_exit(errno);
 
+		if (!keep_stressing())
+			_exit(EXIT_SUCCESS);
 		ret = syscall7(number, 0, 0, 0, 0, 0, 0, 0);
 		if ((ret < 0) && (errno != ENOSYS))
 			_exit(errno);
 
+		if (!keep_stressing())
+			_exit(EXIT_SUCCESS);
 		ret = syscall7(number, 1, 1, 1, 1, 1, 1, 1);
 		if ((ret < 0) && (errno != ENOSYS))
 			_exit(errno);
 
 		for (arg = 2; arg;) {
+			if (!keep_stressing())
+				_exit(EXIT_SUCCESS);
 			ret = syscall7(number, arg, arg, arg,
 				      arg, arg, arg, arg);
 			if ((ret < 0) && (errno != ENOSYS))
@@ -3010,6 +3063,8 @@ static inline int stress_do_syscall(const args_t *args, const long number)
 				_exit(errno);
 		}
 
+		if (!keep_stressing())
+			_exit(EXIT_SUCCESS);
 		ret = syscall7(number, mwc64(), mwc64(), mwc64(),
 			      mwc64(), mwc64(), mwc64(), mwc64());
 		_exit(ret < 0 ? errno : 0);
@@ -3021,7 +3076,6 @@ static inline int stress_do_syscall(const args_t *args, const long number)
 			if (errno != EINTR)
 				pr_dbg("%s: waitpid(): errno=%d (%s)\n",
 					args->name, errno, strerror(errno));
-			(void)kill(pid, SIGTERM);
 			(void)kill(pid, SIGKILL);
 			(void)waitpid(pid, &status, 0);
 
@@ -3045,7 +3099,7 @@ int stress_enosys(const args_t *args)
 	pid_t pid;
 
 again:
-	if (!g_keep_stressing_flag)
+	if (!keep_stressing())
 		return EXIT_SUCCESS;
 	pid = fork();
 	if (pid < 0) {
@@ -3094,6 +3148,8 @@ again:
 		ssize_t j;
 
 		/* Child, wrapped to catch OOMs */
+		if (!keep_stressing())
+			_exit(0);
 
 		(void)setpgid(0, g_pgrp);
 		stress_parent_died_alarm();
@@ -3124,8 +3180,14 @@ again:
 				if (!keep_stressing())
 					goto finish;
 				stress_do_syscall(args, mwc8() & mask);
+				if (!keep_stressing())
+					goto finish;
 				stress_do_syscall(args, mwc16() & mask);
+				if (!keep_stressing())
+					goto finish;
 				stress_do_syscall(args, mwc32() & mask);
+				if (!keep_stressing())
+					goto finish;
 				stress_do_syscall(args, mwc64() & mask);
 			}
 
@@ -3134,8 +3196,14 @@ again:
 				if (!keep_stressing())
 					goto finish;
 				stress_do_syscall(args, number);
+				if (!keep_stressing())
+					goto finish;
 				stress_do_syscall(args, number | 1);
+				if (!keep_stressing())
+					goto finish;
 				stress_do_syscall(args, number | (number << 1));
+				if (!keep_stressing())
+					goto finish;
 				stress_do_syscall(args, ~number);
 			}
 

commit d0e42d6315403fd35f681275cc32ec1e466d96c0

stress-enosys: ensure all known syscalls are added to hash table (LP: #1755358)
Do not use syscall_find to find a syscall before adding it as this
contains short-cut masking tests that alias other syscall numbers
causing system calls to be ommitted from the banned hash list of
syscalls, ultimately leading to calls such as reboot to be missed
and causing us to shutdown systems accidentally.

Fixes: d01abd84f03f ("stress-enosys: wider sanity checks for fork/vfork/cloning")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-enosys.c
@@ -64,6 +64,16 @@ static inline bool HOT OPTIMIZE3 syscall_find(long number)
 {
 	hash_syscall_t *h = hash_syscall_table[number % HASH_SYSCALL_SIZE];
 
+	/* Really make sure reboot is never called */
+#if defined(SYS_reboot)
+	if (number == SYS_reboot)
+		return true;
+#endif
+#if defined(__NR_reboot)
+	if (number == __NR_reboot)
+		return true;
+#endif
+
 #if defined(SYS_clone)
 	if ((number & 0xffff) == SYS_clone)
 		return true;
@@ -108,7 +118,13 @@ static inline bool HOT OPTIMIZE3 syscall_find(long number)
 static inline void HOT OPTIMIZE3 syscall_add(long number)
 {
 	const long hash = number % HASH_SYSCALL_SIZE;
-	hash_syscall_t *newh;
+	hash_syscall_t *newh, *h = hash_syscall_table[hash];
+
+	while (h) {
+		if (h->number == number)
+			return;		/* Already exists */
+		h = h->next;
+	}
 
 	newh = malloc(sizeof(*newh));
 	if (!newh)
@@ -3167,10 +3183,8 @@ again:
 
 		//limit_procs(2);
 
-		for (j = 0; j < (ssize_t)SIZEOF_ARRAY(skip_syscalls) - 1; j++) {
-			if (!syscall_find(skip_syscalls[j]))
-				syscall_add(skip_syscalls[j]);
-		}
+		for (j = 0; j < (ssize_t)SIZEOF_ARRAY(skip_syscalls) - 1; j++)
+			syscall_add(skip_syscalls[j]);
 
 		do {
 			long number;