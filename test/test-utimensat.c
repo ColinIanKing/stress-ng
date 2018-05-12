2f669deade3c74df333fd17080b87dc073d96b67
Colin Ian King <colin.king@canonical.com>
2018-05-12 23:50:22 +0100

    Add build check for utimensat
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

---
+++ test/test-utimensat.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2013-2018 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This code is a complete clean re-write of the stress tool by
+ * Colin Ian King <colin.king@canonical.com> and attempts to be
+ * backwardly compatible with the stress tool by Amos Waterland
+ * <apw@rossby.metr.ou.edu> but has more stress tests and more
+ * functionality.
+ *
+ */
+
+#define _GNU_SOURCE
+
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main(void)
+{
+	int ret;
+	struct timespec times[2];
+
+	memset(times, 0, sizeof(times));
+
+	ret = utimensat(0, "/tmp", times, 0);
+
+	return ret;
+}
