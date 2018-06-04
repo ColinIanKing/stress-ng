ColinIanKing/stress-ng

commit b9912c8ad8c0c4ac6f5ef65a9b573f1dd960528d

stress-cpu: fix parity sum check
forgot to use the (0x6996 >> v) & 1

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-cpu.c
@@ -228,7 +228,7 @@ static void HOT OPTIMIZE3 TARGET_CLONES stress_cpu_bitops(const char *name)
 			v ^= v >> 8;
 			v ^= v >> 4;
 			v &= 0xf;
-			i_sum += v;
+			i_sum += (0x6996 >> v) & 1;
 		}
 		{
 			/* Brian Kernighan count bits */

commit b0a2c416574aaa2657b14c2babe8875b69669116

stress-cpu: fix bitops checksum
Fixes: b9912c8ad8c0 ("stress-cpu: fix parity sum check")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-cpu.c
@@ -205,7 +205,7 @@ static void HOT OPTIMIZE3 TARGET_CLONES stress_cpu_gcd(const char *name)
 static void HOT OPTIMIZE3 TARGET_CLONES stress_cpu_bitops(const char *name)
 {
 	uint32_t i, i_sum = 0;
-	const uint32_t sum = 0x8aadcaab;
+	const uint32_t sum = 0x8aac0aab;
 
 	for (i = 0; i < 16384; i++) {
 		{