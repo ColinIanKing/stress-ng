4234e6a4c30b501d0a7a037739abb5ff96b24943
Colin Ian King <colin.king@canonical.com>
2018-06-05 12:34:33 +0100

    stress-dccp: add missing ,
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-dccp.c
+++ stress-dccp.c
@@ -386,7 +386,7 @@ stressor_info_t stress_dccp_info = {
 };
 #else
 stressor_info_t stress_dccp_info = {
-	.stressor = stress_not_implemented
+	.stressor = stress_not_implemented,
 	.class = CLASS_NETWORK | CLASS_OS
 };
 #endif
