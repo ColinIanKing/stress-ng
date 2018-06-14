cfa1a647e37048abd53a48d4553585982a4028aa
Colin Ian King <colin.king@canonical.com>
2018-06-14 18:02:15 +0100

    stress-memcpy: fix clobbering of stats data when copying b_str
    This fixes a subtle buffer overrun on the shared string copying.
    Fixes clobbering of time stats that only appears when we are
    interrupted before the data is copied back.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-memcpy.c
+++ stress-memcpy.c
@@ -47,7 +47,7 @@ static NOINLINE void *__memmove(void *dest, const void *src, size_t n)
 static int stress_memcpy(const args_t *args)
 {
 	static buffer_t b;
-	buffer_t *b_str = (buffer_t *)g_shared->str_shared;
+	uint8_t *b_str = g_shared->str_shared;
 	uint8_t *str_shared = g_shared->str_shared;
 	uint8_t *aligned_buf = align_address(b.buffer, ALIGN_SIZE);
 
@@ -57,9 +57,9 @@ static int stress_memcpy(const args_t *args)
 		(void)__memcpy(aligned_buf, str_shared, STR_SHARED_SIZE);
 		(void)__memcpy(str_shared, aligned_buf, STR_SHARED_SIZE / 2);
 		(void)__memmove(aligned_buf, aligned_buf + 64, STR_SHARED_SIZE - 64);
-		*b_str = b;
+		(void)__memcpy(b_str, &b, STR_SHARED_SIZE);
 		(void)__memmove(aligned_buf + 64, aligned_buf, STR_SHARED_SIZE - 64);
-		b = *b_str;
+		(void)__memcpy(&b, b_str, STR_SHARED_SIZE);
 		(void)__memmove(aligned_buf + 1, aligned_buf, STR_SHARED_SIZE - 1);
 		(void)__memmove(aligned_buf, aligned_buf + 1, STR_SHARED_SIZE - 1);
 		inc_counter(args);
