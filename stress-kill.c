f62cbd3ba5136ed3d58370581dc40b25dc63c9dd
Colin Ian King <colin.king@canonical.com>
2018-07-03 16:18:01 +0100

    stress-kill: add some early delay to give parent time to spawn stressors
    It seems that repeated kill()s can cause the parent problems in spawning
    off other stressors on some kernels and hardware, so add in a small set
    of sleeps to allow faster spawning of stressors.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-kill.c
+++ stress-kill.c
@@ -30,11 +30,32 @@
  */
 static int stress_kill(const args_t *args)
 {
+	uint64_t udelay = 5000;
+
 	if (stress_sighandler(args->name, SIGUSR1, SIG_IGN, NULL) < 0)
 		return EXIT_FAILURE;
 
 	do {
 		int ret;
+
+		/*
+		 *  With many kill stressors we get into a state
+		 *  where they all hammer on kill system calls and
+		 *  this stops the parent from getting scheduling
+		 *  time to spawn off the rest of the kill stressors
+		 *  causing some lag in getting all the stressors
+		 *  running. Ease this pressure off to being with
+		 *  with some small sleeps that shrink to zero over
+		 *  time. The alternative was to re-nice all the
+		 *  processes, but even then one gets the child
+		 *  stressors all contending and causing a bottle
+		 *  neck.  Any simpler and/or better solutions would
+		 *  be appreciated!
+		 */
+		if (udelay >= 1000) {
+			shim_usleep(udelay);
+			udelay -= 500;
+		}
 
 		ret = kill(args->pid, SIGUSR1);
 		if ((ret < 0) && (g_opt_flags & OPT_FLAGS_VERIFY))
