4931cbdb7bdeaad793214486bbb4ef31f23fc9d8
Colin Ian King <colin.king@canonical.com>
2018-11-21 12:49:49 +0000

    stress-file-ioctl: add in errno info on failure
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-file-ioctl.c
+++ stress-file-ioctl.c
@@ -41,7 +41,9 @@ static void check_flag(
 		flags = fcntl(fd, F_GETFL, 0);
 		if ((set && !(flags & flag)) ||
 		    (!set && (flags & flag)))
-			pr_fail("%s: ioctl %s failed\n", args->name, ioctl_name);
+			pr_fail("%s: ioctl %s failed, errno=%d (%s)\n",
+				args->name, ioctl_name,
+				errno, strerror(errno));
 	}
 #else
 	(void)args;
