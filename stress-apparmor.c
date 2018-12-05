67b53d54abe8c43d60003dc1ad9d6026e2c1bdb6
Colin Ian King <colin.king@canonical.com>
2018-12-05 11:20:04 +0000

    Add build config check for sys/apparmor.h
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-apparmor.c
+++ stress-apparmor.c
@@ -24,8 +24,9 @@
  */
 #include "stress-ng.h"
 
-#if defined(__linux__) &&	\
-    defined(HAVE_APPARMOR) &&	\
+#if defined(__linux__) &&		\
+    defined(HAVE_APPARMOR) &&		\
+    defined(HAVE_SYS_APPARMOR_H) && 	\
     defined(HAVE_SYS_SELECT_H)
 
 #include <sys/select.h>
