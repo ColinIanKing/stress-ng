b83b17ab7993a8f800fa02bdbc22909165fedee3
Colin Ian King <colin.king@canonical.com>
2019-01-08 15:53:58 +0000

    stress-ng.h: minor space reformatting
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-ng.h
+++ stress-ng.h
@@ -1542,7 +1542,7 @@ extern void pr_fail_dbg__(const args_t *args, const char *msg);
 #if defined(__ARM_ARCH_6__)   || defined(__ARM_ARCH_6J__)  || \
     defined(__ARM_ARCH_6K__)  || defined(__ARM_ARCH_6Z__)  || \
     defined(__ARM_ARCH_6ZK__) || defined(__ARM_ARCH_6T2__) || \
-    defined(__ARM_ARCH_6M__)  ||  defined(__ARM_ARCH_7__)  || \
+    defined(__ARM_ARCH_6M__)  || defined(__ARM_ARCH_7__)   || \
     defined(__ARM_ARCH_7A__)  || defined(__ARM_ARCH_7R__)  || \
     defined(__ARM_ARCH_7M__)  || defined(__ARM_ARCH_7EM__) || \
     defined(__ARM_ARCH_8A__)  || defined(__aarch64__)
