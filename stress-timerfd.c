6efbeed7e00307d2de7b3119996bfa77fc862164
Colin Ian King <colin.king@canonical.com>
2019-01-09 12:33:24 +0000

    stress-timerfd: use 256 timers for more stress and throughput
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-timerfd.c
+++ stress-timerfd.c
@@ -25,6 +25,7 @@
 #include "stress-ng.h"
 
 #define COUNT_MAX		(256)
+#define TIMER_MAX		(256)
 
 /*
  *  stress_set_timerfd_freq()
@@ -79,7 +80,7 @@ static int stress_timerfd(const args_t *args)
 {
 	struct itimerspec timer;
 	uint64_t timerfd_freq = DEFAULT_TIMERFD_FREQ;
-	int timerfd, procfd, count = 0;
+	int timerfd[TIMER_MAX], procfd, count = 0, i, max_timerfd = -1;
 	char filename[PATH_MAX];
 
 	if (!get_setting("timerfd-freq", &timerfd_freq)) {
@@ -90,21 +91,38 @@ static int stress_timerfd(const args_t *args)
 	}
 	rate_ns = timerfd_freq ? 1000000000.0 / timerfd_freq : 1000000000.0;
 
-	timerfd = timerfd_create(CLOCK_REALTIME, 0);
-	if (timerfd < 0) {
-		pr_fail_err("timerfd_create");
-		(void)close(timerfd);
+	for (i = 0; i < TIMER_MAX; i++) {
+		timerfd[i] = timerfd_create(CLOCK_REALTIME, 0);
+		if (timerfd[i] < 0) {
+			if ((errno != EMFILE) &&
+			    (errno != ENFILE) &&
+			    (errno != ENOMEM)) {
+				pr_fail_err("timerfd_create");
+				return EXIT_FAILURE;
+			}
+		}
+		count++;
+		if (max_timerfd < timerfd[i])
+			max_timerfd = timerfd[i];
+	}
+
+	if (count == 0) {
+		pr_fail_err("timerfd_create, no timers created");
 		return EXIT_FAILURE;
 	}
+	count = 0;
+
 	stress_timerfd_set(&timer);
-	if (timerfd_settime(timerfd, 0, &timer, NULL) < 0) {
-		pr_fail_err("timer_settime");
-		(void)close(timerfd);
-		return EXIT_FAILURE;
+	for (i = 0; i < TIMER_MAX; i++) {
+		if (timerfd_settime(timerfd[i], 0, &timer, NULL) < 0) {
+			pr_fail_err("timer_settime");
+			(void)close(timerfd[i]);
+			return EXIT_FAILURE;
+		}
 	}
 
 	(void)snprintf(filename, sizeof(filename), "/proc/%d/fdinfo/%d",
-		(int)args->pid, timerfd);
+		(int)args->pid, timerfd[0]);
 	procfd = open(filename, O_RDONLY);
 
 	do {
@@ -115,13 +133,15 @@ static int stress_timerfd(const args_t *args)
 		fd_set rdfs;
 
 		FD_ZERO(&rdfs);
-		FD_SET(timerfd, &rdfs);
+		for (i = 0; i < TIMER_MAX; i++)
+			FD_SET(timerfd[i], &rdfs);
+
 		timeout.tv_sec = 0;
 		timeout.tv_usec = 500000;
 
 		if (!g_keep_stressing_flag)
 			break;
-		ret = select(timerfd + 1, &rdfs, NULL, NULL, &timeout);
+		ret = select(max_timerfd + 1, &rdfs, NULL, NULL, &timeout);
 		if (ret < 0) {
 			if (errno == EINTR)
 				continue;
@@ -131,21 +151,27 @@ static int stress_timerfd(const args_t *args)
 		if (ret < 1)
 			continue; /* Timeout */
 
-		ret = read(timerfd, &exp, sizeof exp);
-		if (ret < 0) {
-			pr_fail_err("timerfd read");
-			break;
-		}
-		if (timerfd_gettime(timerfd, &value) < 0) {
-			pr_fail_err("timerfd_gettime");
-			break;
-		}
-		if (g_opt_flags & OPT_FLAGS_TIMERFD_RAND) {
-			stress_timerfd_set(&timer);
-			if (timerfd_settime(timerfd, 0, &timer, NULL) < 0) {
-				pr_fail_err("timer_settime");
+		for (i = 0; i < TIMER_MAX; i++) {
+			if (!FD_ISSET(timerfd[i], &rdfs))
+				continue;
+
+			ret = read(timerfd[i], &exp, sizeof exp);
+			if (ret < 0) {
+				pr_fail_err("timerfd read");
 				break;
 			}
+			if (timerfd_gettime(timerfd[i], &value) < 0) {
+				pr_fail_err("timerfd_gettime");
+				break;
+			}
+			if (g_opt_flags & OPT_FLAGS_TIMERFD_RAND) {
+				stress_timerfd_set(&timer);
+				if (timerfd_settime(timerfd[i], 0, &timer, NULL) < 0) {
+					pr_fail_err("timer_settime");
+					break;
+				}
+			}
+			inc_counter(args);
 		}
 
 		/*
@@ -163,10 +189,12 @@ static int stress_timerfd(const args_t *args)
 			}
 			count = 0;
 		}
-		inc_counter(args);
 	} while (keep_stressing());
 
-	(void)close(timerfd);
+	for (i = 0; i < TIMER_MAX; i++) {
+		if (timerfd[i] > 0)
+			(void)close(timerfd[i]);
+	}
 	if (procfd > -1)
 		(void)close(procfd);
 
