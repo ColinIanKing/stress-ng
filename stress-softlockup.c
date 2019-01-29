18dff0dc417ec66cc49ed8cf772c5b3935c8d7d5
Colin Ian King <colin.king@canonical.com>
2019-01-29 18:18:12 +0000

    Add build check for sched_setscheduler
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-softlockup.c
+++ stress-softlockup.c
@@ -24,7 +24,8 @@
  */
 #include "stress-ng.h"
 
-#if defined(__linux__)
+#if defined(HAVE_SCHED_GET_PRIORITY_MIN) &&	\
+    defined(HAVE_SCHED_SETSCHEDULER)
 
 /*
  *  stress_softlockup_supported()
---
+++ test/test-sched-setscheduler.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2013-2019 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This code is a complete clean re-write of the stress tool by
+ * Colin Ian King <colin.king@canonical.com> and attempts to be
+ * backwardly compatible with the stress tool by Amos Waterland
+ * <apw@rossby.metr.ou.edu> but has more stress tests and more
+ * functionality.
+ *
+ */
+
+#define _GNU_SOURCE
+
+#include <sched.h>
+
+int main(void)
+{
+	struct sched_param param = { 0 };
+
+	return sched_setscheduler(0, 0, &param);
+}
