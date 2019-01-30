0c4bd51a233a48a4e42c1c9d94fbcac27676cb53
Colin Ian King <colin.king@canonical.com>
2019-01-30 18:49:45 +0000

    stress-sleep: remove debug on join failure on bad threads
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-sleep.c
+++ stress-sleep.c
@@ -191,8 +191,11 @@ tidy:
 	thread_terminate = true;
 	for (i = 0; i < n; i++) {
 		ret = pthread_join(ctxts[i].pthread, NULL);
+		(void)ret;
+		/*
 		if (ret)
-			pr_dbg("%s: pthread join, ret=%d\b", args->name, ret);
+			pr_dbg("%s: pthread join, ret=%d\n", args->name, ret);
+		*/
 	}
 
 	if (limited) {
