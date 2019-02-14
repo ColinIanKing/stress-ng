f138c66702e937ec724d4f0c99f9465305ffe1e4
Colin Ian King <colin.king@canonical.com>
2019-02-14 17:44:15 +0000

    stress-sctp: add yield detection before a blocking accept()
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-sctp.c
+++ stress-sctp.c
@@ -211,7 +211,12 @@ static int stress_sctp_server(
 	}
 
 	do {
-		int sfd = accept(fd, (struct sockaddr *)NULL, NULL);
+		int sfd;
+
+		if (!keep_stressing())
+			break;
+
+		sfd = accept(fd, (struct sockaddr *)NULL, NULL);
 		if (sfd >= 0) {
 			size_t i;
 
