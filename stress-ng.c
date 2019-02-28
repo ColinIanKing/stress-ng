ColinIanKing/stress-ng

commit 2cf9fe7983513a473ec9e994cd932f1062034ee8

job: allow classes to be run as jobs sequentially or in parallel
timeout 2s
log-brief
metrics-brief

sequential 1
verbose
class vm

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-ng.c
@@ -699,6 +699,7 @@ static const struct option long_options[] = {
 	{ "open",	1,	0,	OPT_OPEN },
 	{ "open-ops",	1,	0,	OPT_OPEN_OPS },
 	{ "page-in",	0,	0,	OPT_PAGE_IN },
+	{ "parallel",	1,	0,	OPT_ALL },
 	{ "pathological",0,	0,	OPT_PATHOLOGICAL },
 	{ "perf",	0,	0,	OPT_PERF_STATS },
 	{ "personality",1,	0,	OPT_PERSONALITY },
@@ -960,6 +961,7 @@ static const help_t help_generic[] = {
 	{ NULL,		"no-madvise",		"don't use random madvise options for each mmap" },
 	{ NULL,		"no-rand-seed",		"seed random numbers with the same constant" },
 	{ NULL,		"page-in",		"touch allocated pages that are not in core" },
+	{ NULL,		"parallel N",		"synonym for 'all N'" },
 	{ NULL,		"pathological",		"enable stressors that are known to hang a machine" },
 #if defined(STRESS_PERF_STATS)
 	{ NULL,		"perf",			"display perf statistics" },
@@ -2898,8 +2900,10 @@ next_opt:
 				return EXIT_FAILURE;
 			else if (ret > 0)
 				exit(EXIT_SUCCESS);
-			else
+			else {
 				set_setting("class", TYPE_ID_UINT32, &u32);
+				enable_classes(u32);
+			}
 			break;
 		case OPT_CLONE_MAX:
 			stress_set_clone_max(optarg);
@@ -3589,9 +3593,6 @@ int main(int argc, char **argv)
 	ret = parse_opts(argc, argv, false);
 	if (ret != EXIT_SUCCESS)
 		exit(ret);
-
-	(void)get_setting("class", &class);
-	enable_classes(class);
 
 	/*
 	 *  Throw away excluded stressors

job.c
@@ -64,7 +64,7 @@ static int parse_run(
             !strcmp(argv[2], "seq")) {
 		if (*flag & RUN_PARALLEL)
 			goto err;
-		*flag |= RUN_PARALLEL;
+		*flag |= RUN_SEQUENTIAL;
 		g_opt_flags |= OPT_FLAGS_SEQUENTIAL;
 		return 1;
 	}
@@ -73,8 +73,9 @@ static int parse_run(
 	    !strcmp(argv[2], "together")) {
 		if (*flag & RUN_SEQUENTIAL)
 			goto err;
-		*flag |= RUN_SEQUENTIAL;
+		*flag |= RUN_PARALLEL;
 		g_opt_flags &= ~OPT_FLAGS_SEQUENTIAL;
+		g_opt_flags |= OPT_FLAGS_ALL;
 		return 1;
 	}
 err:

commit 3b117cbff563494f806e7bd9ba28831a8aa53304

stress-ng: fix --class option
This patch can be verified with
./stress-ng --class cpu --sequential 1
which will not work without this patch.

Fixes: 2cf9fe79 ("job: allow classes to be run as jobs sequentially or in parallel")
Signed-off-by: André Wild <wild1@linux.ibm.com>

stress-ng.c
@@ -3680,6 +3680,8 @@ int main(int argc, char **argv)
 	if (stress_exclude() < 0)
 		exit(EXIT_FAILURE);
 
+	(void)get_setting("class", &class);
+
 	/*
 	 *  Setup random stressors if requested
 	 */