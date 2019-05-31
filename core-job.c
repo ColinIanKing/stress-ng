ColinIanKing/stress-ng

commit 76121325d2d5cbf51b8ce79dd62e3fc761b67d30

jobs: parse jobfile name from correct argv[] location
The jobfile name should be parsed from argv[optind] to get the
correct argument, currently it's getting it from the 1st position
and that's incorrect if a user specifies other options on the command
line.

Signed-off-by: Colin Ian King <colin.king@canonical.com>

core-job.c
@@ -114,12 +114,16 @@ int parse_jobfile(
 	uint32_t flag;
 	static uint32_t lineno;
 
+	if (optind >= argc)
+		return 0;
+
 	if (!jobfile) {
 		if (argc < 2)
 			return 0;
-		fp = fopen(argv[1], "r");
+		fp = fopen(argv[optind], "r");
 		if (!fp)
 			return 0;
+		optind++;
 	} else {
 		fp = fopen(jobfile, "r");
 	}

commit bfa5d61428d051c8c3630fdef688c284e8a8d0bd

core-job: fix optind check, should only check for non --job options
The optind position check should be used for non --job options.
Thanks to Rob Colclaser for spotting this mistake.

Fixes: 76121325d2d5 ("jobs: parse jobfile name from correct argv[] location")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

core-job.c
@@ -114,11 +114,9 @@ int parse_jobfile(
 	uint32_t flag;
 	static uint32_t lineno;
 
-	if (optind >= argc)
-		return 0;
 
 	if (!jobfile) {
-		if (argc < 2)
+		if (optind >= argc)
 			return 0;
 		fp = fopen(argv[optind], "r");
 		if (!fp)