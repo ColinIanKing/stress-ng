45be5ee599243abe1efb3f9730c3000583853758
Colin Ian King <colin.king@canonical.com>
2019-06-11 12:00:05 +0100

    stress-resources: fix missing guard on fd_ev
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-resources.c
+++ stress-resources.c
@@ -178,7 +178,9 @@ static void NORETURN waste_resources(
 		info[i].m_mmap = MAP_FAILED;
 		info[i].pipe_ret = -1;
 		info[i].fd_open = -1;
+#if defined(HAVE_EVENTFD)
 		info[i].fd_ev = -1;
+#endif
 #if defined(HAVE_MEMFD_CREATE)
 		info[i].fd_memfd = -1;
 #endif
