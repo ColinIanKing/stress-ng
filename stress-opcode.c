d953f3632bcd1f97b162bbb2fc905339c7c259a9
Colin Ian King <colin.king@canonical.com>
2019-07-24 00:06:34 +0100

    stress-opcode: fix missing space and remove redundant parentheses
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-opcode.c
+++ stress-opcode.c
@@ -135,7 +135,7 @@ static inline uint32_t reverse32(register uint64_t x)
 	x = (((x & 0xcccccccc) >> 2) | ((x & 0x33333333) << 2));
 	x = (((x & 0xf0f0f0f0) >> 4) | ((x & 0x0f0f0f0f) << 4));
 	x = (((x & 0xff00ff00) >> 8) | ((x & 0x00ff00ff) << 8));
-	return((x >> 16) | (x << 16));
+	return (x >> 16) | (x << 16);
 }
 
 static void stress_opcode_random(
