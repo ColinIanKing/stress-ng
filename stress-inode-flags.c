d86a2d36a6bd4e3bcca67d3ba60e46fca0ac6cce
Colin Ian King <colin.king@canonical.com>
2019-09-17 21:15:37 +0200

    stress-inode-flags: move open/close to controlling process
    Rather than open/close directory and file in the ptreads move
    this to the controlling process and just pass the open fds to
    the pthread to simplify the code.  Also make file and directory
    opening a resource exit failure on error rather than major
    stressor failure.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-inode-flags.c
+++ stress-inode-flags.c
@@ -39,6 +39,11 @@ static const help_t help[] = {
 
 #define MAX_INODE_FLAG_THREADS		(4)
 
+typedef struct {
+	int dir_fd;
+	int file_fd;
+} data_t;
+
 static volatile bool keep_running;
 static sigset_t set;
 static shim_pthread_spinlock_t spinlock;
@@ -94,77 +99,56 @@ static inline void stress_inode_flags_ioctl_sane(const int fd)
  */
 static int stress_inode_flags_stressor(
 	const args_t *args,
-	char *filename)
+	data_t *data)
 {
-	char pathname[PATH_MAX];
-	char *path;
-	int fdfile, fddir;
-
-	(void)shim_strlcpy(pathname, filename, PATH_MAX);
-	path = dirname(pathname);
-
-	fddir = open(path, O_RDONLY | O_DIRECTORY);
-	if (fddir < 0) {
-		pr_err("%s: cannot open %s: errno=%d (%s)\n",
-			args->name, path, errno, strerror(errno));
-		return -1;
-	}
-	fdfile = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-	if (fdfile < 0) {
-		pr_err("%s: cannot open %s: errno=%d (%s)\n",
-			args->name, filename, errno, strerror(errno));
-		(void)close(fddir);
-		return -1;
-	}
-
 	while (keep_running && keep_stressing()) {
 		int ret;
 
-		stress_inode_flags_ioctl(args, fddir, 0);
+		stress_inode_flags_ioctl(args, data->dir_fd, 0);
 #if defined(FS_DIRSYNC_FL)
-		stress_inode_flags_ioctl(args, fddir, FS_DIRSYNC_FL);
+		stress_inode_flags_ioctl(args, data->dir_fd, FS_DIRSYNC_FL);
 #endif
 #if defined(FS_PROJINHERIT_FL)
-		stress_inode_flags_ioctl(args, fddir, FS_PROJINHERIT_FL);
+		stress_inode_flags_ioctl(args, data->dir_fd, FS_PROJINHERIT_FL);
 #endif
 #if defined(FS_SYNC_FL)
-		stress_inode_flags_ioctl(args, fddir, FS_SYNC_FL);
+		stress_inode_flags_ioctl(args, data->dir_fd, FS_SYNC_FL);
 #endif
 #if defined(FS_TOPDIR_FL)
-		stress_inode_flags_ioctl(args, fddir, FS_TOPDIR_FL);
+		stress_inode_flags_ioctl(args, data->dir_fd, FS_TOPDIR_FL);
 #endif
 #if defined(FS_APPEND_LF)
-		stress_inode_flags_ioctl(args, fdfile, FS_APPEND_FL);
+		stress_inode_flags_ioctl(args, data->file_fd, FS_APPEND_FL);
 #endif
 #if defined(FS_COMPR_FL)
-		stress_inode_flags_ioctl(args, fdfile, FS_COMPR_FL);
+		stress_inode_flags_ioctl(args, data->file_fd, FS_COMPR_FL);
 #endif
 #if defined(FS_IMMUTABLE_FL)
-		stress_inode_flags_ioctl(args, fdfile, FS_IMMUTABLE_FL);
+		stress_inode_flags_ioctl(args, data->file_fd, FS_IMMUTABLE_FL);
 #endif
 #if defined(FS_JOURNAL_DATA_FL)
-		stress_inode_flags_ioctl(args, fdfile, FS_JOURNAL_DATA_FL);
+		stress_inode_flags_ioctl(args, data->file_fd, FS_JOURNAL_DATA_FL);
 #endif
 #if defined(FS_NOCOW_FL)
-		stress_inode_flags_ioctl(args, fdfile, FS_NOCOW_FL);
+		stress_inode_flags_ioctl(args, data->file_fd, FS_NOCOW_FL);
 #endif
 #if defined(FS_NODUMP_FL)
-		stress_inode_flags_ioctl(args, fdfile, FS_NODUMP_FL);
+		stress_inode_flags_ioctl(args, data->file_fd, FS_NODUMP_FL);
 #endif
 #if defined(FS_NOTAIL_FL)
-		stress_inode_flags_ioctl(args, fdfile, FS_NOTAIL_FL);
+		stress_inode_flags_ioctl(args, data->file_fd, FS_NOTAIL_FL);
 #endif
 #if defined(FS_PROJINHERIT_FL)
-		stress_inode_flags_ioctl(args, fdfile, FS_PROJINHERIT_FL);
+		stress_inode_flags_ioctl(args, data->file_fd, FS_PROJINHERIT_FL);
 #endif
 #if defined(FS_SECRM_FL)
-		stress_inode_flags_ioctl(args, fdfile, FS_SECRM_FL);
+		stress_inode_flags_ioctl(args, data->file_fd, FS_SECRM_FL);
 #endif
 #if defined(FS_SYNC_FL)
-		stress_inode_flags_ioctl(args, fdfile, FS_SYNC_FL);
+		stress_inode_flags_ioctl(args, data->file_fd, FS_SYNC_FL);
 #endif
 #if defined(FS_UNRM_FL)
-		stress_inode_flags_ioctl(args, fdfile, FS_UNRM_FL);
+		stress_inode_flags_ioctl(args, data->file_fd, FS_UNRM_FL);
 #endif
 		ret = shim_pthread_spin_lock(&spinlock);
 		if (!ret) {
@@ -172,11 +156,9 @@ static int stress_inode_flags_stressor(
 			ret = shim_pthread_spin_unlock(&spinlock);
 			(void)ret;
 		}
-		stress_inode_flags_ioctl_sane(fdfile);
+		stress_inode_flags_ioctl_sane(data->file_fd);
 	}
-	stress_inode_flags_ioctl_sane(fdfile);
-	(void)close(fdfile);
-	(void)close(fddir);
+	stress_inode_flags_ioctl_sane(data->file_fd);
 
 	return 0;
 }
@@ -207,7 +189,7 @@ static void *stress_inode_flags_thread(void *arg)
 	if (stress_sigaltstack(stack, SIGSTKSZ) < 0)
 		return &nowt;
 
-	pa->pthread_ret = stress_inode_flags_stressor(pa->args, (char *)pa->data);
+	pa->pthread_ret = stress_inode_flags_stressor(pa->args, pa->data);
 
 	return &nowt;
 }
@@ -219,11 +201,13 @@ static void *stress_inode_flags_thread(void *arg)
  */
 static int stress_inode_flags(const args_t *args)
 {
-	char filename[PATH_MAX];
 	size_t i;
 	pthread_t pthreads[MAX_INODE_FLAG_THREADS];
 	int rc, ret[MAX_INODE_FLAG_THREADS];
 	pthread_args_t pa[MAX_INODE_FLAG_THREADS];
+	data_t data;
+	char tmp[PATH_MAX], file_name[PATH_MAX];
+	char *dir_name;
 
 	rc = shim_pthread_spin_init(&spinlock, SHIM_PTHREAD_PROCESS_SHARED);
         if (rc) {
@@ -235,14 +219,32 @@ static int stress_inode_flags(const args_t *args)
 	if (rc < 0)
 		return exit_status(-rc);
 	(void)stress_temp_filename_args(args,
-		filename, sizeof(filename), mwc32());
+		file_name, sizeof(file_name), mwc32());
+
+	shim_strlcpy(tmp, file_name, sizeof(tmp));
+	dir_name = dirname(tmp);
+
+	data.dir_fd = open(dir_name, O_RDONLY | O_DIRECTORY);
+	if (data.dir_fd < 0) {
+		pr_err("%s: cannot open %s: errno=%d (%s)\n",
+			args->name, dir_name, errno, strerror(errno));
+		rc = EXIT_NO_RESOURCE;
+		goto tidy;
+	}
+	data.file_fd = open(file_name, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	if (data.file_fd < 0) {
+		pr_err("%s: cannot open %s: errno=%d (%s)\n",
+			args->name, file_name, errno, strerror(errno));
+		rc = EXIT_NO_RESOURCE;
+		goto tidy_dir_fd;
+	}
 
 	(void)memset(ret, 0, sizeof(ret));
 	keep_running = true;
 
 	for (i = 0; i < MAX_INODE_FLAG_THREADS; i++) {
 		pa[i].args = args;
-		pa[i].data = (void *)filename;
+		pa[i].data = (void *)&data;
 		pa[i].pthread_ret = 0;
 
 		ret[i] = pthread_create(&pthreads[i], NULL,
@@ -250,7 +252,7 @@ static int stress_inode_flags(const args_t *args)
 	}
 
 	do {
-		stress_inode_flags_stressor(args, filename);
+		stress_inode_flags_stressor(args, &data);
 	} while (keep_stressing());
 
 	keep_running = false;
@@ -264,9 +266,13 @@ static int stress_inode_flags(const args_t *args)
 		}
 	}
 
-	(void)shim_pthread_spin_destroy(&spinlock);
-	(void)unlink(filename);
+	(void)close(data.file_fd);
+tidy_dir_fd:
+	(void)close(data.dir_fd);
+tidy:
+	(void)unlink(file_name);
 	stress_temp_dir_rm_args(args);
+	(void)shim_pthread_spin_destroy(&spinlock);
 
 	return rc;
 }
