f7724a9ff8571475594cdfc9b048a6cf0a874f46
Colin Ian King <colin.king@canonical.com>
2019-11-27 00:00:12 +0000

    stress-msg: add random MSG_STAT_ANY msgctl call
    Add more coverage by performing a random MSG_STAT_ANY
    msgctl fetch.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-msg.c
+++ stress-msg.c
@@ -62,6 +62,14 @@ static int stress_msg_get_stats(const args_t *args, const int msgq_id)
 		return -errno;
 	}
 
+#if defined(MSG_STAT_ANY)
+	/*
+	 * select random msgq index numbers, we may hit
+	 * some that are in use. Ignore failures
+	 */
+	(void)msgctl(mwc8() % (msgq_id + 1), MSG_STAT_ANY, &buf);
+#endif
+
 #if defined(IPC_INFO) &&	\
     defined(HAVE_MSGINFO)
 	{
