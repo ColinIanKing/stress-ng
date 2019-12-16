4a150669bf13faa461ba31e6e39f83db40477ebd
Colin Ian King <colin.king@canonical.com>
2019-12-16 13:54:46 +0000

    stress-vecmath: don't use TARGET_CLONES on ppc64, it breaks some compilers
    stress-vecmath.c: In function ‘stress_vecmath.cpu_power9.0’:
    stress-vecmath.c:90:30: internal compiler error: in expand_debug_locations, at cfgexpand.c:5456
       90 | static int HOT TARGET_CLONES stress_vecmath(const args_t *args)
          |                              ^~~~~~~~~~~~~~
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-vecmath.c
+++ stress-vecmath.c
@@ -87,7 +87,11 @@ typedef __uint128_t vint128_t __attribute__ ((vector_size (16)));
  *  stress_vecmath()
  *	stress GCC vector maths
  */
+#if defined(STRESS_PPC64)
+static int HOT stress_vecmath(const args_t *args)
+#else
 static int HOT TARGET_CLONES stress_vecmath(const args_t *args)
+#endif
 {
 	vint8_t a8 = {
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
