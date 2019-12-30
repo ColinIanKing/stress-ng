a5eab969236e92e4d6d655c7c3e4646b7884c8c4
Colin Ian King <colin.king@canonical.com>
2019-12-30 21:04:17 +0000

    stress-vm-segv: avoid void * pointer arithmetic, it is not portable
    void pointer arithmetic is not portable, cast to uint8_t * pointer
    for addition.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-vm-segv.c
+++ stress-vm-segv.c
@@ -40,7 +40,7 @@ static NOINLINE void vm_unmap_child(const size_t page_size)
 		munmap((void *)0, len - page_size);
 		len >>= 1;
 		clflush(addr);
-		shim_clear_cache(addr, addr + 64);
+		shim_clear_cache(addr, (void *)(((uint8_t *)addr) + 64));
 	}
 }
 
@@ -50,7 +50,7 @@ static NOINLINE void vm_unmap_self(const size_t page_size)
 
 	munmap(addr, page_size);
 	clflush(addr);
-	shim_clear_cache(addr, addr + 64);
+	shim_clear_cache(addr, (void *)(((uint8_t *)addr) + 64));
 }
 
 /*
