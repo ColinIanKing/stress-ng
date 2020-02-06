ColinIanKing/stress-ng

commit 25112343a5d4664375d01d4896f6705bf87a623d

Add stress_oomable_child helper to catch and restart OOM'd child processes
Make a generic helper function to catch OOM'd child processes
and re-start them.  Use the helper function where possible. Factors
out a lot of complex almost identical OOM handling code.

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-clone.c
@@ -284,18 +284,23 @@ static int clone_func(void *arg)
 	return 0;
 }
 
-/*
- *  stress_clone()
- *	stress by cloning and exiting
- */
-static int stress_clone(const args_t *args)
+static int stress_clone_child(const args_t *args, void *context)
 {
+	/* Child */
 	uint64_t max_clones = 0;
 	uint64_t clone_max = DEFAULT_ZOMBIES;
-	pid_t pid;
+	bool use_clone3 = true;
+	const size_t mmap_size = args->page_size * 32768;
+	void *ptr;
 	const ssize_t stack_offset =
 		stress_get_stack_direction() *
 		(CLONE_STACK_SIZE - 64);
+	const int mflags = MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE;
+#else
+	const int mflags = MAP_ANONYMOUS | MAP_PRIVATE;
+#endif
+
+	(void)context;
 
 	if (!get_setting("clone-max", &clone_max)) {
 		if (g_opt_flags & OPT_FLAGS_MAXIMIZE)
@@ -304,158 +309,98 @@ static int stress_clone(const args_t *args)
 			clone_max = MIN_ZOMBIES;
 	}
 
-	set_oom_adjustment(args->name, false);
-again:
-	if (!g_keep_stressing_flag)
-		return EXIT_SUCCESS;
-	pid = fork();
-	if (pid < 0) {
-		if ((errno == EAGAIN) || (errno == ENOMEM))
-			goto again;
-		pr_err("%s: fork failed: errno=%d: (%s)\n",
-			args->name, errno, strerror(errno));
-	} else if (pid > 0) {
-		int status, ret;
-
-		(void)setpgid(pid, g_pgrp);
-		/* Parent, wait for child */
-		ret = shim_waitpid(pid, &status, 0);
-		if (ret < 0) {
-			if (errno != EINTR)
-				pr_dbg("%s: waitpid(): errno=%d (%s)\n",
-					args->name, errno, strerror(errno));
-			(void)kill(pid, SIGALRM);
-			(void)shim_waitpid(pid, &status, 0);
-			/* And kill it for sure */
-			(void)kill(pid, SIGKILL);
-		} else if (WIFSIGNALED(status)) {
-			pr_dbg("%s: child died: %s (instance %d)\n",
-				args->name, stress_strsignal(WTERMSIG(status)),
-				args->instance);
-			/* If we got killed by OOM killer, re-start */
-			if ((WTERMSIG(status) == SIGKILL) ||
-			    (WTERMSIG(status) == SIGTERM)) {
-				if (g_opt_flags & OPT_FLAGS_OOMABLE) {
-					log_system_mem_info();
-					pr_dbg("%s: assuming killed by OOM "
-						"killer, bailing out "
-						"(instance %d)\n",
-						args->name, args->instance);
-					_exit(0);
-				} else {
-					log_system_mem_info();
-					pr_dbg("%s: assuming killed by OOM "
-						"killer, restarting again "
-						"(instance %d)\n",
-						args->name, args->instance);
-					goto again;
-				}
-			}
-		}
-	} else if (pid == 0) {
-		/* Child */
-		int ret;
-		bool use_clone3 = true;
-		const size_t mmap_size = args->page_size * 32768;
-		void *ptr;
 #if defined(MAP_POPULATE)
-		const int mflags = MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE;
-#else
-		const int mflags = MAP_ANONYMOUS | MAP_PRIVATE;
-#endif
 
-		(void)setpgid(0, g_pgrp);
-		stress_parent_died_alarm();
+	/*
+	 * Make child larger than parent to make it more of
+	 * a candidate for a OOMable process
+	 */
+	ptr = mmap(NULL, mmap_size, PROT_READ | PROT_WRITE, mflags, -1, 0);
+	if (ptr != MAP_FAILED)
+		(void)mincore_touch_pages(ptr, mmap_size);
 
-		/* Make sure this is killable by OOM killer */
-		set_oom_adjustment(args->name, true);
+	do {
+		if (clones.length < clone_max) {
+			clone_t *clone_info;
+			stress_clone_args_t clone_arg = { args };
+			const uint32_t rnd = mwc32();
+			const int flag = flags[rnd % SIZEOF_ARRAY(flags)];
+			const bool try_clone3 = rnd >> 31;
 
-		/* Explicitly drop capabilites, makes it more OOM-able */
-		ret = stress_drop_capabilities(args->name);
-		(void)ret;
+			clone_info = stress_clone_new();
+			if (!clone_info)
+				break;
 
-		/*
-		 * Make child larger than parent to make it more of
-		 * a candidate for a OOMable process
-		 */
-		ptr = mmap(NULL, mmap_size, PROT_READ | PROT_WRITE, mflags, -1, 0);
-		if (ptr != MAP_FAILED)
-			(void)mincore_touch_pages(ptr, mmap_size);
+			if (use_clone3 && try_clone3) {
+				struct shim_clone_args cl_args;
+				int pidfd = -1;
+				pid_t child_tid = -1, parent_tid = -1;
 
-		do {
-			if (clones.length < clone_max) {
-				clone_t *clone_info;
-				stress_clone_args_t clone_arg = { args };
-				const uint32_t rnd = mwc32();
-				const int flag = flags[rnd % SIZEOF_ARRAY(flags)];
-				const bool try_clone3 = rnd >> 31;
+				memset(&cl_args, 0, sizeof(cl_args));
 
-				clone_info = stress_clone_new();
-				if (!clone_info)
-					break;
-
-				if (use_clone3 && try_clone3) {
-					struct shim_clone_args cl_args;
-					int pidfd = -1;
-					pid_t child_tid = -1, parent_tid = -1;
-
-					memset(&cl_args, 0, sizeof(cl_args));
-
-					cl_args.flags = flag;
-					cl_args.pidfd = uint64_ptr(&pidfd);
-					cl_args.child_tid = uint64_ptr(&child_tid);
-					cl_args.parent_tid = uint64_ptr(&parent_tid);
-					cl_args.exit_signal = SIGCHLD;
-					cl_args.stack = uint64_ptr(NULL);
-					cl_args.stack_size = 0;
-					cl_args.tls = uint64_ptr(NULL);
-					clone_info->pid = sys_clone3(&cl_args, sizeof(cl_args));
-					if (clone_info->pid < 0) {
-						/* Not available, don't use it again */
-						if (errno == ENOSYS)
-							use_clone3 = false;
-					} else if (clone_info->pid == 0) {
-						/* child */
-						_exit(clone_func(&clone_arg));
-					}
-				} else {
-					char *stack_top = clone_info->stack + stack_offset;
-
-					clone_info->pid = clone(clone_func,
-						align_stack(stack_top), flag, &clone_arg);
+				cl_args.flags = flag;
+				cl_args.pidfd = uint64_ptr(&pidfd);
+				cl_args.child_tid = uint64_ptr(&child_tid);
+				cl_args.parent_tid = uint64_ptr(&parent_tid);
+				cl_args.exit_signal = SIGCHLD;
+				cl_args.stack = uint64_ptr(NULL);
+				cl_args.stack_size = 0;
+				cl_args.tls = uint64_ptr(NULL);
+				clone_info->pid = sys_clone3(&cl_args, sizeof(cl_args));
+				if (clone_info->pid < 0) {
+					/* Not available, don't use it again */
+					if (errno == ENOSYS)
+						use_clone3 = false;
+				} else if (clone_info->pid == 0) {
+					/* child */
+					_exit(clone_func(&clone_arg));
 				}
-				if (clone_info->pid == -1) {
-					/*
-					 * Reached max forks or error
-					 * (e.g. EPERM)? .. then reap
-					 */
-					stress_clone_head_remove();
-					continue;
-				}
-				if (max_clones < clones.length)
-					max_clones = clones.length;
-				inc_counter(args);
 			} else {
-				stress_clone_head_remove();
+				char *stack_top = clone_info->stack + stack_offset;
+
+				clone_info->pid = clone(clone_func,
+					align_stack(stack_top), flag, &clone_arg);
 			}
-		} while (keep_stressing());
-
-		pr_inf("%s: created a maximum of %" PRIu64 " clones\n",
-			args->name, max_clones);
-
-		if (ptr != MAP_FAILED)
-			(void)munmap(ptr, mmap_size);
-		/* And reap */
-		while (clones.head) {
+			if (clone_info->pid == -1) {
+				/*
+				 * Reached max forks or error
+				 * (e.g. EPERM)? .. then reap
+				 */
+				stress_clone_head_remove();
+				continue;
+			}
+			if (max_clones < clones.length)
+				max_clones = clones.length;
+			inc_counter(args);
+		} else {
 			stress_clone_head_remove();
 		}
-		/* And free */
-		stress_clone_free();
+	} while (keep_stressing());
 
-		_exit(0);
+	pr_inf("%s: created a maximum of %" PRIu64 " clones\n",
+		args->name, max_clones);
+
+	if (ptr != MAP_FAILED)
+		(void)munmap(ptr, mmap_size);
+	/* And reap */
+	while (clones.head) {
+		stress_clone_head_remove();
 	}
+	/* And free */
+	stress_clone_free();
 
 	return EXIT_SUCCESS;
+}
+
+/*
+ *  stress_clone()
+ *	stress by cloning and exiting
+ */
+static int stress_clone(const args_t *args)
+{
+	set_oom_adjustment(args->name, false);
+
+	return stress_oomable_child(args, NULL, stress_clone_child, STRESS_OOMABLE_DROP_CAP);
 }
 
 stressor_info_t stress_clone_info = {

core-out-of-memory.c
@@ -163,3 +163,109 @@ bool process_oomed(const pid_t pid)
 	return false;
 }
 #endif
+
+/*
+ *  stress_oomable_child()
+ *  	generic way to run a process that is possibly going to be
+ *	OOM'd and we retry if it gets killed.
+ */
+int stress_oomable_child(
+	const args_t *args,
+	void *context,
+	stress_oomable_child_func_t func,
+	const int flag)
+{
+	pid_t pid;
+	int ooms = 0;
+	int segvs = 0;
+	int buserrs = 0;
+
+again:
+	if (!keep_stressing())
+		return EXIT_SUCCESS;
+	pid = fork();
+	if (pid < 0) {
+		/* Keep trying if we are out of resources */
+		if ((errno == EAGAIN) || (errno == ENOMEM))
+			goto again;
+		pr_err("%s: fork failed: errno=%d: (%s)\n",
+			args->name, errno, strerror(errno));
+		return -1;
+	} else if (pid > 0) {
+		/* Parent, wait for child */
+		int status, ret;
+
+		(void)setpgid(pid, g_pgrp);
+		ret = shim_waitpid(pid, &status, 0);
+		if (ret < 0) {
+			if (errno != EINTR)
+				pr_dbg("%s: waitpid(): errno=%d (%s)\n",
+					args->name, errno, strerror(errno));
+			(void)kill(pid, SIGTERM);
+			(void)kill(pid, SIGKILL);
+			(void)shim_waitpid(pid, &status, 0);
+		} else if (WIFSIGNALED(status)) {
+			pr_dbg("%s: child died: %s (instance %d)\n",
+				args->name, stress_strsignal(WTERMSIG(status)),
+				args->instance);
+			/* Bus error death? retry */
+			if (WTERMSIG(status) == SIGBUS) {
+                                buserrs++;
+                                goto again;
+                        }
+
+			/* If we got killed by OOM killer, re-start */
+			if (WTERMSIG(status) == SIGKILL) {
+				if (g_opt_flags & OPT_FLAGS_OOMABLE) {
+					log_system_mem_info();
+					pr_dbg("%s: assuming killed by OOM "
+						"killer, bailing out "
+						"(instance %d)\n",
+						args->name, args->instance);
+					_exit(0);
+				} else {
+					log_system_mem_info();
+					pr_dbg("%s: assuming killed by OOM "
+						"killer, restarting again "
+						"(instance %d)\n",
+						args->name, args->instance);
+					ooms++;
+					goto again;
+				}
+			}
+			/* If we got killed by sigsegv, re-start */
+			if (WTERMSIG(status) == SIGSEGV) {
+				pr_dbg("%s: killed by SIGSEGV, "
+					"restarting again "
+					"(instance %d)\n",
+					args->name, args->instance);
+				segvs++;
+				goto again;
+			}
+		}
+	} else if (pid == 0) {
+		/* Child */
+
+		(void)setpgid(0, g_pgrp);
+		stress_parent_died_alarm();
+
+		/* Make sure this is killable by OOM killer */
+		set_oom_adjustment(args->name, true);
+
+		/* Explicitly drop capabilites, makes it more OOM-able */
+		if (flag & STRESS_OOMABLE_DROP_CAP) {
+			int ret;
+
+			ret = stress_drop_capabilities(args->name);
+			(void)ret;
+		}
+		_exit(func(args, context));
+	}
+	if (ooms + segvs + buserrs > 0)
+		pr_dbg("%s: OOM restarts: %d"
+			", SIGSEGV restarts: %d"
+			", SIGBUS restarts: %d\n",
+			args->name, ooms, segvs, buserrs);
+
+	return EXIT_SUCCESS;
+}

stress-bigheap.c
@@ -45,20 +45,16 @@ static int stress_set_bigheap_growth(const char *opt)
 	return set_setting("bigheap-growth", TYPE_ID_UINT64, &bigheap_growth);
 }
 
-/*
- *  stress_bigheap()
- *	stress heap allocation
- */
-static int stress_bigheap(const args_t *args)
+static int stress_bigheap_child(const args_t *args, void *context)
 {
+	uint64_t bigheap_growth = DEFAULT_BIGHEAP_GROWTH;
 	void *ptr = NULL, *last_ptr = NULL;
 	const size_t page_size = args->page_size;
 	const size_t stride = page_size;
 	size_t size = 0;
-	uint32_t ooms = 0, segvs = 0, nomems = 0;
-	pid_t pid;
 	uint8_t *last_ptr_end = NULL;
-	uint64_t bigheap_growth = DEFAULT_BIGHEAP_GROWTH;
+
+	(void)context;
 
 	if (!get_setting("bigheap-growth", &bigheap_growth)) {
 		if (g_opt_flags & OPT_FLAGS_MAXIMIZE)
@@ -71,143 +67,84 @@ static int stress_bigheap(const args_t *args)
 
 	/* Round growth size to nearest page size */
 	bigheap_growth &= ~(page_size - 1);
-again:
-	if (!keep_stressing())
-		return EXIT_SUCCESS;
-	pid = fork();
-	if (pid < 0) {
-		if (errno == EAGAIN)
-			goto again;
-		pr_err("%s: fork failed: errno=%d: (%s)\n",
-			args->name, errno, strerror(errno));
-	} else if (pid > 0) {
-		int status, ret;
 
-		(void)setpgid(pid, g_pgrp);
-		/* Parent, wait for child */
-		ret = shim_waitpid(pid, &status, 0);
-		if (ret < 0) {
-			if (errno != EINTR)
-				pr_dbg("%s: waitpid(): errno=%d (%s)\n",
-					args->name, errno, strerror(errno));
-			(void)kill(pid, SIGTERM);
-			(void)kill(pid, SIGKILL);
-			(void)shim_waitpid(pid, &status, 0);
-		} else if (WIFSIGNALED(status)) {
-			pr_dbg("%s: child died: %s (instance %d)\n",
-				args->name, stress_strsignal(WTERMSIG(status)),
+	do {
+		void *old_ptr = ptr;
+		size += (size_t)bigheap_growth;
+
+		/*
+		 * With many instances running it is wise to
+		 * double check before the next realloc as
+		 * sometimes process start up is delayed for
+		 * some time and we should bail out before
+		 * exerting any more memory pressure
+		 */
+		if (!keep_stressing())
+			goto abort;
+
+		ptr = realloc(old_ptr, size);
+		if (ptr == NULL) {
+			pr_dbg("%s: out of memory at %" PRIu64
+				" MB (instance %d)\n",
+				args->name, (uint64_t)(4096ULL * size) >> 20,
 				args->instance);
-			/* If we got killed by OOM killer, re-start */
-			if (WTERMSIG(status) == SIGKILL) {
-				if (g_opt_flags & OPT_FLAGS_OOMABLE) {
-					log_system_mem_info();
-					pr_dbg("%s: assuming killed by OOM "
-						"killer, bailing out "
-						"(instance %d)\n",
-						args->name, args->instance);
-					_exit(0);
-				} else {
-					log_system_mem_info();
-					pr_dbg("%s: assuming killed by OOM "
-						"killer, restarting again "
-						"(instance %d)\n",
-						args->name, args->instance);
-					ooms++;
-					goto again;
-				}
+			free(old_ptr);
+			size = 0;
+		} else {
+			size_t i, n;
+			uint8_t *u8ptr, *tmp;
+
+			if (last_ptr == ptr) {
+				tmp = u8ptr = last_ptr_end;
+				n = (size_t)bigheap_growth;
+			} else {
+				tmp = u8ptr = ptr;
+				n = size;
 			}
-			/* If we got killed by sigsegv, re-start */
-			if (WTERMSIG(status) == SIGSEGV) {
-				pr_dbg("%s: killed by SIGSEGV, "
-					"restarting again "
-					"(instance %d)\n",
-					args->name, args->instance);
-				segvs++;
-				goto again;
-			}
-		}
-	} else if (pid == 0) {
-		(void)setpgid(0, g_pgrp);
-		stress_parent_died_alarm();
-
-		/* Make sure this is killable by OOM killer */
-		set_oom_adjustment(args->name, true);
-
-		do {
-			void *old_ptr = ptr;
-			size += (size_t)bigheap_growth;
-
-			/*
-			 * With many instances running it is wise to
-			 * double check before the next realloc as
-			 * sometimes process start up is delayed for
-			 * some time and we should bail out before
-			 * exerting any more memory pressure
-			 */
 			if (!keep_stressing())
 				goto abort;
 
-			ptr = realloc(old_ptr, size);
-			if (ptr == NULL) {
-				pr_dbg("%s: out of memory at %" PRIu64
-					" MB (instance %d)\n",
-					args->name, (uint64_t)(4096ULL * size) >> 20,
-					args->instance);
-				free(old_ptr);
-				size = 0;
-				nomems++;
-			} else {
-				size_t i, n;
-				uint8_t *u8ptr, *tmp;
+			if (page_size > 0) {
+				size_t sz = page_size - 1;
+				uintptr_t pg_ptr = ((uintptr_t)ptr + sz) & ~sz;
+				size_t len = size - (pg_ptr - (uintptr_t)ptr);
+				(void)mincore_touch_pages_interruptible((void *)pg_ptr, len);
+			}
 
-				if (last_ptr == ptr) {
-					tmp = u8ptr = last_ptr_end;
-					n = (size_t)bigheap_growth;
-				} else {
-					tmp = u8ptr = ptr;
-					n = size;
-				}
+			for (i = 0; i < n; i+= stride, u8ptr += stride) {
 				if (!keep_stressing())
 					goto abort;
+				*u8ptr = (uint8_t)i;
+			}
 
-				if (page_size > 0) {
-					size_t sz = page_size - 1;
-					uintptr_t pg_ptr = ((uintptr_t)ptr + sz) & ~sz;
-					size_t len = size - (pg_ptr - (uintptr_t)ptr);
-					(void)mincore_touch_pages_interruptible((void *)pg_ptr, len);
-				}
-
-				for (i = 0; i < n; i+= stride, u8ptr += stride) {
+			if (g_opt_flags & OPT_FLAGS_VERIFY) {
+				for (i = 0; i < n; i+= stride, tmp += stride) {
 					if (!keep_stressing())
 						goto abort;
-					*u8ptr = (uint8_t)i;
+					if (*tmp != (uint8_t)i)
+						pr_fail("%s: byte at location %p was 0x%" PRIx8
+							" instead of 0x%" PRIx8 "\n",
+							args->name, (void *)u8ptr, *tmp, (uint8_t)i);
 				}
-
-				if (g_opt_flags & OPT_FLAGS_VERIFY) {
-					for (i = 0; i < n; i+= stride, tmp += stride) {
-						if (!keep_stressing())
-							goto abort;
-						if (*tmp != (uint8_t)i)
-							pr_fail("%s: byte at location %p was 0x%" PRIx8
-								" instead of 0x%" PRIx8 "\n",
-								args->name, (void *)u8ptr, *tmp, (uint8_t)i);
-					}
-				}
-				last_ptr = ptr;
-				last_ptr_end = u8ptr;
 			}
-			inc_counter(args);
-		} while (keep_stressing());
+			last_ptr = ptr;
+			last_ptr_end = u8ptr;
+		}
+		inc_counter(args);
+	} while (keep_stressing());
 abort:
-		free(ptr);
-	}
-	if (ooms + segvs + nomems > 0)
-		pr_dbg("%s: OOM restarts: %" PRIu32
-			", SEGV restarts: %" PRIu32
-			", out of memory restarts: %" PRIu32 ".\n",
-			args->name, ooms, segvs, nomems);
+	free(ptr);
 
 	return EXIT_SUCCESS;
+}
+
+/*
+ *  stress_bigheap()
+ *	stress heap allocation
+ */
+static int stress_bigheap(const args_t *args)
+{
+	return stress_oomable_child(args, NULL, stress_bigheap_child, STRESS_OOMABLE_NORMAL);
 }
 
 static const opt_set_func_t opt_set_funcs[] = {

stress-brk.c
@@ -44,142 +44,74 @@ static const opt_set_func_t opt_set_funcs[] = {
 	{ 0,			NULL }
 };
 
+static int stress_brk_child(const args_t *args, void *context)
+{
+	uint8_t *start_ptr;
+	int i = 0;
+	const size_t page_size = args->page_size;
+	bool brk_notouch = false;
+
+	(void)context;
+
+	(void)get_setting("brk-notouch", &brk_notouch);
+
+	start_ptr = shim_sbrk(0);
+	if (start_ptr == (void *) -1) {
+		pr_fail_err("sbrk(0)");
+		return EXIT_FAILURE;
+	}
+
+	do {
+		uint8_t *ptr;
+
+		i++;
+		if (i < 8) {
+			/* Expand brk by 1 page */
+			ptr = shim_sbrk((intptr_t)page_size);
+		} else if (i < 9) {
+			/* brk to same brk position */
+			ptr = shim_sbrk(0);
+			if (shim_brk(ptr) < 0)
+				ptr = (void *)-1;
+		} else {
+			/* Shrink brk by 1 page */
+			i = 0;
+			ptr = shim_sbrk(0);
+			ptr -= page_size;
+			if (shim_brk(ptr) < 0)
+				ptr = (void *)-1;
+		}
+
+		if (ptr == (void *)-1) {
+			if ((errno == ENOMEM) || (errno == EAGAIN)) {
+				int ignore;
+
+				ignore = shim_brk(start_ptr);
+				(void)ignore;
+			} else {
+				pr_err("%s: sbrk(%d) failed: errno=%d (%s)\n",
+					args->name, (int)page_size, errno,
+					strerror(errno));
+				return EXIT_FAILURE;
+			}
+		} else {
+			/* Touch page, force it to be resident */
+			if (!brk_notouch)
+				*(ptr - 1) = 0;
+		}
+		inc_counter(args);
+	} while (keep_stressing());
+
+	return EXIT_SUCCESS;
+}
+
 /*
  *  stress_brk()
  *	stress brk and sbrk
  */
 static int stress_brk(const args_t *args)
 {
-	pid_t pid;
-	uint32_t ooms = 0, segvs = 0, nomems = 0;
-	const size_t page_size = args->page_size;
-	bool brk_notouch = false;
-
-	(void)get_setting("brk-notouch", &brk_notouch);
-
-again:
-	if (!g_keep_stressing_flag)
-		return EXIT_SUCCESS;
-	pid = fork();
-	if (pid < 0) {
-		if ((errno == EAGAIN) || (errno == ENOMEM))
-			goto again;
-		pr_err("%s: fork failed: errno=%d: (%s)\n",
-			args->name, errno, strerror(errno));
-	} else if (pid > 0) {
-		int status, ret;
-
-		(void)setpgid(pid, g_pgrp);
-		/* Parent, wait for child */
-		ret = shim_waitpid(pid, &status, 0);
-		if (ret < 0) {
-			if (errno != EINTR)
-				pr_dbg("%s: waitpid(): errno=%d (%s)\n",
-					args->name, errno, strerror(errno));
-			(void)kill(pid, SIGTERM);
-			(void)kill(pid, SIGKILL);
-			(void)shim_waitpid(pid, &status, 0);
-		} else if (WIFSIGNALED(status)) {
-			pr_dbg("%s: child died: %s (instance %d)\n",
-				args->name, stress_strsignal(WTERMSIG(status)),
-				args->instance);
-			/* If we got killed by OOM killer, re-start */
-			if (WTERMSIG(status) == SIGKILL) {
-				if (g_opt_flags & OPT_FLAGS_OOMABLE) {
-					log_system_mem_info();
-					pr_dbg("%s: assuming killed by OOM "
-						"killer, bailing out "
-						"(instance %d)\n",
-						args->name, args->instance);
-					_exit(0);
-				} else {
-					log_system_mem_info();
-					pr_dbg("%s: assuming killed by OOM "
-						"killer, restarting again "
-						"(instance %d)\n",
-						args->name, args->instance);
-					ooms++;
-					goto again;
-				}
-			}
-			/* If we got killed by sigsegv, re-start */
-			if (WTERMSIG(status) == SIGSEGV) {
-				pr_dbg("%s: killed by SIGSEGV, "
-					"restarting again "
-					"(instance %d)\n",
-					args->name, args->instance);
-				segvs++;
-				goto again;
-			}
-		}
-	} else if (pid == 0) {
-		uint8_t *start_ptr;
-		int ret, i = 0;
-
-		(void)setpgid(0, g_pgrp);
-		stress_parent_died_alarm();
-
-		/* Make sure this is killable by OOM killer */
-		set_oom_adjustment(args->name, true);
-
-		/* Explicitly drop capabilites, makes it more OOM-able */
-		ret = stress_drop_capabilities(args->name);
-		(void)ret;
-
-		start_ptr = shim_sbrk(0);
-		if (start_ptr == (void *) -1) {
-			pr_fail_err("sbrk(0)");
-			_exit(EXIT_FAILURE);
-		}
-
-		do {
-			uint8_t *ptr;
-
-			i++;
-			if (i < 8) {
-				/* Expand brk by 1 page */
-				ptr = shim_sbrk((intptr_t)page_size);
-			} else if (i < 9) {
-				/* brk to same brk position */
-				ptr = shim_sbrk(0);
-				if (shim_brk(ptr) < 0)
-					ptr = (void *)-1;
-			} else {
-				/* Shrink brk by 1 page */
-				i = 0;
-				ptr = shim_sbrk(0);
-				ptr -= page_size;
-				if (shim_brk(ptr) < 0)
-					ptr = (void *)-1;
-			}
-
-			if (ptr == (void *)-1) {
-				if ((errno == ENOMEM) || (errno == EAGAIN)) {
-					int ignore;
-					nomems++;
-					ignore = shim_brk(start_ptr);
-					(void)ignore;
-				} else {
-					pr_err("%s: sbrk(%d) failed: errno=%d (%s)\n",
-						args->name, (int)page_size, errno,
-						strerror(errno));
-					_exit(EXIT_FAILURE);
-				}
-			} else {
-				/* Touch page, force it to be resident */
-				if (!brk_notouch)
-					*(ptr - 1) = 0;
-			}
-			inc_counter(args);
-		} while (keep_stressing());
-	}
-	if (ooms + segvs + nomems > 0)
-		pr_dbg("%s: OOM restarts: %" PRIu32
-			", SEGV restarts: %" PRIu32
-			", out of memory restarts: %" PRIu32 ".\n",
-			args->name, ooms, segvs, nomems);
-
-	return EXIT_SUCCESS;
+	return stress_oomable_child(args, NULL, stress_brk_child, STRESS_OOMABLE_DROP_CAP);
 }
 
 stressor_info_t stress_brk_info = {

stress-malloc.c
@@ -75,20 +75,17 @@ static inline size_t stress_alloc_size(const size_t malloc_bytes)
 	return len ? len : 1;
 }
 
-/*
- *  stress_malloc()
- *	stress malloc by performing a mix of
- *	allocation and frees
- */
-static int stress_malloc(const args_t *args)
+static int stress_malloc_child(const args_t *args, void *context)
 {
-	pid_t pid;
-	uint32_t restarts = 0, nomems = 0;
+	size_t j;
 	size_t malloc_bytes = DEFAULT_MALLOC_BYTES;
 	size_t malloc_max = DEFAULT_MALLOC_MAX;
 #if defined(__GNUC__) && defined(M_MMAP_THRESHOLD) && defined(HAVE_MALLOPT)
 	size_t malloc_threshold = DEFAULT_MALLOC_THRESHOLD;
 #endif
+	void **addr;
+
+	(void)context;
 
 	if (!get_setting("malloc-bytes", &malloc_bytes)) {
 		if (g_opt_flags & OPT_FLAGS_MAXIMIZE)
@@ -112,127 +109,82 @@ static int stress_malloc(const args_t *args)
 	if (get_setting("malloc-threshold", &malloc_threshold))
 		(void)mallopt(M_MMAP_THRESHOLD, (int)malloc_threshold);
 #endif
-
-again:
-	pid = fork();
-	if (pid < 0) {
-		if (g_keep_stressing_flag &&
-		    ((errno == EAGAIN) || (errno == ENOMEM)))
-			goto again;
-		pr_err("%s: fork failed: errno=%d: (%s)\n",
+	addr = (void **)calloc(malloc_max, sizeof(void *));
+	if (!addr) {
+		pr_dbg("%s: cannot allocate address buffer: %d (%s)\n",
 			args->name, errno, strerror(errno));
-	} else if (pid > 0) {
-		int status, ret;
-
-		(void)setpgid(pid, g_pgrp);
-		stress_parent_died_alarm();
-
-		/* Parent, wait for child */
-		ret = shim_waitpid(pid, &status, 0);
-		if (ret < 0) {
-			if (errno != EINTR)
-				pr_dbg("%s: waitpid(): errno=%d (%s)\n",
-					args->name, errno, strerror(errno));
-			(void)kill(pid, SIGTERM);
-			(void)kill(pid, SIGKILL);
-			(void)shim_waitpid(pid, &status, 0);
-		} else if (WIFSIGNALED(status)) {
-			pr_dbg("%s: child died: %s (instance %d)\n",
-				args->name, stress_strsignal(WTERMSIG(status)),
-				args->instance);
-			/* If we got killed by OOM killer, re-start */
-			if (WTERMSIG(status) == SIGKILL) {
-				if (g_opt_flags & OPT_FLAGS_OOMABLE) {
-					log_system_mem_info();
-					pr_dbg("%s: assuming killed by OOM "
-						"killer, bailing out "
-						"(instance %d)\n",
-						args->name, args->instance);
-					_exit(0);
-				} else {
-						log_system_mem_info();
-						pr_dbg("%s: assuming killed by OOM "
-							"killer, restarting again "
-							"(instance %d)\n",
-							args->name, args->instance);
-						restarts++;
-						goto again;
-				}
-			}
-		}
-	} else if (pid == 0) {
-		void *addr[malloc_max];
-		size_t j;
-
-		(void)setpgid(0, g_pgrp);
-		(void)memset(addr, 0, sizeof(addr));
-
-		/* Make sure this is killable by OOM killer */
-		set_oom_adjustment(args->name, true);
-
-		do {
-			const unsigned int rnd = mwc32();
-			const unsigned int i = rnd % malloc_max;
-			const unsigned int action = (rnd >> 12) & 1;
-			const unsigned int do_calloc = (rnd >> 14) & 0x1f;
-
-			/*
-			 * With many instances running it is wise to
-			 * double check before the next allocation as
-			 * sometimes process start up is delayed for
-			 * some time and we should bail out before
-			 * exerting any more memory pressure
-			 */
-			if (!g_keep_stressing_flag)
-				goto abort;
-
-			if (addr[i]) {
-				/* 50% free, 50% realloc */
-				if (action) {
-					free(addr[i]);
-					addr[i] = NULL;
-					inc_counter(args);
-				} else {
-					void *tmp;
-					const size_t len = stress_alloc_size(malloc_bytes);
-
-					tmp = realloc(addr[i], len);
-					if (tmp) {
-						addr[i] = tmp;
-						(void)mincore_touch_pages_interruptible(addr[i], len);
-						inc_counter(args);
-					}
-				}
-			} else {
-				/* 50% free, 50% alloc */
-				if (action) {
-					size_t len = stress_alloc_size(malloc_bytes);
-
-					if (do_calloc == 0) {
-						size_t n = ((rnd >> 15) % 17) + 1;
-						addr[i] = calloc(n, len / n);
-						len = n * (len / n);
-					} else {
-						addr[i] = malloc(len);
-					}
-					if (addr[i]) {
-						inc_counter(args);
-						(void)mincore_touch_pages_interruptible(addr[i], len);
-					}
-				}
-			}
-		} while (keep_stressing());
-abort:
-		for (j = 0; j < malloc_max; j++) {
-			free(addr[j]);
-		}
+		return EXIT_NO_RESOURCE;
 	}
-	if (restarts + nomems > 0)
-		pr_dbg("%s: OOM restarts: %" PRIu32
-			", out of memory restarts: %" PRIu32 ".\n",
-			args->name, restarts, nomems);
+
+	do {
+		const unsigned int rnd = mwc32();
+		const unsigned int i = rnd % malloc_max;
+		const unsigned int action = (rnd >> 12) & 1;
+		const unsigned int do_calloc = (rnd >> 14) & 0x1f;
+
+		/*
+		 * With many instances running it is wise to
+		 * double check before the next allocation as
+		 * sometimes process start up is delayed for
+		 * some time and we should bail out before
+		 * exerting any more memory pressure
+		 */
+		if (!g_keep_stressing_flag)
+			goto abort;
+
+		if (addr[i]) {
+			/* 50% free, 50% realloc */
+			if (action) {
+				free(addr[i]);
+				addr[i] = NULL;
+				inc_counter(args);
+			} else {
+				void *tmp;
+				const size_t len = stress_alloc_size(malloc_bytes);
+
+				tmp = realloc(addr[i], len);
+				if (tmp) {
+					addr[i] = tmp;
+					(void)mincore_touch_pages_interruptible(addr[i], len);
+					inc_counter(args);
+				}
+			}
+		} else {
+			/* 50% free, 50% alloc */
+			if (action) {
+				size_t len = stress_alloc_size(malloc_bytes);
+
+				if (do_calloc == 0) {
+					size_t n = ((rnd >> 15) % 17) + 1;
+					addr[i] = calloc(n, len / n);
+					len = n * (len / n);
+				} else {
+					addr[i] = malloc(len);
+				}
+				if (addr[i]) {
+					inc_counter(args);
+					(void)mincore_touch_pages_interruptible(addr[i], len);
+				}
+			}
+		}
+	} while (keep_stressing());
+abort:
+	for (j = 0; j < malloc_max; j++) {
+		free(addr[j]);
+	}
+	free(addr);
 
 	return EXIT_SUCCESS;
+}
+
+/*
+ *  stress_malloc()
+ *	stress malloc by performing a mix of
+ *	allocation and frees
+ */
+static int stress_malloc(const args_t *args)
+{
+	return stress_oomable_child(args, NULL, stress_malloc_child, STRESS_OOMABLE_NORMAL);
 }
 
 static const opt_set_func_t opt_set_funcs[] = {

commit e5bfb3314d6b0a76525450a30bb8a3a6a24bab97

stress-clone: move a #if defined that got moved to the wrong place
A line of code got moved to the wrong place during the child helper
changes. Fix this.

Fixes: commit 25112343a5d4 ("Add stress_oomable_child helper to catch and restart OOM'd child processes")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-clone.c
@@ -295,6 +295,7 @@ static int stress_clone_child(const args_t *args, void *context)
 	const ssize_t stack_offset =
 		stress_get_stack_direction() *
 		(CLONE_STACK_SIZE - 64);
+#if defined(MAP_POPULATE)
 	const int mflags = MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE;
 #else
 	const int mflags = MAP_ANONYMOUS | MAP_PRIVATE;
@@ -308,8 +309,6 @@ static int stress_clone_child(const args_t *args, void *context)
 		if (g_opt_flags & OPT_FLAGS_MINIMIZE)
 			clone_max = MIN_ZOMBIES;
 	}
-
-#if defined(MAP_POPULATE)
 
 	/*
 	 * Make child larger than parent to make it more of