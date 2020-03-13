ColinIanKing/stress-ng

commit 3a60efbb8e302346b21802142b9f324a0b32e1a6

Rename arch macros, add _ARCH to signify they are architecture specific settings
The arch macros should have _ARCH inside them to signifiy they are
architecture specific macros

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-ng.h
@@ -1563,7 +1563,7 @@ extern void pr_fail_dbg__(const stress_args_t *args, const char *msg);
 /* Arch specific, x86 */
 #if defined(__x86_64__) || defined(__x86_64) || \
     defined(__i386__)   || defined(__i386)
-#define STRESS_X86	1
+#define STRESS_ARCH_X86		(1)
 #endif
 
 /* Arch specific, ARM */
@@ -1574,17 +1574,17 @@ extern void pr_fail_dbg__(const stress_args_t *args, const char *msg);
     defined(__ARM_ARCH_7A__)  || defined(__ARM_ARCH_7R__)  || \
     defined(__ARM_ARCH_7M__)  || defined(__ARM_ARCH_7EM__) || \
     defined(__ARM_ARCH_8A__)  || defined(__aarch64__)
-#define STRESS_ARM      1
+#define STRESS_ARCH_ARM		(1)
 #endif
 
 /* Arch specific, IBM S390 */
 #if defined(__s390__)
-#define STRESS_S390
+#define STRESS_ARCH_S390	(1)
 #endif
 
 /* Arch specific PPC64 */
 #if defined(__PPC64__)
-#define STRESS_PPC64
+#define STRESS_ARCH_PPC64	(1)
 #endif
 
 /* GCC5.0+ target_clones attribute */

core-cpu.c
@@ -27,7 +27,7 @@
 bool stress_cpu_is_x86(void)
 {
 #if defined(HAVE_CPUID_H) &&	\
-    defined(STRESS_X86) && 	\
+    defined(STRESS_ARCH_X86) && 	\
     defined(HAVE_CPUID) &&	\
     NEED_GNUC(4,6,0)
 	uint32_t eax, ebx, ecx, edx;

core-ignite-cpu.c
@@ -50,7 +50,7 @@ static uint32_t max_cpus;		/* max cpus configured */
 	{ path, default_setting, 0, NULL, 0, false }
 
 static stress_settings_t settings[] = {
-#if defined(__linux__) && defined(STRESS_X86)
+#if defined(__linux__) && defined(STRESS_ARCH_X86)
 	/* x86 Intel P-State maximizing settings */
 	SETTING("/sys/devices/system/cpu/intel_pstate/max_perf_pct", "100"),
 	SETTING("/sys/devices/system/cpu/intel_pstate/no_turbo", "0"),

core-shim.c
@@ -264,7 +264,7 @@ int shim_getrandom(void *buff, size_t buflen, unsigned int flags)
  */
 void shim_clear_cache(char* begin, char *end)
 {
-#if defined(__GNUC__) && defined(STRESS_ARM)
+#if defined(__GNUC__) && defined(STRESS_ARCH_ARM)
 	__clear_cache(begin, end);
 #else
 	(void)begin;

stress-cpu.c
@@ -2049,7 +2049,7 @@ static void TARGET_CLONES stress_cpu_div64(const char *name)
  *	get CPU id info, x86 only
  */
 #if defined(HAVE_CPUID_H) &&	\
-    defined(STRESS_X86) &&	\
+    defined(STRESS_ARCH_X86) &&	\
     defined(HAVE_CPUID) &&	\
     NEED_GNUC(4,6,0)
 static void TARGET_CLONES stress_cpu_cpuid(const char *name)
@@ -2367,7 +2367,7 @@ static const stress_cpu_method_info_t cpu_methods[] = {
 	{ "collatz",		stress_cpu_collatz },
 	{ "correlate",		stress_cpu_correlate },
 #if defined(HAVE_CPUID_H) &&	\
-    defined(STRESS_X86) &&	\
+    defined(STRESS_ARCH_X86) &&	\
     defined(HAVE_CPUID) &&	\
     NEED_GNUC(4,6,0)
 	{ "cpuid",		stress_cpu_cpuid },

stress-dev.c
@@ -830,7 +830,7 @@ static void stress_dev_mem_mmap_linux(const int fd, const bool read_page)
 
 		/* Try seeking */
 		off = lseek(fd, (off_t)0, SEEK_SET);
-#if defined(STRESS_X86)
+#if defined(STRESS_ARCH_X86)
 		if (off == 0) {
 			char buffer[page_size];
 			ssize_t ret;
@@ -925,7 +925,7 @@ static void stress_dev_hpet_linux(
 }
 #endif
 
-#if defined(__linux__) && defined(STRESS_X86)
+#if defined(__linux__) && defined(STRESS_ARCH_X86)
 static void stress_dev_port_linux(
 	const char *name,
 	const int fd,
@@ -1123,7 +1123,7 @@ static const stress_dev_func_t dev_funcs[] = {
 	DEV_FUNC("/dev/kmsg",	stress_dev_kmsg_linux),
 	DEV_FUNC("/dev/nvram",	stress_dev_nvram_linux),
 #endif
-#if defined(__linux__) && defined(STRESS_X86)
+#if defined(__linux__) && defined(STRESS_ARCH_X86)
 	DEV_FUNC("/dev/port",	stress_dev_port_linux),
 #endif
 #if defined(HAVE_LINUX_HPET_H)

stress-icache.c
@@ -30,8 +30,8 @@ static const stress_help_t help[] = {
 	{ NULL,	NULL,		NULL }
 };
 
-#if (defined(STRESS_X86) || defined(STRESS_ARM) || \
-     defined(STRESS_S390) || defined(STRESS_PPC64)) && \
+#if (defined(STRESS_ARCH_X86) || defined(STRESS_ARCH_ARM) || \
+     defined(STRESS_ARCH_S390) || defined(STRESS_ARCH_PPC64)) && \
      defined(__GNUC__) && NEED_GNUC(4,6,0) && \
      defined(HAVE_MPROTECT)
 

stress-ioport.c
@@ -73,7 +73,7 @@ static const stress_opt_set_func_t opt_set_funcs[] = {
 	{ 0,			NULL }
 };
 
-#if defined(STRESS_X86) && 	\
+#if defined(STRESS_ARCH_X86) && 	\
     defined(HAVE_IOPORT) &&	\
     defined(HAVE_SYS_IO_H)
 

commit dee0f535614b3d8770eff6ec23284d2239999892

Fix _ARCH macros not being used because of bad rename
Forgot to rename arch macros. Fix these.

Fixes: 3a60efbb8e30 ("Rename arch macros, add _ARCH to signify they are architecture specific settings")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-ng.h
@@ -1589,9 +1589,9 @@ extern void pr_fail_dbg__(const stress_args_t *args, const char *msg);
 #endif
 
 /* GCC5.0+ target_clones attribute */
-#if defined(HAVE_TARGET_CLONES) && defined(STRESS_X86)
+#if defined(HAVE_TARGET_CLONES) && defined(STRESS_ARCH_X86)
 #define TARGET_CLONES	__attribute__((target_clones("mmx","sse","sse2","ssse3", "sse4.1", "sse4a", "avx", "avx2", "avx512f", "default")))
-#elif defined(HAVE_TARGET_CLONES) && defined(STRESS_PPC64)
+#elif defined(HAVE_TARGET_CLONES) && defined(STRESS_ARCH_PPC64)
 #define TARGET_CLONES	__attribute__((target_clones("cpu=power9,default")))
 #else
 #define TARGET_CLONES
@@ -3229,7 +3229,7 @@ extern void stress_cwd_readwriteable(void);
 
 extern const char *stress_strsignal(const int signum);
 
-#if defined(STRESS_X86)
+#if defined(STRESS_ARCH_X86)
 /*
  *  shim_clflush()
  *	flush a cache line
@@ -3251,7 +3251,7 @@ static inline void ALWAYS_INLINE shim_mfence(void)
 #if NEED_GNUC(4, 2, 0) && !defined(__PCC__)
 	__sync_synchronize();
 #else
-#if defined(STRESS_X86)
+#if defined(STRESS_ARCH_X86)
 	asm volatile("mfence" : : : "memory");
 #else
 	/* Other arches not yet implemented for older GCC flavours */