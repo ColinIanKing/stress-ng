ColinIanKing/stress-ng

commit 9177399e32338fcb37ad07c68616d7e895c8858f

stress-chmod: Add fchmodat check and add it to chmod testing
Exercise fchmodat if it is available

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-chmod.c
@@ -75,9 +75,9 @@ static const mode_t modes[] = {
  *  as a "known" error on invalid chmod mode bits
  */
 #if defined(EFTYPE)
-#define CHECK(x) if ((x) && (errno != EFTYPE)) return -1
+#define CHECK(x) if ((x) && ((errno != ENOSYS) || (errno != EFTYPE))) return -1
 #else
-#define CHECK(x) if (x) return -1
+#define CHECK(x) if ((x) && (errno != ENOSYS)) return -1
 #endif
 
 /*
@@ -110,6 +110,8 @@ static int do_fchmod(
  *		inverse all mode flags or'd together
  */
 static int do_chmod(
+	const int dfd,
+	const char *filebase,
 	const char *filename,
 	const int i,
 	const mode_t mask,
@@ -119,6 +121,18 @@ static int do_chmod(
 	CHECK(chmod(filename, mask) < 0);
 	CHECK(chmod(filename, modes[i] ^ all_mask) < 0);
 	CHECK(chmod(filename, mask ^ all_mask) < 0);
+
+#if defined(HAVE_FCHMODAT)
+	if (dfd >= 0) {
+		CHECK(fchmodat(dfd, filebase, modes[i], 0) < 0);
+		CHECK(fchmodat(dfd, filebase, mask, 0) < 0);
+		CHECK(fchmodat(dfd, filebase, modes[i] ^ all_mask, 0) < 0);
+		CHECK(fchmodat(dfd, filebase, mask ^ all_mask, 0) < 0);
+	}
+#else
+	(void)dfd;
+	(void)filebase;
+#endif
 	return 0;
 }
 
@@ -129,9 +143,10 @@ static int do_chmod(
 static int stress_chmod(const stress_args_t *args)
 {
 	const pid_t ppid = getppid();
-	int i, fd = -1, rc = EXIT_FAILURE, retries = 0;
+	int i, fd = -1, rc = EXIT_FAILURE, retries = 0, dfd = -1;
 	mode_t all_mask = 0;
 	char filename[PATH_MAX], pathname[PATH_MAX];
+	char tmp[PATH_MAX], *filebase;
 
 	/*
 	 *  Allow for multiple workers to chmod the *same* file
@@ -144,8 +159,12 @@ static int stress_chmod(const stress_args_t *args)
 			return rc;
 		}
 	}
+	dfd = open(pathname, O_DIRECTORY | O_RDONLY);
+
 	(void)stress_temp_filename(filename, sizeof(filename),
 		args->name, ppid, 0, 0);
+	(void)strcpy(tmp, filename);
+	filebase = basename(tmp);
 
 	if (args->instance == 0) {
 		if ((fd = creat(filename, S_IRUSR | S_IWUSR)) < 0) {
@@ -192,7 +211,7 @@ static int stress_chmod(const stress_args_t *args)
 			if (do_fchmod(fd, i, mask, all_mask) < 0) {
 				pr_fail_err("fchmod");
 			}
-			if (do_chmod(filename, i, mask, all_mask) < 0) {
+			if (do_chmod(dfd, filebase, filename, i, mask, all_mask) < 0) {
 				if (errno == ENOENT || errno == ENOTDIR) {
 					/*
 					 * File was removed during test by
@@ -209,6 +228,8 @@ static int stress_chmod(const stress_args_t *args)
 
 	rc = EXIT_SUCCESS;
 tidy:
+	if (dfd >= 0)
+		(void)close(dfd);
 	if (fd >= 0) {
 		(void)fchmod(fd, 0666);
 		(void)close(fd);

Makefile.config
@@ -1572,6 +1572,14 @@ $(info autoconfig: using fanotify)
 endif
 endif
 
+ifndef $(HAVE_FCHMODAT)
+HAVE_FCHMODAT = $(shell $(MAKE) -f Makefile.config --no-print-directory $(HAVE_NOT) TEST_PROG=test-fchmodat have_test_prog)
+ifeq ($(HAVE_FCHMODAT),1)
+	CONFIG_CFLAGS += -DHAVE_FCHMODAT
+$(info autoconfig: using fchmodat)
+endif
+endif
+
 ifndef $(HAVE_FCHOWNAT)
 HAVE_FCHOWNAT = $(shell $(MAKE) -f Makefile.config --no-print-directory $(HAVE_NOT) TEST_PROG=test-fchownat have_test_prog)
 ifeq ($(HAVE_FCHOWNAT),1)

test/test-fchmodat.c
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2013-2020 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This code is a complete clean re-write of the stress tool by
+ * Colin Ian King <colin.king@canonical.com> and attempts to be
+ * backwardly compatible with the stress tool by Amos Waterland
+ * <apw@rossby.metr.ou.edu> but has more stress tests and more
+ * functionality.
+ *
+ */
+
+#include <unistd.h>
+
+int main(void)
+{
+	return fchmodat(0, "", 0, 0);
+}

commit 23cb851de667bdfab95a3a3703207708fc5ae5bd

stress-chmod: fix BSD EFTYPE error failure
Got the logic wrong when checking for ENOSYS and EFTYPE. Fix it.

Fixes: 9177399e3233 ("stress-chmod: Add fchmodat check and add it to chmod testing")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-chmod.c
@@ -75,7 +75,7 @@ static const mode_t modes[] = {
  *  as a "known" error on invalid chmod mode bits
  */
 #if defined(EFTYPE)
-#define CHECK(x) if ((x) && ((errno != ENOSYS) || (errno != EFTYPE))) return -1
+#define CHECK(x) if ((x) && ((errno != ENOSYS) && (errno != EFTYPE))) return -1
 #else
 #define CHECK(x) if ((x) && (errno != ENOSYS)) return -1
 #endif