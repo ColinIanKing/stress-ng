ColinIanKing/stress-ng

commit 4bc4c33696158ffa27ed4156f61db3f9f0a8d06a

stress-pty: exercise tcdrain, tcflush, tcflow and tcgetattr
These termios exercise lower level ioctls so exercise them to
improve kernel coverage.  Add the tc* function detection at
build time.

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-pty.c
@@ -135,6 +135,75 @@ static int stress_pty(const stress_args_t *args)
 		for (i = 0; i < n; i++) {
 			if ((ptys[i].master < 0) || (ptys[i].slave < 0))
 				continue;
+
+#if defined(DHAVE_TCGETATTR)
+			{
+				struct termios ios;
+
+				if (tcgetattr(ptys[i].master, &ios) < 0) {
+					pr_fail("%s: tcgetattr on master pty failed, errno=%d (%s)\n",
+						args->name, errno, strerror(errno));
+				}
+			}
+#endif
+#if defined(HAVE_TCDRAIN)
+			{
+				if (tcdrain(ptys[i].slave) < 0) {
+					pr_fail("%s: tcdrain on slave pty failed, errno=%d (%s)\n",
+						args->name, errno, strerror(errno));
+				}
+			}
+#endif
+#if defined(HAVE_TCFLUSH)
+			{
+#if defined(TCIFLUSH)
+				if (tcflush(ptys[i].slave, TCIFLUSH) < 0) {
+					pr_fail("%s: tcflush TCIFLUSH on slave pty failed, errno=%d (%s)\n",
+						args->name, errno, strerror(errno));
+				}
+#endif
+#if defined(TCOFLUSH)
+				if (tcflush(ptys[i].slave, TCOFLUSH) < 0) {
+					pr_fail("%s: tcflush TCOFLUSH on slave pty failed, errno=%d (%s)\n",
+						args->name, errno, strerror(errno));
+				}
+#endif
+#if defined(TCIOFLUSH)
+				if (tcflush(ptys[i].slave, TCIOFLUSH) < 0) {
+					pr_fail("%s: tcflush TCOOFLUSH on slave pty failed, errno=%d (%s)\n",
+						args->name, errno, strerror(errno));
+				}
+#endif
+			}
+#endif
+#if defined(HAVE_TCFLOW)
+#if defined(TCOOFF) && \
+    defined(TCOON)
+			{
+				if (tcflow(ptys[i].slave, TCOOFF) < 0) {
+					pr_fail("%s: tcflow TCOOFF on slave pty failed, errno=%d (%s)\n",
+						args->name, errno, strerror(errno));
+				}
+				if (tcflow(ptys[i].slave, TCOON) < 0) {
+					pr_fail("%s: tcflow TCOON on slave pty failed, errno=%d (%s)\n",
+						args->name, errno, strerror(errno));
+				}
+#endif
+			}
+#if defined(TCIOFF) && \
+    defined(TCION)
+			{
+				if (tcflow(ptys[i].slave, TCIOFF) < 0) {
+					pr_fail("%s: tcflow TCIOFF on slave pty failed, errno=%d (%s)\n",
+						args->name, errno, strerror(errno));
+				}
+				if (tcflow(ptys[i].slave, TCION) < 0) {
+					pr_fail("%s: tcflow TCION on slave pty failed, errno=%d (%s)\n",
+						args->name, errno, strerror(errno));
+				}
+#endif
+			}
+#endif
 #if defined(TCGETS)
 			{
 				struct termios ios;

Makefile.config
@@ -2717,6 +2717,38 @@ $(info autoconfig: using SYSV shared memory)
 endif
 endif
 
+ifndef $(HAVE_TCDRAIN)
+HAVE_TCDRAIN = $(shell $(MAKE) -f Makefile.config --no-print-directory $(HAVE_NOT) TEST_PROG=test-tcdrain have_test_prog)
+ifeq ($(HAVE_TCDRAIN),1)
+	CONFIG_CFLAGS += -DHAVE_TCDRAIN
+$(info autoconfig: using tcdrain)
+endif
+endif
+
+ifndef $(HAVE_TCFLOW)
+HAVE_TCFLOW = $(shell $(MAKE) -f Makefile.config --no-print-directory $(HAVE_NOT) TEST_PROG=test-tcflow have_test_prog)
+ifeq ($(HAVE_TCFLOW),1)
+	CONFIG_CFLAGS += -DHAVE_TCFLOW
+$(info autoconfig: using tcflow)
+endif
+endif
+
+ifndef $(HAVE_TCFLUSH)
+HAVE_TCFLUSH = $(shell $(MAKE) -f Makefile.config --no-print-directory $(HAVE_NOT) TEST_PROG=test-tcflush have_test_prog)
+ifeq ($(HAVE_TCFLUSH),1)
+	CONFIG_CFLAGS += -DHAVE_TCFLUSH
+$(info autoconfig: using tcflush)
+endif
+endif
+
+ifndef $(HAVE_TCGETATTR)
+HAVE_TCGETATTR = $(shell $(MAKE) -f Makefile.config --no-print-directory $(HAVE_NOT) TEST_PROG=test-tcgetattr have_test_prog)
+ifeq ($(HAVE_TCGETATTR),1)
+	CONFIG_CFLAGS += -DHAVE_TCGETATTR
+$(info autoconfig: using tcgetattr)
+endif
+endif
+
 ifndef $(HAVE_TEE)
 HAVE_TEE = $(shell $(MAKE) -f Makefile.config --no-print-directory $(HAVE_NOT) TEST_PROG=test-tee have_test_prog)
 ifeq ($(HAVE_TEE),1)

test/test-tcdrain.c
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2013-2020 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This code is a complete clean re-write of the stress tool by
+ * Colin Ian King <colin.king@canonical.com> and attempts to be
+ * backwardly compatible with the stress tool by Amos Waterland
+ * <apw@rossby.metr.ou.edu> but has more stress tests and more
+ * functionality.
+ *
+ */
+#include <termios.h>
+#include <unistd.h>
+
+int main(void)
+{
+	return tcdrain(0);
+}

test/test-tcflow.c
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2013-2020 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This code is a complete clean re-write of the stress tool by
+ * Colin Ian King <colin.king@canonical.com> and attempts to be
+ * backwardly compatible with the stress tool by Amos Waterland
+ * <apw@rossby.metr.ou.edu> but has more stress tests and more
+ * functionality.
+ *
+ */
+#include <termios.h>
+#include <unistd.h>
+
+int main(void)
+{
+	return tcflow(0, 0);
+}

test/test-tcflush.c
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2013-2020 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This code is a complete clean re-write of the stress tool by
+ * Colin Ian King <colin.king@canonical.com> and attempts to be
+ * backwardly compatible with the stress tool by Amos Waterland
+ * <apw@rossby.metr.ou.edu> but has more stress tests and more
+ * functionality.
+ *
+ */
+#include <termios.h>
+#include <unistd.h>
+
+int main(void)
+{
+	return tcflush(0, 0);
+}

test/test-tcgetattr.c
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2013-2020 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This code is a complete clean re-write of the stress tool by
+ * Colin Ian King <colin.king@canonical.com> and attempts to be
+ * backwardly compatible with the stress tool by Amos Waterland
+ * <apw@rossby.metr.ou.edu> but has more stress tests and more
+ * functionality.
+ *
+ */
+#include <termios.h>
+#include <unistd.h>
+
+int main(void)
+{
+	struct termios t;
+
+	return tcgetattr(0, &t);
+}

commit 4e133ee96ba28221ec1965e43bf1b28da868cf2b

stress-pty: fix incorrect placing of #endif's
The #endif's are in the wrong place. Fix them.

Fixes: 4bc4c3369615 ("stress-pty: exercise tcdrain, tcflush, tcflow and tcgetattr")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-pty.c
@@ -188,8 +188,8 @@ static int stress_pty(const stress_args_t *args)
 					pr_fail("%s: tcflow TCOON on slave pty failed, errno=%d (%s)\n",
 						args->name, errno, strerror(errno));
 				}
-#endif
 			}
+#endif
 #if defined(TCIOFF) && \
     defined(TCION)
 			{
@@ -201,8 +201,8 @@ static int stress_pty(const stress_args_t *args)
 					pr_fail("%s: tcflow TCION on slave pty failed, errno=%d (%s)\n",
 						args->name, errno, strerror(errno));
 				}
-#endif
 			}
+#endif
 #endif
 #if defined(TCGETS)
 			{