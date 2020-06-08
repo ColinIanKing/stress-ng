394e78e1875e67dce9efaa70a3625d2d2d73442a
Colin Ian King <colin.king@canonical.com>
2020-06-08 11:53:18 +0100

    stress-sysinval: only build tests syscall.h syscall() is available
    Don't build if syscall is not available
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-sysinval.c
+++ stress-sysinval.c
@@ -30,7 +30,8 @@ static const stress_help_t help[] = {
 	{ NULL,	NULL,		    NULL }
 };
 
-#if defined(__linux__)
+#if defined(HAVE_SYSCALL_H) &&	\
+    defined(__linux__)
 
 #define ARG_MASK(x, mask)	(((x) & (mask)) == (mask))
 
