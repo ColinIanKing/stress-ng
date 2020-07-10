ColinIanKing/stress-ng

commit 2181a693db4e5b74c3c80a17a2edf5a3c89361f4

stress-ng: add --max-fd soft limit on file descriptors
This sets the number of files limit allowed to be used by stress-ng.
One can use a setting 10..max rlimit fd setting, or a percentage.

Signed-off-by: Colin Ian King <colin.king@canonical.com>

core-limit.c
@@ -81,14 +81,27 @@ static const int limits[] = {
 void stress_set_max_limits(void)
 {
 	size_t i;
+	struct rlimit rlim;
 
 	for (i = 0; i < SIZEOF_ARRAY(limits); i++) {
-		struct rlimit rlim;
-
 		if (getrlimit(limits[i], &rlim) < 0)
 			continue;
 		rlim.rlim_cur = rlim.rlim_max;
 		(void)setrlimit(limits[i], &rlim);
 	}
+
+#if defined(RLIMIT_NOFILE)
+	{
+		uint64_t max_fd;
+
+		(void)stress_get_setting("max-fd", &max_fd);
+		if (max_fd != 0) {
+			rlim.rlim_cur = (rlim_t)max_fd + 1;
+			rlim.rlim_max = (rlim_t)max_fd + 1;
+			(void)setrlimit(RLIMIT_NOFILE, &rlim);
+		}
+	}
+#endif
+
 }
 

stress-ng.1
@@ -2,7 +2,7 @@
 .\" First parameter, NAME, should be all caps
 .\" Second parameter, SECTION, should be 1-8, maybe w/ subsection
 .\" other parameters are allowed: see man(7), man(1)
-.TH STRESS-NG 1 "Jun 17, 2020"
+.TH STRESS-NG 1 "Jun 22, 2020"
 .\" Please adjust this date whenever revising the manpage.
 .\"
 .\" Some roff macros, for reference:
@@ -176,6 +176,15 @@ write messages to the specified log file.
 overrides the default stressor settings and instead sets these to the maximum
 settings allowed.  These defaults can always be overridden by the per stressor
 settings options if required.
+.TP
+.B \-\-max\-fd N
+set the maximum limit on file descriptors (value or a % of system allowed
+maximum).  By default, stress-ng can use all the available file descriptors;
+this option sets the limit in the range from 10 up to the maximum limit of
+RLIMIT_NOFILE.  One can use a % setting too, e.g. 50% is half the maximum
+allowed file descriptors.  Note that stress-ng will use about 5 of the
+available file descriptors so take this into consideration when using this
+setting.
 .TP
 .B \-\-metrics
 output number of bogo operations in total performed by the stress processes.

stress-ng.c
@@ -484,6 +484,7 @@ static const struct option long_options[] = {
 	{ "matrix-3d-size",1,	0,	OPT_matrix_3d_size },
 	{ "matrix-3d-zyx",0,	0,	OPT_matrix_3d_zyx },
 	{ "maximize",	0,	0,	OPT_maximize },
+	{ "max-fd",	1,	0,	OPT_max_fd },
 	{ "mcontend",	1,	0,	OPT_mcontend },
 	{ "mcontend-ops",1,	0,	OPT_mcontend_ops },
 	{ "membarrier",	1,	0,	OPT_membarrier },
@@ -926,6 +927,7 @@ static const stress_help_t help_generic[] = {
 	{ NULL,		"log-brief",		"less verbose log messages" },
 	{ NULL,		"log-file filename",	"log messages to a log file" },
 	{ NULL,		"maximize",		"enable maximum stress options" },
+	{ NULL,		"max-fd",		"set maximum file descriptior limit" },
 	{ "M",		"metrics",		"print pseudo metrics of activity" },
 	{ NULL,		"metrics-brief",	"enable metrics and only show non-zero results" },
 	{ NULL,		"minimize",		"enable minimal stress options" },
@@ -2547,6 +2549,7 @@ int stress_parse_opts(int argc, char **argv, const bool jobmode)
 		int64_t i64;
 		int32_t i32;
 		uint32_t u32;
+		uint64_t u64, max_fds;
 		int16_t i16;
 		int c, option_index, ret;
 		size_t i;
@@ -2661,6 +2664,13 @@ next_opt:
 			break;
 		case OPT_log_file:
 			stress_set_setting_global("log-file", TYPE_ID_STR, (void *)optarg);
+			break;
+		case OPT_max_fd:
+			max_fds = (uint64_t)stress_get_file_limit();
+			u64 = stress_get_uint64_percent(optarg, 1, max_fds,
+				"Cannot determine maximum file descriptor limit");
+			stress_check_range(optarg, u64, 8, max_fds);
+			stress_set_setting_global("max-fd", TYPE_ID_UINT64, &u64);
 			break;
 		case OPT_no_madvise:
 			g_opt_flags &= ~OPT_FLAGS_MMAP_MADVISE;

stress-ng.h
@@ -2618,6 +2618,7 @@ typedef enum {
 	OPT_matrix_3d_zyx,
 
 	OPT_maximize,
+	OPT_max_fd,
 
 	OPT_mcontend,
 	OPT_mcontend_ops,

commit c444a13958ee00fa54d3b4953d47533c7ae31def

core-limit: ensire max_fd is initialized
Currently max_fd is just a garbage value on the stack because it is
not initialized.  Fix this by setting it to zero.

Fixes: 2181a693db4e ("stress-ng: add --max-fd soft limit on file descriptors")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

core-limit.c
@@ -92,7 +92,7 @@ void stress_set_max_limits(void)
 
 #if defined(RLIMIT_NOFILE)
 	{
-		uint64_t max_fd;
+		uint64_t max_fd = 0;
 
 		(void)stress_get_setting("max-fd", &max_fd);
 		if (max_fd != 0) {