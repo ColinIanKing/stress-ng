ColinIanKing/stress-ng

commit 4199a4df9b672fb5e21a81c7461c05a3a6f685fd

stress-pipeherd: fix where empty line is located
The empty line after pid is declared is in the wrong place, fix this.

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-pipeherd.c
@@ -115,9 +115,9 @@ static int stress_pipeherd(const stress_args_t *args)
 #endif
 	for (i = 0; i < PIPE_HERD_MAX; i++) {
 		pid_t pid;
-		pid = fork();
 
-		if (pid == 0) {
+		pid = fork();
+		if (pid == 1) {
 			(void)setpgid(0, g_pgrp);
 			stress_parent_died_alarm();
 			(void)sched_settings_apply(true);

commit 608acc2e6f14fb3e8d0da77af65f2d747602887f

stress-pipeherd: fix typo on child pid comparison
Fixes: 4199a4df9b67 ("stress-pipeherd: fix where empty line is located")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-pipeherd.c
@@ -117,7 +117,7 @@ static int stress_pipeherd(const stress_args_t *args)
 		pid_t pid;
 
 		pid = fork();
-		if (pid == 1) {
+		if (pid == 0) {
 			(void)setpgid(0, g_pgrp);
 			stress_parent_died_alarm();
 			(void)sched_settings_apply(true);