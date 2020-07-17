9985cd2be64eab274654aeb625ca850997a4fe29
Colin Ian King <colin.king@canonical.com>
2020-07-17 19:34:35 +0100

    stress-nanosleep: remove unused opt_set_funcs
    There is a redundant use of undefined opt_set_funcs. Remove it.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-nanosleep.c
+++ stress-nanosleep.c
@@ -160,7 +160,6 @@ stressor_info_t stress_nanosleep_info = {
 stressor_info_t stress_nanosleep_info = {
 	.stressor = stress_not_implemented,
 	.class = CLASS_INTERRUPT | CLASS_SCHEDULER | CLASS_OS,
-	.opt_set_funcs = opt_set_funcs,
 	.help = help
 };
 #endif
