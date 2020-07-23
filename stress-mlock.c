ColinIanKing/stress-ng

commit ec6b610efda54f45ef2a77ba92f12b4180146077

Add mlock stressor
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-mlock.c
@@ -1,0 +1,140 @@
+/*
+ * Copyright (C) 2013-2015 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This code is a complete clean re-write of the stress tool by
+ * Colin Ian King <colin.king@canonical.com> and attempts to be
+ * backwardly compatible with the stress tool by Amos Waterland
+ * <apw@rossby.metr.ou.edu> but has more stress tests and more
+ * functionality.
+ *
+ */
+#define _GNU_SOURCE
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#include "stress-ng.h"
+
+#define MLOCK_MAX	(256*1024)
+
+/*
+ *  stress_mlock()
+ *	stress mlock with pages being locked/unlocked
+ */
+int stress_mlock(
+	uint64_t *const counter,
+	const uint32_t instance,
+	const uint64_t max_ops,
+	const char *name)
+{
+	const size_t page_size = stress_get_pagesize();
+	pid_t pid;
+	size_t max = sysconf(_SC_MAPPED_FILES);
+	max = max > MLOCK_MAX ? MLOCK_MAX : max;
+
+	(void)instance;
+
+again:
+	pid = fork();
+	if (pid < 0) {
+		if (opt_do_run && (errno == EAGAIN))
+			goto again;
+		pr_err(stderr, "%s: fork failed: errno=%d: (%s)\n",
+			name, errno, strerror(errno));
+	} else if (pid > 0) {
+		int status, ret;
+		/* Parent, wait for child */
+		ret = waitpid(pid, &status, 0);
+		if (ret < 0) {
+			if (errno != EINTR)
+				pr_dbg(stderr, "%s: waitpid(): errno=%d (%s)\n",
+					name, errno, strerror(errno));
+			(void)kill(pid, SIGTERM);
+			(void)kill(pid, SIGKILL);
+			(void)waitpid(pid, &status, 0);
+		} else if (WIFSIGNALED(status)) {
+			pr_dbg(stderr, "%s: child died: %d (instance %d)\n",
+				name, WTERMSIG(status), instance);
+			/* If we got killed by OOM killer, re-start */
+			if (WTERMSIG(status) == SIGKILL) {
+				pr_dbg(stderr, "%s: assuming killed by OOM killer, "
+					"restarting again (instance %d)\n",
+					name, instance);
+				goto again;
+			}
+		}
+	} else if (pid == 0) {
+		uint8_t *mappings[max];
+		size_t i, n;
+
+		/* Make sure this is killable by OOM killer */
+		set_oom_adjustment(name, true);
+
+		do {
+			for (n = 0; opt_do_run && (n < max); n++) {
+				int ret;
+				if (!opt_do_run || (max_ops && *counter >= max_ops))
+					break;
+
+				mappings[n] = mmap(NULL, page_size * 3,
+					PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+				if (mappings[n] == MAP_FAILED)
+					break;
+				ret = mlock(mappings[n] + page_size, page_size);
+				if (ret < 0) {
+					if (errno == EAGAIN)
+						continue;
+					if (errno == ENOMEM)
+						break;
+					pr_err(stderr, "%s: mlock failed: errno=%d (%s)\n",
+						name, errno, strerror(errno));
+					break;
+				} else {
+					/*
+					 * Mappings are always page aligned so
+					 * we can use the bottom bit to 
+					 * indicate if the page has been 
+					 * mlocked or not
+				 	 */
+					mappings[n] = (uint8_t *)
+						((ptrdiff_t)mappings[n] | 1);
+					(*counter)++;
+				}
+			}
+
+			for (i = 0; i < n;  i++) {
+				ptrdiff_t addr = (ptrdiff_t)mappings[i];
+				ptrdiff_t mlocked = addr & 1;
+
+				addr ^= mlocked;
+				if (mlocked)
+					(void)munlock((void *)addr + page_size, page_size);
+				munmap((void *)addr, page_size * 3);
+			}
+		} while (opt_do_run && (!max_ops || *counter < max_ops));
+	}
+
+	return EXIT_SUCCESS;
+}

Makefile
@@ -65,6 +65,7 @@ SRC =   stress-affinity.c \
 	stress-memcpy.c \
 	stress-memfd.c \
 	stress-mincore.c \
+	stress-mlock.c \
 	stress-mmap.c \
 	stress-mmapmany.c \
 	stress-mremap.c \

stress-ng.1
@@ -803,6 +803,12 @@ instead of walking through pages sequentially, select pages at random. The chose
 .B \-\-minimize
 overrides the default stressor settings and instead sets these to the minimum settings allowed.  These defaults can always be overridden by the per stressor settings options if required.
 .TP
+.B \-\-mlock N
+start N stressors that lock and unlock memory mapped pages using mlock(2) and munlock(2). This is achieved by the mapping of three contiguous pages and then locking the second page, hence ensuring non-contiguous pages are locked . This is then repeated until the maximum allowed mlocks or a maximum of 262144 mappings are made.
+.TP
+.B \-\-mlock\-ops N
+stop after N mlock bogo operations.
+.TP
 .B \-\-mmap N
 start N processes continuously calling mmap(2)/munmap(2).  The initial mapping is a large chunk (size specified by \-\-mmap\-bytes) followed by pseudo-random 4K unmappings, then pseudo-random 4K mappings, and then linear 4K unmappings. Note that this can cause systems to trip the kernel OOM killer on Linux systems if not enough physical memory and swap is not available.  The MAP_POPULATE option is used to populate pages into memory on systems that support this.  By default, anonymous mappings are used, however, the \-\-mmap\-file and \-\-mmap\-async options allow one to perform file based mappings if desired.
 .TP

stress-ng.c
@@ -204,6 +204,7 @@ static const stress_t stressors[] = {
 #if defined(STRESS_MINCORE)
 	STRESSOR(mincore, MINCORE, CLASS_OS | CLASS_MEMORY),
 #endif
+	STRESSOR(mlock, MLOCK, CLASS_VM | CLASS_OS),
 	STRESSOR(mmap, MMAP, CLASS_VM | CLASS_OS),
 	STRESSOR(mmapmany, MMAPMANY, CLASS_VM | CLASS_OS),
 #if defined(STRESS_MREMAP)
@@ -472,6 +473,10 @@ static const struct option long_options[] = {
 	{ "mincore-random",0,	0,	OPT_MINCORE_RAND },
 #endif
 	{ "minimize",	0,	0,	OPT_MINIMIZE },
+#if defined(STRESS_MLOCK)
+	{ "mlock",	1,	0,	OPT_MLOCK },
+	{ "mlock-ops",	1,	0,	OPT_MLOCK_OPS },
+#endif
 	{ "mmap",	1,	0,	OPT_MMAP },
 	{ "mmap-ops",	1,	0,	OPT_MMAP_OPS },
 	{ "mmap-async",	0,	0,	OPT_MMAP_ASYNC },
@@ -827,6 +832,10 @@ static const help_t help[] = {
 	{ NULL,		"mincore-random",	"randomly select pages rather than linear scan" },
 #endif
 	{ NULL,		"minimize",		"enable minimal stress options" },
+#if defined(STRESS_MLOCK)
+	{ NULL,		"mlock N",		"start N workers exercising mlock/munlock" },
+	{ NULL,		"mlock-ops N",		"stop when N mlock bogo operations completed" },
+#endif
 	{ NULL,		"mmap N",		"start N workers stressing mmap and munmap" },
 	{ NULL,		"mmap-ops N",		"stop when N mmap bogo operations completed" },
 	{ NULL,		"mmap-async",		"using asynchronous msyncs for file based mmap" },

stress-ng.h
@@ -558,6 +558,10 @@ typedef enum {
 	__STRESS_MEMFD,
 #define STRESS_MEMFD __STRESS_MEMFD
 #endif
+#if defined(_POSIX_MEMLOCK_RANGE)
+	__STRESS_MLOCK,
+#define STRESS_MLOCK __STRESS_MLOCK
+#endif
 	STRESS_MMAP,
 	STRESS_MMAPMANY,
 #if defined(__linux__) && NEED_GLIBC(2,4,0)
@@ -894,6 +898,11 @@ typedef enum {
 #endif
 
 	OPT_MINIMIZE,
+
+#if defined(STRESS_MLOCK)
+	OPT_MLOCK,
+	OPT_MLOCK_OPS,
+#endif
 
 	OPT_MMAP,
 	OPT_MMAP_OPS,
@@ -1386,6 +1395,7 @@ STRESS(stress_matrix);
 STRESS(stress_memcpy);
 STRESS(stress_memfd);
 STRESS(stress_mincore);
+STRESS(stress_mlock);
 STRESS(stress_mmap);
 STRESS(stress_mmapmany);
 STRESS(stress_mremap);

commit 250d10e15b087d8d3b329cfa2289e68b0f092121

stress-mlock: fix max limit on mlocked region size
Currently the mlock limit is using the wrong kernel defined limit,
use _SC_MEMLOCK instead of _SC_MAPPED_FILES.

Fixes: ec6b610efda5 ("Add mlock stressor")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-mlock.c
@@ -84,7 +84,7 @@ static int stress_mlock_child(const stress_args_t *args, void *context)
 	size_t i, n;
 	uint8_t **mappings;
 	const size_t page_size = args->page_size;
-	size_t max = sysconf(_SC_MAPPED_FILES);
+	size_t max = sysconf(_SC_MEMLOCK );
 	max = max > MLOCK_MAX ? MLOCK_MAX : max;
 
 	(void)context;