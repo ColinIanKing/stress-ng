af6f504277f2664fc27c0106de3e4ca7d6009200
Colin Ian King <colin.king@canonical.com>
2020-08-19 14:14:30 +0100

    stress-dev: add check for v4l2_std_id
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-dev.c
+++ stress-dev.c
@@ -271,7 +271,8 @@ static void stress_dev_video_linux(
 		(void)ret;
 	}
 #endif
-#if defined(VIDIOC_QUERYSTD)
+#if defined(VIDIOC_QUERYSTD) &&	\
+    defined(HAVE_V4L2_STD_ID)
 	{
 		v4l2_std_id a;
 		int ret;
