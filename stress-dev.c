ColinIanKing/stress-ng

commit e7dc13e87d85bee391747d0316c01851cbe1c1c0

stress-dev: add build autodetection for struct termios
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-dev.c
@@ -317,7 +317,8 @@ static void stress_dev_video_linux(
 #endif
 
 #if defined(HAVE_TERMIOS_H) &&	\
-    defined(TCGETS)
+    defined(TCGETS) &&		\
+    defined(HAVE_TERMIOS)
 static void stress_dev_tty(
 	const char *name,
 	const int fd,

Makefile.config
@@ -2861,6 +2861,14 @@ $(info autoconfig: using struct shminfo)
 endif
 endif
 
+ifndef $(HAVE_TERMIOS)
+HAVE_TERMIOS = $(shell $(MAKE) $(MAKE_OPTS) TEST_PROG=test-termios have_test_prog)
+ifeq ($(HAVE_TERMIOS),1)
+	CONFIG_CFLAGS += -DHAVE_TERMIOS
+$(info autoconfig: using struct termios)
+endif
+endif
+
 ifndef $(HAVE_USER_DESC)
 HAVE_USER_DESC = $(shell $(MAKE) $(MAKE_OPTS) TEST_PROG=test-user-desc have_test_prog)
 ifeq ($(HAVE_USER_DESC),1)

test/test-termios.c
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2013-2020 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This code is a complete clean re-write of the stress tool by
+ * Colin Ian King <colin.king@canonical.com> and attempts to be
+ * backwardly compatible with the stress tool by Amos Waterland
+ * <apw@rossby.metr.ou.edu> but has more stress tests and more
+ * functionality.
+ *
+ */
+#include <sys/termios.h>
+
+int main(void)
+{
+	struct termios t;
+
+	(void)t;
+
+	return sizeof(t);
+}

commit 1e70bd3a400416367de745d07ab507245deb2b3e

Fix error if only HAVE_TERMIOS is not set
Signed-off-by: André Wild <wild1@linux.ibm.com>
Fixes: e7dc13e87d85 ("stress-dev: add build autodetection for struct termios")

stress-dev.c
@@ -2328,7 +2328,8 @@ static inline void stress_dev_rw(
 		double t_start;
 		bool timeout = false;
 #if defined(HAVE_TERMIOS_H) &&	\
-    defined(TCGETS)
+    defined(TCGETS) && \
+    defined(HAVE_TERMIOS)
 		struct termios tios;
 #endif
 		ret = shim_pthread_spin_lock(&lock);
@@ -2373,7 +2374,8 @@ static inline void stress_dev_rw(
 #endif
 		}
 #if defined(HAVE_TERMIOS_H) &&	\
-    defined(TCGETS)
+    defined(TCGETS) && \
+    defined(HAVE_TERMIOS)
 		if (S_ISCHR(buf.st_mode) &&
 		    strncmp("/dev/vsock", path, 9) &&
 		    strncmp("/dev/dri", path, 7) &&