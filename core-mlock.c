6024e3773ef1f3d03161460880f10ab03f0732eb
Colin Ian King <colin.king@canonical.com>
2020-10-26 17:07:58 +0000

    core-mlock: use shim version of mlock
    Use the shim wrapper for mlock to avoid const warnings on
    OpenIndiana
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- core-mlock.c
+++ core-mlock.c
@@ -40,7 +40,7 @@ int stress_mlock_region(const void *addr_start, const void *addr_end)
 		~(page_size - 1));
 	const size_t len = (intptr_t)m_addr_end - (intptr_t)m_addr_start;
 
-	return mlock((const void *)m_addr_start, len);
+	return shim_mlock((const void *)m_addr_start, len);
 #else
 	(void)addr_start;
 	(void)addr_end;
