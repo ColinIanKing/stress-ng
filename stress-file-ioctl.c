cb0f793e3a7e4733563c66979ce4bc8d4338ba05
Colin Ian King <colin.king@canonical.com>
2020-10-27 14:30:10 +0000

    stress-file-ioctl: enable FIFREEZE and FITHAW file ioctls
    These have been disabled for a while, enable them and see if there
    any issues.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-file-ioctl.c
+++ stress-file-ioctl.c
@@ -199,7 +199,6 @@ static int stress_file_ioctl(const stress_args_t *args)
 #endif
 
 /* Disable this at the moment, it is fragile */
-#if 0
 #if defined(FIFREEZE) && defined(FITHAW)
 		{
 			ret = ioctl(fd, FIFREEZE);
@@ -209,7 +208,6 @@ static int stress_file_ioctl(const stress_args_t *args)
 
 			exercised++;
 		}
-#endif
 #endif
 
 #if defined(FIGETBSZ)
