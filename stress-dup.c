3e4ae5a8bc03454a5095ab47c1005c688fa45462
Colin Ian King <colin.king@canonical.com>
2020-11-09 18:29:20 +0000

    stress-dup: add dup2 race stressing for Linux
    dup2 has a race condition if a child process shares the same
    file descriptor table and one process is blocked on a fifo open
    that has a partially open fd and the child dup2's and picks up
    the same fd because the fifo open has not completed. Add a
    test case for this to force out this dup2 race to exercise the
    kernel race detection and EBUSY return path on dup2. Linux only.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-dup.c
+++ stress-dup.c
@@ -30,6 +30,177 @@ static const stress_help_t help[] = {
 	{ NULL,	NULL,		NULL }
 };
 
+#if defined(__linux__) &&	\
+    defined(HAVE_CLONE) &&	\
+    defined(CLONE_VM) &&	\
+    defined(CLONE_FILES)
+#define STRESS_DUP2_RACE	(1)
+#endif
+
+#if defined(STRESS_DUP2_RACE)
+
+#define DUP_STACK_SIZE	(16364 / sizeof(uint64_t))
+
+/*
+ *  dup2 race context
+ */
+typedef struct {
+	int	fd;			/* temp file descriptor */
+	int	fd_pipe;		/* fifo file descriptor */
+	uint64_t race_count;		/* count of dup2 races */
+	uint64_t try_count;		/* dup2 race attempts */
+	char 	fifoname[PATH_MAX];	/* name of fifo file */
+	pid_t	pid_clone;		/* pid of clone process */
+	uint64_t stack[DUP_STACK_SIZE];	/* clone stack */
+} info_t;
+
+static int stress_dup2_race_clone(void *arg)
+{
+	int fd, fd_dup;
+	info_t *info = (info_t *)arg;
+
+	/* Should never be null, but weird things may happen  */
+	if (!info)
+		_exit(1);
+
+	if ((fd = open("/dev/null", O_RDONLY)) == -1)
+		_exit(1);
+
+	/*
+	 *  Unexpected, the next fd is the same as an earlier
+	 *  previous fd, so don't dup2 on this
+	 */
+	if (fd == info->fd)
+		_exit(0);
+
+	/*
+	 *  Race with the fd on the fifo open. The fifo
+	 *  is in a partially opened state and this dup2
+	 *  call will use the same fd and if there is a
+	 *  fd race collisiom we get EBUSY on linux.
+	 */
+	fd_dup = dup2(fd, info->fd);
+	if ((fd_dup < 0) && (errno == EBUSY))
+		info->race_count++;
+	(void)close(fd);
+
+	_exit(0);
+}
+
+static void stress_dup2_sigalrm(int sig)
+{
+	(void)sig;
+}
+
+static int static_dup2_child(info_t *info)
+{
+	struct sigaction action;
+	struct itimerval timer;
+	pid_t child_tid = -1, parent_tid = -1;
+	const ssize_t stack_offset =
+                stress_get_stack_direction() *
+                (sizeof(info->stack)- 64);
+	char *stack_top = ((char *)info->stack) + stack_offset;
+
+	info->fd_pipe = -1;
+	info->pid_clone = -1;
+
+	(void)memset(&action, 0, sizeof(action));
+	action.sa_flags = 0;
+	action.sa_handler = stress_dup2_sigalrm;
+	if (sigaction(SIGALRM, &action, NULL) < 0)
+		_exit(1);
+
+	/*
+	 *  Find next free fd, we're going to race on this
+	 *  fd number with a process that shares the same fd table
+	 */
+	info->fd = open("/dev/null", O_RDONLY);
+	if (info->fd < 0)
+		_exit(1);
+	(void)close(info->fd);
+
+	info->pid_clone = clone(stress_dup2_race_clone,
+		stress_align_stack(stack_top),
+		CLONE_VM | CLONE_FILES | SIGCHLD, info, &parent_tid,
+		NULL, &child_tid);
+	if (info->pid_clone < 0)
+		_exit(1);
+
+	/*
+	 *  Set a timer to unblock pipe read after 1ms
+	 */
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = 1000;
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_usec = 1000;
+	if (setitimer(ITIMER_REAL, &timer, NULL) < 0)
+		_exit(1);
+
+	info->try_count++;
+	/*
+	 *  Open will block until we get a sigalarm on Linux,
+	 *  can't guarantee this on other kernels. This will
+	 *  block and cause a race -EBUSY return with the
+	 *  clone dup2 open process
+	 */
+	info->fd_pipe = open(info->fifoname, O_RDONLY);
+
+	/*
+	 *  Cancel timer
+	 */
+	(void)memset(&timer, 0, sizeof(timer));
+	(void)setitimer(ITIMER_PROF, &timer, NULL);
+
+	/*
+	 *  Unlikely to be open, but close it to be a good citizen
+	 */
+	if (info->fd_pipe >= 0)
+		(void)close(info->fd_pipe);
+
+	/*
+	 *  Should always be true..
+	 */
+	if (info->pid_clone >= 0) {
+		int status;
+
+		kill(info->pid_clone, SIGKILL);
+		waitpid(info->pid_clone, &status, __WCLONE);
+	}
+
+	(void)close(info->fd);
+
+	return 0;
+}
+
+/*
+ *  Run the dup2 parent/clone in a new process context
+ *  to avoid any potential breaking of the parent fd
+ *  table (avoid any weird issues).
+ */
+static int stress_dup2_race(info_t *info)
+{
+	pid_t pid;
+	if (mkfifo(info->fifoname, S_IRUSR | S_IWUSR))
+		return -1;
+
+	pid = fork();
+	if (pid < 0) {
+		return -1;
+	} else if (pid == 0) {
+		static_dup2_child(info);
+		_exit(0);
+	} else {
+		int status;
+
+		(void)waitpid(pid, &status, 0);
+	}
+	(void)unlink(info->fifoname);
+
+	return 0;
+}
+#endif
+
 /*
  *  stress_dup()
  *	stress system by rapid dup/close calls
@@ -41,6 +212,20 @@ static int stress_dup(const stress_args_t *args)
 	size_t i;
 	bool do_dup3 = true;
 	const int bad_fd = stress_get_bad_fd();
+#if defined(STRESS_DUP2_RACE)
+	info_t *info;
+
+	info = mmap(NULL, sizeof(*info), PROT_READ | PROT_WRITE,
+		MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+	if (info != MAP_FAILED) {
+		if (stress_temp_dir_mk(args->name, args->pid, args->instance) < 0)
+			goto tidy_dir;
+
+		(void)stress_temp_filename_args(args, info->fifoname,
+			sizeof(info->fifoname), stress_mwc32());
+	}
+#endif
 
 	if (max_fd > SIZEOF_ARRAY(fds))
 		max_fd = SIZEOF_ARRAY(fds);
@@ -160,6 +345,12 @@ static int stress_dup(const stress_args_t *args)
 			if (!keep_stressing())
 				break;
 #endif
+
+#if defined(STRESS_DUP2_RACE)
+			if (info != MAP_FAILED)
+				stress_dup2_race(info);
+#endif
+
 			inc_counter(args);
 		}
 		for (i = 1; i < n; i++) {
@@ -171,6 +362,18 @@ static int stress_dup(const stress_args_t *args)
 		}
 	} while (keep_stressing());
 	(void)close(fds[0]);
+
+#if defined(STRESS_DUP2_RACE)
+tidy_dir:
+	if (info != MAP_FAILED) {
+		(void)stress_temp_dir_rm_args(args);
+		pr_dbg("%s: dup2: %" PRIu64 " races from %" PRIu64 " attempts (%.2f%%)\n",
+			args->name, info->race_count, info->try_count,
+			info->try_count > 0 ?
+				(float)info->race_count / (float)info->try_count * 100.0 : 0.0);
+		(void)munmap(info, sizeof(*info));
+	}
+#endif
 
 	return EXIT_SUCCESS;
 }
