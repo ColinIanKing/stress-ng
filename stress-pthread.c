5a37e4cff2f831aed66930f78d0f844f83916955
Colin Ian King <colin.king@canonical.com>
2020-12-03 00:19:57 +0000

    stress-pthread: remove duplicate declaration of ret to clean up build warning
    Clean up pedantic build warning by removing ret, it's a duplicate.
    
    stress-pthread.c: In function ‘stress_pthread_func’:
    stress-pthread.c:190:7: warning: declaration of ‘ret’ shadows a previous local [-Wshadow]
      190 |   int ret;
          |       ^~~
    stress-pthread.c:165:6: note: shadowed declaration is here
      165 |  int ret;
          |      ^~~
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-pthread.c
+++ stress-pthread.c
@@ -187,8 +187,6 @@ static void *stress_pthread_func(void *parg)
 		}
 	} else {
 #if defined(HAVE_SET_ROBUST_LIST) && defined(HAVE_LINUX_FUTEX_H)
-		int ret;
-
 		if (sys_set_robust_list(head, len) < 0) {
 			if (errno != ENOSYS) {
 				pr_fail("%s: set_robust_list failed, tid=%d, errno=%d (%s)\n",
