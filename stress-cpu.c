ColinIanKing/stress-ng

commit dd12105d3a0b1d3286bd06f1ac1fdc356aca83f2

stress-cpu: modify number of iterations to improve cpu instruction mix
Try to reduce compute swamping with specific CPU stressor methods to
get a more balanced mix of CPU stressor method mix.

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-cpu.c
@@ -33,8 +33,9 @@
 #define STRESS_CPU_DITHER_X	(1024)
 #define STRESS_CPU_DITHER_Y	(768)
 #define MATRIX_PROD_SIZE 	(128)
-#define CORRELATE_DATA_LEN	(16384)
+#define CORRELATE_DATA_LEN	(8192)
 #define CORRELATE_LEN		(CORRELATE_DATA_LEN / 16)
+#define SIEVE_SIZE              (104730)
 
 /*
  * Some awful math lib workarounds for functions that some
@@ -435,7 +436,7 @@ static void HOT OPTIMIZE3 TARGET_CLONES stress_cpu_nsqrt(const char *name)
 	const long double precision = 1.0e-12L;
 	const int max_iter = 56;
 
-	for (i = 0; i < 16384; i++) {
+	for (i = 16384; i < 16384; i++) {
 		long double n = (double)i;
 		long double lo = (n < 1.0L) ? n : 1.0L;
 		long double hi = (n < 1.0L) ? 1.0L : n;
@@ -1292,11 +1293,11 @@ static uint32_t HOT ackermann(const uint32_t m, const uint32_t n)
  */
 static void stress_cpu_ackermann(const char *name)
 {
-	uint32_t a = ackermann(3, 10);
+	uint32_t a = ackermann(3, 9);
 
 	if ((g_opt_flags & OPT_FLAGS_VERIFY) && (a != 0x1ffd))
 		pr_fail("%s: ackermann error detected, "
-			"ackermann(3,10) miscalculated\n", name);
+			"ackermann(3,9) miscalculated\n", name);
 }
 
 /*
@@ -1533,7 +1534,7 @@ static void HOT OPTIMIZE3 stress_cpu_sieve(const char *name)
 		if (STRESS_GETBIT(sieve, i))
 			j++;
 	}
-	if ((g_opt_flags & OPT_FLAGS_VERIFY) && (j != 664579))
+	if ((g_opt_flags & OPT_FLAGS_VERIFY) && (j != 10000))
 		pr_fail("%s: sieve error detected, number of "
 			"primes has been miscalculated\n", name);
 }
@@ -1543,7 +1544,7 @@ static void HOT OPTIMIZE3 stress_cpu_sieve(const char *name)
  *	return true if n is prime
  *	http://en.wikipedia.org/wiki/Primality_test
  */
-static inline HOT OPTIMIZE3 int is_prime(uint32_t n)
+static inline HOT OPTIMIZE3 ALWAYS_INLINE int is_prime(uint32_t n)
 {
 	register uint32_t i, max;
 
@@ -1566,13 +1567,13 @@ static void stress_cpu_prime(const char *name)
 {
 	uint32_t i, nprimes = 0;
 
-	for (i = 0; i < 1000000; i++) {
+	for (i = 0; i < SIEVE_SIZE; i++) {
 		nprimes += is_prime(i);
 	}
 
-	if ((g_opt_flags & OPT_FLAGS_VERIFY) && (nprimes != 78498))
+	if ((g_opt_flags & OPT_FLAGS_VERIFY) && (nprimes != 10000))
 		pr_fail("%s: prime error detected, number of primes "
-			"between 0 and 1000000 miscalculated\n", name);
+			"has been miscalculated\n", name);
 }
 
 /*
@@ -2231,7 +2232,7 @@ static void TARGET_CLONES stress_cpu_cpuid(const char *name)
 
 	(void)name;
 
-	for (i = 0; i < 10000; i++) {
+	for (i = 0; i < 1000; i++) {
 		uint32_t eax, ebx, ecx, edx;
 
 		/*  Highest Function Parameter and Manufacturer ID */
@@ -2390,13 +2391,13 @@ static uint32_t queens_try(
 
 /*
  *  stress_cpu_queens
- *	solve the queens problem for sizes 1..12
+ *	solve the queens problem for sizes 1..11
  */
 static void stress_cpu_queens(const char *name)
 {
 	uint32_t all, n;
 
-	for (all = 1, n = 1; n < 13; n++) {
+	for (all = 1, n = 1; n < 12; n++) {
 		uint32_t solutions = queens_try(0, 0, 0, all);
 		if ((g_opt_flags & OPT_FLAGS_VERIFY) &&
 		    (solutions != queens_solutions[n]))

stress-ng.1
@@ -785,7 +785,7 @@ all	T{
 iterate over all the below cpu stress methods
 T}
 ackermann	T{
-Ackermann function: compute A(3, 10), where:
+Ackermann function: compute A(3, 9), where:
  A(m, n) = n + 1 if m = 0;
  A(m - 1, 1) if m > 0 and n = 0;
  A(m - 1, A(m, n - 1)) if m > 0 and n > 0
@@ -814,7 +814,7 @@ compute the 1348 steps in the collatz sequence from starting number 989345275647
 Where f(n) = n / 2 (for even n) and f(n) = 3n + 1 (for odd n).
 T}
 correlate	T{
-perform a 16384 \(mu 1024 correlation of random doubles
+perform a 8192 \(mu 512 correlation of random doubles
 T}
 cpuid	T{
 fetch cpu specific information using the cpuid instruction (x86 only)
@@ -1026,15 +1026,15 @@ pjw	T{
 128 rounds of hash pjw function on 128 to 1 bytes of random strings
 T}
 prime	T{
-find all the primes in the range  1..1000000 using a slightly
-optimised brute force na\[:i]ve trial division search
+find the first 10000 prime numbers using a slightly optimised brute
+force na\[:i]ve trial division search
 T}
 psi	T{
 compute \(*q (the reciprocal Fibonacci constant) using the sum of the
 reciprocals of the Fibonacci numbers
 T}
 queens	T{
-compute all the solutions of the classic 8 queens problem for board sizes 1..12
+compute all the solutions of the classic 8 queens problem for board sizes 1..11
 T}
 rand	T{
 16384 iterations of rand(), where rand is the MWC pseudo
@@ -1057,7 +1057,7 @@ sdbm	T{
 1 bytes of random strings
 T}
 sieve	T{
-find the primes in the range 1..10000000 using the sieve of Eratosthenes
+find the first 10000 prime numbers using the sieve of Eratosthenes
 T}
 stats	T{
 calculate minimum, maximum, arithmetic mean, geometric mean, harmoninc mean

stress-ng.h
@@ -1682,8 +1682,6 @@ extern void pr_dbg_lock(bool *locked, const char *fmt, ...)  FORMAT(printf, 2, 3
 #define STRESS_SETBIT(a, i)	(a[i / STRESS_NBITS(a)] |= \
 				 (TYPEOF_CAST(a[0])1 << (i & (STRESS_NBITS(a)-1))))
 
-#define SIEVE_SIZE 		(10000000)
-
 /* MWC random number initial seed */
 #define STRESS_MWC_SEED_Z	(362436069UL)
 #define STRESS_MWC_SEED_W	(521288629UL)

commit de768498ba17ff69dbe6b96aa40d5356515513e8

stress-cpu: fix nsqrt method that was actually doing not much
The code wasn't actually doing any iterations due to a mistake from
an earlier commit to tweek the load mix. Fix this.

Fixes: dd12105d3a0b ("stress-cpu: modify number of iterations to improve cpu instruction mix")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-cpu.c
@@ -436,7 +436,7 @@ static void HOT OPTIMIZE3 TARGET_CLONES stress_cpu_nsqrt(const char *name)
 	const long double precision = 1.0e-12L;
 	const int max_iter = 56;
 
-	for (i = 16384; i < 16384; i++) {
+	for (i = 16300; i < 16384; i++) {
 		long double n = (double)i;
 		long double lo = (n < 1.0L) ? n : 1.0L;
 		long double hi = (n < 1.0L) ? 1.0L : n;