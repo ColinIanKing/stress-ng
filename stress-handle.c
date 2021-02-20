8a6b5e0ea5de031afc83f954eb619c3dd426d316
Colin Ian King <colin.king@canonical.com>
2021-02-20 00:17:53 +0000

    stress-handle: ensure mount_fd is closed on a realloc failure
    If realloc fails currently mount_fd is not closed and we eventually
    end up running out of fds.  Fix this by closing mount_fd.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-handle.c
+++ stress-handle.c
@@ -182,7 +182,8 @@ static int stress_handle_child(const stress_args_t *args, void *context)
 		/* Exercise with large invalid size, EINVAL */
 		fhp->handle_bytes = 4096;
 		tmp = realloc(fhp, sizeof(struct file_handle) + fhp->handle_bytes);
-		if (tmp == NULL) {
+		if (!tmp) {
+			(void)close(mount_fd);
 			free(fhp);
 			continue;
 		}
