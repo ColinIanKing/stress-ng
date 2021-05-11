fd96664ea7093c80359fd2e41db0168aa4586768
Colin Ian King <colin.king@canonical.com>
2021-05-11 19:35:07 +0100

    stress-sysinval: add quotactl_path system call
    Exercise the new Linux 5.13 quotactl_path system call
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-sysinval.c
+++ stress-sysinval.c
@@ -1146,6 +1146,9 @@ static const stress_syscall_arg_t stress_syscall_args[] = {
 #if DEFSYS(quotactl)
 	{ SYS(quotactl), 5, { ARG_INT, ARG_PTR, ARG_INT, ARG_PTR, 0, 0 } },
 #endif
+#if DEFSYS(quotactl_path)
+	{ SYS(quotactl_path), 4, { ARG_INT, ARG_PTR, ARG_INT, ARG_PTR, 0, 0 } },
+#endif
 #if DEFSYS(read)
 	{ SYS(read), 3, { ARG_FD, ARG_PTR, ARG_LEN, 0, 0, 0 } },
 	{ SYS(read), 3, { ARG_FD, ARG_PTR_WR, ARG_LEN, 0, 0, 0 } },
