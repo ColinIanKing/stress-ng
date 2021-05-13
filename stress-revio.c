50c2b229e6dbd73170b22135a87c96e1e898caf3
Colin Ian King <colin.king@canonical.com>
2021-05-13 17:27:34 +0100

    stress-revio: unlink file as soon as possible
    In the case where ftruncate fails the test file is not removed and
    the test directory cannot be removed.  Fix this by unlinking the
    opened test file as early as possible.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-revio.c
+++ stress-revio.c
@@ -388,13 +388,13 @@ static int stress_revio(const stress_args_t *args)
 				args->name, filename, errno, strerror(errno));
 			goto finish;
 		}
+		(void)unlink(filename);
 		if (ftruncate(fd, (off_t)revio_bytes) < 0) {
 			pr_fail("%s: ftruncate failed, errno=%d (%s)\n",
 				args->name, errno, strerror(errno));
 			(void)close(fd);
 			goto finish;
 		}
-		(void)unlink(filename);
 
 		if (stress_revio_advise(args, fd, fadvise_flags) < 0) {
 			(void)close(fd);
