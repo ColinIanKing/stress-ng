ColinIanKing/stress-ng

commit 81455f59e0a3c6826e7e8354ce24125d9b475ef2

stress-dev: add TIOCGPTPEER ioctl
This will fail. but add it as another ioctl to exercise

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-dev.c
@@ -459,6 +459,7 @@ static void stress_dev_tty(
 
 	if (!isatty(fd))
 		return;
+pr_inf("%s %s\n", name, devpath);
 
 	ret = tcgetattr(fd, &t);
 	(void)ret;
@@ -614,6 +615,14 @@ static void stress_dev_tty(
 		}
 #endif
 		(void)ret;
+	}
+#endif
+
+#if defined(TIOCGPTPEER)
+	{
+		ret = ioctl(fd, TIOCGPTPEER, O_RDWR);
+		if (ret > -1)
+			(void)close(ret);
 	}
 #endif
 

commit 3fbd7582534b327675838350b653b09a10635f6b

stress-dev: fix incorrect close, it should be removed
Fixes: 81455f59e0a3 ("stress-dev: add TIOCGPTPEER ioctl")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-dev.c
@@ -620,8 +620,7 @@ static void stress_dev_tty(
 #if defined(TIOCGPTPEER)
 	{
 		ret = ioctl(fd, TIOCGPTPEER, O_RDWR);
-		if (ret > -1)
-			(void)close(ret);
+		(void)ret;
 	}
 #endif
 