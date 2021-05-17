8be85c573e8f30c321a3052e773c18c5b0e55651
Colin Ian King <colin.king@canonical.com>
2021-05-17 20:57:10 +0100

    stress-version.h: Rename _VER_ to STRESS_VERSION_NUMBER
    Macros with _ prefixes should be avoided, rename _VER_ to
    STRESS_VERSION_NUMBER.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-version.h
+++ stress-version.h
@@ -25,13 +25,14 @@
 #if !defined(STRESS_VERSION_H)
 #define STRESS_VERSION_H
 
-#define _VER_(major, minor, patchlevel)			\
+#define STRESS_VERSION_NUMBER(major, minor, patchlevel)		\
 	((major * 10000) + (minor * 100) + patchlevel)
 
 #if defined(__GLIBC__) &&	\
     defined(__GLIBC_MINOR__)
 #define NEED_GLIBC(major, minor, patchlevel) 			\
-	_VER_(major, minor, patchlevel) <= _VER_(__GLIBC__, __GLIBC_MINOR__, 0)
+	STRESS_VERSION_NUMBER(major, minor, patchlevel) <=	\
+	STRESS_VERSION_NUMBER(__GLIBC__, __GLIBC_MINOR__, 0)
 #else
 #define NEED_GLIBC(major, minor, patchlevel) 	(0)
 #endif
@@ -40,10 +41,12 @@
     defined(__GNUC_MINOR__)
 #if defined(__GNUC_PATCHLEVEL__)
 #define NEED_GNUC(major, minor, patchlevel) 			\
-	_VER_(major, minor, patchlevel) <= _VER_(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__)
+	STRESS_VERSION_NUMBER(major, minor, patchlevel) <= 	\
+	STRESS_VERSION_NUMBER(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__)
 #else
 #define NEED_GNUC(major, minor, patchlevel) 			\
-	_VER_(major, minor, patchlevel) <= _VER_(__GNUC__, __GNUC_MINOR__, 0)
+	STRESS_VERSION_NUMBER(major, minor, patchlevel) <=	\
+	STRESS_VERSION_NUMBER(__GNUC__, __GNUC_MINOR__, 0)
 #endif
 #else
 #define NEED_GNUC(major, minor, patchlevel) 	(0)
@@ -53,8 +56,9 @@
     defined(__clang_major__) &&	\
     defined(__clang_minor__) && \
     defined(__clang_patchlevel__)
-#define NEED_CLANG(major, minor, patchlevel)	\
-	_VER_(major, minor, patchlevel) <= _VER_(__clang_major__, __clang_minor__, __clang_patchlevel__)
+#define NEED_CLANG(major, minor, patchlevel)			\
+	STRESS_VERSION_NUMBER(major, minor, patchlevel) <=	\
+	STRESS_VERSION_NUMBER(__clang_major__, __clang_minor__, __clang_patchlevel__)
 #else
 #define NEED_CLANG(major, minor, patchlevel)	(0)
 #endif
