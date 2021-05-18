21716a00cbad2e4f1a5387bbcc5658095df95ef6
Colin Ian King <colin.king@canonical.com>
2021-05-18 14:54:46 +0100

    stress-branch: simplify LIKELY macro expression to clean up warning
    Split the LIKELY macro away from the call to keep_stressing_flag to
    clean up clang pedantic warnings
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-branch.c
+++ stress-branch.c
@@ -58,9 +58,10 @@ static inline uint8_t jmp_stress_mwc8(void)
 {							\
 	register bool do_more;				\
 	register uint16_t index = jmp_stress_mwc8();	\
+	register bool flag = keep_stressing_flag();	\
 							\
 	inc_counter(args);				\
-	do_more = LIKELY((int)keep_stressing_flag()) &	\
+	do_more = LIKELY(flag) &			\
 		(((int)!args->max_ops) | 		\
 		 (get_counter(args) < args->max_ops));	\
 	index |= (do_more << 8);			\
