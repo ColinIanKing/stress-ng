c59e51102bd0c7dc104a5a145e1160f6b0383643
Colin Ian King <colin.king@canonical.com>
2021-05-19 16:57:07 +0100

    stress-crypt: remove _XOPEN_SOURCE define
    Not needed. Remove.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-crypt.c
+++ stress-crypt.c
@@ -24,9 +24,6 @@
  */
 #include "stress-ng.h"
 
-#undef _XOPEN_SOURCE
-#define _XOPEN_SOURCE	600
-
 static const stress_help_t help[] = {
 	{ NULL,	"crypt N",	"start N workers performing password encryption" },
 	{ NULL,	"crypt-ops N",	"stop after N bogo crypt operations" },
