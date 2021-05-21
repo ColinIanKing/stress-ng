63a1286de935f3dbf57c6c1dd8eed78c4ad60963
Colin Ian King <colin.king@canonical.com>
2021-05-22 00:21:43 +0100

    stress-sigfpe: remove SA_SIGINFO check on err_code
    The err_code is always required even if SA_SIGINFO is not available,
    remove the #ifdef check
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-sigfpe.c
+++ stress-sigfpe.c
@@ -127,9 +127,7 @@ static int stress_sigfpe(const stress_args_t *args)
 
 	typedef struct {
 		unsigned int exception;
-#if defined(SA_SIGINFO)
 		int	err_code;	/* cppcheck-suppress unusedStructMember */
-#endif
 	} stress_fpe_err_t;
 
 	/*
