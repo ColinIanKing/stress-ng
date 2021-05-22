0fcb4613a2fed3fae61f89b5fdb27c3640df16f1
Colin Ian King <colin.king@canonical.com>
2021-05-22 15:31:07 +0100

    test/test-mq-sysv.c: memset msg to clean up cppcheck warning
    clean up warning:
    test/test-mq-sysv.c:67:19: error: Uninitialized variable: msg [uninitvar]
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- test/test-mq-sysv.c
+++ test/test-mq-sysv.c
@@ -64,6 +64,7 @@ int main(int argc, char **argv)
 	if (msgq_id < 0)
 		return -1;
 
+	(void)memset(&msg, 0, sizeof(msg));
 	(void)strcpy(msg.msg, "TESTMSG");
 	msg.mtype = 1;
 	ret = msgsnd(msgq_id, &msg, sizeof(msg.msg), 0);
