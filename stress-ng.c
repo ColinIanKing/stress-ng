ColinIanKing/stress-ng

commit c973e4727a022caa5b855cebde09391078e8a4ce

Add per-stressor metrics to metrics output
Several stressors have metrics that can be added to the metrics output
for the --metrics option.  Allow a maximum of 10 per-stressor metrics
that are averaged at the end of the run.  Also add these to the yaml
output

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-ng.c
@@ -1796,7 +1796,7 @@ static stress_stressor_t *stress_get_nth_stressor(const uint32_t n)
 	for (i = 0; ss && (i < n); i++)
 		ss = ss->next;
 
-	return ss; 
+	return ss;
 }
 
 /*
@@ -1816,7 +1816,7 @@ static uint32_t stress_get_num_stressors(void)
 
 /*
  *  stress_stressors_free()
- *	free stressor info from stressor list 
+ *	free stressor info from stressor list
  */
 static void stress_stressors_free(void)
 {
@@ -1860,6 +1860,19 @@ static void NORETURN stress_child_atexit(void)
 	_exit(EXIT_BY_SYS_EXIT);
 }
 
+void stress_misc_stats_set(
+	stress_misc_stats_t *misc_stats,
+	const int idx,
+	const char *description,
+	const double value)
+{
+	if ((idx < 0) || (idx >= STRESS_MISC_STATS_MAX))
+		return;
+
+	(void)strlcpy(misc_stats[idx].description, description, sizeof(misc_stats[idx].description));
+	misc_stats[idx].value = value;
+}
+
 /*
  *  stress_run ()
  *	kick off and run stressors
@@ -1890,6 +1903,7 @@ static void MLOCKED_TEXT stress_run(
 		 */
 		for (j = 0; j < g_stressor_current->num_instances; j++, (*checksum)++) {
 			int rc = EXIT_SUCCESS;
+			size_t i;
 			pid_t pid;
 			char name[64];
 			int64_t backoff = DEFAULT_BACKOFF;
@@ -1907,6 +1921,9 @@ static void MLOCKED_TEXT stress_run(
 			stats->counter_ready = true;
 			stats->counter = 0;
 			stats->checksum = *checksum;
+			for (i = 0; i < SIZEOF_ARRAY(stats->misc_stats); i++) {
+				stress_misc_stats_set(stats->misc_stats, i, "", -1);
+			}
 again:
 			if (!keep_stressing_flag())
 				break;
@@ -1975,7 +1992,8 @@ again:
 						.pid = getpid(),
 						.ppid = getppid(),
 						.page_size = stress_get_pagesize(),
-						.mapped = &g_shared->mapped
+						.mapped = &g_shared->mapped,
+						.misc_stats = stats->misc_stats
 					};
 
 					(void)memset(*checksum, 0, sizeof(**checksum));
@@ -2168,6 +2186,30 @@ static void stress_metrics_check(bool *success)
 	}
 }
 
+static char *stess_description_yamlify(const char *description)
+{
+	static char yamlified[40];
+	char *dst, *end = yamlified + sizeof(yamlified);
+	const char *src;
+
+	for (dst = yamlified, src = description; *src; src++) {
+		register int ch = (int)*src;
+
+		if (isalpha(ch)) {
+			*(dst++) = (char)tolower(ch);
+		} else if (isdigit(ch)) {
+			*(dst++) = (char)ch;
+		} else if (ch == ' ') {
+			*(dst++) = '-';
+		}
+		if (dst >= end - 1)
+			break;
+	}
+	*dst = '\0';
+
+	return yamlified;
+}
+
 /*
  *  stress_metrics_dump()
  *	output metrics
@@ -2199,9 +2241,11 @@ static void stress_metrics_dump(
 		uint64_t c_total = 0, u_total = 0, s_total = 0;
 		double   r_total = 0.0;
 		int32_t  j;
+		size_t i;
 		const char *munged = stress_munge_underscore(ss->stressor->name);
 		double u_time, s_time, t_time, bogo_rate_r_time, bogo_rate, cpu_usage;
 		bool run_ok = false;
+		bool lock = false;
 
 		for (j = 0; j < ss->started_instances; j++) {
 			const stress_stats_t *const stats = ss->stats[j];
@@ -2225,7 +2269,7 @@ static void stress_metrics_dump(
 		u_time = (ticks_per_sec > 0) ? (double)u_total / (double)ticks_per_sec : 0.0;
 		s_time = (ticks_per_sec > 0) ? (double)s_total / (double)ticks_per_sec : 0.0;
 		t_time = u_time + s_time;
-		
+
 		/* Total usr + sys time of all procs */
 		bogo_rate_r_time = (r_total > 0.0) ? (double)c_total / r_total : 0.0;
 		{
@@ -2236,6 +2280,7 @@ static void stress_metrics_dump(
 		cpu_usage = (r_total > 0) ? 100.0 * t_time / r_total : 0.0;
 		cpu_usage = ss->started_instances ? cpu_usage / ss->started_instances : 0.0;
 
+		pr_lock(&lock);
 		if (g_opt_flags & OPT_FLAGS_METRICS_BRIEF) {
 			pr_inf("%-13s %9" PRIu64 " %9.2f %9.2f %9.2f %12.2f %14.2f\n",
 				munged,		/* stress test name */
@@ -2257,6 +2302,24 @@ static void stress_metrics_dump(
 				bogo_rate,	/* bogo ops per second */
 				cpu_usage);	/* % cpu usage */
 		}
+		for (i = 0; i < SIZEOF_ARRAY(ss->stats[j]->misc_stats); i++) {
+			double total = 0.0;
+			const char *description = ss->stats[0]->misc_stats[i].description;
+
+			if (*description) {
+				double metric;
+
+				for (j = 0; j < ss->started_instances; j++) {
+					const stress_stats_t *const stats = ss->stats[j];
+
+					total += stats->misc_stats[i].value;
+				}
+				metric = ss->started_instances ? total / ss->started_instances : 0.0;
+				pr_inf("%-13s %9.2f %s (average per stressor)\n",
+					munged, metric, description);
+			};
+		}
+		pr_unlock(&lock);
 
 		pr_yaml(yaml, "    - stressor: %s\n", munged);
 		pr_yaml(yaml, "      bogo-ops: %" PRIu64 "\n", c_total);
@@ -2266,6 +2329,24 @@ static void stress_metrics_dump(
 		pr_yaml(yaml, "      user-time: %f\n", u_time);
 		pr_yaml(yaml, "      system-time: %f\n", s_time);
 		pr_yaml(yaml, "      cpu-usage-per-instance: %f\n", cpu_usage);
+
+		for (i = 0; i < SIZEOF_ARRAY(ss->stats[j]->misc_stats); i++) {
+			double total = 0.0;
+			const char *description = ss->stats[0]->misc_stats[i].description;
+
+			if (*description) {
+				double metric;
+
+				for (j = 0; j < ss->started_instances; j++) {
+					const stress_stats_t *const stats = ss->stats[j];
+
+					total += stats->misc_stats[i].value;
+				}
+				metric = ss->started_instances ? total / ss->started_instances : 0.0;
+				pr_yaml(yaml, "      %s: %f\n", stess_description_yamlify(description), metric);
+			};
+		}
+
 		pr_yaml(yaml, "\n");
 	}
 }

stress-aio.c
@@ -180,6 +180,7 @@ static int stress_aio(const stress_args_t *args)
 	struct sigaction sa, sa_old;
 	char filename[PATH_MAX];
 	uint32_t total = 0, i, opt_aio_requests = DEFAULT_AIO_REQUESTS;
+	double t1 = 0.0, t2 = 0.0, dt;
 
 	if (!stress_get_setting("aio-requests", &opt_aio_requests)) {
 		if (g_opt_flags & OPT_FLAGS_MAXIMIZE)
@@ -234,6 +235,7 @@ static int stress_aio(const stress_args_t *args)
 
 	stress_set_proc_state(args->name, STRESS_STATE_RUN);
 
+	t1 = stress_time_now();
 	do {
 		(void)shim_usleep_interruptible(250000); /* wait until a signal occurs */
 
@@ -268,6 +270,7 @@ static int stress_aio(const stress_args_t *args)
 			}
 		}
 	} while (keep_stressing(args));
+	t2 = stress_time_now();
 
 	rc = EXIT_SUCCESS;
 
@@ -287,6 +290,10 @@ finish:
 	pr_dbg("%s: total of %" PRIu32 " async I/O signals "
 		"caught (instance %d)\n",
 		args->name, total, args->instance);
+
+	dt = t2 - t1;
+	if (dt > 0.0)
+		stress_misc_stats_set(args->misc_stats, 0, "async I/O signals per sec", (double)total / dt);
 	(void)stress_temp_dir_rm_args(args);
 	return rc;
 }

stress-dccp.c
@@ -222,6 +222,7 @@ static int stress_dccp_server(
 	struct sockaddr *addr = NULL;
 	uint64_t msgs = 0;
 	int rc = EXIT_SUCCESS;
+	double t1 = 0.0, t2 = 0.0, dt;
 
 	(void)setpgid(pid, g_pgrp);
 
@@ -269,6 +270,7 @@ static int stress_dccp_server(
 		goto die_close;
 	}
 
+	t1 = stress_time_now();
 	do {
 		int sfd;
 
@@ -380,6 +382,7 @@ again:
 die_close:
 	(void)close(fd);
 die:
+	t2 = stress_time_now();
 #if defined(AF_UNIX) &&		\
     defined(HAVE_SOCKADDR_UN)
 	if (addr && (dccp_domain == AF_UNIX)) {
@@ -388,11 +391,16 @@ die:
 		(void)unlink(addr_un->sun_path);
 	}
 #endif
+
 	if (pid) {
 		(void)kill(pid, SIGKILL);
 		(void)shim_waitpid(pid, &status, 0);
 	}
 	pr_dbg("%s: %" PRIu64 " messages sent\n", args->name, msgs);
+
+	dt = t2 - t1;
+	if (dt > 0.0)
+		stress_misc_stats_set(args->misc_stats, 0, "messages per sec", (double)msgs / dt);
 
 	return rc;
 }

stress-fault.c
@@ -63,6 +63,7 @@ static int stress_fault(const stress_args_t *args)
 	const size_t len = stress_text_addr(&start, &end);
 	const size_t page_size = args->page_size;
 	void *mapto;
+	double t1 = 0.0, t2 = 0.0, dt;
 
 	ret = stress_temp_dir_mk_args(args);
 	if (ret < 0)
@@ -82,6 +83,7 @@ static int stress_fault(const stress_args_t *args)
 
 	stress_set_proc_state(args->name, STRESS_STATE_RUN);
 
+	t1 = stress_time_now();
 	do {
 		int fd;
 		uint8_t *ptr;
@@ -196,6 +198,7 @@ next:
 		i++;
 		inc_counter(args);
 	} while (keep_stressing(args));
+	t2 = stress_time_now();
 	/* Clean up, most times this is redundant */
 
 	stress_set_proc_state(args->name, STRESS_STATE_DEINIT);
@@ -208,9 +211,17 @@ next:
 #if defined(HAVE_GETRUSAGE) &&		\
     defined(RUSAGE_SELF) &&		\
     defined(HAVE_RUSAGE_RU_MINFLT)
+
 	if (!shim_getrusage(RUSAGE_SELF, &usage)) {
 		pr_dbg("%s: page faults: minor: %lu, major: %lu\n",
 			args->name, usage.ru_minflt, usage.ru_majflt);
+	}
+	dt = t2 - t1;
+	if (dt > 0.0) {
+		stress_misc_stats_set(args->misc_stats, 0, "minor page faults per sec",
+			(double)usage.ru_minflt / dt);
+		stress_misc_stats_set(args->misc_stats, 1, "major page faults per sec",
+			(double)usage.ru_majflt / dt);
 	}
 #endif
 	return EXIT_SUCCESS;

stress-ipsec-mb.c
@@ -690,10 +690,16 @@ static int stress_ipsec_mb(const stress_args_t *args)
 	} while (keep_stressing(args));
 
 	for (i = 0; i < n_features; i++) {
-		if (((init_mb[i].features & features) == init_mb[i].features) && (t[i] > 0.0))
+		if (((init_mb[i].features & features) == init_mb[i].features) && (t[i] > 0.0)) {
+			char tmp[32];
+
 			pr_dbg("%s: %s %.3f bogo/ops per second\n",
 				args->name, init_mb[i].name,
 				(double)count / t[i]);
+
+			(void)snprintf(tmp, sizeof(tmp), "%s bogo/ops per second", init_mb[i].name);
+			stress_misc_stats_set(args->misc_stats, i, tmp, (double)count / t[i]);
+		}
 	}
 
 	stress_set_proc_state(args->name, STRESS_STATE_DEINIT);

stress-lease.c
@@ -186,6 +186,7 @@ static int stress_lease(const stress_args_t *args)
 	int ret, fd, status;
 	pid_t l_pids[MAX_LEASE_BREAKERS];
 	uint64_t i, lease_breakers = DEFAULT_LEASE_BREAKERS;
+	double t1 = 0.0, t2 = 0.0, dt;
 
 	if (!stress_get_setting("lease-breakers", &lease_breakers)) {
 		if (g_opt_flags & OPT_FLAGS_MAXIMIZE)
@@ -228,6 +229,7 @@ static int stress_lease(const stress_args_t *args)
 
 	stress_set_proc_state(args->name, STRESS_STATE_RUN);
 
+	t1 = stress_time_now();
 	do {
 		ret = stress_try_lease(args, filename, O_WRONLY | O_APPEND, F_WRLCK);
 		if (ret != EXIT_SUCCESS)
@@ -236,6 +238,7 @@ static int stress_lease(const stress_args_t *args)
 		if (ret != EXIT_SUCCESS)
 			break;
 	} while (keep_stressing(args));
+	t2 = stress_time_now();
 
 reap:
 	stress_set_proc_state(args->name, STRESS_STATE_DEINIT);
@@ -251,6 +254,11 @@ reap:
 	(void)stress_temp_dir_rm_args(args);
 
 	pr_dbg("%s: %" PRIu64 " lease sigio interrupts caught\n", args->name, lease_sigio);
+	dt = t2 - t1;
+	if (dt > 0.0) {
+		stress_misc_stats_set(args->misc_stats, 0, "lease sigio interrupts per sec",
+			(double)lease_sigio / dt);
+	}
 
 	return ret;
 }

stress-longjmp.c
@@ -95,9 +95,10 @@ static int OPTIMIZE1 stress_longjmp(const stress_args_t *args)
 		stress_longjmp_func();
 
 	if (n) {
+		const double rate = (double)STRESS_NANOSECOND * t_total / (double)n;
 		pr_dbg("%s: about %.3f nanoseconds per longjmp call\n",
-			args->name,
-			(double)STRESS_NANOSECOND * t_total / (double)n);
+			args->name, rate);
+		stress_misc_stats_set(args->misc_stats, 0, "nanoseconds per longjmp call", rate);
 	}
 	stress_set_proc_state(args->name, STRESS_STATE_DEINIT);
 

stress-memrate.c
@@ -339,13 +339,19 @@ static int stress_memrate(const stress_args_t *args)
 
 	pr_lock(&lock);
 	for (i = 0; i < memrate_items; i++) {
-		if (context.stats[i].duration > 0.001)
+		if (context.stats[i].duration > 0.001) {
+			char tmp[32];
+			const double rate = context.stats[i].kbytes / (context.stats[i].duration * KB);
+
 			pr_inf_lock(&lock, "%s: %8.8s: %12.2f MB/sec\n",
-				args->name, memrate_info[i].name,
-				context.stats[i].kbytes / (context.stats[i].duration * KB));
-		else
+				args->name, memrate_info[i].name, rate);
+
+			(void)snprintf(tmp, sizeof(tmp), "%s MB/sec", memrate_info[i].name);
+			stress_misc_stats_set(args->misc_stats, i, tmp, rate);
+		} else {
 			pr_inf_lock(&lock, "%s: %8.8s: interrupted early\n",
 				args->name, memrate_info[i].name);
+		}
 	}
 	pr_unlock(&lock);
 

commit 30a6404101368fb1886e1141eb3f1b24b1372598

stress-ng: use shim wrapped strlcpy
Should be using shim_strlcpy to rather than strlcpy. Thanks to
Rob Colclaser III for reporting this issue.

Fixes: c973e4727a02 ("Add per-stressor metrics to metrics output")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-ng.c
@@ -1870,7 +1870,8 @@ void stress_misc_stats_set(
 	if ((idx < 0) || (idx >= STRESS_MISC_STATS_MAX))
 		return;
 
-	(void)strlcpy(misc_stats[idx].description, description, sizeof(misc_stats[idx].description));
+	(void)shim_strlcpy(misc_stats[idx].description, description,
+			sizeof(misc_stats[idx].description));
 	misc_stats[idx].value = value;
 }
 