ColinIanKing/stress-ng

commit e6db218214bc4c4464837f28e7d23aea8e599bab

stress-dev: zero filename array, cleans up valgrind warnings
Avoid condition jump issues found with valgrind:

==41694== Conditional jump or move depends on uninitialised value(s)
==41694==    at 0x1911A0: stress_dev_rw (stress-dev.c:2767)
==41694==    by 0x1920A4: stress_dev_thread (stress-dev.c:2964)
==41694==    by 0x774044F: start_thread (pthread_create.c:481)
==41694==    by 0x7662E72: clone (clone.S:95)

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-dev.c
@@ -3097,6 +3097,7 @@ static void stress_dev_dir(
 			}
 			ret = shim_pthread_spin_lock(&lock);
 			if (!ret) {
+				(void)memcpy(filename, 0, sizeof(filename));
 				(void)shim_strlcpy(filename, tmp, sizeof(filename));
 				dev_path = filename;
 				(void)shim_pthread_spin_unlock(&lock);

commit fa45cbd91055be6be5f5466e029c28dbf5388901

stress-dev: fix memcpy/memset thinko
Should be memset and not memcpy, fix this.

Fixes: e6db218214bc ("stress-dev: zero filename array, cleans up valgrind warnings")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-dev.c
@@ -3097,7 +3097,7 @@ static void stress_dev_dir(
 			}
 			ret = shim_pthread_spin_lock(&lock);
 			if (!ret) {
-				(void)memcpy(filename, 0, sizeof(filename));
+				(void)memset(filename, 0, sizeof(filename));
 				(void)shim_strlcpy(filename, tmp, sizeof(filename));
 				dev_path = filename;
 				(void)shim_pthread_spin_unlock(&lock);