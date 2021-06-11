22ec89747b4998a77e78cb12a47597ecd9a90df3
Colin Ian King <colin.king@canonical.com>
2021-06-11 11:10:30 +0100

    stress-signest: cater for systems that don't have alternative signal stacks
    Minix (and others) don't have alterative signal stacks, so don't
    use this to compute the signal stack depth and average frame usage per call.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-signest.c
+++ stress-signest.c
@@ -39,6 +39,7 @@ static const int signals[] = {
 #if defined(SIGILL)
 	SIGILL,
 #endif
+/*
 #if defined(SIGQUIT)
 	SIGQUIT,
 #endif
@@ -87,13 +88,16 @@ static const int signals[] = {
 #if defined(SIGWINCH)
 	SIGWINCH,
 #endif
+*/
 };
 
 typedef struct {
 	const stress_args_t *args;
 	uint32_t signalled;	/* bitmap of index into signals[] handled */
 	bool stop;		/* true to stop further nested signalling */
-	intptr_t stack;		/* stack start */
+	intptr_t altstack;	/* alternative stack push start */
+	intptr_t altstack_start;/* alternative start mmap start */
+	intptr_t altstack_end;	/* alternative start mmap end */
 	ptrdiff_t stack_depth;	/* approx stack depth */
 	int depth;		/* call depth */
 	int max_depth;		/* max call depth */
@@ -117,17 +121,23 @@ static void MLOCKED_TEXT stress_signest_handler(int signum)
 {
 	ssize_t i;
 	const intptr_t addr = (intptr_t)&i;
-	ptrdiff_t delta = signal_info.stack - addr;
 	double run_time = stress_time_now() - signal_info.time_start;
 
 	signal_info.depth++;
 	if (signal_info.depth > signal_info.max_depth)
 		signal_info.max_depth = signal_info.depth;
 
-	if (delta < 0)
-		delta = -delta;
-	if (delta > signal_info.stack_depth)
-		signal_info.stack_depth = delta;
+	/* using alternative signal stack? */
+	if ((addr >= signal_info.altstack_start) &&
+	    (addr < signal_info.altstack_end)) {
+		ptrdiff_t delta;
+
+ 		delta = signal_info.altstack - addr;
+		if (delta < 0)
+			delta = -delta;
+		if (delta > signal_info.stack_depth)
+			signal_info.stack_depth = delta;
+	}
 
 	if (run_time > (double)g_opt_timeout)
 		siglongjmp(jmp_env, 1);
@@ -186,8 +196,10 @@ static int stress_signest(const stress_args_t *args)
 
 	signal_info.args = args;
 	signal_info.stop = false;
-	signal_info.stack = (intptr_t)(stress_get_stack_direction() > 0 ?
+	signal_info.altstack = (intptr_t)(stress_get_stack_direction() > 0 ?
 		altstack : altstack + altstack_size);
+	signal_info.altstack_start = (intptr_t)altstack;
+	signal_info.altstack_end = (intptr_t)altstack + altstack_size;
 	signal_info.depth = 0;
 	signal_info.time_start = stress_time_now();
 
@@ -203,7 +215,6 @@ static int stress_signest(const stress_args_t *args)
 	}
 
 	stress_set_proc_state(args->name, STRESS_STATE_RUN);
-
 
 	do {
 		raise(signals[0]);
@@ -241,9 +252,13 @@ finish:
 	} else {
 		pr_inf("%s: %d unique nested signals handled\n", args->name, n);
 	}
-	pr_dbg("%s: stack depth %td bytes (~%td bytes per signal)\n",
-		args->name, signal_info.stack_depth,
-		signal_info.max_depth ? signal_info.stack_depth / signal_info.max_depth : 0);
+	if (signal_info.stack_depth) {
+		pr_dbg("%s: stack depth %td bytes (~%td bytes per signal)\n",
+			args->name, signal_info.stack_depth,
+			signal_info.max_depth ? signal_info.stack_depth / signal_info.max_depth : 0);
+	} else {
+		pr_dbg("%s: stack depth unknown, didn't use alternative signal stack\n", args->name);
+	}
 
 	stress_set_proc_state(args->name, STRESS_STATE_DEINIT);
 
