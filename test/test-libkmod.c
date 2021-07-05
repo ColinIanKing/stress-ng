b97a3d27432d4ae1ad0c5264f2cc5bc60c65b961
Colin Ian King <colin.king@canonical.com>
2021-07-05 16:48:43 +0100

    test/test-libkmod: clean up test code
    Remove white spaces and add error checking.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- test/test-libkmod.c
+++ test/test-libkmod.c
@@ -29,21 +29,25 @@
 
 int main(void)
 {
-        struct kmod_list *l, *list = NULL;
-        struct kmod_ctx *ctx;
+	struct kmod_list *l, *list = NULL;
+	struct kmod_ctx *ctx;
 	int ret;
 
 	ctx = kmod_new(NULL, NULL);
+	if (!ctx)
+		return -1;
 	ret = kmod_module_new_from_lookup(ctx, "snd", &list);
 	if (ret < 0)
-		return -1;
+	return -1;
 
 	kmod_list_foreach(l, list) {
 		struct kmod_module *mod = kmod_module_get_module(l);
 		const char *module_name = kmod_module_get_name(mod);
 		
 		ret = kmod_module_get_initstate(mod);
+		(void)ret;
 		ret = kmod_module_get_refcnt(mod);
+		(void)ret;
 	}	
 
 	kmod_module_unref_list(list);
