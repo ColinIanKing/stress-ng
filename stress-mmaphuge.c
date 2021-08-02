67589fe481e2eeb635c3f0e5b32608251afd839a
Colin Ian King <colin.king@canonical.com>
2021-08-02 10:49:40 +0100

    stress-mmaphuge: fix info name, copy-n-paste error
    Fix build error on OS/X
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-mmaphuge.c
+++ stress-mmaphuge.c
@@ -231,7 +231,7 @@ stressor_info_t stress_mmaphuge_info = {
 
 #else
 
-stressor_info_t stress_tsc_info = {
+stressor_info_t stress_mmaphuge_info = {
 	.stressor = stress_not_implemented,
 	.class = CLASS_VM | CLASS_OS,
 	.opt_set_funcs = opt_set_funcs,
