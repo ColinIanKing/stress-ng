2276a69e711ab345edc050aeff043f7ada095c04
Colin Ian King <colin.king@canonical.com>
2021-08-20 08:52:41 +0100

    stress-cache: remove __builtin___clear_cache call on bad_addr (LP: #1940593)
    This is causing corruption on ARM64 platforms and breaking
    regression tests. Remove it for now until this is properly
    understood.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-cache.c
+++ stress-cache.c
@@ -234,7 +234,6 @@ static void stress_cache_flush(void *addr, void *bad_addr, int size)
 	(void)shim_cacheflush(bad_addr, size, SHIM_ICACHE | SHIM_DCACHE);
 #if defined(HAVE_BUILTIN___CLEAR_CACHE)
 	__builtin___clear_cache(addr, (void *)(addr - 1));
-	__builtin___clear_cache(bad_addr, (void *)(bad_addr + size));
 #endif
 	shim_clflush(bad_addr);
 }
