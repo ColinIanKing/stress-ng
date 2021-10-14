fdb9b8b07877ca7f3912ad20a07b97dcb0798ed7
Colin Ian King <colin.king@canonical.com>
2021-10-14 14:56:41 +0100

    stress-bad-altstack: move pragmas to be around the function stress_bad_altstack_force_fault
    Fix build issue on Oracle Linux.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- stress-bad-altstack.c
+++ stress-bad-altstack.c
@@ -46,18 +46,18 @@ static void *bus_stack;
 #endif
 static sigjmp_buf jmpbuf;
 
+STRESS_PRAGMA_PUSH
+STRESS_PRAGMA_WARN_OFF
 static inline void stress_bad_altstack_force_fault(uint8_t *stack_start)
 {
 	volatile uint8_t *vol_stack = (volatile uint8_t *)stack_start;
 	/* trigger segfault on stack */
 
-STRESS_PRAGMA_PUSH
-STRESS_PRAGMA_WARN_OFF
 	stress_uint8_put(*vol_stack);	/* cppcheck-suppress nullPointer */
 	*vol_stack = 0;			/* cppcheck-suppress nullPointer */
 	(void)*vol_stack;		/* cppcheck-suppress nullPointer */
-STRESS_PRAGMA_POP
 }
+STRESS_PRAGMA_POP
 
 #if defined(SIGXCPU) &&	\
     defined(RLIMIT_CPU)
