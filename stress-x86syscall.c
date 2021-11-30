ColinIanKing/stress-ng

commit 34044d8d13f022e17b8c80c070dcb326752f5dcc

stress-x86syscall: add build time checks for __NR* macros
Ensure __NR_* macros are defined, if not, don't implement system call
test.

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-x86syscall.c
@@ -83,7 +83,15 @@ static int stress_x86syscall_supported(const char *name)
 			"does not support the syscall instruction\n", name);
 		return -1;
 	}
+
+#if defined(__NR_getcpu) ||		\
+    defined(__NR_gettimeofday) ||	\
+    defined(__NR_time)
 	return 0;
+#else
+	pr_inf("%s: stressor will be skipped, no definitions for __NR_getcpu, __NR_gettimeofday or __NR_time\n", name);
+	return -1;
+#endif
 }
 
 /*
@@ -155,6 +163,7 @@ static inline long x86_64_syscall3(long number, long arg1, long arg2, long arg3)
 	return ret;
 }
 
+#if defined(__NR_getcpu)
 /*
  *  wrap_getcpu()
  *	invoke getcpu()
@@ -165,7 +174,9 @@ static int wrap_getcpu(void)
 
 	return x86_64_syscall3(__NR_getcpu, (long)&cpu, (long)&node, (long)NULL);
 }
+#endif
 
+#if defined(__NR_gettimeofday)
 /*
  *  wrap_gettimeofday()
  *	invoke gettimeofday()
@@ -176,7 +187,9 @@ static int wrap_gettimeofday(void)
 
 	return x86_64_syscall2(__NR_gettimeofday, (long)&tv, (long)NULL);
 }
+#endif
 
+#if defined(__NR_time)
 /*
  *  wrap_time()
  *	invoke time()
@@ -187,6 +200,7 @@ static int wrap_time(void)
 
 	return x86_64_syscall1(__NR_time, (long)&t);
 }
+#endif
 
 /*
  *  wrap_dummy()
@@ -203,9 +217,17 @@ static int wrap_dummy(void)
  *  mapping of wrappers to function symbol name
  */
 static stress_x86syscall_t x86syscalls[] = {
+#if defined(__NR_getcpu)
 	{ wrap_getcpu,		"getcpu",		true },
+#endif
+#if defined(__NR_gettimeofday)
 	{ wrap_gettimeofday,	"gettimeofday",		true },
+#endif
+#if defined(__NR_time)
 	{ wrap_time,		"time",			true },
+#endif
+	/* Null entry is ignored */
+	{ NULL,			NULL,			false },
 };
 
 /*

commit 4fbaa539ed67a94f88810ccd6ae2659abef1c986

stress-x86syscall: fix --x86syscall-func segfault
Adding a NULL to the end of the array and not checking for it wasn't the brightest
idea. Fix this.

Fixes: 34044d8d13f0 ("stress-x86syscall: add build time checks for __NR* macros")
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-x86syscall.c
@@ -242,7 +242,7 @@ static char *x86syscall_list_str(void)
 	char *str = NULL;
 	size_t i, len = 0;
 
-	for (i = 0; i < SIZEOF_ARRAY(x86syscalls); i++) {
+	for (i = 0; x86syscalls[i].name; i++) {
 		if (x86syscalls[i].exercise) {
 			char *tmp;
 
@@ -256,7 +256,6 @@ static char *x86syscall_list_str(void)
 				*tmp = '\0';
 			} else {
 				(void)strcat(tmp, " ");
-				str = tmp;
 			}
 			(void)strcat(tmp, x86syscalls[i].name);
 			str = tmp;
@@ -279,7 +278,7 @@ static int x86syscall_check_x86syscall_func(void)
 	if (!stress_get_setting("x86syscall-func", &name))
 		return 0;
 
-	for (i = 0; i < SIZEOF_ARRAY(x86syscalls); i++) {
+	for (i = 0; x86syscalls[i].name; i++) {
 		const bool match = !strcmp(x86syscalls[i].name, name);
 
 		exercise |= match;
@@ -288,7 +287,7 @@ static int x86syscall_check_x86syscall_func(void)
 
 	if (!exercise) {
 		(void)fprintf(stderr, "invalid x86syscall-func '%s', must be one of:", name);
-		for (i = 0; i < SIZEOF_ARRAY(x86syscalls); i++)
+		for (i = 0; x86syscalls[i].name; i++)
 			(void)fprintf(stderr, " %s", x86syscalls[i].name);
 		(void)fprintf(stderr, "\n");
 		return -1;
@@ -324,7 +323,7 @@ static int stress_x86syscall(const stress_args_t *args)
 	do {
 		size_t i;
 
-		for (i = 0; i < SIZEOF_ARRAY(x86syscalls); i++) {
+		for (i = 0; x86syscalls[i].name; i++) {
 			if (x86syscalls[i].exercise) {
 				x86syscalls[i].func();
 				inc_counter(args);