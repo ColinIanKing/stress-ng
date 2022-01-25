184035cb2d2cd016398a0a661d9db4f2c781a1e9
Colin Ian King <colin.i.king@gmail.com>
2022-01-25 16:26:25 +0000

    stress-ng.h: remove ioprio macros
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-ng.h
+++ stress-ng.h
@@ -1469,35 +1469,6 @@ extern void pr_dbg_lock(bool *locked, const char *fmt, ...)  FORMAT(printf, 2, 3
 #define STRESS_ARCH_MIPS	(1)
 #endif
 
-/*
- *  See ioprio_set(2) and linux/ioprio.h, glibc has no definitions
- *  for these at present. Also refer to Documentation/block/ioprio.txt
- *  in the Linux kernel source.
- */
-#if !defined(IOPRIO_CLASS_RT)
-#define IOPRIO_CLASS_RT         (1)
-#endif
-#if !defined(IOPRIO_CLASS_BE)
-#define IOPRIO_CLASS_BE         (2)
-#endif
-#if !defined(IOPRIO_CLASS_IDLE)
-#define IOPRIO_CLASS_IDLE       (3)
-#endif
-
-#if !defined(IOPRIO_WHO_PROCESS)
-#define IOPRIO_WHO_PROCESS      (1)
-#endif
-#if !defined(IOPRIO_WHO_PGRP)
-#define IOPRIO_WHO_PGRP         (2)
-#endif
-#if !defined(IOPRIO_WHO_USER)
-#define IOPRIO_WHO_USER         (3)
-#endif
-
-#if !defined(IOPRIO_PRIO_VALUE)
-#define IOPRIO_PRIO_VALUE(class, data)  (((class) << 13) | data)
-#endif
-
 /* prctl(2) timer slack support */
 #if defined(HAVE_SYS_PRCTL_H) && \
     defined(HAVE_PRCTL) && \
