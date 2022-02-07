2f92efd206b1356b81bd384781200d25eb4b0baf
Colin Ian King <colin.i.king@gmail.com>
2022-02-07 12:06:09 +0000

    core-log: add ( ) to clarify operator precedence
    Clean up cppcheck style warning
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- core-log.c
+++ core-log.c
@@ -29,7 +29,7 @@ static FILE	*log_file = NULL;
 
 static inline FILE *pr_file(void)
 {
-	return g_opt_flags & OPT_FLAGS_STDOUT ? stdout : stderr;
+	return (g_opt_flags & OPT_FLAGS_STDOUT) ? stdout : stderr;
 }
 
 /*
