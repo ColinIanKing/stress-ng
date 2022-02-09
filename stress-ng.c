d8985877d79fc9bf687aad178b71c8a456d05384
Colin Ian King <colin.i.king@gmail.com>
2022-02-09 09:27:20 +0000

    stress-ng: fix typo: untrustyworthy -> untrustworthy
    There is a spelling mistake in a literal string. Fix it.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-ng.c
+++ stress-ng.c
@@ -1554,7 +1554,7 @@ static char *stress_exit_status_to_string(const int status)
 	case EXIT_BY_SYS_EXIT:
 		return "stressor terminated using _exit()";
 	case EXIT_METRICS_UNTRUSTWORTHY:
-		return "metrics may be untrustyworthy";
+		return "metrics may be untrustworthy";
 	default:
 		return "unknown";
 	}
