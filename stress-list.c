8788e554e4b96eec3231ec1f917df5317731543a
Colin Ian King <colin.i.king@gmail.com>
2022-03-04 15:17:46 +0000

    stress-list: add verify flag since the lists are being santity checked
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-list.c
+++ stress-list.c
@@ -589,6 +589,7 @@ stressor_info_t stress_list_info = {
 	.stressor = stress_list,
 	.class = CLASS_CPU_CACHE | CLASS_CPU | CLASS_MEMORY,
 	.opt_set_funcs = opt_set_funcs,
+	.verify = VERIFY_ALWAYS,
 	.help = help
 };
 #else
