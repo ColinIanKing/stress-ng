560429cc66ba1de7977e0273b697b79c2c18e514
Colin Ian King <colin.i.king@gmail.com>
2022-03-04 23:42:38 +0000

    stress-sigpipe: report system errors with pr_err rather than pr_fail
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-sigpipe.c
+++ stress-sigpipe.c
@@ -103,7 +103,7 @@ again:
 		(void)close(pipefds[1]);
 		if (!keep_stressing(args))
 			goto finish;
-		pr_fail("%s: fork failed, errno=%d (%s)\n",
+		pr_err("%s: fork failed, errno=%d (%s)\n",
 			args->name, errno, strerror(errno));
 		return EXIT_FAILURE;
 	} else if (pid == 0) {
