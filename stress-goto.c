0e8ad16c586822949de165a2d0a84eaa4689c53d
Colin Ian King <colin.i.king@gmail.com>
2022-03-04 13:27:20 +0000

    stress-goto: fix cut-n-paste error in --goto-direction error message
    Fix the error message text, it was copied from the hdd stressor.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-goto.c
+++ stress-goto.c
@@ -48,7 +48,7 @@ static int stress_set_goto_direction(const char *opts)
 			return stress_set_setting("goto-direction", TYPE_ID_INT, &stress_goto_direction[i].direction);
 		}
 	}
-	(void)fprintf(stderr, "hdd-opt option '%s' not known, options are:", opts);
+	(void)fprintf(stderr, "goto-option option '%s' not known, options are:", opts);
 	for (i = 0; i < SIZEOF_ARRAY(stress_goto_direction); i++)
 		(void)fprintf(stderr, "%s %s", i == 0 ? "" : ",", stress_goto_direction[i].option);
 	(void)fprintf(stderr, "\n");
