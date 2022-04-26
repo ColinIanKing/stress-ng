f1afc20a963e3a6de213694c6de7f3a788221ddf
Colin Ian King <colin.i.king@gmail.com>
2022-04-26 09:16:15 +0100

    Update copyright on various source files
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- test/test-statx.c
+++ test/test-statx.c
@@ -1,6 +1,5 @@
 /*
- * Copyright (C)      2021 Colin Ian King.
- * Copyright (C)      2022 Colin Ian King.
+ * Copyright (C) 2021-2022 Colin Ian King.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
