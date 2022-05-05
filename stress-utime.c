7b8065de3c68ebd169b16d5c84f5f67f3df53111
Colin Ian King <colin.i.king@gmail.com>
2022-05-05 12:49:36 +0100

    stress-utime: only declare variable verify when it is required
    Clean up ppc64 debian build warning
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-utime.c
+++ stress-utime.c
@@ -76,8 +76,11 @@ static int stress_utime(const stress_args_t *args)
 	char hugename[PATH_MAX + 16];
 	int ret, fd;
 	bool utime_fsync = false;
+#if defined(O_DIRECTORY) &&	\
+    defined(O_PATH) &&		\
+    defined(UTIME_NOW)
 	const bool verify = !!(g_opt_flags & OPT_FLAGS_VERIFY);
-
+#endif
 	(void)stress_get_setting("utime-fsync", &utime_fsync);
 
 	ret = stress_temp_dir_mk_args(args);
