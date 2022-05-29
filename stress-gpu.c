494322d12d4a5317ef6485cfe0b1b1fb7eb6868d
Colin Ian King <colin.i.king@gmail.com>
2022-05-29 19:26:11 +0000

    stress-gpu: update copyright text
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-gpu.c
+++ stress-gpu.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2022, Red Hat Inc, Dorinda Bassey <dbassey@redhat.com>
+ * Copyright (C)      2022, Red Hat Inc, Dorinda Bassey <dbassey@redhat.com>
+ * Copyright (C)      2022 Colin Ian King
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
