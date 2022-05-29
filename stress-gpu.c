56e2d1d3009cb6d37cd9eb1254cc4c1b9ba472dd
Colin Ian King <colin.i.king@gmail.com>
2022-05-29 19:25:10 +0100

    stress-gpu: remove trailing space
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-gpu.c
+++ stress-gpu.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2022, Red Hat Inc, Dorinda Bassey <dbassey@redhat.com>
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
