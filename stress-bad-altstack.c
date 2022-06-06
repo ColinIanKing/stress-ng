70dfeabf3c049784dd5285f6b0cdec83f40de6e3
Colin Ian King <colin.i.king@gmail.com>
2022-06-06 10:18:30 +0000

    stress-bad-altstack: remove inline from stress_bad_altstack_force_fault
    Inlining this helper is wasteful and not speed critical, remove inline
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-bad-altstack.c
+++ stress-bad-altstack.c
@@ -49,7 +49,7 @@ static sigjmp_buf jmpbuf;
 
 STRESS_PRAGMA_PUSH
 STRESS_PRAGMA_WARN_OFF
-static inline void stress_bad_altstack_force_fault(uint8_t *stack_start)
+static void stress_bad_altstack_force_fault(uint8_t *stack_start)
 {
 	volatile uint8_t *vol_stack = (volatile uint8_t *)stack_start;
 	/* trigger segfault on stack */
