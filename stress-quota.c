ColinIanKing/stress-ng

commit 491b4698e30b0f2c3461eb64e58c5a0c3d2ddab6

stress-quota: change quotactl_path syscall to quotactl_fd
Linux 5.13 commit 64c2c2c62f92339b176ea24403d8db16db36f9e6
("quota: Change quotactl_path() systcall to an fd-based one)
replaced quotactl_path to a fd based quotactl_fd call instead.
Rework stress-quota to match the change in the kernel system
call.

Signed-off-by: Colin Ian King <colin.king@canonical.com>

stress-quota.c
@@ -95,32 +95,35 @@ static int do_quotactl_call(
 	int id,
 	caddr_t addr)
 {
-#if defined(HAVE_QUOTACTL_PATH)
-	static bool have_quotactl_path = true;
-	int ret;
+	static bool have_quotactl_fd = true;
+	int ret, fd;
 
 	/*
-	 *  quotactl_path() failed on ENOSYS or random choice
+	 *  quotactl_fd() failed on ENOSYS or random choice
 	 *  then do normal quotactl call
 	 */
-	if (!have_quotactl_path || stress_mwc1())
+	if (!have_quotactl_fd || stress_mwc1())
 		goto do_quotactl;
 
 	/*
-	 *  try quotactl_path() instead, it may not exist
+	 *  try quotactl_fd() instead, it may not exist
 	 *  so flag this for next time and do normal quotactl
 	 *  call
 	 */
-	ret = shim_quotactl_path(cmd, dev->mount, id, addr);
+	fd = open(dev->mount, O_DIRECTORY | O_RDONLY);
+	if (fd < 0)
+		goto do_quotactl;
+	ret = shim_quotactl_fd(cmd, fd, id, addr);
 	if ((ret < 0) && (errno == ENOSYS)) {
 		/* We don't have quotactl_path, use quotactl */
-		have_quotactl_path = false;
+		have_quotactl_fd = false;
+		(void)close(fd);
 		goto do_quotactl;
 	}
+	(void)close(fd);
 	return ret;
 
 do_quotactl:
-#endif
 	return quotactl(cmd, dev->name, id, addr);
 }
 

Makefile.config
@@ -3196,12 +3196,12 @@ endif
 $(call using,$(HAVE_POSIX_OPENPT),posix_openpt)
 endif
 
-ifndef $(HAVE_QUOTACTL_PATH)
-HAVE_QUOTACTL_PATH = $(shell $(MAKE) $(MAKE_OPTS) TEST_PROG=test-quotactl-path have_test_prog)
-ifeq ($(HAVE_QUOTACTL_PATH),1)
-	CONFIG_CFLAGS += -DHAVE_QUOTACTL_PATH
+ifndef $(HAVE_QUOTACTL_FD)
+HAVE_QUOTACTL_FD = $(shell $(MAKE) $(MAKE_OPTS) TEST_PROG=test-quotactl-fd have_test_prog)
+ifeq ($(HAVE_QUOTACTL_FD),1)
+	CONFIG_CFLAGS += -DHAVE_QUOTACTL_FD
 endif
-$(call using,$(HAVE_QUOTACTL_PATH),quotactl_path)
+$(call using,$(HAVE_QUOTACTL_FD),quotactl_fd)
 endif
 
 ifndef $(HAVE_SEM_POSIX)

core-shim.c
@@ -1978,16 +1978,16 @@ int shim_getrusage(int who, struct rusage *usage)
 }
 
 /*
- *  shim_quotactl_path()
- *  	wrapper for Linux 5.13 quotactl_path
+ *  shim_quotactl_fd()
+ *  	wrapper for Linux 5.13 quotactl_fd
  */
-int shim_quotactl_path(int cmd, const char *mountpoint, int id, void *addr)
+int shim_quotactl_fd(unsigned int fd, unsigned int cmd, int id, void *addr)
 {
-#if defined(HAVE_QUOTACTL_PATH)
-	return quotactl_path(cmd, mountpoint, id, addr);
-#elif defined(__NR_quotactl_path)
-	return (int)syscall(__NR_quotactl_path, cmd, mountpoint, id, addr);
+#if defined(HAVE_QUOTACTL_FD)
+	return quotactl_fd(fd, cmd, id, addr);
+#elif defined(__NR_quotactl_fd)
+	return (int)syscall(__NR_quotactl_fd, fd, cmd, id, addr);
 #else
-	return (int)shim_enosys(0, cmd, mountpoint, id, addr);
+	return (int)shim_enosys(0, fd, cmd, id, addr);
 #endif
 }

stress-ng.h
@@ -4487,7 +4487,7 @@ extern int shim_pkey_get(int pkey);
 extern int shim_pkey_set(int pkey, unsigned int rights);
 extern ssize_t shim_process_madvise(int pidfd, const struct iovec *iovec,
 	unsigned long vlen, int advice, unsigned int flags);
-extern int shim_quotactl_path(int cmd, const char *mountpoint, int id, void *addr);
+extern int shim_quotactl_fd(unsigned int fd, unsigned int cmd, int id, void *addr);
 extern ssize_t shim_readlink(const char *pathname, char *buf, size_t bufsiz);
 extern int shim_reboot(int magic, int magic2, int cmd, void *arg);
 extern int shim_removexattr(const char *path, const char *name);

test/test-quotactl-fd.c
@@ -28,5 +28,5 @@
 
 int main(void)
 {
-	return quotactl_path(0, "", 0, (void *)0);
+	return quotactl_fd(0, 0, 0, (void *)0);
 }

commit 1c353aa74a6b1fee146726ea2dd678c219a408df

stress-quota: Fix wrong arguments order in shim_quotactl_fd()
After Linux 5.13 and Glibc 2.34, they support quotactl_fd() system call.
We found it will return errno 9 (Bad file descriptor) after shim_quotactl_fd()
when running stress-ng quota test case. It is caused by passing wrong arguments
order in shim_quotactl_fd(). Let's fix it.

Fixes: 491b4698 ("stress-quota: change quotactl_path syscall to quotactl_fd")
Signed-off-by: Eric Lin <eric.lin@sifive.com>

stress-quota.c
@@ -113,7 +113,7 @@ static int do_quotactl_call(
 	fd = open(dev->mount, O_DIRECTORY | O_RDONLY);
 	if (fd < 0)
 		goto do_quotactl;
-	ret = shim_quotactl_fd((unsigned int)cmd, (unsigned int)fd, id, addr);
+	ret = shim_quotactl_fd((unsigned int)fd, (unsigned int)cmd, id, addr);
 	if ((ret < 0) && (errno == ENOSYS)) {
 		/* We don't have quotactl_path, use quotactl */
 		have_quotactl_fd = false;