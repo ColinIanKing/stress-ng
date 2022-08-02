90ea41035523b9da5c571289b81b7baf162519a9
Colin Ian King <colin.i.king@gmail.com>
2022-08-02 11:28:11 +0000

    stress-regs: add xmm8..xmm15 for SSE enabled CPUs
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-regs.c
+++ stress-regs.c
@@ -28,6 +28,7 @@ static volatile __uint128_t stash128;
 #endif
 
 #define CPU_X86_MMX	(0x00000001)
+#define CPU_X86_SSE	(0x00000002)
 
 static int cpu_flags;
 
@@ -166,6 +167,61 @@ do {			\
 }
 #endif
 
+#if defined(HAVE_INT128_T)
+static void OPTIMIZE0 stress_regs_helper_sse(const stress_args_t *args, register uint64_t v)
+{
+	__uint128_t v128 = ((__uint128_t)v << 64) | (v ^ 0xa55a5555aaaaULL);
+	register __uint128_t xmm0  __asm__("xmm0")  = v128;
+	register __uint128_t xmm1  __asm__("xmm1")  = v128 >> 1;
+	register __uint128_t xmm2  __asm__("xmm2")  = v128 << 1;
+	register __uint128_t xmm3  __asm__("xmm3")  = v128 >> 2;
+	register __uint128_t xmm4  __asm__("xmm4")  = v128 << 2;
+	register __uint128_t xmm5  __asm__("xmm5")  = ~xmm0;
+	register __uint128_t xmm6  __asm__("xmm6")  = ~xmm1;
+	register __uint128_t xmm7  __asm__("xmm7")  = ~xmm2;
+	register __uint128_t xmm8  __asm__("xmm8")  = ~xmm3;
+	register __uint128_t xmm9  __asm__("xmm9")  = ~xmm4;
+	register __uint128_t xmm10 __asm__("xmm10") = xmm0 ^ 0xa5a5a5a5a5a5a5a5ULL;
+	register __uint128_t xmm11 __asm__("xmm11") = xmm1 ^ 0xa5a5a5a5a5a5a5a5ULL;
+	register __uint128_t xmm12 __asm__("xmm12") = xmm2 ^ 0xa5a5a5a5a5a5a5a5ULL;
+	register __uint128_t xmm13 __asm__("xmm13") = xmm3 ^ 0xa5a5a5a5a5a5a5a5ULL;
+	register __uint128_t xmm14 __asm__("xmm14") = xmm4 ^ 0xa5a5a5a5a5a5a5a5ULL;
+	register __uint128_t xmm15 __asm__("xmm15") = xmm0 ^ 0xaa55aa55aa55aa55ULL;
+
+#define SHUFFLE_REGS()	\
+do {			\
+	xmm15 = xmm0;	\
+	xmm0  = xmm1;	\
+	xmm1  = xmm2;	\
+	xmm2  = xmm3;	\
+	xmm3  = xmm4;	\
+	xmm4  = xmm5;	\
+	xmm5  = xmm6;	\
+	xmm6  = xmm7;	\
+	xmm7  = xmm8;	\
+	xmm8  = xmm9;	\
+	xmm9  = xmm10;	\
+	xmm10 = xmm11;	\
+	xmm11 = xmm12;	\
+	xmm12 = xmm13;	\
+	xmm13 = xmm14;	\
+	xmm14 = xmm15;	\
+} while (0);
+
+	SHUFFLE_REGS16();
+
+	stash128 = xmm14;
+	REGS_CHECK(args, "xmm5", v128, stash128);
+
+	stash128 = xmm0 + xmm1 + xmm2 + xmm3 +
+		   xmm4 + xmm5 + xmm6 + xmm7 +
+		   xmm8 + xmm9 + xmm10 + xmm11 +
+		   xmm12 + xmm13 + xmm14 + xmm15;
+
+#undef SHUFFLE_REGS
+}
+#endif
+
 /*
  *  stress_regs_helper(void)
  *	stress x86_64 registers
@@ -218,7 +274,9 @@ do {			\
 #undef SHUFFLE_REGS
 
 #if defined(HAVE_INT128_T)
-	if (cpu_flags & CPU_X86_MMX)
+	if (cpu_flags & CPU_X86_SSE)
+		stress_regs_helper_sse(args, v);
+	else if (cpu_flags & CPU_X86_MMX)
 		stress_regs_helper_mmx(args, v);
 #endif
 
@@ -896,6 +954,7 @@ static int stress_regs(const stress_args_t *args)
 
 #if defined(STRESS_ARCH_X86)
 	cpu_flags |= stress_cpu_x86_has_mmx() ? CPU_X86_MMX : 0;
+	cpu_flags |= stress_cpu_x86_has_sse() ? CPU_X86_SSE : 0;
 #endif
 
 	do {
