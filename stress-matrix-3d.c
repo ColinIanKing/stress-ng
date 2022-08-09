789db8c487fd9100abcb05718b6b543496707610
Colin Ian King <colin.i.king@gmail.com>
2022-08-09 13:43:22 +0000

    stress-matrix-3d: move voidification after variable declarations
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-matrix-3d.c
+++ stress-matrix-3d.c
@@ -341,9 +341,9 @@ static void OPTIMIZE3 TARGET_CLONES stress_matrix_3d_zyx_div(
 	stress_matrix_3d_type_t r[RESTRICT n][n][n])
 {
 	register size_t k;
+	stress_matrix_3d_type_t v = b[0][0][0];
 
 	(void)b;
-	stress_matrix_3d_type_t v = b[0][0][0];
 
 	for (k = 0; k < n; k++) {
 		register size_t j;
