ColinIanKing/stress-ng

commit 424e6b75cfa5b612165cf457f81f76cf5e2c03a8

Usage rusage stats if they are available rather than times
The rusage() stats are preferred to times() because these are
in terms of seconds/useconds rather than clock ticks, so use
these if they are available.

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-ng.c
@@ -2073,8 +2073,24 @@ void stress_misc_stats_set(
 	misc_stats[idx].value = value;
 }
 
+#if defined(HAVE_GETRUSAGE)
 /*
- *  stress_run ()
+ *  stress_getrusage()
+ *	accumulate rusgage stats
+ */
+static void stress_getrusage(const int who, stress_stats_t *stats)
+{
+	struct rusage usage;
+
+	if (shim_getrusage(who, &usage) == 0) {
+		stats->rusage_utime += (double)usage.ru_utime.tv_sec + ((double)usage.ru_utime.tv_usec) / 1000000.0;
+		stats->rusage_stime += (double)usage.ru_stime.tv_sec + ((double)usage.ru_stime.tv_usec) / 1000000.0;
+	}
+}
+#endif
+
+/*
+ *  stress_run()
  *	kick off and run stressors
  */
 static void MLOCKED_TEXT stress_run(
@@ -2237,10 +2253,19 @@ again:
 					(void)stress_tz_get_temperatures(&g_shared->tz_info, &stats->tz);
 #endif
 				stats->finish = stress_time_now();
+#if defined(HAVE_GETRUSAGE)
+				stats->rusage_utime = 0.0;
+				stats->rusage_stime = 0.0;
+				stress_getrusage(RUSAGE_SELF, stats);
+				stress_getrusage(RUSAGE_CHILDREN, stats);
+#else
+				(void)memset(&stats->tms, 0, sizeof(stats->tms));
 				if (times(&stats->tms) == (clock_t)-1) {
 					pr_dbg("times failed: errno=%d (%s)\n",
 						errno, strerror(errno));
 				}
+#endif
+
 				pr_dbg("%s: exited [%d] (instance %" PRIu32 ")\n",
 					name, (int)getpid(), j);
 
@@ -2461,10 +2486,13 @@ static void stress_metrics_dump(
 
 			run_ok  |= stats->run_ok;
 			c_total += stats->counter;
-			u_total += (uint64_t)(stats->tms.tms_utime +
-					      stats->tms.tms_cutime);
-			s_total += (uint64_t)(stats->tms.tms_stime +
-					      stats->tms.tms_cstime);
+#if defined(HAVE_GETRUSAGE)
+			u_total += stats->rusage_utime;
+			s_total += stats->rusage_stime;
+#else
+			u_total += (uint64_t)(stats->tms.tms_utime + stats->tms.tms_cutime);
+			s_total += (uint64_t)(stats->tms.tms_stime + stats->tms.tms_cstime);
+#endif
 			r_total += stats->finish - stats->start;
 		}
 		/* Real time in terms of average wall clock time of all procs */
@@ -2475,8 +2503,13 @@ static void stress_metrics_dump(
 		    (c_total == 0) && (!run_ok))
 			continue;
 
+#if defined(HAVE_GETRUSAGE)
+		u_time = u_total;
+		s_time = u_total;
+#else
 		u_time = (ticks_per_sec > 0) ? (double)u_total / (double)ticks_per_sec : 0.0;
 		s_time = (ticks_per_sec > 0) ? (double)s_total / (double)ticks_per_sec : 0.0;
+#endif
 		t_time = u_time + s_time;
 
 		/* Total usr + sys time of all procs */

stress-ng.h
@@ -951,7 +951,6 @@ typedef struct {
 typedef struct {
 	uint64_t counter;		/* number of bogo ops */
 	bool counter_ready;		/* counter can be read */
-	struct tms tms;			/* run time stats of process */
 	double start;			/* wall clock start time */
 	double finish;			/* wall clock stop time */
 #if defined(STRESS_PERF_STATS)
@@ -963,6 +962,12 @@ typedef struct {
 	bool run_ok;			/* true if stressor exited OK */
 	stress_checksum_t *checksum;	/* pointer to checksum data */
 	stress_misc_stats_t misc_stats[STRESS_MISC_STATS_MAX];
+#if defined(HAVE_GETRUSAGE)
+	double rusage_utime;		/* rusage user time */
+	double rusage_stime;		/* rusage system time */
+#else
+	struct tms tms;			/* run time stats of process */
+#endif
 } stress_stats_t;
 
 #define	STRESS_WARN_HASH_MAX		(128)

commit 893e0a522046f22d9d690760dbd13073051b76e6

stress-ng: fix rusage and CPU consumed metrics
The % CPU consumed is incorrect and so is the system time because
of an incorrect assignment. Fix this.

Fixes: 424e6b75cfa5 ("Usage rusage stats if they are available rather than times")
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-ng.c
@@ -2640,7 +2640,7 @@ static void stress_metrics_dump(
 
 #if defined(HAVE_GETRUSAGE)
 		u_time = u_total;
-		s_time = u_total;
+		s_time = s_total;
 #else
 		u_time = (ticks_per_sec > 0) ? (double)u_total / (double)ticks_per_sec : 0.0;
 		s_time = (ticks_per_sec > 0) ? (double)s_total / (double)ticks_per_sec : 0.0;
@@ -2650,10 +2650,11 @@ static void stress_metrics_dump(
 		/* Total usr + sys time of all procs */
 		bogo_rate_r_time = (r_total > 0.0) ? (double)c_total / r_total : 0.0;
 		{
-			register uint64_t us_total = u_total + s_total;
+			double us_total = u_total + s_total;
 
-			bogo_rate = (us_total > 0) ? (double)c_total / ((double)us_total / (double)ticks_per_sec) : 0.0;
+			bogo_rate = (us_total > 0.0) ? (double)c_total / (us_total / (double)ticks_per_sec) : 0.0;
 		}
+
 		cpu_usage = (r_total > 0) ? 100.0 * t_time / r_total : 0.0;
 		cpu_usage = ss->started_instances ? cpu_usage / ss->started_instances : 0.0;
 