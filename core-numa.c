ColinIanKing/stress-ng

commit 0c95c78b7e7fb9d761cd7faf45c45494895c55e7

Add NUMA --mbind option
Try to mbind to NUMA nodes where possible. Linux only

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-numa.c
@@ -1,0 +1,215 @@
+/*
+ * Copyright (C)      2022 Colin Ian King.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#include "stress-ng.h"
+
+#if defined(HAVE_LINUX_MEMPOLICY_H)
+#include <linux/mempolicy.h>
+#endif
+
+static const char option[] = "option --mbind";
+
+#if defined(__NR_get_mempolicy) &&      \
+    defined(__NR_mbind) &&              \
+    defined(__NR_migrate_pages) &&      \
+    defined(__NR_move_pages) &&         \
+    defined(__NR_set_mempolicy)
+
+
+/*
+ * stress_check_numa_range()
+ * @max_node: maximum NUMA node allowed, 0..N
+ * @node: node number to check
+ */
+static void stress_check_numa_range(
+	const unsigned long max_node,
+	const unsigned long node)
+{
+	if (node >= max_node) {
+		if (max_node > 1) {
+			(void)fprintf(stderr, "%s: invalid range, %lu is not allowed, "
+				"allowed range: 0 to %lu\n", option,
+				node, max_node - 1);
+		} else {
+			(void)fprintf(stderr, "%s: invalid range, %lu is not allowed, "
+				"allowed range: 0\n", option, node);
+		}
+		_exit(EXIT_FAILURE);
+	}
+}
+
+/*
+ *  stress_numa_count_mem_nodes()
+ *	collect number of NUMA memory nodes, add them to a
+ *	circular linked list - also, return maximum number
+ *	of nodes
+ */
+static int stress_numa_count_mem_nodes(unsigned long *max_node)
+{
+	FILE *fp;
+	unsigned long node_id = 0;
+	char buffer[8192], *str = NULL, *ptr;
+
+	*max_node = 0;
+
+	fp = fopen("/proc/self/status", "r");
+	if (!fp)
+		return -1;
+
+	while (fgets(buffer, sizeof(buffer), fp)) {
+		if (!strncmp(buffer, "Mems_allowed:", 13)) {
+			str = buffer + 13;
+			break;
+		}
+	}
+	(void)fclose(fp);
+
+	if (!str)
+		return -1;
+
+	ptr = buffer + strlen(buffer) - 2;
+
+	/*
+	 *  Parse hex digits into NUMA node ids, these
+	 *  are listed with least significant node last
+	 *  so we need to scan backwards from the end of
+	 *  the string back to the start.
+	 */
+	while ((*ptr != ' ') && (ptr > str)) {
+		int val, i;
+
+		/* Skip commas */
+		if (*ptr == ',') {
+			ptr--;
+			continue;
+		}
+
+		if (sscanf(ptr, "%1x", &val) != 1)
+			return -1;
+
+		/* Each hex digit represent 4 memory nodes */
+		for (i = 0; i < 4; i++) {
+			node_id++;
+			if (*max_node < node_id)
+				*max_node = node_id;
+		}
+		ptr--;
+	}
+
+	return 0;
+}
+
+/*
+ * stress_parse_node()
+ * @str: parse string containing decimal NUMA node number
+ *
+ * Returns: NUMA node number, or exits the program on invalid number in str
+ */
+static unsigned long stress_parse_node(char *const str)
+{
+	unsigned long val;
+
+	if (sscanf(str, "%lu", &val) != 1) {
+		(void)fprintf(stderr, "%s: invalid number '%s'\n", option, str);
+		_exit(EXIT_FAILURE);
+	}
+	return val;
+}
+
+/*
+ * stress_set_mbind()
+ * @arg: list of NUMA nodes to bind to, comma separated
+ *
+ * Returns: 0 - OK
+ */
+int stress_set_mbind(const char *arg)
+{
+	char *str, *ptr, *token;
+	unsigned long max_node;
+	unsigned long *nodemask;
+	const size_t nodemask_bits = sizeof(*nodemask) * 8;
+	size_t nodemask_sz;
+
+	if (stress_numa_count_mem_nodes(&max_node) < 0) {
+		(void)fprintf(stderr, "no NUMA nodes found, ignoring --mbind setting '%s'\n", arg);
+		return 0;
+	}
+
+	nodemask_sz = (max_node + (nodemask_bits - 1)) / nodemask_bits;
+	nodemask = calloc(nodemask_sz, sizeof(*nodemask));
+	if (!nodemask) {
+		(void)fprintf(stderr, "parsing --mbind: cannot allocate NUMA nodemask, out of memory\n");
+		_exit(EXIT_FAILURE);
+	}
+
+	str = stress_const_optdup(arg);
+	if (!str) {
+		(void)fprintf(stderr, "out of memory duplicating argument '%s'\n", arg);
+		_exit(EXIT_FAILURE);
+	}
+
+	for (ptr = str; (token = strtok(ptr, ",")) != NULL; ptr = NULL) {
+		unsigned long i, lo, hi;
+		char *tmpptr = strstr(token, "-");
+
+		hi = lo = stress_parse_node(token);
+		if (tmpptr) {
+			tmpptr++;
+			if (*tmpptr)
+				hi = stress_parse_node(tmpptr);
+			else {
+				(void)fprintf(stderr, "%s: expecting number following "
+					"'-' in '%s'\n", option, token);
+				free(str);
+				_exit(EXIT_FAILURE);
+			}
+			if (hi <= lo) {
+				(void)fprintf(stderr, "%s: invalid range in '%s' "
+					"(end value must be larger than "
+					"start value\n", option, token);
+				free(str);
+				_exit(EXIT_FAILURE);
+			}
+		}
+		stress_check_numa_range(max_node, lo);
+		stress_check_numa_range(max_node, hi);
+
+		for (i = lo; i <= hi; i++) {
+			STRESS_SETBIT(nodemask, i);
+			if (shim_set_mempolicy(MPOL_BIND, nodemask, max_node) < 0) {
+				(void)fprintf(stderr, "%s: could not set NUMA memory policy for node %lu, errno=%d (%s)\n",
+					option, i, errno, strerror(errno));
+				free(str);
+				_exit(EXIT_FAILURE);
+			}
+		}
+	}
+
+	free(str);
+	return 0;
+}
+
+#else
+int stress_set_mbind(const char *arg)
+{
+	(void)arg;
+
+	(void)fprintf(stderr, "%s: setting NUMA memory policy binding not supported\n", option);
+	_exit(EXIT_FAILURE);
+}
+#endif

Makefile
@@ -442,6 +442,7 @@ CORE_SRC = \
 	core-mounts.c \
 	core-mwc.c \
 	core-net.c \
+	core-numa.c \
 	core-out-of-memory.c \
 	core-parse-opts.c \
 	core-perf.c \

stress-ng.1
@@ -242,6 +242,14 @@ allowed file descriptors.  Note that stress-ng will use about 5 of the
 available file descriptors so take this into consideration when using this
 setting.
 .TP
+.B \-\-mbind list
+set strict NUMA memory allocation based on the list of NUMA nodes provided;
+page allocations will come from the node with sufficient free memory closest
+to the specified node(s) where the allocation takes place. This uses the
+Linux set_mempolicy(2) call using the MPOL_BIND mode.  The NUMA nodes to be
+used are specified by a comma separated list of node (0 to N-1). One can
+specify a range of NUMA nodes using '-', for example: \-\-mbind 0,2-3,6,7-11
+.TP
 .B \-\-metrics
 output number of bogo operations in total performed by the stress processes.
 Note that these are not a reliable metric of performance or throughput and

stress-ng.c
@@ -580,6 +580,7 @@ static const struct option long_options[] = {
 	{ "matrix-3d-zyx",	0,	0,	OPT_matrix_3d_zyx },
 	{ "maximize",		0,	0,	OPT_maximize },
 	{ "max-fd",		1,	0,	OPT_max_fd },
+	{ "mbind",		1,	0,	OPT_mbind },
 	{ "mcontend",		1,	0,	OPT_mcontend },
 	{ "mcontend-ops",	1,	0,	OPT_mcontend_ops },
 	{ "membarrier",		1,	0,	OPT_membarrier },
@@ -1130,6 +1131,7 @@ static const stress_help_t help_generic[] = {
 	{ NULL,		"log-file filename",	"log messages to a log file" },
 	{ NULL,		"maximize",		"enable maximum stress options" },
 	{ NULL,		"max-fd",		"set maximum file descriptor limit" },
+	{ NULL,		"mbind",		"set NUMA memory binding to specific nodes" },
 	{ "M",		"metrics",		"print pseudo metrics of activity" },
 	{ NULL,		"metrics-brief",	"enable metrics and only show non-zero results" },
 	{ NULL,		"minimize",		"enable minimal stress options" },
@@ -3383,6 +3385,10 @@ next_opt:
 				"Cannot determine maximum file descriptor limit");
 			stress_check_range(optarg, u64, 8, max_fds);
 			stress_set_setting_global("max-fd", TYPE_ID_UINT64, &u64);
+			break;
+		case OPT_mbind:
+			if (stress_set_mbind(optarg) < 0)
+				exit(EXIT_FAILURE);
 			break;
 		case OPT_no_madvise:
 			g_opt_flags &= ~OPT_FLAGS_MMAP_MADVISE;

stress-ng.h
@@ -1879,6 +1879,8 @@ typedef enum {
 	OPT_madvise,
 	OPT_madvise_ops,
 
+	OPT_mbind,
+
 	OPT_malloc,
 	OPT_malloc_ops,
 	OPT_malloc_bytes,

commit a0d102c2d8762d632366d024cdc4181dbf5b9a67

core-numa: Free nodemask, fix memory leak
scan-build detected a memory leak, fix it:

core-numa.c:204:2: warning: Potential leak of memory pointed to by 'nodemask' [unix.Malloc]
        free(str);

Fixes: 0c95c78b7e7f ("Add NUMA --mbind option")
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-numa.c
@@ -201,6 +201,7 @@ int stress_set_mbind(const char *arg)
 		}
 	}
 
+	free(nodemask);
 	free(str);
 	return 0;
 }