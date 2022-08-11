34d2e2ceeb3764e591c18df96da15c49f2928071
Colin Ian King <colin.i.king@gmail.com>
2022-08-11 08:23:05 +0000

    Makefile.config: add pread and pwrite build-time detection
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

---
+++ test/test-pwrite.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C)      2022 Colin Ian King
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#define  _GNU_SOURCE
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void)
+{
+	char data[] = "Test";
+	int fd, rc;
+
+	fd = open("/dev/null", O_WRONLY);
+	if (fd < 0)
+		return -1;
+
+	rc = pwrite(fd, data, sizeof(data), 0);
+	(void)close(fd);
+
+	return rc;
+}
