6a6ecdc49e4109d925b97b4c5186c2195ed9f7d7
Colin Ian King <colin.i.king@gmail.com>
2022-08-18 15:18:52 +0000

    stress-mmap: remove #if defined checks from retry label
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-mmap.c
+++ stress-mmap.c
@@ -390,11 +390,7 @@ static int stress_mmap_child(const stress_args_t *args, void *ctxt)
 		int rnd, rnd_flag;
 		uint8_t *buf = NULL;
 
-#if defined(MAP_HUGETLB) ||		\
-    defined(MAP_UNINITIALIZED) || 	\
-    defined(MAP_DENYWRITE)
 retry:
-#endif
 		if (no_mem_retries >= NO_MEM_RETRIES_MAX) {
 			pr_inf("%s: gave up trying to mmap, no available memory\n",
 				args->name);
