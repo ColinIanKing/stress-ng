c8bc37a45e53f8c04b774fc85772711cdbfb0e97
Colin Ian King <colin.i.king@gmail.com>
2022-09-22 20:25:07 +0100

    stress-dir: constify gray_code variable
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-dir.c
+++ stress-dir.c
@@ -173,7 +173,7 @@ static void stress_dir_tidy(
 
 	for (i = 0; i < n; i++) {
 		char path[PATH_MAX];
-		uint64_t gray_code = (i >> 1) ^ i;
+		const uint64_t gray_code = (i >> 1) ^ i;
 
 		(void)stress_temp_filename_args(args,
 			path, sizeof(path), gray_code);
@@ -323,7 +323,7 @@ static int stress_dir(const stress_args_t *args)
 
 		for (i = 0; keep_stressing(args) && (i < n); i++) {
 			char path[PATH_MAX];
-			uint64_t gray_code = (i >> 1) ^ i;
+			const uint64_t gray_code = (i >> 1) ^ i;
 
 			(void)stress_temp_filename_args(args,
 				path, sizeof(path), gray_code);
