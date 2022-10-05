85b26a3b56c5be3872a97b9b58a72f4b7412747a
Colin Ian King <colin.i.king@gmail.com>
2022-10-05 09:41:52 +0100

    stress-bigheap: fix short help, should mention realloc and not calloc
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-bigheap.c
+++ stress-bigheap.c
@@ -28,7 +28,7 @@
 #define DEFAULT_BIGHEAP_GROWTH	(64 * KB)
 
 static const stress_help_t help[] = {
-	{ "B N","bigheap N",		"start N workers that grow the heap using calloc()" },
+	{ "B N","bigheap N",		"start N workers that grow the heap using realloc()" },
 	{ NULL,	"bigheap-ops N",	"stop after N bogo bigheap operations" },
 	{ NULL,	"bigheap-growth N",	"grow heap by N bytes per iteration" },
 	{ NULL,	NULL,			NULL }
