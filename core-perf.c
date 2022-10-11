b80eda5610f24008358ce97de782ff751cb96e3f
Colin Ian King <colin.i.king@gmail.com>
2022-10-11 14:33:16 +0000

    core-perf: use && instead of & in expression
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- core-perf.c
+++ core-perf.c
@@ -723,7 +723,7 @@ void stress_perf_stat_dump(FILE *yaml, stress_stressor_t *stressors_list, const 
 				if (sscanf(buffer, "%5d", &level) == 1)
 					paranoid = true;
 			}
-			if (paranoid & (level > 1)) {
+			if (paranoid && (level > 1)) {
 				pr_inf("Cannot read perf counters, "
 					"do not have CAP_SYS_ADMIN capability "
 					"or %s is set too high (%d)\n",
