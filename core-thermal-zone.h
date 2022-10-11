6300c862908a551d4fa14e90ab007b9aa1b1045c
Colin Ian King <colin.i.king@gmail.com>
2022-10-11 14:36:06 +0000

    core-thermal-zone.h: rename 2nd arg in stress_tz_dump()
    Make sure name matches the function definition
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- core-thermal-zone.h
+++ core-thermal-zone.h
@@ -25,7 +25,7 @@ extern int stress_tz_init(stress_tz_info_t **tz_info_list);
 extern void stress_tz_free(stress_tz_info_t **tz_info_list);
 extern int stress_tz_get_temperatures(stress_tz_info_t **tz_info_list,
 	stress_tz_t *tz);
-extern void stress_tz_dump(FILE *yaml, stress_stressor_t *procs_head);
+extern void stress_tz_dump(FILE *yaml, stress_stressor_t *stressors_list);
 #endif
 
 #endif
