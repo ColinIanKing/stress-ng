bbecbc406e6fc27ffe4753298dfaa7a43ac84fc9
Colin Ian King <colin.i.king@gmail.com>
2022-10-25 18:15:02 +0100

    test/test-builtin-nontemporal-load: voidify returns
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- test/test-builtin-nontemporal-load.c
+++ test/test-builtin-nontemporal-load.c
@@ -25,8 +25,11 @@ int main(int argc, char **argv)
 	uint32_t v32, data32 = ~0;
 
 	v128 = __builtin_nontemporal_load(&data128);
+	(void)v128;
 	v64 = __builtin_nontemporal_load(&data64);
+	(void)v64;
 	v32 = __builtin_nontemporal_load(&data32);
+	(void)v32;
 
 	return 0;
 }
