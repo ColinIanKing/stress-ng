7dbb284ab497fc6e220b0a852ddb568725dc605a
Florian Weimer <fweimer@redhat.com>
2022-12-13 08:43:28 +0100

    test-pidfd-getfd: Include <syspidfd.h> if available for pidfd_getfd
    Future compilers are likely to require explicit declarations for all
    functions.  Without this change, this test will then always fail, not
    testing anything.

--- test/test-pidfd-getfd.c
+++ test/test-pidfd-getfd.c
@@ -20,6 +20,10 @@
 
 #define _GNU_SOURCE
 
+#if defined(__has_include) && __has_include(<sys/pidfd.h>)
+#include <sys/pidfd.h>
+#endif
+
 int main(void)
 {
 	/* We don't care about the args, we just want to see if it links */
