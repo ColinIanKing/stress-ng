7bb881703e119529a4cb929cf71a29eb3508496b
Colin Ian King <colin.i.king@gmail.com>
2022-12-27 14:22:19 +0000

    core-sort: fix missing int type for j
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- core-sort.c
+++ core-sort.c
@@ -71,7 +71,7 @@ void stress_sort_data_int32_shuffle(int32_t *data, const size_t n)
 
 	for (i = 0; i < n; i++) {
 		register uint32_t tmp;
-		register j = seed % n;
+		register uint32_t j = seed % n;
 
 		seed = (a * seed + c);
 		tmp = data32[i];
