13c1692172aa1746af43449c01ff9addf5ab5bdd
Colin Ian King <colin.i.king@gmail.com>
2023-03-08 19:52:47 +0000

    stress-ng: only define OPTIMIZE_FAST_MAST with fast math attribute for gcc 10+
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-ng.h
+++ stress-ng.h
@@ -526,7 +526,9 @@ typedef struct stressor_info {
 #define CASE_FALLTHROUGH
 #endif
 
-#if defined(HAVE_ATTRIBUTE_FAST_MATH)
+#if defined(HAVE_ATTRIBUTE_FAST_MATH) &&		\
+    defined(__GNUC__) &&				\
+    NEED_GNUC(10, 0, 0)
 #define OPTIMIZE_FAST_MATH __attribute__((optimize("fast-math")))
 #else
 #define OPTIMIZE_FAST_MATH
