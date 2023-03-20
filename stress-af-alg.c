ColinIanKing/stress-ng

commit 6c1191b29ac83ca271d75691c812194427587dd0

stress-af-alg: check if HAVE_LIB_IPSEC_MB is defined
Fix builds for non-existent ipsec-mb library

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-af-alg.c
@@ -46,8 +46,9 @@ static const stress_opt_set_func_t opt_set_funcs[] = {
 	{ 0,			NULL }
 };
 
-#if defined(HAVE_LINUX_IF_ALG_H) && \
-    defined(HAVE_LINUX_SOCKET_H) && \
+#if defined(HAVE_LINUX_IF_ALG_H) &&	\
+    defined(HAVE_LINUX_SOCKET_H) &&	\
+    defined(HAVE_LIB_IPSEC_MB) &&	\
     defined(AF_ALG)
 
 #if !defined(SOL_ALG)

commit 5ba43b6e9b2e85bfe3fa44fa176bce16c30097fb

stress-af-alg: remove check for HAVE_LIB_IPSEC_MB
This was erroneously added in an earlier commit. Remove it.

Issue: https://github.com/ColinIanKing/stress-ng/issues/274
Fixes: 6c1191b29ac83 ("stress-af-alg: check if HAVE_LIB_IPSEC_MB is defined")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-af-alg.c
@@ -48,7 +48,6 @@ static const stress_opt_set_func_t opt_set_funcs[] = {
 
 #if defined(HAVE_LINUX_IF_ALG_H) &&	\
     defined(HAVE_LINUX_SOCKET_H) &&	\
-    defined(HAVE_LIB_IPSEC_MB) &&	\
     defined(AF_ALG)
 
 #if !defined(SOL_ALG)