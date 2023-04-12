080fba1c5d3750e7352bbf8a7dc4e6cabba79f33
Colin Ian King <colin.i.king@gmail.com>
2023-04-12 17:53:18 +0100

    stress-timerfd: add missing space in failure message
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-timerfd.c
+++ stress-timerfd.c
@@ -237,7 +237,7 @@ static int stress_timerfd(const stress_args_t *args)
 	if (!cap_wake_alarm) {
 		ret = timerfd_create(CLOCK_REALTIME_ALARM, 0);
 		if (ret >= 0) {
-			pr_fail("%s: timerfd_create without capability CAP_WAKE_ALARM unexpectedly"
+			pr_fail("%s: timerfd_create without capability CAP_WAKE_ALARM unexpectedly "
 					"succeeded, errno=%d (%s)\n", args->name, errno, strerror(errno));
 			(void)close(ret);
 		}
