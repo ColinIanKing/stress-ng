fc4e50c996ac6203ce017f7ff3c1de3f2e1d7006
Colin Ian King <colin.i.king@gmail.com>
2023-04-21 13:49:55 +0100

    stress-seek: add conditional parentheses
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-seek.c
+++ stress-seek.c
@@ -282,7 +282,7 @@ re_read:
 			int i;
 
 			offset = (off_t)stress_mwc64modn(seek_size);
-			for (i = 0; i < 20 && keep_stressing(args); i++) {
+			for (i = 0; (i < 20) && keep_stressing(args); i++) {
 				offset = stress_shim_lseek(fd, offset, SEEK_DATA);
 				if (UNLIKELY(offset < 0))
 					break;
