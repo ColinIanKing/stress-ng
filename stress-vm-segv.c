7ad46219f278d89f19f9e5d1fa3140d3d29cbd79
Colin Ian King <colin.i.king@gmail.com>
2023-05-10 12:38:52 +0100

    stress-vm-segv: use pipe for a synchronization mechanism
    use pipe, it's simple, it's universal and it works
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-vm-segv.c
+++ stress-vm-segv.c
@@ -20,17 +20,13 @@
 #include "stress-ng.h"
 #include "core-cpu-cache.h"
 
-#if defined(HAVE_PTRACE)
-#include <sys/ptrace.h>
-#else
-UNEXPECTED
-#endif
-
 static const stress_help_t help[] = {
 	{ NULL,	"vm-segv N",	 "start N workers that unmap their address space" },
 	{ NULL,	"vm-segv-ops N", "stop after N vm-segv unmap'd SEGV faults" },
 	{ NULL,	NULL,		 NULL }
 };
+
+#define MSG_CHILD_STARTED	(0x271efb9c)
 
 static NOINLINE void vm_unmap_child(const size_t page_size)
 {
@@ -74,6 +70,14 @@ static int stress_vm_segv(const stress_args_t *args)
 
 	do {
 		pid_t pid;
+		int fd[2];
+
+		if (pipe(fd) < 0) {
+			pr_inf("%s: pipe failed, errno=%d (%s), skipping stressor\n",
+				args->name, errno, strerror(errno));
+			stress_set_proc_state(args->name, STRESS_STATE_DEINIT);
+			return EXIT_NO_RESOURCE;
+		}
 
 again:
 		pid = fork();
@@ -86,76 +90,64 @@ again:
 				args->name, errno, strerror(errno));
 			return EXIT_NO_RESOURCE;
 		} else if (pid > 0) {
-			int status, ret;
-	
+			int status, ret, msg;
+			ssize_t rret;
+
+			(void)close(fd[1]);
+
 			/* Parent, wait for child */
+			rret = read(fd[0], &msg, sizeof(msg));
+			if (rret < (ssize_t)sizeof(msg))
+				goto kill_child;
+
+			if (msg == MSG_CHILD_STARTED) {
+				test_valid = true;
+			} else {
+				pr_dbg("%s: did not get child start message 0x%x got 0x%x instead\n",
+					args->name, MSG_CHILD_STARTED, msg);
+				goto kill_child;
+			}
+
 			ret = shim_waitpid(pid, &status, 0);
 			if (ret < 0)
 				goto kill_child;
-
-			test_valid = true;
-#if !defined(HAVE_PTRACE)
-			if (WTERMSIG(status) == SIGSEGV) {
+			if (WTERMSIG(status) == SIGSEGV)
 				inc_counter(args);
-				continue;
-			}
-#else
-
-			(void)ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD);
-
-			while (keep_stressing(args)) {
-				(void)ptrace(PTRACE_SYSCALL, pid, 0, 0);
-
-				ret = shim_waitpid(pid, &status, 0);
-				if (ret < 0)
-					goto kill_child;
-				if (WIFSTOPPED(status)) {
-					int signum = WSTOPSIG(status);
-
-					if ((signum & 0x7f) == SIGSEGV) {
-						inc_counter(args);
-						break;
-					}
-					if (signum & 0x80)
-						continue;
-				}
-				if (WIFEXITED(status)) {
-					inc_counter(args);
-					break;
-				}
-			}
-#endif
 kill_child:
+			(void)close(fd[0]);
 			stress_kill_and_wait(args, pid, SIGTERM, false);
 		} else {
 			/* Child */
 			sigset_t set;
+			ssize_t wret;
 			const size_t page_size = args->page_size;
+			const int msg = MSG_CHILD_STARTED;
 
 			stress_set_oom_adjustment(args->name, true);
 			stress_process_dumpable(false);
 			(void)sched_settings_apply(true);
 
-#if defined(HAVE_PTRACE)
-			(void)ptrace(PTRACE_TRACEME);
-			(void)kill(getpid(), SIGSTOP);
-#endif
-			(void)sigemptyset(&set);
-			(void)sigaddset(&set, SIGSEGV);
-			(void)sigprocmask(SIG_BLOCK, &set, NULL);
+			(void)close(fd[0]);
 
-			/*
-			 *  Try to ummap the child's address space, should cause
-			 *  a SIGSEGV at some point..
-			 */
-			vm_unmap_child(page_size);
+			wret = write(fd[1], &msg, sizeof(msg));
+			if (wret == (ssize_t)sizeof(msg)) {
+				(void)sigemptyset(&set);
+				(void)sigaddset(&set, SIGSEGV);
+				(void)sigprocmask(SIG_BLOCK, &set, NULL);
 
-			/*
-			 *  That failed, so try unmapping this function
-			 */
-			vm_unmap_self(page_size);
+				/*
+				 *  Try to ummap the child's address space, should cause
+				 *  a SIGSEGV at some point..
+				 */
+				vm_unmap_child(page_size);
 
+				/*
+				 *  That failed, so try unmapping this function
+				 */
+				vm_unmap_self(page_size);
+			}
 			/* No luck, well that's unexpected.. */
+			(void)close(fd[1]);
 			_exit(EXIT_FAILURE);
 		}
 	} while (keep_stressing(args));
