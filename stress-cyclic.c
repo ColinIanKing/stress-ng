ColinIanKing/stress-ng

commit a5cad7c2a88e47dcb41aa45ed43f6a75d9d001d3

core-setting: remove TYPE_ID_UINTPTR_T type
Passing address of methods is awkward and stashing addresses is hard
to sanity check out of bounds errors. Remove the TYPE_ID_UINTPTR_T type
and just pass method settings using the method index into the method
arrays rather than the pointer.

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-cyclic.c
@@ -530,9 +530,7 @@ static int stress_set_cyclic_method(const char *name)
 
 	for (i = 0; i < SIZEOF_ARRAY(cyclic_methods); i++) {
 		if (!strcmp(cyclic_methods[i].name, name)) {
-			const stress_cyclic_method_info_t *info = &cyclic_methods[i];
-
-			stress_set_setting("cyclic-method", TYPE_ID_UINTPTR_T, &info);
+			stress_set_setting("cyclic-method", TYPE_ID_SIZE_T, &i);
 			return 0;
 		}
 	}
@@ -627,7 +625,6 @@ static int stress_cyclic_supported(const char *name)
 
 static int stress_cyclic(const stress_args_t *args)
 {
-	const stress_cyclic_method_info_t *cyclic_method = &cyclic_methods[0];
 	const uint32_t num_instances = args->num_instances;
 	struct sigaction old_action_xcpu;
 	struct rlimit rlim;
@@ -639,6 +636,7 @@ static int stress_cyclic(const stress_args_t *args)
 	size_t cyclic_samples = DEFAULT_SAMPLES;
 	int policy, rc = EXIT_SUCCESS;
 	size_t cyclic_policy = 0;
+	size_t cyclic_method;
 	const double start = stress_time_now();
 	stress_rt_stats_t *rt_stats;
 	const size_t page_size = args->page_size;
@@ -658,7 +656,7 @@ static int stress_cyclic(const stress_args_t *args)
 	(void)stress_get_setting("cyclic-samples", &cyclic_samples);
 	(void)stress_get_setting("cyclic-sleep", &cyclic_sleep);
 
-	func = cyclic_method->func;
+	func = cyclic_methods[cyclic_method].func;
 	policy = policies[cyclic_policy].policy;
 
 	if (!args->instance) {
@@ -720,7 +718,7 @@ static int stress_cyclic(const stress_args_t *args)
 	}
 
 	if (args->instance == 0)
-		pr_dbg("%s: using method '%s'\n", args->name, cyclic_method->name);
+		pr_dbg("%s: using method '%s'\n", args->name, cyclic_methods[cyclic_method].name);
 
 	stress_set_proc_state(args->name, STRESS_STATE_RUN);
 

core-setting.c
@@ -155,10 +155,6 @@ static int stress_set_setting_generic(
 		setting->u.boolean = *(const bool *)value;
 		DBG("%s: BOOL: %s -> %d\n", __func__, name, setting->u.boolean);
 		break;
-	case TYPE_ID_UINTPTR_T:
-		setting->u.uintptr = *(const uintptr_t *)value;
-		DBG("%s: UINTPTR_R: %s -> %p\n", __func__, name, (void *)setting->u.uintptr);
-		break;
 	case TYPE_ID_UNDEFINED:
 	default:
 		DBG("%s: UNDEF: %s -> ?\n", __func__, name);
@@ -308,11 +304,6 @@ bool stress_get_setting(const char *name, void *value)
 				set = true;
 				*(bool *)value = setting->u.boolean;
 				DBG("%s: BOOL: %s -> %d\n", __func__, name, setting->u.boolean);
-				break;
-			case TYPE_ID_UINTPTR_T:
-				set = true;
-				*(uintptr_t *)value = setting->u.uintptr;
-				DBG("%s: UINTPTR_T: %s -> %p\n", __func__, name, (void *)setting->u.uintptr);
 				break;
 			case TYPE_ID_UNDEFINED:
 			default:

stress-memcpy.c
@@ -288,9 +288,7 @@ static int stress_set_memcpy_method(const char *name)
 
 	for (i = 0; i < SIZEOF_ARRAY(stress_memcpy_methods); i++) {
 		if (!strcmp(stress_memcpy_methods[i].name, name)) {
-			const stress_memcpy_method_info_t *info = &stress_memcpy_methods[i];
-
-			stress_set_setting("memcpy-method", TYPE_ID_UINTPTR_T, &info);
+			stress_set_setting("memcpy-method", TYPE_ID_SIZE_T, &i);
 			return 0;
 		}
 	}
@@ -316,7 +314,8 @@ static void stress_memcpy_set_default(void)
 static int stress_memcpy(const stress_args_t *args)
 {
 	uint8_t *buf, *str1, *str2, *str3;
-	const stress_memcpy_method_info_t *memcpy_method = &stress_memcpy_methods[0];
+	size_t memcpy_method = 0;
+	stress_memcpy_func func;
 
 	buf = (uint8_t *)mmap(NULL, 3 * MEMCPY_MEMSIZE, PROT_READ | PROT_WRITE,
 				MAP_ANONYMOUS | MAP_PRIVATE, -1 , 0);
@@ -340,13 +339,12 @@ static int stress_memcpy(const stress_args_t *args)
 	}
 
 	(void)stress_get_setting("memcpy-method", &memcpy_method);
-
+	func = stress_memcpy_methods[memcpy_method].func;
 	stress_rndbuf(str3, ALIGN_SIZE);
-
 	stress_set_proc_state(args->name, STRESS_STATE_RUN);
 
 	do {
-		memcpy_method->func(str1, str2, str3);
+		func(str1, str2, str3);
 		stress_bogo_inc(args);
 	} while (stress_continue(args));
 

stress-memthrash.c
@@ -706,9 +706,8 @@ static int stress_set_memthrash_method(const char *name)
 	size_t i;
 
 	for (i = 0; i < SIZEOF_ARRAY(memthrash_methods); i++) {
-		const stress_memthrash_method_info_t *info = &memthrash_methods[i];
 		if (!strcmp(memthrash_methods[i].name, name)) {
-			stress_set_setting("memthrash-method", TYPE_ID_UINTPTR_T, &info);
+			stress_set_setting("memthrash-method", TYPE_ID_SIZE_T, &i);
 			return 0;
 		}
 	}
@@ -898,6 +897,7 @@ reap_mem:
 static int stress_memthrash(const stress_args_t *args)
 {
 	stress_memthrash_context_t context;
+	size_t memthrash_method = 0;
 	int rc;
 
 	if (stress_sigchld_set_handler(args) < 0)
@@ -907,7 +907,6 @@ static int stress_memthrash(const stress_args_t *args)
 
 	context.args = args;
 	context.total_cpus = (uint32_t)stress_get_processors_online();
-	context.memthrash_method = &memthrash_methods[0];
 	context.max_threads = stress_memthrash_max(args->num_instances, context.total_cpus);
 #if defined(HAVE_MEMTHRASH_NUMA)
 	{
@@ -938,7 +937,8 @@ static int stress_memthrash(const stress_args_t *args)
 	}
 #endif
 
-	(void)stress_get_setting("memthrash-method", &context.memthrash_method);
+	(void)stress_get_setting("memthrash-method", &memthrash_method);
+	context.memthrash_method = &memthrash_methods[memthrash_method];
 
 	if (args->instance == 0) {
 		pr_dbg("%s: using method '%s'\n", args->name, context.memthrash_method->name);

stress-misaligned.c
@@ -1045,7 +1045,7 @@ static stress_misaligned_method_info_t stress_misaligned_methods[] = {
 #if defined(HAVE_ATOMIC_FETCH_ADD_8) &&	\
     defined(HAVE_ATOMIC) &&		\
     defined(__ATOMIC_SEQ_CST)
-	{ "int128tomic",stress_misaligned_int128atomic,	false,	false },
+	{ "int128atomic",stress_misaligned_int128atomic,false,	false },
 #endif
 #endif
 };
@@ -1168,10 +1168,8 @@ static int stress_set_misaligned_method(const char *name)
 	size_t i;
 
 	for (i = 0; i < SIZEOF_ARRAY(stress_misaligned_methods); i++) {
-		const stress_misaligned_method_info_t *info = &stress_misaligned_methods[i];
-
-		if (!strcmp(info->name, name)) {
-			stress_set_setting("misaligned-method", TYPE_ID_UINTPTR_T, &info);
+		if (!strcmp(stress_misaligned_methods[i].name, name)) {
+			stress_set_setting("misaligned-method", TYPE_ID_SIZE_T, &i);
 			return 0;
 		}
 	}
@@ -1197,7 +1195,8 @@ static void stress_misaligned_set_default(void)
 static int stress_misaligned(const stress_args_t *args)
 {
 	uint8_t *buffer;
-	stress_misaligned_method_info_t *misaligned_method = &stress_misaligned_methods[0];
+	size_t misaligned_method = 0;
+	stress_misaligned_method_info_t *method;
 	int ret, rc;
 	const size_t page_size = args->page_size;
 	const size_t buffer_size = page_size << 1;
@@ -1242,7 +1241,8 @@ static int stress_misaligned(const stress_args_t *args)
 
 	stress_set_proc_state(args->name, STRESS_STATE_RUN);
 
-	current_method = misaligned_method;
+	method = &stress_misaligned_methods[misaligned_method];
+	current_method = method;
 	ret = sigsetjmp(jmp_env, 1);
 	if (args->instance == 0) {
 		switch (ret) {
@@ -1264,15 +1264,15 @@ static int stress_misaligned(const stress_args_t *args)
 
 	rc = EXIT_SUCCESS;
 	do {
-		if (misaligned_method->disabled) {
+		if (method->disabled) {
 			rc = EXIT_NO_RESOURCE;
 			break;
 		}
 #if defined(HAVE_TIMER_FUNCTIONALITY)
 		stress_misaligned_reset_timer();
 #endif
-		misaligned_method->func(args, (uintptr_t)buffer, page_size, &succeeded);
-		misaligned_method->exercised = true;
+		method->func(args, (uintptr_t)buffer, page_size, &succeeded);
+		method->exercised = true;
 		stress_bogo_inc(args);
 	} while (stress_continue(args));
 

stress-ng.h
@@ -396,7 +396,6 @@ typedef enum {
 	TYPE_ID_OFF_T,
 	TYPE_ID_STR,
 	TYPE_ID_BOOL,
-	TYPE_ID_UINTPTR_T
 } stress_type_id_t;
 
 typedef struct {

stress-nop.c
@@ -223,7 +223,7 @@ STRESS_NOP_SPIN_OP(s390_nopr, stress_op_s390_nopr);
 static void stress_nop_random(const stress_args_t *args, const bool flag,
 			      double *duration, double *count);
 
-static stress_nop_instr_t nop_instr[] = {
+static stress_nop_instr_t nop_instrs[] = {
 	{ "nop",	stress_nop_spin_nop,		NULL,	false,	false },
 #if defined(STRESS_ARCH_X86)
 	{ "nop2",	stress_nop_spin_x86_nop2,	NULL,	false,	false },
@@ -298,32 +298,32 @@ static void stress_nop_random(
 
 	do {
 		/* the -1 stops us from calling random recursively */
-		const size_t n = stress_mwc8modn(SIZEOF_ARRAY(nop_instr) - 1);
+		const size_t n = stress_mwc8modn(SIZEOF_ARRAY(nop_instrs) - 1);
 
-		current_instr = &nop_instr[n];
+		current_instr = &nop_instrs[n];
 		stress_nop_callfunc(current_instr, args, false, duration, count);
 	} while (stress_continue(args));
 }
 
 static int stress_set_nop_instr(const char *opt)
 {
-	stress_nop_instr_t *instr;
 	size_t i;
 
-	current_instr = &nop_instr[0];
+	current_instr = &nop_instrs[0];
 
-	for (i = 0; i < SIZEOF_ARRAY(nop_instr); i++) {
-		instr = &nop_instr[i];
+	for (i = 0; i < SIZEOF_ARRAY(nop_instrs); i++) {
+		stress_nop_instr_t *instr = &nop_instrs[i];
+
 		if (!strcmp(instr->name, opt)) {
-			stress_set_setting("nop-instr", TYPE_ID_UINTPTR_T, &instr);
+			stress_set_setting("nop-instr", TYPE_ID_SIZE_T, &i);
 			current_instr = instr;
 			return 0;
 		}
 	}
 
 	(void)fprintf(stderr, "nop-instr must be one of:");
-	for (i = 0; i < SIZEOF_ARRAY(nop_instr); i++) {
-		(void)fprintf(stderr, " %s", nop_instr[i].name);
+	for (i = 0; i < SIZEOF_ARRAY(nop_instrs); i++) {
+		(void)fprintf(stderr, " %s", nop_instrs[i].name);
 	}
 	(void)fprintf(stderr, "\n");
 
@@ -345,11 +345,13 @@ static void NORETURN stress_sigill_nop_handler(int signum)
  */
 static int stress_nop(const stress_args_t *args)
 {
-	stress_nop_instr_t *instr = &nop_instr[0];
+	size_t nop_instr = 0;
+	stress_nop_instr_t *instr;
 	bool do_random;
 	double duration = 0.0, count = 0.0, rate;
 
-	(void)stress_get_setting("nop-instr", &instr);
+	(void)stress_get_setting("nop-instr", &nop_instr);
+	instr = &nop_instrs[nop_instr];
 
 	if (stress_sighandler(args->name, SIGILL, stress_sigill_nop_handler, NULL) < 0)
 		return EXIT_NO_RESOURCE;
@@ -358,7 +360,7 @@ static int stress_nop(const stress_args_t *args)
 
 	if (sigsetjmp(jmpbuf, 1) != 0) {
 		/* We reach here on an SIGILL trap */
-		if (current_instr == &nop_instr[0]) {
+		if (current_instr == &nop_instrs[0]) {
 			/* Really should be able to do nop, skip */
 			pr_inf_skip("%s: 'nop' instruction was illegal, skipping stressor\n",
 				args->name);
@@ -366,7 +368,7 @@ static int stress_nop(const stress_args_t *args)
 		} else {
 			/* not random choice?, then default to nop */
 			if (!do_random)
-				instr = &nop_instr[0];
+				instr = &nop_instrs[0];
 			pr_inf("%s: '%s' instruction was illegal, ignoring, defaulting to nop\n",
 				args->name, current_instr->name);
 		}

stress-opcode.c
@@ -348,10 +348,8 @@ static int stress_set_opcode_method(const char *name)
 	size_t i;
 
 	for (i = 0; i < SIZEOF_ARRAY(stress_opcode_methods); i++) {
-		stress_opcode_method_info_t const *info = &stress_opcode_methods[i];
-
-		if (!strcmp(info->name, name)) {
-			stress_set_setting("opcode-method", TYPE_ID_UINTPTR_T, &info);
+		if (!strcmp(stress_opcode_methods[i].name, name)) {
+			stress_set_setting("opcode-method", TYPE_ID_SIZE_T, &i);
 			return 0;
 		}
 	}
@@ -373,10 +371,10 @@ static int stress_opcode(const stress_args_t *args)
 {
 	const size_t page_size = args->page_size;
 	int rc;
-	size_t i;
+	size_t i, opcode_method = 0;
+	const stress_opcode_method_info_t *method;
 	const size_t opcode_bytes = STRESS_OPCODE_SIZE >> 3;
 	const size_t opcode_loops = page_size / opcode_bytes;
-	const stress_opcode_method_info_t *opcode_method = &stress_opcode_methods[0];
 	double op_start, rate, t, duration, percent;
 	const double num_opcodes = pow(2.0, STRESS_OPCODE_SIZE);
 	uint64_t forks = 0;
@@ -412,6 +410,7 @@ static int stress_opcode(const stress_args_t *args)
 	(void)shim_memset(opcodes, 0x00, page_size * PAGES);
 
 	(void)stress_get_setting("opcode-method", &opcode_method);
+	method = &stress_opcode_methods[opcode_method];
 
 	stress_set_proc_state(args->name, STRESS_STATE_RUN);
 
@@ -427,7 +426,7 @@ static int stress_opcode(const stress_args_t *args)
 		 *  gets a different random value on each fork
 		 */
 		(void)stress_mwc32();
-		if (opcode_method->func == stress_opcode_inc) {
+		if (method->func == stress_opcode_inc) {
 			char buf[32];
 
 			(void)snprintf(buf, sizeof(buf), "opcode-0x%*.*" PRIx64 " [run]",
@@ -474,7 +473,7 @@ again:
 			(void)mprotect((void *)ops_begin, page_size, PROT_WRITE);
 
 			/* Populate with opcodes */
-			opcode_method->func(page_size, ops_begin, ops_end, &vstate->opcode);
+			method->func(page_size, ops_begin, ops_end, &vstate->opcode);
 
 			/* Make read-only executable and force I$ flush */
 			(void)mprotect((void *)ops_begin, page_size, PROT_READ | PROT_EXEC);

commit 9f056933232cec0a8a7458b323cd873bc8a8217a

stress-cyclic: Fix segv caused by uninitialized variable cyclic_method
[ 2513.886855] User process fault: interruption code 0038 ilc:3 in stress-ng[2aa00d80000+1e1000]
[ 2513.886873] Failing address: 0fa61490c3962000 TEID: 0fa61490c3962800
[ 2513.886876] Fault in primary space mode while using user ASCE.
[ 2513.886880] AS:00000000cf4481c7 R3:0000000000000024
[ 2513.886885] CPU: 6 PID: 250291 Comm: stress-ng-cycli Tainted: P           O      5.15.0-79-generic #86-Ubuntu
[ 2513.886889] Hardware name: IBM 2964 N63 400 (LPAR)
[ 2513.886890] User PSW : 0705000180000000 000002aa00e1cb3e
[ 2513.886892]            R:0 T:1 IO:1 EX:1 Key:0 M:1 W:0 P:1 AS:0 CC:0 PM:0 RI:0 EA:3
[ 2513.886895] User GPRS: 0000000000000000 0000000000000006 0fa611e6c295ab30 000002aa01007f28
[ 2513.886897]            000002aa02534be7 0000000000000001 000003ffa8ea8cd8 000003ff00000004
[ 2513.886899]            000002aa01225110 000002aa010386e0 0000000000001000 000003fff367e398
[ 2513.886901]            000003ffa8f30f90 000002aa00f91e80 000002aa00e1cb0e 000003fff367e0b8
[ 2513.886910] User Code: 000002aa00e1cb30: 58113000		l	%r1,0(%r1,%r3)
                          000002aa00e1cb34: eb220004000d	sllg	%r2,%r2,4
                         #000002aa00e1cb3a: 5010f0c8		st	%r1,200(%r15)
                         >000002aa00e1cb3e: e31230500004	lg	%r1,80(%r2,%r3)
                          000002aa00e1cb44: e310f0c00024	stg	%r1,192(%r15)
                          000002aa00e1cb4a: e31090000004	lg	%r1,0(%r9)
                          000002aa00e1cb50: a71fffff		cghi	%r1,-1

Kudos to P.H. Lin for reporting and bisecting this issue.

Fixes: a5cad7c2a88e ("core-setting: remove TYPE_ID_UINTPTR_T type")
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-cyclic.c
@@ -636,7 +636,7 @@ static int stress_cyclic(const stress_args_t *args)
 	size_t cyclic_samples = DEFAULT_SAMPLES;
 	int policy, rc = EXIT_SUCCESS;
 	size_t cyclic_policy = 0;
-	size_t cyclic_method;
+	size_t cyclic_method = 0;
 	const double start = stress_time_now();
 	stress_rt_stats_t *rt_stats;
 	const size_t page_size = args->page_size;