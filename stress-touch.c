387f426af2211ffc7d1647a3a3b30fd1081b34c6
Colin Ian King <colin.i.king@gmail.com>
2023-09-05 08:31:17 +0100

    stress-touch: fix missing help args for --touch-method and --touch-opts
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-touch.c
+++ stress-touch.c
@@ -83,10 +83,10 @@ static void *touch_lock;
 	 TOUCH_OPT_TRUNC )
 
 static const stress_help_t help[] = {
-	{ NULL,	"touch N",	"start N stressors that touch and remove files" },
-	{ NULL, "touch-method",	"specify method to touch tile file, open | create" },
-	{ NULL,	"touch-ops N",	"stop after N touch bogo operations" },
-	{ NULL, "touch-opts",	"touch open options all, direct, dsync, excl, noatime, sync, trunc" },
+	{ NULL,	"touch N",		"start N stressors that touch and remove files" },
+	{ NULL, "touch-method M",	"specify method to touch tile file, open | create" },
+	{ NULL,	"touch-ops N",		"stop after N touch bogo operations" },
+	{ NULL, "touch-opts list",	"touch open options all, direct, dsync, excl, noatime, sync, trunc" },
 	{ NULL,	NULL,		NULL }
 };
 
