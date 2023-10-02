3ba06499d3f0c57391ded9337c573dd92023c10f
Colin Ian King <colin.i.king@gmail.com>
2023-10-02 10:47:43 +0100

    core-pragma.h: remove -Wstringop-overflow for older toolchains
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- core-pragma.h
+++ core-pragma.h
@@ -54,8 +54,7 @@
 					_Pragma("GCC diagnostic ignored \"-Wextra\"") \
 					_Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"") \
 					_Pragma("GCC diagnostic ignored \"-Wcast-qual\"") \
-					_Pragma("GCC diagnostic ignored \"-Wnonnull\"")	\
-					_Pragma("GCC diagnostic ignored \"-Wstringop-overflow\"")
+					_Pragma("GCC diagnostic ignored \"-Wnonnull\"")
 #else
 #define STRESS_PRAGMA_PUSH
 #define STRESS_PRAGMA_POP
