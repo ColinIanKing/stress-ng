603da83464b0d3d351b56e67cc2d673c01fd6811
Colin Ian King <colin.i.king@gmail.com>
2023-10-02 10:44:51 +0100

    core-pragma: use -Wstringop-overflow on toolchains that support it
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- core-pragma.h
+++ core-pragma.h
@@ -36,6 +36,17 @@
 #define STRESS_PRAGMA_WARN_OFF		_Pragma("GCC diagnostic ignored \"-Weverything\"")
 #elif defined(HAVE_COMPILER_GCC) &&	\
       defined(HAVE_PRAGMA) &&		\
+      NEED_GNUC(7, 5, 0)
+#define STRESS_PRAGMA_PUSH		_Pragma("GCC diagnostic push")
+#define STRESS_PRAGMA_POP		_Pragma("GCC diagnostic pop")
+#define STRESS_PRAGMA_WARN_OFF		_Pragma("GCC diagnostic ignored \"-Wall\"") \
+					_Pragma("GCC diagnostic ignored \"-Wextra\"") \
+					_Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"") \
+					_Pragma("GCC diagnostic ignored \"-Wcast-qual\"") \
+					_Pragma("GCC diagnostic ignored \"-Wnonnull\"")	\
+					_Pragma("GCC diagnostic ignored \"-Wstringop-overflow\"")
+#elif defined(HAVE_COMPILER_GCC) &&	\
+      defined(HAVE_PRAGMA) &&		\
       NEED_GNUC(4, 6, 0)
 #define STRESS_PRAGMA_PUSH		_Pragma("GCC diagnostic push")
 #define STRESS_PRAGMA_POP		_Pragma("GCC diagnostic pop")
