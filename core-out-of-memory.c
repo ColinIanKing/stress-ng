ColinIanKing/stress-ng

commit 39d337dbae343dcd198e26c2d83894a7f15ec4b8

core-out-of-memory: add timeout checks on re-fork and stressor re-invocation phases
It may take time before a stressor child gets started so check at fork retry
time and stressor function re-invocation call time for any timeouts just in case
the SIGALRM was missed (which happens only every second). This allows stops
needless re-starting killed stressors in very low memory scenarios.

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-out-of-memory.c
@@ -223,6 +223,8 @@ int stress_oomable_child(
 again:
 	if (!stress_continue(args))
 		return EXIT_SUCCESS;
+	if (stress_time_now() > args->time_end)
+		return EXIT_SUCCESS;
 	pid = fork();
 	if (pid < 0) {
 		/* Keep trying if we are out of resources */
@@ -336,11 +338,18 @@ rewait:
 		if (flag & STRESS_OOMABLE_DROP_CAP) {
 			VOID_RET(int, stress_drop_capabilities(args->name));
 		}
-		if (!stress_continue(args)) {
+		/*
+		 * Process may have exceeded run time by the time it was
+		 * fully runnable, so check for this before doing expensive
+		 * stressor invocation
+		 */
+		if (!stress_continue(args) ||
+		    (stress_time_now() > args->time_end)) {
 			stress_set_proc_state(args->name, STRESS_STATE_EXIT);
 			_exit(EXIT_SUCCESS);
 		}
 
+		/* ..and finally re-start the stressor */
 		ret = func(args, context);
 		pr_fail_check(&rc);
 		if (rc != EXIT_SUCCESS)

commit ba47bef218936145cd5e7b861c6db376e9a4d951

core-out-of-memory: treat zero timeout as ignore timeout check
Don't check timeout if zero timeout is provided. Fixes case where
stress-ng --timeout 0 --vm 1 exits immediately.

Fixes: 39d337d ("core-out-of-memory: add timeout checks on re-fork and stressor re-invocation phases")
Closes: https://github.com/ColinIanKing/stress-ng/issues/323

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-out-of-memory.c
@@ -210,6 +210,7 @@ int stress_oomable_child(
 	int rc = EXIT_SUCCESS;
 	size_t signal_idx = 0;
 	const bool not_quiet = !(flag & STRESS_OOMABLE_QUIET);
+	const bool valid_timeout = (g_opt_timeout > 0);
 
 	/*
 	 *  Kill child multiple times, start with SIGALRM and work up
@@ -226,8 +227,9 @@ int stress_oomable_child(
 again:
 	if (!stress_continue(args))
 		return EXIT_SUCCESS;
-	if (stress_time_now() > args->time_end)
+	if (valid_timeout && (stress_time_now() > args->time_end)) {
 		return EXIT_SUCCESS;
+	}
 	pid = fork();
 	if (pid < 0) {
 		/* Keep trying if we are out of resources */
@@ -347,7 +349,7 @@ rewait:
 		 * stressor invocation
 		 */
 		if (!stress_continue(args) ||
-		    (stress_time_now() > args->time_end)) {
+		    (valid_timeout && (stress_time_now() > args->time_end))) {
 			stress_set_proc_state(args->name, STRESS_STATE_EXIT);
 			_exit(EXIT_SUCCESS);
 		}