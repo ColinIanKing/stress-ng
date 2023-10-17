ed913ecc1d469beb3112520191a9939ae6b37a1b
Colin Ian King <colin.i.king@gmail.com>
2023-10-17 14:45:37 +0100

    test/test-libxxhash.c: include string.h and make buffer static
    Minor cleanups
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- test/test-libxxhash.c
+++ test/test-libxxhash.c
@@ -17,10 +17,11 @@
  *
  */
 #include <xxhash.h>
+#include <string.h>
 
 int main(void)
 {
-	const char buffer[] = "test123";
+	static const char buffer[] = "test123";
 
 	XXH64_hash_t hash = XXH64(buffer, strlen(buffer), 0x12345678);
 
