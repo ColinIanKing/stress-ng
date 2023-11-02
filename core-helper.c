624b97b62b6301df3c3c37014733e5b7572c7c26
Colin Ian King <colin.i.king@gmail.com>
2023-11-02 22:46:15 +0000

    core-helper: add missing check for HAVE_SYS_PRCTL_H when using prctl
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- core-helper.c
+++ core-helper.c
@@ -824,8 +824,9 @@ void stress_get_memlimits(
  */
 void stress_ksm_memory_merge(const int flag)
 {
-#if defined(__linux__) &&	\
-    defined(PR_SET_MEMORY_MERGE)
+#if defined(__linux__) &&		\
+    defined(PR_SET_MEMORY_MERGE) &&	\
+    defined(HAVE_SYS_PRCTL_H)
 	if ((flag >= 0) && (flag <= 1)) {
 		static int prev_flag = -1;
 
