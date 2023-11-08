b167b12a3261288298a79919dc34068d593a25e9
Colin Ian King <colin.i.king@gmail.com>
2023-11-08 16:52:22 +0000

    stress-utime: cast integer constants to time_t for time_t struct values
    Cleans up *BSD build warnings
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-utime.c
+++ stress-utime.c
@@ -171,32 +171,32 @@ static int OPTIMIZE3 stress_utime(const stress_args_t *args)
 		VOID_RET(int, utimes(hugename, timevals));
 
 		/* Exercise with time outside FAT time range */
-		timevals[0].tv_sec = 283996800;	/* Monday, 1 January 1979 00:00:00 */
+		timevals[0].tv_sec = (time_t)283996800;	/* Monday, 1 January 1979 00:00:00 */
 		timevals[0].tv_usec = 0;
-		timevals[1].tv_sec = 283996800;
+		timevals[1].tv_sec = (time_t)283996800;
 		timevals[1].tv_usec = 0;
 		VOID_RET(int, utimes(filename, timevals));
 
 #if defined(LONG_MAX) && (LONG_MAX > 4354819200)
 		/* Exercise with time outside FAT time range */
-		timevals[0].tv_sec = 4354819200; /* Monday, 1 January 2108 00:00:00 */
+		timevals[0].tv_sec = (time_t)4354819200; /* Monday, 1 January 2108 00:00:00 */
 		timevals[0].tv_usec = 0;
-		timevals[1].tv_sec = 4354819200;
+		timevals[1].tv_sec = (time_t)4354819200;
 		timevals[1].tv_usec = 0;
 		VOID_RET(int, utimes(filename, timevals));
 #endif
 
 		/* Exercise with time outside of UNIX EPOCH  */
-		timevals[0].tv_sec = 2147558400; /* Wednesday 20 January 2038 */
+		timevals[0].tv_sec = (time_t)2147558400; /* Wednesday 20 January 2038 */
 		timevals[0].tv_usec = 0;
-		timevals[1].tv_sec = 2147558400;
+		timevals[1].tv_sec = (time_t)2147558400;
 		timevals[1].tv_usec = 0;
 		VOID_RET(int, utimes(filename, timevals));
 
 		/* Exercise with time before of UNIX EPOCH  */
-		timevals[0].tv_sec = 0x7fffffff;
+		timevals[0].tv_sec = (time_t)0x7fffffff;
 		timevals[0].tv_usec = 0;
-		timevals[1].tv_sec = 0x7fffffff;
+		timevals[1].tv_sec = (time_t)0x7fffffff;
 		timevals[1].tv_usec = 0;
 		VOID_RET(int, utimes(filename, timevals));
 
@@ -254,32 +254,32 @@ static int OPTIMIZE3 stress_utime(const stress_args_t *args)
 		}
 
 		/* Exercise with time outside FAT time range */
-		ts[0].tv_sec = 283996800;	/* Monday, 1 January 1979 00:00:00 */
+		ts[0].tv_sec = (time_t)283996800;	/* Monday, 1 January 1979 00:00:00 */
 		ts[0].tv_nsec = 0;
-		ts[1].tv_sec = 283996800;
+		ts[1].tv_sec = (time_t)283996800;
 		ts[1].tv_nsec = 0;
 		VOID_RET(int, futimens(fd, ts));
 
 #if defined(LONG_MAX) && (LONG_MAX > 4354819200)
 		/* Exercise with time outside FAT time range */
-		ts[0].tv_sec = 4354819200; /* Monday, 1 January 2108 00:00:00 */
+		ts[0].tv_sec = (time_t)4354819200; /* Monday, 1 January 2108 00:00:00 */
 		ts[0].tv_nsec = 0;
-		ts[1].tv_sec = 4354819200;
+		ts[1].tv_sec = (time_t)4354819200;
 		ts[1].tv_nsec = 0;
 		VOID_RET(int, futimens(fd, ts));
 #endif
 
 		/* Exercise with time outside of UNIX EPOCH  */
-		ts[0].tv_sec = 2147558400; /* Wednesday 20 January 2038 */
+		ts[0].tv_sec = (time_t)2147558400; /* Wednesday 20 January 2038 */
 		ts[0].tv_nsec = 0;
-		ts[1].tv_sec = 2147558400;
+		ts[1].tv_sec = (time_t)2147558400;
 		ts[1].tv_nsec = 0;
 		VOID_RET(int, futimens(fd, ts));
 
 		/* Exercise with time before of UNIX EPOCH  */
-		ts[0].tv_sec = 0x7fffffff;
+		ts[0].tv_sec = (time_t)0x7fffffff;
 		ts[0].tv_nsec = 0;
-		ts[1].tv_sec = 0x7fffffff;
+		ts[1].tv_sec = (time_t)0x7fffffff;
 		ts[1].tv_nsec = 0;
 		VOID_RET(int, futimens(fd, ts));
 #else
