ColinIanKing/stress-ng

commit e1cb759f7c43f67124f6f3093dcb978d65dc52a7

stress-mmapfixed: make vec array uint32 to speed up mincore checking
Since we're looping over the vec array to check if any bytes are non-zero
we can optimize this for 32/64 bit systems by a factor of 4 by making
the vec array uint32_t rather than unisgned char so that 4 byters per
loop can be checked for non-zero values.

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-mmapfixed.c
@@ -53,21 +53,21 @@ static bool OPTIMIZE3 stress_mmapfixed_is_mapped_slow(
 	size_t len,
 	const size_t page_size)
 {
-	unsigned char vec[PAGE_CHUNKS] ALIGN64;
+	uint32_t vec[PAGE_CHUNKS / sizeof(uint32_t)] ALIGN64;
 	ssize_t n = (ssize_t)len;
 	size_t n_pages = len / page_size;
 
 	if (n_pages > PAGE_CHUNKS)
 		n_pages = PAGE_CHUNKS;
 
-	(void)shim_memset(vec, 0, PAGE_CHUNKS);
+	(void)shim_memset(vec, 0, sizeof(vec));
 	while (n > 0) {
 		int ret;
 		register const size_t sz = n_pages * page_size;
 		register size_t j;
 
 		n -= n_pages;
-		ret = shim_mincore(addr, sz, vec);
+		ret = shim_mincore(addr, sz, (unsigned char *)vec);
 		if (UNLIKELY(ret == ENOSYS))
 			return false;	/* Dodgy, assume not in memory */
 

commit 7200954ce0fa364b3f77acb9395f10c98f23e86e

stress-mmapfixed: fix array overflow, iterate over only the array elements
The loop iteration is using the wrong size, use SIZEOF_ARRAY to fix this.
Fixes: e1cb759f7c43 ("stress-mmapfixed: make vec array uint32 to speed up mincore checking")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-mmapfixed.c
@@ -72,7 +72,7 @@ static bool OPTIMIZE3 stress_mmapfixed_is_mapped_slow(
 			return false;	/* Dodgy, assume not in memory */
 
 PRAGMA_UNROLL_N(4)
-		for (j = 0; j < n_pages; j++) {
+		for (j = 0; j < SIZEOF_ARRAY(vec); j++) {
 			if (vec[j])
 				return true;
 		}