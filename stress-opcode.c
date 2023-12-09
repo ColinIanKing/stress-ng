ColinIanKing/stress-ng

commit f8094c7eb15ba778d77d4b1a4ddbc8a3b15339cd

stress-ng: move ci counter struct into args and args into stat struct
The bogo-op counter shim helpers currently have to dereference the
counter stuct via a pointer and this is an overhead in a heavily
accessed field. Remove the indirection by moving the counter ci
struct into args. This makes args read/writeable so all const
pointer refs to args need to be deconstified.

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-opcode.c
@@ -370,7 +370,7 @@ static int stress_set_opcode_method(const char *name)
  *  stress_opcode
  *	stress with random opcodes
  */
-static int stress_opcode(const stress_args_t *args)
+static int stress_opcode(stress_args_t *args)
 {
 	const size_t page_size = args->page_size;
 	int rc;

commit 6ce7519103238506fe1b7879c724be23061b198e

stress-opcode: don't unmap shared memory, just make it read-only
Fix segfault on access of unmapped shared memory when dereferencing
args. Make shared memory read-only instead of unmapping it.

Fixes: f8094c7eb15b ("stress-ng: move ci counter struct into args and args into stat struct")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-opcode.c
@@ -463,7 +463,7 @@ again:
 			(void)sched_settings_apply(true);
 
 			/* We don't want bad ops clobbering this region */
-			stress_shared_unmap();
+			stress_shared_readonly();
 
 			/* We don't want core dumps either */
 			stress_process_dumpable(false);