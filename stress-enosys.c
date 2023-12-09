ColinIanKing/stress-ng

commit f8094c7eb15ba778d77d4b1a4ddbc8a3b15339cd

stress-ng: move ci counter struct into args and args into stat struct
The bogo-op counter shim helpers currently have to dereference the
counter stuct via a pointer and this is an overhead in a heavily
accessed field. Remove the indirection by moving the counter ci
struct into args. This makes args read/writeable so all const
pointer refs to args need to be deconstified.

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-enosys.c
@@ -174,7 +174,7 @@ static void exit_if_child(const pid_t pid)
 	}
 }
 
-static void itimer_set(const stress_args_t *args)
+static void itimer_set(stress_args_t *args)
 {
 	struct itimerval it;
 
@@ -193,7 +193,7 @@ static void itimer_set(const stress_args_t *args)
 }
 
 static void exercise_syscall(
-	const stress_args_t *args,
+	stress_args_t *args,
 	long number, long arg1, long arg2,
 	long arg3, long arg4, long arg5,
 	long arg6, long arg7)
@@ -3518,7 +3518,7 @@ static void NORETURN MLOCKED_TEXT stress_sig_handler(int signum)
  *  the parent
  */
 static inline int stress_do_syscall(
-	const stress_args_t *args,
+	stress_args_t *args,
 	const long number,
 	const bool random)
 {
@@ -3616,7 +3616,7 @@ static inline int stress_do_syscall(
  *  stress_enosys
  *	stress system calls
  */
-static int stress_enosys(const stress_args_t *args)
+static int stress_enosys(stress_args_t *args)
 {
 	pid_t pid;
 

commit 3ed75a7b5b93be224fc520cd42220f010c500fe1

stress-enosys: don't unmap shared memory, just make it read-only
Fix segfault on access of unmapped shared memory when dereferencing
args. Make shared memory read-only instead of unmapping it.

Fixes: f8094c7eb15b ("stress-ng: move ci counter struct into args and args into stat struct")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-enosys.c
@@ -3914,7 +3914,7 @@ static inline int stress_do_syscall(
 		limit_procs(2);
 
 		/* We don't want bad ops clobbering this region */
-		stress_shared_unmap();
+		stress_shared_readonly();
 
 		/* Drop all capabilities */
 		if (stress_drop_capabilities(args->name) < 0) {