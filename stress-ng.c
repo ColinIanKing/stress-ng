ColinIanKing/stress-ng

commit f8094c7eb15ba778d77d4b1a4ddbc8a3b15339cd

stress-ng: move ci counter struct into args and args into stat struct
The bogo-op counter shim helpers currently have to dereference the
counter stuct via a pointer and this is an overhead in a heavily
accessed field. Remove the indirection by moving the counter ci
struct into args. This makes args read/writeable so all const
pointer refs to args need to be deconstified.

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-ng.c
@@ -1277,7 +1277,7 @@ static void NORETURN stress_child_atexit(void)
  *	copy of another dup by another stressor process (to save memory).
  */
 void stress_metrics_set_const_check(
-	const stress_args_t *args,
+	stress_args_t *args,
 	const size_t idx,
 	char *description,
 	const bool const_description,
@@ -1424,23 +1424,22 @@ static int MLOCKED_TEXT stress_run_child(
 	if (g_opt_timeout)
 		(void)alarm((unsigned int)g_opt_timeout);
 	if (stress_continue_flag() && !(g_opt_flags & OPT_FLAGS_DRY_RUN)) {
-		const stress_args_t args = {
-			.ci = &stats->ci,
-			.name = name,
-			.max_ops = g_stressor_current->bogo_ops,
-			.instance = (uint32_t)instance,
-			.num_instances = (uint32_t)g_stressor_current->num_instances,
-			.pid = child_pid,
-			.page_size = page_size,
-			.time_end = stress_time_now() + (double)g_opt_timeout,
-			.mapped = &g_shared->mapped,
-			.metrics = stats->metrics,
-			.info = g_stressor_current->stressor->info
-		};
-		stress_set_oom_adjustment(&args, false);
+		stats->args.name = name,
+		stats->args.max_ops = g_stressor_current->bogo_ops,
+		stats->args.instance = (uint32_t)instance,
+		stats->args.num_instances = (uint32_t)g_stressor_current->num_instances,
+		stats->args.pid = child_pid,
+		stats->args.page_size = page_size,
+		stats->args.time_end = stress_time_now() + (double)g_opt_timeout,
+		stats->args.mapped = &g_shared->mapped,
+		stats->args.metrics = stats->metrics,
+		stats->args.info = g_stressor_current->stressor->info;
+
+		stress_set_oom_adjustment(&stats->args, false);
+
 		(void)shim_memset(*checksum, 0, sizeof(**checksum));
 		stats->start = stress_time_now();
-		rc = g_stressor_current->stressor->info->stressor(&args);
+		rc = g_stressor_current->stressor->info->stressor(&stats->args);
 		stress_block_signals();
 		(void)alarm(0);
 		if (g_opt_flags & OPT_FLAGS_INTERRUPTS) {
@@ -1472,7 +1471,7 @@ static int MLOCKED_TEXT stress_run_child(
 #endif
 		stats->completed = true;
 		ok = (rc == EXIT_SUCCESS);
-		stats->ci.run_ok = ok;
+		stats->args.ci.run_ok = ok;
 		(*checksum)->data.ci.run_ok = ok;
 		/* Ensure reserved padding is zero to not confuse checksum */
 		(void)shim_memset((*checksum)->data.pad, 0, sizeof((*checksum)->data.pad));
@@ -1483,14 +1482,14 @@ static int MLOCKED_TEXT stress_run_child(
 		 *  if not then flag up that the counter may
 		 *  be untrustyworthy
 		 */
-		if ((!stats->ci.counter_ready) && (!stats->ci.force_killed)) {
+		if ((!stats->args.ci.counter_ready) && (!stats->args.ci.force_killed)) {
 			pr_warn("%s: WARNING: bogo-ops counter in non-ready state, "
 				"metrics are untrustworthy (process may have been "
 				"terminated prematurely)\n",
 				name);
 			rc = EXIT_METRICS_UNTRUSTWORTHY;
 		}
-		(*checksum)->data.ci.counter = args.ci->counter;
+		(*checksum)->data.ci.counter = stats->args.ci.counter;
 		stress_hash_checksum(*checksum);
 	}
 #if defined(STRESS_PERF_STATS) &&	\
@@ -1506,7 +1505,7 @@ static int MLOCKED_TEXT stress_run_child(
 #endif
 	finish = stress_time_now();
 	stats->duration = finish - stats->start;
-	stats->counter_total += stats->ci.counter;
+	stats->counter_total += stats->args.ci.counter;
 	stats->duration_total += stats->duration;
 
 	stress_get_usage_stats(ticks_per_sec, stats);
@@ -1520,10 +1519,10 @@ static int MLOCKED_TEXT stress_run_child(
 	 * Apparently succeeded but terminated early?
 	 * Could be a bug, so report a warning
 	 */
-	if (stats->ci.run_ok &&
+	if (stats->args.ci.run_ok &&
 	    (g_shared && !g_shared->caught_sigint) &&
 	    (run_duration < (double)g_opt_timeout) &&
-	    (!(g_stressor_current->bogo_ops && stats->ci.counter >= g_stressor_current->bogo_ops))) {
+	    (!(g_stressor_current->bogo_ops && stats->args.ci.counter >= g_stressor_current->bogo_ops))) {
 
 		pr_warn("%s: WARNING: finished prematurely after just %s\n",
 			name, stress_duration_to_str(run_duration, true));
@@ -1602,8 +1601,8 @@ static void MLOCKED_TEXT stress_run(
 				goto abort;
 #endif
 			stats->pid = -1;
-			stats->ci.counter_ready = true;
-			stats->ci.counter = 0;
+			stats->args.ci.counter_ready = true;
+			stats->args.ci.counter = 0;
 			stats->checksum = *checksum;
 again:
 			if (!stress_continue_flag())
@@ -1806,7 +1805,7 @@ static void stress_metrics_check(bool *success)
 			if (!stats->completed)
 				continue;
 
-			counter_check |= stats->ci.counter;
+			counter_check |= stats->args.ci.counter;
 			if (stats->duration < min_run_time)
 				min_run_time = stats->duration;
 
@@ -1818,20 +1817,20 @@ static void stress_metrics_check(bool *success)
 			}
 
 			(void)shim_memset(&stats_checksum, 0, sizeof(stats_checksum));
-			stats_checksum.data.ci.counter = stats->ci.counter;
-			stats_checksum.data.ci.run_ok = stats->ci.run_ok;
+			stats_checksum.data.ci.counter = stats->args.ci.counter;
+			stats_checksum.data.ci.run_ok = stats->args.ci.run_ok;
 			stress_hash_checksum(&stats_checksum);
 
-			if (stats->ci.counter != checksum->data.ci.counter) {
+			if (stats->args.ci.counter != checksum->data.ci.counter) {
 				pr_fail("%s instance %d corrupted bogo-ops counter, %" PRIu64 " vs %" PRIu64 "\n",
 					ss->stressor->name, j,
-					stats->ci.counter, checksum->data.ci.counter);
+					stats->args.ci.counter, checksum->data.ci.counter);
 				ok = false;
 			}
-			if (stats->ci.run_ok != checksum->data.ci.run_ok) {
+			if (stats->args.ci.run_ok != checksum->data.ci.run_ok) {
 				pr_fail("%s instance %d corrupted run flag, %d vs %d\n",
 					ss->stressor->name, j,
-					stats->ci.run_ok, checksum->data.ci.run_ok);
+					stats->args.ci.run_ok, checksum->data.ci.run_ok);
 				ok = false;
 			}
 			if (stats_checksum.hash != checksum->hash) {
@@ -1936,7 +1935,7 @@ static void stress_metrics_dump(FILE *yaml)
 			if (stats->completed)
 				ss->completed_instances++;
 
-			run_ok  |= stats->ci.run_ok;
+			run_ok  |= stats->args.ci.run_ok;
 			c_total += stats->counter_total;
 			u_total += stats->rusage_utime_total;
 			s_total += stats->rusage_stime_total;

stress-ng.h
@@ -165,6 +165,7 @@ typedef struct {
 	bool counter_ready;		/* ready flag */
 	bool run_ok;			/* stressor run w/o issues */
 	bool force_killed;		/* true if sent SIGKILL */
+	bool padding;			/* padding */
 } stress_counter_info_t;
 
 typedef struct {
@@ -181,11 +182,11 @@ typedef struct {
 
 /* stressor args */
 typedef struct {
-	stress_counter_info_t *ci;	/* counter info struct */
 	const char *name;		/* stressor name */
 	uint64_t max_ops;		/* max number of bogo ops */
-	const uint32_t instance;	/* stressor instance # */
-	const uint32_t num_instances;	/* number of instances */
+	stress_counter_info_t ci;	/* counter info struct */
+	uint32_t instance;		/* stressor instance # */
+	uint32_t num_instances;		/* number of instances */
 	pid_t pid;			/* stressor pid */
 	size_t page_size;		/* page size */
 	double time_end;		/* when to end */
@@ -400,7 +401,7 @@ typedef enum {
 
 /* stressor information */
 typedef struct stressor_info {
-	int (*stressor)(const stress_args_t *args);	/* stressor function */
+	int (*stressor)(stress_args_t *args);	/* stressor function */
 	int (*supported)(const char *name);	/* return 0 = supported, -1, not */
 	void (*init)(void);		/* stressor init, NULL = ignore */
 	void (*deinit)(void);		/* stressor de-init, NULL = ignore */
@@ -506,7 +507,7 @@ typedef struct {
 
 /* Per stressor statistics and accounting info */
 typedef struct stress_stats {
-	stress_counter_info_t ci;	/* counter info */
+	stress_args_t args;		/* stressor args */
 	double start;			/* wall clock start time */
 	double duration;		/* finish - start */
 	uint64_t counter_total;		/* counter total */
@@ -669,15 +670,13 @@ static inline void ALWAYS_INLINE OPTIMIZE3 stress_continue_set_flag(const bool s
  *	the counter in a child and the child is force KILL'd then indicate
  *	so with the stress_force_killed_bogo() call from the parent.
  */
-static inline void ALWAYS_INLINE OPTIMIZE3 stress_bogo_add(const stress_args_t *args, const uint64_t inc)
+static inline void ALWAYS_INLINE OPTIMIZE3 stress_bogo_add(stress_args_t *args, const uint64_t inc)
 {
-	register stress_counter_info_t * const ci = args->ci;
-
-	ci->counter_ready = false;
+	args->ci.counter_ready = false;
 	stress_asm_mb();
-	ci->counter += inc;
+	args->ci.counter += inc;
 	stress_asm_mb();
-	ci->counter_ready = true;
+	args->ci.counter_ready = true;
 }
 
 /*
@@ -688,26 +687,22 @@ static inline void ALWAYS_INLINE OPTIMIZE3 stress_bogo_add(const stress_args_t *
  *	the counter in a child and the child is force KILL'd then indicate
  *	so with the stress_force_killed_bogo() call from the parent.
  */
-static inline void ALWAYS_INLINE OPTIMIZE3 stress_bogo_inc(const stress_args_t *args)
+static inline void ALWAYS_INLINE OPTIMIZE3 stress_bogo_inc(stress_args_t *args)
 {
-	register stress_counter_info_t * const ci = args->ci;
-
-	ci->counter_ready = false;
+	args->ci.counter_ready = false;
 	stress_asm_mb();
-	ci->counter++;
+	args->ci.counter++;
 	stress_asm_mb();
-	ci->counter_ready = true;
+	args->ci.counter_ready = true;
 }
 
 /*
  *  stress_bogo_get()
  *	get the stessor bogo ops counter
  */
-static inline uint64_t ALWAYS_INLINE OPTIMIZE3 stress_bogo_get(const stress_args_t *args)
+static inline uint64_t ALWAYS_INLINE OPTIMIZE3 stress_bogo_get(stress_args_t *args)
 {
-	register const stress_counter_info_t * const ci = args->ci;
-
-	return ci->counter;
+	return args->ci.counter;
 }
 
 /*
@@ -718,15 +713,13 @@ static inline uint64_t ALWAYS_INLINE OPTIMIZE3 stress_bogo_get(const stress_args
  *	the counter in a child and the child is force KILL'd then indicate
  *	so with the stress_force_killed_bogo() call from the parent.
  */
-static inline void ALWAYS_INLINE OPTIMIZE3 stress_bogo_set(const stress_args_t *args, const uint64_t val)
+static inline void ALWAYS_INLINE OPTIMIZE3 stress_bogo_set(stress_args_t *args, const uint64_t val)
 {
-	register stress_counter_info_t * const ci = args->ci;
-
-	ci->counter_ready = false;
+	args->ci.counter_ready = false;
 	stress_asm_mb();
-	ci->counter = val;
+	args->ci.counter = val;
 	stress_asm_mb();
-	ci->counter_ready = true;
+	args->ci.counter_ready = true;
 }
 
 /*
@@ -735,16 +728,16 @@ static inline void ALWAYS_INLINE OPTIMIZE3 stress_bogo_set(const stress_args_t *
  *	be ignored. Use only if the parent kills the child *and* the child
  *	was used to increment the bogo-op counter.
  */
-static inline void ALWAYS_INLINE stress_force_killed_bogo(const stress_args_t *args)
+static inline void ALWAYS_INLINE stress_force_killed_bogo(stress_args_t *args)
 {
-	args->ci->force_killed = true;
+	args->ci.force_killed = true;
 }
 
 /*
  *  stress_continue()
  *      returns true if we can keep on running a stressor
  */
-static inline bool ALWAYS_INLINE OPTIMIZE3 stress_continue(const stress_args_t *args)
+static inline bool ALWAYS_INLINE OPTIMIZE3 stress_continue(stress_args_t *args)
 {
 	if (UNLIKELY(!g_stress_continue_flag))
 		return false;
@@ -758,7 +751,7 @@ static inline bool ALWAYS_INLINE OPTIMIZE3 stress_continue(const stress_args_t *
  *	add val to the stessor bogo ops counter with lock, return true
  *	if stress_continue is true
  */
-static inline void stress_bogo_add_lock(const stress_args_t *args, void *lock, const int64_t val)
+static inline void stress_bogo_add_lock(stress_args_t *args, void *lock, const int64_t val)
 {
 	/*
 	 *  Failure in lock acquire, don't bump counter
@@ -776,7 +769,7 @@ static inline void stress_bogo_add_lock(const stress_args_t *args, void *lock, c
  *	increment the stessor bogo ops counter with lock, return true
  *	if stress_continue is true
  */
-static inline bool stress_bogo_inc_lock(const stress_args_t *args, void *lock, const bool inc)
+static inline bool stress_bogo_inc_lock(stress_args_t *args, void *lock, const bool inc)
 {
 	bool ret;
 
@@ -803,7 +796,7 @@ static inline bool stress_bogo_inc_lock(const stress_args_t *args, void *lock, c
 extern WARN_UNUSED int stress_parse_opts(int argc, char **argv, const bool jobmode);
 extern void stress_shared_unmap(void);
 extern void stress_log_system_mem_info(void);
-extern void stress_metrics_set_const_check(const stress_args_t *args,
+extern void stress_metrics_set_const_check(stress_args_t *args,
 	const size_t idx, char *description, const bool const_description, const double value, const int mean_type);
 #if defined(HAVE_BUILTIN_CONSTANT_P)
 #define stress_metrics_set(args, idx, description, value, mean_type)	\

stress-sysinval.c
@@ -2365,7 +2365,7 @@ static void MLOCKED_TEXT stress_syscall_itimer_handler(int sig)
  *	  crashes
  */
 static void syscall_permute(
-	const stress_args_t *args,
+	stress_args_t *args,
 	const int arg_num,
 	const stress_syscall_arg_t *stress_syscall_arg,
 	volatile bool *syscall_exercised)
@@ -2533,7 +2533,7 @@ timed_out:
  *  Call a system call in a child context so we don't clobber
  *  the parent
  */
-static inline int stress_do_syscall(const stress_args_t *args)
+static inline int stress_do_syscall(stress_args_t *args)
 {
 	pid_t pid;
 	int rc = 0;
@@ -2647,7 +2647,7 @@ static inline int stress_do_syscall(const stress_args_t *args)
 	return rc;
 }
 
-static int stress_sysinval_child(const stress_args_t *args, void *context)
+static int stress_sysinval_child(stress_args_t *args, void *context)
 {
 	(void)context;
 
@@ -2663,7 +2663,7 @@ static int stress_sysinval_child(const stress_args_t *args, void *context)
  *  stress_sysinval
  *	stress system calls with bad addresses
  */
-static int stress_sysinval(const stress_args_t *args)
+static int stress_sysinval(stress_args_t *args)
 {
 	int ret, rc = EXIT_NO_RESOURCE;
 	size_t i;

commit e1594ee8b4835e9e2c069e1b31975f2da44ffc61

stress-sysinval: don't unmap shared memory, just make it read-only
Fix segfault on access of unmapped shared memory when dereferencing
args. Make shared memory read-only instead of unmapping it.

Fixes: f8094c7eb15b ("stress-ng: move ci counter struct into args and args into stat struct")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-ng.c
@@ -2445,6 +2445,16 @@ err_unmap_shared:
 }
 
 /*
+ *  stress_shared_readonly()
+ *	unmap shared region
+ */
+void stress_shared_readonly(void)
+{
+	(void)mprotect((void *)g_shared->checksum.checksums, g_shared->checksum.length, PROT_READ);
+	(void)mprotect((void *)g_shared, g_shared->length, PROT_READ);
+}
+
+/*
  *  stress_shared_unmap()
  *	unmap shared region
  */

stress-ng.h
@@ -792,6 +792,7 @@ static inline bool stress_bogo_inc_lock(stress_args_t *args, void *lock, const b
 #define STRESS_HARMONIC_MEAN	(2)
 
 extern WARN_UNUSED int stress_parse_opts(int argc, char **argv, const bool jobmode);
+extern void stress_shared_readonly(void);
 extern void stress_shared_unmap(void);
 extern void stress_log_system_mem_info(void);
 extern void stress_metrics_set_const_check(stress_args_t *args,

stress-sysinval.c
@@ -2598,7 +2598,7 @@ static inline int stress_do_syscall(stress_args_t *args)
 
 		/* We don't want bad ops clobbering this region */
 		stress_set_stack_smash_check_flag(false);
-		stress_shared_unmap();
+		stress_shared_readonly();
 		stress_process_dumpable(false);
 
 		/* Drop all capabilities */