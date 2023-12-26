ColinIanKing/stress-ng

commit 966de78608cdc76928cb8531660b044e0110139a

stress-acl: add ACL set vs get verification
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-acl.c
@@ -21,14 +21,18 @@
 #if defined(HAVE_SYS_ACL_H)
 #include <sys/acl.h>
 #endif
+#if defined(HAVE_ACL_LIBACL_H)
+#include <acl/libacl.h>
+#endif
 
 static const stress_help_t help[] = {
-	{ NULL,	"acl N",	"start N workers thrashing acl file mode bits " },
+	{ NULL,	"acl N",	"start N workers exercising valid acl file mode bits " },
 	{ NULL,	"acl-ops N",	"stop acl workers after N bogo operations" },
 	{ NULL,	NULL,		NULL }
 };
 
-#if defined(HAVE_LIB_ACL) &&	\
+#if defined(HAVE_LIB_ACL) &&		\
+    defined(HAVE_ACL_LIBACL_H) &&	\
     defined(HAVE_SYS_ACL_H)
 
 static acl_tag_t stress_acl_tags[] = {
@@ -76,6 +80,86 @@ static inline void stress_acl_delete_all(const char *filename)
 	if (acl_valid(acl) == 0)
 		acl_set_file(filename, ACL_TYPE_ACCESS, acl);
 	acl_free(acl);
+}
+
+/*
+ *  stress_acl_cmp()
+ *	naive acl comparison, assumes that acl_to_text generates
+ *	the same strings for identical acls
+ */
+static inline int stress_acl_cmp(acl_t acl1, acl_t acl2)
+{
+	char *acl_txt1, *acl_txt2;
+	ssize_t len1, len2;
+	int ret = -1;
+
+	acl_txt1 = acl_to_text(acl1, &len1);
+	if (!acl_txt1)
+		return 0;
+	acl_txt2 = acl_to_text(acl2, &len2);
+	if (!acl_txt2) {
+		acl_free((void *)acl_txt1);
+		return 0;
+	}
+	if (len1 == len2)
+		ret = strcmp(acl_txt1, acl_txt2);
+
+	acl_free((void *)acl_txt2);
+	acl_free((void *)acl_txt1);
+
+	return ret;
+}
+
+/*
+ *  stress_acl_perms()
+ *	convert ACL permission bits to string
+ */
+static void stress_acl_perms(acl_t acl, char *str, const size_t str_len)
+{
+	int which = ACL_FIRST_ENTRY;
+
+	if (str_len < 18)
+		return;
+
+	/* user, group and other permission bits */
+	(void)snprintf(str, str_len, "u:--- g:--- o:---");
+
+	for (;;) {
+		acl_tag_t tag;
+		acl_entry_t entry;
+		acl_permset_t permset;
+		int index = 0;
+
+		if (acl_get_entry(acl, which, &entry) <= 0)
+			break;
+		which = ACL_NEXT_ENTRY;
+
+		if (acl_get_tag_type(entry, &tag) != 0)
+			continue;
+		if (acl_get_permset(entry, &permset) != 0)
+			continue;
+
+		switch (tag) {
+		case ACL_USER:
+			index = 2;
+			break;
+		case ACL_GROUP:
+			index = 8;
+			break;
+		case ACL_OTHER:
+			index = 14;
+			break;
+		default:
+			continue;
+		}
+
+		if (acl_get_perm(permset, ACL_READ))
+			str[index + 0] = 'r';
+		if (acl_get_perm(permset, ACL_WRITE))
+			str[index + 1] = 'w';
+		if (acl_get_perm(permset, ACL_EXECUTE))
+			str[index + 2] = 'x';
+	}
 }
 
 /*
@@ -208,7 +292,7 @@ static int stress_acl_exercise(
 {
 	size_t i = 0;
 
-	for (i = 0; i < acl_count; i++) {
+	for (i = 0; i < acl_count && stress_continue(args); i++) {
 		const double t1 = stress_time_now();
 
 		if (LIKELY(acl_set_file(filename, ACL_TYPE_ACCESS, acls[i]) == 0)) {
@@ -221,6 +305,15 @@ static int stress_acl_exercise(
 			if (acl) {
 				metrics[1].duration += (stress_time_now() - t2);
 				metrics[1].count += 1.0;
+				if (stress_acl_cmp(acls[i], acl)) {
+					char setacl[32], getacl[32];
+
+					stress_acl_perms(acls[i], setacl, sizeof(setacl));
+					stress_acl_perms(acl, getacl, sizeof(getacl));
+
+					pr_fail("%s: mismatch between set acl %s and get acl %s\n",
+						args->name, setacl, getacl);
+				}
 				acl_free(acl);
 			}
 
@@ -355,5 +448,6 @@ stressor_info_t stress_acl_info = {
 	.class = CLASS_FILESYSTEM | CLASS_OS,
 	.verify = VERIFY_ALWAYS,
 	.help = help
+	.unimplemented_reason = "build without libacl or acl/libacl.h or sys/acl.h";
 };
 #endif

Makefile.config
@@ -396,7 +396,7 @@ LIB_Z:
 
 .PHONY: headers
 headers: \
-	AIO_H ASM_CACHECTL_H ASM_LDT_H ASM_MTRR_H ASM_PRCTL_H ATTR_XATTR_H \
+	ACL_LIBACL_H AIO_H ASM_CACHECTL_H ASM_LDT_H ASM_MTRR_H ASM_PRCTL_H ATTR_XATTR_H \
 	BSD_STDLIB_H BSD_STRING_H BSD_SYS_TREE_H BSD_UNISTD_H BSD_WCHAR \
 	COMPLEX_H WCHAR CRYPT_H EGL_H EGL_EXT_H FEATURES_H FENV_H FLOAT_H \
 	GBM_H GLES2_H GMP_H GRP_H IFADDRS_H IMMINTRIN_H INTEL_IPSEC_MB_H JPEG_H \
@@ -429,6 +429,9 @@ headers: \
 	SYS_VFS_H SYS_VMMETER_H SYS_XATTR_H TERMIO_H TERMIOS_H NET_IF_H \
 	NETINET_IP_H NETINET_IP_ICMP_H NETINET_SCTP_H NETINET_TCP_H NETINET_UDP_H \
 	UCONTEXT_H USTAT_H UTIME_H UVM_UVM_EXTERN_H X86INTRIN_H XMMINTRIN_H XXHASH_H
+
+ACL_LIBACL_H:
+	$(call check_header,acl/libacl.h,HAVE_ACL_LIBACL_H)
 
 AIO_H:
 	$(call check_header,aio.h,HAVE_AIO_H)

test/test-libacl.c
@@ -17,12 +17,27 @@
  *
  */
 #include <sys/acl.h>
+#include <acl/libacl.h>
 
 /* The following functions from libacl are used by stress-ng */
 
 static void *aio_funcs[] = {
-	acl_init,
+	acl_add_perm,
+	acl_calc_mask,
+	acl_clear_perms,
+	acl_create_entry,
+	acl_delete_entry,
 	acl_free,
+	acl_get_entry,
+	acl_get_file,
+	acl_get_perm,
+	acl_get_permset,
+	acl_get_tag_type,
+	acl_init,
+	acl_set_file,
+	acl_set_permset,
+	acl_set_qualifier,
+	acl_valid,
 };
 
 int main(void)

commit e5432a64dcd0339cefcfbff9d72d57ca9caab6e5

stress-acl: Fix stress_acl_info definition
Currently, stress-acl.c doesn't compile due to the missing comma and the
unneeded semicolon.

Fixes: 966de78608cd ("stress-acl: add ACL set vs get verification")
Signed-off-by: Munehisa Kamata <kamatam@amazon.com>

stress-acl.c
@@ -496,7 +496,7 @@ stressor_info_t stress_acl_info = {
 	.class = CLASS_FILESYSTEM | CLASS_OS,
 	.opt_set_funcs = opt_set_funcs,
 	.verify = VERIFY_ALWAYS,
-	.help = help
-	.unimplemented_reason = "build without libacl or acl/libacl.h or sys/acl.h";
+	.help = help,
+	.unimplemented_reason = "build without libacl or acl/libacl.h or sys/acl.h"
 };
 #endif