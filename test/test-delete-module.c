81602fd86bebd367de19f699f6c8895d43761e70
Colin Ian King <colin.i.king@gmail.com>
2024-01-01 21:34:28 +0000

    Update copyright year to 2024
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- test/test-delete-module.c
+++ test/test-delete-module.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Luis Chamberlain <mcgrof@kernel.org>
+ * Copyright (C) 2024 Luis Chamberlain <mcgrof@kernel.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
