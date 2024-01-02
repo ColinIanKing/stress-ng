8d63e57ec30d24b8c5194e488a52294846fe8e95
Colin Ian King <colin.i.king@gmail.com>
2024-01-02 14:16:37 +0000

    stress-metamix: ignore EBADF on fdatasync on read-only file
    Some systems allow fdatasync on readonly files, others don't. So
    don't throw an error if EBADF is returned.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-metamix.c
+++ stress-metamix.c
@@ -158,7 +158,7 @@ static int stress_metamix_file(
 	}
 	if (stress_mwc8() > 240) {
 		if (shim_fdatasync(fd) < 0) {
-			if ((errno != EINTR) && (errno != ENOSYS)) {
+			if ((errno != EINTR) && (errno != ENOSYS) && (errno != EBADF)) {
 				pr_inf("%s: fdatasync on %s failed, errno=%d (%s)%s\n",
 					args->name, filename, errno, strerror(errno), fs_type);
 				rc = EXIT_FAILURE;
