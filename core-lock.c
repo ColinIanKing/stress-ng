ColinIanKing/stress-ng

commit d32613839d3b69c0a1291353709f18bc6bb58a4e

core-lock: add pause/yield in spinlock for architectures other than x86
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-lock.c
@@ -17,6 +17,7 @@
  *
  */
 #include "stress-ng.h"
+#include "core-asm-arm.h"
 #include "core-asm-x86.h"
 #include "core-builtin.h"
 #include "core-pthread.h"
@@ -154,6 +155,14 @@ static int stress_atomic_lock_acquire(stress_lock_t *lock)
 		while (test_and_set(&lock->u.flag) == true) {
 #if defined(HAVE_ASM_X86_PAUSE)
 			stress_asm_x86_pause();
+#elif defined(HAVE_ASM_ARM_YIELD)
+			stress_asm_arm_yield();
+#elif defined(HAVE_ASM_LOONG64_DBAR)
+			stress_waitcpu_loong64_dbar();
+#elif defined(STRESS_ARCH_PPC64)
+			stress_asm_ppc64_yield();
+#elif defined(STRESS_ARCH_RISCV)
+			stress_asm_riscv_pause();
 #else
 			shim_sched_yield();
 #endif

commit f4ea9eb5edc894efbfc025b3eb40bd6fc2672e00

core-lock: include asm headers for arch specific pause/yield ops
Fixes: d32613839d3b ("core-lock: add pause/yield in spinlock for architectures other than x86")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-lock.c
@@ -18,6 +18,9 @@
  */
 #include "stress-ng.h"
 #include "core-asm-arm.h"
+#include "core-asm-ppc64.h"
+#include "core-asm-loong64.h"
+#include "core-asm-riscv.h"
 #include "core-asm-x86.h"
 #include "core-builtin.h"
 #include "core-pthread.h"