ColinIanKing/stress-ng

commit 6e62dc1fb48fc766c3267a6c9442d08d6e79345c

Makefile: force vnni to be built with at least -O2
Fix build issue with -O0, -O1, -Og being used with gcc-13 and vnni
stressor build failures. Use -O2 in these cases, or any -O option
greater than -O2 if it is provided.

Works around issue: https://github.com/ColinIanKing/stress-ng/issues/315

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

Makefile
@@ -26,11 +26,23 @@ KERNEL=$(shell uname -s)
 NODENAME=$(shell uname -n)
 
 override CFLAGS += -Wall -Wextra -DVERSION='"$(VERSION)"' -std=gnu99
+
+#
+#  Building stress-vnni with less than -O2 causes breakage with
+#  gcc-13.2, so remove then and ensure at least -O2 is used or
+#  honour flags > -O2 if they are provided
+#
+VNNI_OFLAGS_REMOVE=-O0 -O1 -Og
+VNNI_CFLAGS += $(filter-out $(VNNI_OFLAGS_REMOVE),$(CFLAGS))
+
 #
 # Default -O2 if optimization level not defined
 #
 ifeq "$(findstring -O,$(CFLAGS))" ""
 	override CFLAGS += -O2
+endif
+ifeq "$(findstring -O,$(VNNI_CFLAGS))" ""
+	override VNNI_CFLAGS += -O2
 endif
 
 #
@@ -128,6 +140,7 @@ BINDIR=/usr/bin
 MANDIR=/usr/share/man/man1
 JOBDIR=/usr/share/stress-ng/example-jobs
 BASHDIR=/usr/share/bash-completion/completions
+
 
 #
 # Header files
@@ -596,6 +609,10 @@ all: config.h stress-ng
 %.o: %.c $(HEADERS) $(HEADERS_GEN)
 	$(PRE_Q)echo "CC $<"
 	$(PRE_V)$(CC) $(CFLAGS) -c -o $@ $<
+
+stress-vnni.o: stress-vnni.c $(HEADERS) $(HEADERS_GEN)
+	$(PRE_Q)echo "CC $<"
+	$(PRE_V)$(CC) $(VNNI_CFLAGS) -c -o $@ $<
 
 #
 #  Use CC for linking if eigen is not being used, otherwise use CXX

commit d41d66483075838bd8c97c8ceb6bcde307f3e760

stress-vnni: force -O2 at -Os/-Oz as well
stress-vnni fails to compile with -Os in much the same way as #315.
Extend the override introduced in 6e62dc1 ("Makefile: force vnni to be
built with at least -O2") to cover -Os & -Oz as well.

Fixes: 6e62dc1 ("Makefile: force vnni to be built with at least -O2")

Makefile
@@ -61,7 +61,7 @@ override CFLAGS += -Wall -Wextra -DVERSION='"$(VERSION)"' -std=gnu99
 #  gcc-13.2, so remove then and ensure at least -O2 is used or
 #  honour flags > -O2 if they are provided
 #
-VNNI_OFLAGS_REMOVE=-O0 -O1 -Og
+VNNI_OFLAGS_REMOVE=-O0 -O1 -Os -Oz -Og
 VNNI_CFLAGS += $(filter-out $(VNNI_OFLAGS_REMOVE),$(CFLAGS))
 
 #