583fd6fedadcdfd58c1dcd6b5bdd195cc4e2154b
Colin Ian King <colin.i.king@gmail.com>
2024-05-29 13:00:16 +0100

    stress-expmath: rename variable index to avoid name shadowing with lib function index
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-expmath.c
+++ stress-expmath.c
@@ -413,18 +413,18 @@ static int stress_set_expmath_method(const char *opt)
 	return -1;
 }
 
-static bool stress_expmath_exercise(stress_args_t *args, const size_t index)
+static bool stress_expmath_exercise(stress_args_t *args, const size_t idx)
 {
 	bool ret;
 	const double t = stress_time_now();
 
-	ret = stress_expmath_methods[index].expmath_func(args);
-	stress_expmath_metrics[index].duration += (stress_time_now() - t);
-	stress_expmath_metrics[index].count += 1.0;
+	ret = stress_expmath_methods[idx].expmath_func(args);
+	stress_expmath_metrics[idx].duration += (stress_time_now() - t);
+	stress_expmath_metrics[idx].count += 1.0;
 	if (ret) {
-		if (index != 0)
+		if (idx != 0)
 			pr_fail("expmath: %s does not match expected result\n",
-				stress_expmath_methods[index].name);
+				stress_expmath_methods[idx].name);
 	}
 	return ret;
 }
