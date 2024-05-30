1af186230cb777af44b96e4369db495506773beb
Hsieh-Tseng Shen <woodrow.shen@sifive.com>
2024-05-30 03:43:22 -0700

    core-asm-riscv.h: support riscv zicboz ext cbo.zero instruction
    Zicboz is part of RISC-V CMO extension to support cache block based
    instructions to manipulate clean, flush, inval, and zero etc. So far
    only cbo.zero can be used for userspace, and this should provide a basic
    capability to DCache.
    
    Signed-off-by: Hsieh-Tseng Shen <woodrow.shen@sifive.com>

--- core-asm-riscv.h
+++ core-asm-riscv.h
@@ -23,6 +23,38 @@
 #include "core-arch.h"
 
 #if defined(STRESS_ARCH_RISCV)
+#define STRESS_ZICBOZ_CBO_ZERO	(4)
+#define STRESS_ZICBOZ_RS1	(10)
+#define STRESS_ZICBOZ_FUNCT3	(2)
+#define STRESS_ZICBOZ_OPCODE	(15)
+
+#if defined(__NR_riscv_hwprobe)
+#include <asm/hwprobe.h>
+#endif
+
+#if defined(__BYTE_ORDER__) &&	\
+    defined(__ORDER_BIG_ENDIAN__)
+#if __BYTE_ORDER__  == __ORDER_BIG_ENDIAN__
+#define __bswap32(x) ((uint32_t)__builtin_bswap32(x))
+#else
+#define __bswap32(x) (x)
+#endif
+#endif
+
+#define MK_CBO(op) __bswap32((uint32_t)(op) << 20 |                             \
+                          STRESS_ZICBOZ_RS1 << 15 |                             \
+                       STRESS_ZICBOZ_FUNCT3 << 12 |                             \
+                                           0 << 7 |                             \
+                             STRESS_ZICBOZ_OPCODE )
+
+#define CBO_INSN(base, op)                                                      \
+({                                                                              \
+        __asm__ __volatile__(                                                   \
+        "mv     a0, %0\n"                                                       \
+        "li     a1, %1\n"                                                       \
+        ".4byte %2\n"                                                           \
+        : : "r" (base), "i" (op), "i" (MK_CBO(op)) : "a0", "a1", "memory");     \
+})
 
 static inline uint64_t ALWAYS_INLINE stress_asm_riscv_rdtime(void)
 {
@@ -56,6 +88,18 @@ static inline void ALWAYS_INLINE stress_asm_riscv_pause(void)
 	/* pause is encoded as a fence instruction with pred=W, succ=0, and fm=0 */
 	__asm__ __volatile__ (".4byte 0x100000F");
 }
+
+/* cbo.zero instrution */
+#if defined(HAVE_ASM_RISCV_CBO_ZERO)
+static inline void ALWAYS_INLINE stress_asm_riscv_cbo_zero(char *addr)
+{
+        __asm__ __volatile__(
+        "mv     a0, %0\n"
+        "li     a1, %1\n"
+        ".4byte %2\n"
+        : : "r" (addr), "i" (STRESS_ZICBOZ_CBO_ZERO), "i" (MK_CBO(STRESS_ZICBOZ_CBO_ZERO)) : "a0", "a1", "memory");
+}
+#endif
 
 /* #if defined(STRESS_ARCH_RISCV) */
 #endif
