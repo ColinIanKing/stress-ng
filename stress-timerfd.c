9e3120d7199dde295f49549fc159e8bbffb04a48
Colin Ian King <colin.i.king@gmail.com>
2024-06-27 13:36:42 +0100

    stress-timerfd: configure timer after sync start has completed
    The timer should only be started once all timer stressors are
    ready, otherwise we hammer the system with timer events and not
    handle them.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-timerfd.c
+++ stress-timerfd.c
@@ -249,6 +249,9 @@ static int stress_timerfd(stress_args_t *args)
 	}
 #endif
 
+	stress_set_proc_state(args->name, STRESS_STATE_RUN);
+	stress_sync_start_wait(args);
+
 #if defined(CLOCK_REALTIME)
 	/* Exercise timerfd_create with invalid flags */
 	ret = timerfd_create(CLOCK_REALTIME, ~0);
@@ -274,9 +277,6 @@ static int stress_timerfd(stress_args_t *args)
 			goto free_pollfds;
 		}
 	}
-
-	stress_set_proc_state(args->name, STRESS_STATE_RUN);
-	stress_sync_start_wait(args);
 
 	do {
 		uint64_t expval;
