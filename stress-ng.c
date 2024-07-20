a33496b71e27b870983194f7dc04dc0e661a4a7c
Colin Ian King <colin.i.king@gmail.com>
2024-07-20 13:51:49 +0100

    stress-ng: stress_exclude_unsupported: add check for null stressor->info
    Move the check out of two if statements to a level higher
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-ng.c
+++ stress-ng.c
@@ -2782,28 +2782,30 @@ static inline void stress_exclude_unsupported(bool *unsupported)
 		stress_stressor_t *ss;
 		char munged[64];
 
-		if (stressor->info && stressor->info->supported) {
-			for (ss = stressors_head; ss; ss = ss->next) {
-				if (!ss->ignore.run) {
-					(void)stress_munge_underscore(munged, ss->stressor->name, sizeof(munged));
+		if (stressor->info) {
+			if (stressor->info->supported) {
+				for (ss = stressors_head; ss; ss = ss->next) {
+					if (!ss->ignore.run) {
+						(void)stress_munge_underscore(munged, ss->stressor->name, sizeof(munged));
 
-					if ((ss->stressor == stressor) && ss->num_instances &&
-					    (stressor->info->supported(munged) < 0)) {
-						stress_ignore_stressor(ss, STRESS_STRESSOR_UNSUPPORTED);
-						*unsupported = true;
+						if ((ss->stressor == stressor) && ss->num_instances &&
+						    (stressor->info->supported(munged) < 0)) {
+							stress_ignore_stressor(ss, STRESS_STRESSOR_UNSUPPORTED);
+							*unsupported = true;
+						}
 					}
 				}
 			}
-		}
-		if (stressors->info && (stressor->info->stressor == stress_unimplemented)) {
-			for (ss = stressors_head; ss; ss = ss->next) {
-				if (!ss->ignore.run) {
-					(void)stress_munge_underscore(munged, ss->stressor->name, sizeof(munged));
+			if (stressor->info->stressor == stress_unimplemented) {
+				for (ss = stressors_head; ss; ss = ss->next) {
+					if (!ss->ignore.run) {
+						(void)stress_munge_underscore(munged, ss->stressor->name, sizeof(munged));
 
-					if ((ss->stressor == stressor) && ss->num_instances) {
-						stress_exclude_unimplemented(munged, stressor->info);
-						stress_ignore_stressor(ss, STRESS_STRESSOR_UNSUPPORTED);
-						*unsupported = true;
+						if ((ss->stressor == stressor) && ss->num_instances) {
+							stress_exclude_unimplemented(munged, stressor->info);
+							stress_ignore_stressor(ss, STRESS_STRESSOR_UNSUPPORTED);
+							*unsupported = true;
+						}
 					}
 				}
 			}
