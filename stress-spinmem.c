4b9d3757d350c4a2693d6b21a86c87885ffa5d4c
Colin Ian King <colin.i.king@gmail.com>
2024-07-23 16:34:13 +0100

    stress-spinmem: add CLASS_CPU_CACHE to class flags
    This hammers CPU d-cache so add this flag
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-spinmem.c
+++ stress-spinmem.c
@@ -202,7 +202,7 @@ static const stress_opt_t opts[] = {
 
 stressor_info_t stress_spinmem_info = {
 	.stressor = stress_spinmem,
-	.class = CLASS_CPU | CLASS_MEMORY,
+	.class = CLASS_CPU | CLASS_MEMORY | CLASS_CPU_CACHE,
 	.verify = VERIFY_ALWAYS,
 	.opts = opts,
 	.help = help
