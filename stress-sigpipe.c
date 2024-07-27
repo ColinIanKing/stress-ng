c34f7642c296e0b4824ece52c7a9d33ce84af567
Colin Ian King <colin.i.king@gmail.com>
2024-07-27 14:23:01 +0100

    stress-sigpipe: remove need for a parent/child mechanism
    We can generate SIGPIPE by closing the reader end and writing a single
    byte. The stressor can be simplified by removing the need for large
    pipe I/O buffers, pipe size setting and parent/child writer/reader
    processes.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-sigpipe.c
+++ stress-sigpipe.c
@@ -22,7 +22,8 @@
 
 #include <sched.h>
 
-static stress_args_t *s_args;
+uint64_t sigpipe_max_count;
+volatile uint64_t sigpipe_count;
 
 static const stress_help_t help[] = {
 	{ NULL,	"sigpipe N",	 "start N workers exercising SIGPIPE" },
@@ -30,122 +31,18 @@ static const stress_help_t help[] = {
 	{ NULL,	NULL,		 NULL }
 };
 
-#define CLONE_STACK_SIZE	(16 * 1024)
-
 static void stress_sigpipe_handler(int signum)
 {
-	(void)signum;
-
-	if (LIKELY(s_args != NULL))
-		stress_bogo_inc(s_args);
+	if (LIKELY(signum == SIGPIPE))
+		sigpipe_count++;
 }
 
-#if defined(HAVE_CLONE)
-static inline void NORETURN no_return_pipe_child(void *ptr)
+static void stress_sigpipe_handler_count_check(int signum)
 {
-	int *pipefds = (int *)ptr;
-
-	(void)close(pipefds[0]);	/* causes SIGPIPE */
-	(void)close(pipefds[1]);
-	_exit(EXIT_SUCCESS);
-}
-
-static int pipe_child(void *ptr)
-{
-	no_return_pipe_child(ptr);
-	return -1;
-}
-#endif
-
-static inline int stress_sigpipe_write(
-	stress_args_t *args,
-	const char *buf,
-	const size_t buf_len,
-	uint64_t *pipe_count,
-	uint64_t *epipe_count)
-{
-	pid_t pid;
-	int pipefds[2];
-
-	if (UNLIKELY(pipe(pipefds) < 0)) {
-		pr_fail("%s: pipe failed, errno=%d (%s)\n",
-			args->name, errno, strerror(errno));
-		return EXIT_FAILURE;
-	}
-	(*pipe_count)++;
-
-#if defined(F_SETPIPE_SZ)
-	/*
-	 *  Try to limit pipe max size to 1 page
-	 *  and hence writes more than this will
-	 *  block and fail with SIGPIPE when
-	 *  the child exits and closes the pipe
-	 */
-	(void)fcntl(pipefds[1], F_SETPIPE_SZ, args->page_size);
-#endif
-
-again:
-#if defined(HAVE_CLONE)
-	{
-		static bool clone_enosys = false;
-
-		if (UNLIKELY(clone_enosys)) {
-			pid = fork();
-		} else {
-			static char stack[CLONE_STACK_SIZE];
-			char *stack_top = (char *)stress_get_stack_top((void *)stack, CLONE_STACK_SIZE);
-
-			pid = clone(pipe_child, stress_align_stack(stack_top),
-				CLONE_VM | CLONE_FS | CLONE_SIGHAND | SIGCHLD, pipefds);
-			/*
-			 *  May not have clone, so fall back to fork instead.
-			 */
-			if (UNLIKELY((pid < 0) && (errno == ENOSYS))) {
-				clone_enosys = true;
-				goto again;
-			}
-		}
-	}
-#else
-	pid = fork();
-#endif
-	if (UNLIKELY(pid < 0)) {
-		if (stress_redo_fork(args, errno))
-			goto again;
-
-		(void)close(pipefds[0]);
-		(void)close(pipefds[1]);
-		if (!stress_continue(args))
-			goto finish;
-		pr_err("%s: fork failed, errno=%d (%s)\n",
-			args->name, errno, strerror(errno));
-		return EXIT_FAILURE;
-	} else if (pid == 0) {
-		/* Child, only for non-clone path */
-		(void)close(pipefds[0]);	/* causes SIGPIPE */
-		(void)close(pipefds[1]);
-		_exit(EXIT_SUCCESS);
-	} else {
-		/* Parent */
-		(void)close(pipefds[0]);
-
-		do {
-			ssize_t ret;
-
-			/* cause SIGPIPE if pipe closed */
-			ret = write(pipefds[1], buf, buf_len);
-			if (LIKELY(ret <= 0)) {
-				if (errno == EPIPE)
-					(*epipe_count)++;
-				break;
-			}
-		} while (stress_continue(args));
-
-		(void)close(pipefds[1]);
-		(void)stress_kill_pid_wait(pid, NULL);
-	}
-finish:
-	return EXIT_SUCCESS;
+	if (LIKELY(signum == SIGPIPE))
+		sigpipe_count++;
+	if (sigpipe_count >= sigpipe_max_count)
+		stress_continue_set_flag(false);
 }
 
 /*
@@ -154,46 +51,51 @@ finish:
  */
 static int stress_sigpipe(stress_args_t *args)
 {
-	const size_t buf_size = args->page_size * 2;
-	char *buf;
-	uint64_t pipe_count = 0, epipe_count = 0;
+	uint64_t epipe_count = 0;
 	int rc = EXIT_SUCCESS;
+	int pipefds[2];
+	sigpipe_count = 0;
+	sigpipe_max_count = args->max_ops;
 
-	buf = (char *)stress_mmap_populate(NULL, buf_size,
-			PROT_READ | PROT_WRITE,
-			MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-	if (buf == MAP_FAILED) {
-		pr_inf_skip("%s: failed to allocate buffer of %zd bytes, skipping stressor\n",
-			args->name, buf_size);
+	if (stress_sighandler(args->name, SIGPIPE,
+		(args->max_ops == 0) ? stress_sigpipe_handler :
+				       stress_sigpipe_handler_count_check, NULL) < 0)
+		return EXIT_FAILURE;
+
+	if (UNLIKELY(pipe(pipefds) < 0)) {
+		pr_inf_skip("%s: pipe failed, errno=%d (%s), skipping stressor\n",
+			args->name, errno, strerror(errno));
 		return EXIT_NO_RESOURCE;
 	}
-	stress_set_vma_anon_name(buf, buf_size, "io-buffer");
-
-	s_args = args;
-	if (stress_sighandler(args->name, SIGPIPE, stress_sigpipe_handler, NULL) < 0) {
-		(void)munmap((void *)buf, buf_size);
-		return EXIT_FAILURE;
-	}
+	(void)close(pipefds[0]);
 
 	stress_set_proc_state(args->name, STRESS_STATE_SYNC_WAIT);
 	stress_sync_start_wait(args);
 	stress_set_proc_state(args->name, STRESS_STATE_RUN);
 
 	do {
-		stress_sigpipe_write(args, buf, buf_size, &pipe_count, &epipe_count);
-	} while (stress_continue(args));
+		char data;
+		register int ret;
 
+		/* cause SIGPIPE if pipe closed */
+		ret = write(pipefds[1], &data, sizeof(data));
+		if (LIKELY(ret <= 0)) {
+			if (errno == EPIPE)
+				epipe_count++;
+		}
+	} while (stress_continue_flag());
+
+	stress_bogo_set(args, sigpipe_count);
 	stress_set_proc_state(args->name, STRESS_STATE_DEINIT);
 
 	/* simple sanity check */
-	if ((pipe_count + epipe_count > 0) && (stress_bogo_get(args) < 1)) {
-		pr_fail("%s: %" PRIu64 " pipes closed and %" PRIu64 " EPIPE "
-			"writes occurred but got 0 SIGPIPE signals\n",
-			args->name, pipe_count, epipe_count);
+	if ((epipe_count > 0) && (sigpipe_count < 1)) {
+		pr_fail("%s: %" PRIu64 " writes occurred but got 0 SIGPIPE signals\n",
+			args->name, epipe_count);
 		rc = EXIT_FAILURE;
 	}
 
-	(void)munmap((void *)buf, buf_size);
+	(void)close(pipefds[1]);
 
 	return rc;
 }
