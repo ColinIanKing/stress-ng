ColinIanKing/stress-ng

commit 6022566efc3e582843f2a2484979a4aa4edb432b

stress-*: name mmap'd regions to help debugging
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-bad-altstack.c
@@ -334,6 +334,7 @@ static int stress_bad_altstack(stress_args_t *args)
 			args->name, (size_t)stress_minsigstksz, errno, strerror(errno));
 		return EXIT_NO_RESOURCE;
 	}
+	stress_set_vma_anon_name(stack, stress_minsigstksz, "altstack");
 	(void)stress_madvise_mergeable(stack, stress_minsigstksz);
 
 #if defined(O_TMPFILE)
@@ -346,19 +347,23 @@ static int stress_bad_altstack(stress_args_t *args)
 				PROT_READ | PROT_WRITE,
 				MAP_PRIVATE | map_stackflags, tmp_fd, 0);
 		(void)close(tmp_fd);
+		if (bus_stack != MAP_FAILED)
+			stress_set_vma_anon_name(bus_stack, stress_minsigstksz, "altstack-bus-errors");
 	}
 #else
 	UNEXPECTED
 #endif
 
 	fd = open("/dev/zero", O_RDONLY);
-	if (fd >= 0) {
+	if (fd < 0) {
+		zero_stack = MAP_FAILED;
+	} else {
 		zero_stack = stress_mmap_populate(NULL,
 			stress_minsigstksz, PROT_READ,
 			MAP_PRIVATE, fd, 0);
 		(void)close(fd);
-	} else {
-		zero_stack = MAP_FAILED;
+		if (zero_stack != MAP_FAILED)
+			stress_set_vma_anon_name(bus_stack, stress_minsigstksz, "altstack-zero-ro");
 	}
 
 	/*

commit 13dde83faeceecf2d5f7cbf6b8a164294524ff9a

stress-bad-alt-stack: use zero_stack instead of bad_stack
Used incorrect stack, fix this.

Fixes: 6022566efc3e ("stress-*: name mmap'd regions to help debugging")
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-bad-altstack.c
@@ -363,7 +363,7 @@ static int stress_bad_altstack(stress_args_t *args)
 			MAP_PRIVATE, fd, 0);
 		(void)close(fd);
 		if (zero_stack != MAP_FAILED)
-			stress_set_vma_anon_name(bus_stack, stress_minsigstksz, "altstack-zero-ro");
+			stress_set_vma_anon_name(zero_stack, stress_minsigstksz, "altstack-zero-ro");
 	}
 
 	/*