ColinIanKing/stress-ng

commit f03849c228a94f4ec50a9eb1a0c4640471c53fb1

stress-*: constify stressor_info_t structs
These structs are read-only, so constify them

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-mlockmany.c
@@ -1,4 +1,4 @@
-/*
+const /*
  * Copyright (C) 2013-2021 Canonical, Ltd.
  * Copyright (C) 2022-2024 Colin Ian King.
  *
@@ -249,7 +249,7 @@ unmap:
 	return EXIT_SUCCESS;
 }
 
-stressor_info_t stress_mlockmany_info = {
+const stressor_info_t stress_mlockmany_info = {
 	.stressor = stress_mlockmany,
 	.class = CLASS_VM | CLASS_OS | CLASS_PATHOLOGICAL,
 	.opts = opts,
@@ -258,7 +258,7 @@ stressor_info_t stress_mlockmany_info = {
 
 #else
 
-stressor_info_t stress_mlockmany_info = {
+const stressor_info_t stress_mlockmany_info = {
 	.stressor = stress_unimplemented,
 	.class = CLASS_VM | CLASS_OS | CLASS_PATHOLOGICAL,
 	.opts = opts,

commit a187cc2166be860d5f072c02d2c394fd1b7ff16c

stress-mlockmany: remove stray const in start of source
Fixes: f03849c228a9 ("stress-*: constify stressor_info_t structs")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-mlockmany.c
@@ -1,4 +1,4 @@
-const /*
+/*
  * Copyright (C) 2013-2021 Canonical, Ltd.
  * Copyright (C) 2022-2024 Colin Ian King.
  *