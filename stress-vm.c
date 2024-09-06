ColinIanKing/stress-ng

commit bca728b8f636935d5e13151b0886eadcd679d9a1

stress-vm: provide more information when mmap fails
It's useful to provide the user more information why mmap fails, for
example in LP#2078871 it would be useful to know how much memory is
being mapped on failure and what the errno is.

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-vm.c
@@ -3268,17 +3268,12 @@ static int stress_vm_child(stress_args_t *args, void *ctxt)
 		advice = vm_madvise_info[vm_madvise].advice;
 
 	do {
-		if (no_mem_retries >= NO_MEM_RETRIES_MAX) {
-			pr_inf_skip("%s: gave up trying to mmap, no available memory, skipping stressor\n",
-				args->name);
-			rc = EXIT_NO_RESOURCE;
-			break;
-		}
 		if (!vm_keep || (buf == NULL)) {
 			if (!stress_continue_flag())
 				return EXIT_SUCCESS;
-			if ((g_opt_flags & OPT_FLAGS_OOM_AVOID) && stress_low_memory(buf_sz)) {
+			if (1 | ((g_opt_flags & OPT_FLAGS_OOM_AVOID) && stress_low_memory(buf_sz))) {
 				buf = MAP_FAILED;
+				errno = ENOMEM;
 			} else {
 #if defined(HAVE_MPROTECT) &&	\
     defined(PROT_NONE)
@@ -3298,6 +3293,17 @@ static int stress_vm_child(stress_args_t *args, void *ctxt)
 			if (buf == MAP_FAILED) {
 				buf = NULL;
 				no_mem_retries++;
+				if (no_mem_retries >= NO_MEM_RETRIES_MAX) {
+					char str[32];
+
+					(void)stress_uint64_to_str(str, sizeof(str), (uint64_t)buf_sz);
+					pr_inf_skip("%s: gave up trying to mmap %s after %d attempts, "
+						"errno=%d (%s), skipping stressor\n",
+						args->name, str, NO_MEM_RETRIES_MAX,
+						errno, strerror(errno));
+					rc = EXIT_NO_RESOURCE;
+					break;
+				}
 				(void)shim_usleep(100000);
 				continue;	/* Try again */
 			}

commit 8cc021b51b2e6ff3d7c7c670c7c42aa4e58dfe87

stress-vm: remove debugging setting if in statement
Remove 1 | in if statement, accidentially left in.

Fixes: bca728b8f636 ("stress-vm: provide more information when mmap fails)
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-vm.c
@@ -3271,7 +3271,7 @@ static int stress_vm_child(stress_args_t *args, void *ctxt)
 		if (!vm_keep || (buf == NULL)) {
 			if (!stress_continue_flag())
 				return EXIT_SUCCESS;
-			if (1 | ((g_opt_flags & OPT_FLAGS_OOM_AVOID) && stress_low_memory(buf_sz))) {
+			if (((g_opt_flags & OPT_FLAGS_OOM_AVOID) && stress_low_memory(buf_sz))) {
 				buf = MAP_FAILED;
 				errno = ENOMEM;
 			} else {