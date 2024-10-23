c80bf3d957efd2be89cc00681fd1c6f6d8d07095
Colin Ian King <colin.i.king@gmail.com>
2024-10-23 23:13:30 +0100

    stress-plugin: add anonymous mapped name for signal counter data
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-plugin.c
+++ stress-plugin.c
@@ -285,6 +285,7 @@ static int stress_plugin(stress_args_t *args)
 		(void)dlclose(stress_plugin_so_dl);
 		return EXIT_NO_RESOURCE;
 	}
+	stress_set_vma_anon_name(sig_count, sig_count_size, "signal-counters");
 
 	func = stress_plugin_methods[plugin_method].func;
 	if (args->instance == 0)
