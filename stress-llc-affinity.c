d9dd024b8b3c8db4741798351fadf66d43210d03
Colin Ian King <colin.i.king@gmail.com>
2024-10-28 11:31:55 +0000

    stress-llc-affinity: fix resource leak on cpus array
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-llc-affinity.c
+++ stress-llc-affinity.c
@@ -173,6 +173,7 @@ static int stress_llc_affinity(stress_args_t *args)
 	if (llc_size == 0) {
 		pr_inf_skip("%s: cannot determine cache details, skipping stressor\n",
 			args->name);
+		stress_free_usable_cpus(&cpus);
 		return EXIT_NO_RESOURCE;
 	}
         llc_size *= numa_nodes;
@@ -195,6 +196,7 @@ static int stress_llc_affinity(stress_args_t *args)
 		MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 	if (buf == MAP_FAILED) {
 		pr_fail("%s: mmap'd region of %zu bytes failed\n", args->name, mmap_sz);
+		stress_free_usable_cpus(&cpus);
 		return EXIT_NO_RESOURCE;
 	}
 	stress_set_vma_anon_name(buf, mmap_sz, "llc-buffer");
