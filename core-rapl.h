aeb16fc692a65fc790b72982c0fcb53a9221241d
Colin Ian King <colin.i.king@gmail.com>
2024-12-07 19:23:28 +0000

    core-rapl: remove NONNULL attributes
    Args may be null, so don't add the NONNULL atrribute to rapl API.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- core-rapl.h
+++ core-rapl.h
@@ -55,12 +55,11 @@ typedef struct {
         double power_watts[STRESS_RAPL_DOMAINS_MAX];
 } stress_rapl_t;
 
-extern void stress_rapl_free_domains(stress_rapl_domain_t *rapl_domains) NONNULL(1);
-extern int stress_rapl_get_domains(stress_rapl_domain_t **rapl_domains) NONNULL(1);
-extern int stress_rapl_get_power_raplstat(stress_rapl_domain_t *rapl_domains) NONNULL(1);
-extern int stress_rapl_get_power_stressor(stress_rapl_domain_t *rapl_domains, stress_rapl_t *rapl) NONNULL(1);
-extern void stress_rapl_dump(FILE *yaml, stress_stressor_t *stressors_list, stress_rapl_domain_t *rapl_domains)
-	NONNULL(2, 3);
+extern void stress_rapl_free_domains(stress_rapl_domain_t *rapl_domains);
+extern int stress_rapl_get_domains(stress_rapl_domain_t **rapl_domains);
+extern int stress_rapl_get_power_raplstat(stress_rapl_domain_t *rapl_domains);
+extern int stress_rapl_get_power_stressor(stress_rapl_domain_t *rapl_domains, stress_rapl_t *rapl);
+extern void stress_rapl_dump(FILE *yaml, stress_stressor_t *stressors_list, stress_rapl_domain_t *rapl_domains);
 #endif
 
 #endif
