dcfa203fe217845fe4469979457cd680103c6ff2
Colin Ian King <colin.i.king@gmail.com>
2025-01-28 09:45:44 +0000

    stress-mprotect: add some UNLIKELY branch hints
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-mprotect.c
+++ stress-mprotect.c
@@ -118,12 +118,12 @@ static int stress_mprotect_mem(
 		for (i = 0; (i < 10) && stress_continue(args); i++) {
 			const int j = stress_mwc16modn(n_flags);
 
-			if (mprotect((void *)ptr, size, prot_flags[j]) == 0) {
+			if (LIKELY(mprotect((void *)ptr, size, prot_flags[j]) == 0)) {
 				stress_bogo_inc(args);
 
 #if defined(PROT_READ) &&	\
     defined(PROT_WRITE)
-				if ((prot_flags[j] & (PROT_READ | PROT_WRITE)) == 0) {
+				if (UNLIKELY((prot_flags[j] & (PROT_READ | PROT_WRITE)) == 0)) {
 					char str[128];
 
 					stress_uint8_put(*ptr);
@@ -138,7 +138,7 @@ static int stress_mprotect_mem(
 #endif
 #if defined(PROT_WRITE)
 				/* not writeable, should not get here */
-				if ((prot_flags[j] & PROT_WRITE) == 0) {
+				if (UNLIKELY((prot_flags[j] & PROT_WRITE) == 0)) {
 					char str[128];
 
 					*ptr = 1;
