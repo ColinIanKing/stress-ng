138d264b73449b86790f69985e50e2224b630b17
Colin Ian King <colin.i.king@gmail.com>
2025-02-06 14:02:08 +0000

    stress-fstat: constify pointer d
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-fstat.c
+++ stress-fstat.c
@@ -300,7 +300,7 @@ static int stress_fstat(stress_args_t *args)
 {
 	stress_stat_info_t *si;
 	static stress_stat_info_t *stat_info;
-	struct dirent *d;
+	const struct dirent *d;
 	NOCLOBBER int ret = EXIT_FAILURE;
 	bool stat_some;
 	const uid_t euid = geteuid();
