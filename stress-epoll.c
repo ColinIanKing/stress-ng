e436a22ea2360369fd0af941043e11d0264e93ac
Colin Ian King <colin.i.king@gmail.com>
2025-02-06 11:33:55 +0000

    stress-epoll: make pointer addr_un const
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-epoll.c
+++ stress-epoll.c
@@ -647,7 +647,7 @@ retry:
 #if defined(AF_UNIX) &&		\
     defined(HAVE_SOCKADDR_UN)
 	if (addr && (epoll_domain == AF_UNIX)) {
-		struct sockaddr_un *addr_un = (struct sockaddr_un *)addr;
+		const struct sockaddr_un *addr_un = (struct sockaddr_un *)addr;
 
 		(void)shim_unlink(addr_un->sun_path);
 	}
@@ -953,7 +953,7 @@ die:
 #if defined(AF_UNIX) &&		\
     defined(HAVE_SOCKADDR_UN)
 	if (addr && (epoll_domain == AF_UNIX)) {
-		struct sockaddr_un *addr_un = (struct sockaddr_un *)addr;
+		const struct sockaddr_un *addr_un = (struct sockaddr_un *)addr;
 
 		(void)shim_unlink(addr_un->sun_path);
 	}
