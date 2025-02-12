252db54aa7231ca576fd6b85954982372858e254
Colin Ian King <colin.i.king@gmail.com>
2025-02-12 10:51:20 +0000

    stress-lockmix: add LIKELY/UNLIKELY hints
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-lockmix.c
+++ stress-lockmix.c
@@ -441,7 +441,7 @@ static int stress_lockmix(stress_args_t *args)
 	(void)shim_memset(buffer, 0, sizeof(buffer));
 	for (offset = 0; offset < LOCK_FILE_SIZE; offset += sizeof(buffer)) {
 redo:
-		if (!stress_continue_flag()) {
+		if (UNLIKELY(!stress_continue_flag())) {
 			ret = EXIT_SUCCESS;
 			goto tidy;
 		}
@@ -494,7 +494,7 @@ again:
 	if (cpid < 0) {
 		if (stress_redo_fork(args, errno))
 			goto again;
-		if (!stress_continue(args))
+		if (UNLIKELY(!stress_continue(args)))
 			goto tidy;
 		pr_err("%s: fork failed, errno=%d (%s)\n",
 			args->name, errno, strerror(errno));
