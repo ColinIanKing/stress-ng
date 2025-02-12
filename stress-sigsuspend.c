0e80d5313358bf423944ce25812541d5a7fd70cb
Colin Ian King <colin.i.king@gmail.com>
2025-02-12 11:41:42 +0000

    stress-sigsuspend: add LIKELY/UNLIKELY hints
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-sigsuspend.c
+++ stress-sigsuspend.c
@@ -74,7 +74,7 @@ again:
 		if (pid[n] < 0) {
 			if (stress_redo_fork(args, errno))
 				goto again;
-			if (!stress_continue(args))
+			if (UNLIKELY(!stress_continue(args)))
 				goto reap;
 			pr_err("%s: fork failed, errno=%d (%s)\n",
 				args->name, errno, strerror(errno));
