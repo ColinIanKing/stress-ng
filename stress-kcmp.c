29dc2dd70b42debbc7edc0b5ca114f11a93df32e
Colin Ian King <colin.i.king@gmail.com>
2025-02-12 10:41:09 +0000

    stress-kcmp: add LIKELY/UNLIKELY hints
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-kcmp.c
+++ stress-kcmp.c
@@ -212,7 +212,7 @@ again:
 		if (stress_redo_fork(args, errno))
 			goto again;
 		(void)close(fd1);
-		if (!stress_continue(args))
+		if (UNLIKELY(!stress_continue(args)))
 			goto finish;
 		pr_fail("%s: fork failed, errno=%d (%s)\n",
 			args->name, errno, strerror(errno));
