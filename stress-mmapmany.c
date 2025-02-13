dac03816a290d121f6756e0ed12b3f4aaadb9683
Colin Ian King <colin.i.king@gmail.com>
2025-02-13 09:47:32 +0000

    stress-mmapmany: add LIKELY/UNLIKELY hints for stress_continue_flag
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-mmapmany.c
+++ stress-mmapmany.c
@@ -105,7 +105,7 @@ static int stress_mmapmany_child(stress_args_t *args, void *context)
 	do {
 		size_t i, n;
 
-		for (n = 0; stress_continue_flag() && (n < (size_t)max); n++) {
+		for (n = 0; LIKELY(stress_continue_flag() && (n < (size_t)max)); n++) {
 			uint64_t *ptr;
 
 			if (UNLIKELY(!stress_continue(args)))
