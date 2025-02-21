6e5aacd2f7ec48d706265fd032e37a04b2f3fcb7
Colin Ian King <colin.i.king@gmail.com>
2025-02-21 12:36:00 +0000

    test: add shm-open and shm-unlink build time config checks
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

---
+++ test/test-shm-open.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2025      Colin Ian King.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#define _GNU_SOURCE
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int shm_fd;
+
+	shm_fd = shm_open("shmtest", O_RDWR | O_CREAT, 0600);
+	if (shm_fd > -1)
+		(void)close(shm_fd);
+
+	return 0;
+}
