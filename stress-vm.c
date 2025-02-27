ColinIanKing/stress-ng

commit 4b25f1e1d7d5560647a74951fed86b8ee9889f01

stress-vm: free context.numa_mask on error return path
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-vm.c
@@ -3463,6 +3463,8 @@ static int stress_vm(stress_args_t *args)
 				"retry count=%zu, errno=%d (%s)\n",
 				args->name, retries, err, strerror(err));
 		}
+		if (context.numa_mask)
+			stress_numa_mask_free(context.numa_mask);
 		return EXIT_NO_RESOURCE;
 	}
 	stress_set_vma_anon_name(context.bit_error_count, page_size, "bit-error-count");

commit faedb5b6b78172e34da13716aa065d01943230ca

stress-vm: add HAVE_LINUX_MEMPOLICY_H check when using numa.mask
Need to check for HAVE_LINUX_MEMPOLICY_H

Fixes: 4b25f1e1d7d5 ("stress-vm: free context.numa_mask on error return path")
Closes: https://github.com/ColinIanKing/stress-ng/issues/482

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-vm.c
@@ -3463,8 +3463,10 @@ static int stress_vm(stress_args_t *args)
 				"retry count=%zu, errno=%d (%s)\n",
 				args->name, retries, err, strerror(err));
 		}
+#if defined(HAVE_LINUX_MEMPOLICY_H)
 		if (context.numa_mask)
 			stress_numa_mask_free(context.numa_mask);
+#endif
 		return EXIT_NO_RESOURCE;
 	}
 	stress_set_vma_anon_name(context.bit_error_count, page_size, "bit-error-count");