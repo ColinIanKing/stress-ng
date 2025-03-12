2fad20c4d3a4cda1219afcaf7f998b0bf4e5581c
Colin Ian King <colin.i.king@gmail.com>
2025-03-12 23:04:31 +0000

    core-heler: add stress_madvise_collapse to collapse pages to hugepages
    If possible, try to collapse to hugepages to improve performance
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- core-helper.h
+++ core-helper.h
@@ -197,5 +197,6 @@ extern uint64_t stress_get_machine_id(void);
 extern void stress_zero_metrics(stress_metrics_t *metrics, const size_t n);
 extern void stress_backtrace(void);
 extern bool OPTIMIZE3 stress_data_is_not_zero(uint64_t *buffer, const size_t len);
+extern void stress_madvise_collapse(void *addr, size_t len);
 
 #endif
