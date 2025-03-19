ColinIanKing/stress-ng

commit c73b66d5aa6af5d42bd722d0b2ee423f1fc5afd9

core-cpu-cache: use uint8_t for pointers to avoid void * pointer arithmetic
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-cpu-cache.c
@@ -1333,17 +1333,18 @@ free_cpu_caches:
  */
 void OPTIMIZE3 stress_cpu_data_cache_flush(void *addr, const size_t len)
 {
-	register void *addr_end = (void *)(((uintptr_t)addr) + len);
+	register uint8_t *ptr = (uint8_t *)addr;
+	register const uint8_t *ptr_end = ptr + len;
+
 #if defined(HAVE_ASM_X86_CLFLUSHOPT)
-	while (addr < addr_end) {
-		stress_asm_x86_clflushopt(addr);
-		addr += 64;
+	while (ptr < ptr_end) {
+		stress_asm_x86_clflushopt((void *)ptr);
+		ptr += 64;
 	}
 #elif defined(HAVE_ASM_X86_CLFLUSH)
-
-	while (addr < addr_end) {
-		stress_asm_x86_clflush(addr);
-		addr += 64;
+	while (ptr < ptr_end) {
+		stress_asm_x86_clflush((void *)ptr);
+		ptr += 64;
 	}
 #elif defined(HAVE_BUILTIN___CLEAR_CACHE)
 	__builtin___clear_cache(addr, addr_end);

commit b2ad9d1b7b59e17fa8715f02ce2b676ab9013afc

core-cpu-cache: fix build issue, use renamed variable
Fixes: c73b66d5aa6a ("core-cpu-cache: use uint8_t for pointers to avoid void * pointer arithmetic")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-cpu-cache.c
@@ -1347,9 +1347,9 @@ void OPTIMIZE3 stress_cpu_data_cache_flush(void *addr, const size_t len)
 		ptr += 64;
 	}
 #elif defined(HAVE_BUILTIN___CLEAR_CACHE)
-	__builtin___clear_cache(addr, addr_end);
+	__builtin___clear_cache(addr, (void *)ptr_end);
 #else
-	__builtin___clear_cache(addr, addr_end);
+	__builtin___clear_cache(addr, (void *)ptr_end);
 	shim_cacheflush(addr, len, SHIM_ICACHE);
 #endif
 }