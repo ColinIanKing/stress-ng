62fbe50bb048f3a6208233c2dd9715764687037f
Colin Ian King <colin.i.king@gmail.com>
2025-04-25 12:18:38 +0100

    core-interrupts.h: remove include of core-attribute.h
    This is no longer required, remove it.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- core-interrupts.h
+++ core-interrupts.h
@@ -19,8 +19,6 @@
 #ifndef CORE_INTERRUPTS_H
 #define CORE_INTERRUPTS_H
 
-#include "core-attribute.h"
-
 extern void stress_interrupts_start(stress_interrupts_t *counters);
 extern void stress_interrupts_stop(stress_interrupts_t *counters);
 extern void stress_interrupts_check_failure(const char *name,
