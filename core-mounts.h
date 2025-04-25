a987d977026987ad67073b96ec893a4367cb03de
Colin Ian King <colin.i.king@gmail.com>
2025-04-25 12:38:39 +0100

    core-mounts: remove include of core-attribute.h
    This is no longer required, remove it.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- core-mounts.h
+++ core-mounts.h
@@ -19,8 +19,6 @@
 #ifndef CORE_MOUNTS_H
 #define CORE_MOUNTS_H
 
-#include "core-attribute.h"
-
 extern void stress_mount_free(char *mnts[], const int n);
 extern int stress_mount_get(char *mnts[], const int max);
 
