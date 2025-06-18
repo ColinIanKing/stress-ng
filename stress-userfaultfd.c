aea6f4f3587d777f9f4559bf347b7b0ef13fa014
Colin Ian King <colin.i.king@gmail.com>
2025-06-18 10:53:31 +0100

    stress-userfaultfd: report how much memory is being used
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-userfaultfd.c
+++ stress-userfaultfd.c
@@ -256,7 +256,7 @@ static int stress_userfaultfd_child(stress_args_t *args, void *context)
 	bool do_poll = true;
 	static uint8_t stack[STACK_SIZE]; /* Child clone stack */
 	uint8_t *stack_top = (uint8_t *)stress_get_stack_top((void *)stack, STACK_SIZE);
-	size_t userfaultfd_bytes = DEFAULT_USERFAULT_BYTES;
+	size_t userfaultfd_bytes, userfaultfd_bytes_total = DEFAULT_USERFAULT_BYTES;
 	double t, duration = 0.0, rate;
 
 	if (stress_sigchld_set_handler(args) < 0)
@@ -265,17 +265,19 @@ static int stress_userfaultfd_child(stress_args_t *args, void *context)
 	(void)context;
 	(void)shim_memset(stack, 0, sizeof(stack));
 
-	if (!stress_get_setting("userfaultfd-bytes", &userfaultfd_bytes)) {
+	if (!stress_get_setting("userfaultfd-bytes", &userfaultfd_bytes_total)) {
 		if (g_opt_flags & OPT_FLAGS_MAXIMIZE)
-			userfaultfd_bytes = MAX_32;
+			userfaultfd_bytes_total = MAX_32;
 		if (g_opt_flags & OPT_FLAGS_MINIMIZE)
-			userfaultfd_bytes = MIN_USERFAULT_BYTES;
+			userfaultfd_bytes_total = MIN_USERFAULT_BYTES;
 	}
-	userfaultfd_bytes /= args->instances;
+	userfaultfd_bytes = userfaultfd_bytes_total / args->instances;
 	if (userfaultfd_bytes < MIN_USERFAULT_BYTES)
 		userfaultfd_bytes = MIN_USERFAULT_BYTES;
 	if (userfaultfd_bytes < args->page_size)
 		userfaultfd_bytes = args->page_size;
+	if (args->instance == 0)
+		stress_usage_bytes(args, userfaultfd_bytes, userfaultfd_bytes * args->instances);
 
 	sz = userfaultfd_bytes & ~(page_size - 1);
 
