9a69680771eb86f17c37e3df6543512247448301
Colin Ian King <colin.i.king@gmail.com>
2025-06-24 09:42:17 +0100

    core-arch: sort architectures in alphabetic order
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- core-arch.h
+++ core-arch.h
@@ -33,17 +33,12 @@
 #endif
 #endif
 
-/* Arch specific, x86-64 */
-#if defined(__x86_64__) || defined(__x86_64) || \
-    defined(__amd64__) || defined(__amd64)
-#define STRESS_ARCH_X86		(1)
-#define STRESS_ARCH_X86_64	(1)
-#endif
-
-/* Arch specific, x86-32 (i386 et al) */
-#if defined(__i386__)   || defined(__i386)
-#define STRESS_ARCH_X86		(1)
-#define STRESS_ARCH_X86_32	(1)
+/* Arch specific ALPHA */
+#if defined(__alpha) ||		\
+    defined(__alpha__)
+#define STRESS_ARCH_ALPHA	(1)
+#define STRESS_OPCODE_SIZE	(32)
+#define STRESS_OPCODE_MASK	(0xffffffffUL)
 #endif
 
 /* Arch specific, ARC64 */
@@ -66,78 +61,6 @@
 #define STRESS_OPCODE_MASK	(0xffffffffUL)
 #endif
 
-/* Arch specific RISC-V */
-#if defined(__riscv) || \
-    defined(__riscv__)
-#define STRESS_ARCH_RISCV	(1)
-#define STRESS_OPCODE_SIZE	(32)
-#define STRESS_OPCODE_MASK	(0xffffffffUL)
-#endif
-
-/* Arch specific, IBM S390 */
-#if defined(__s390__)
-#define STRESS_ARCH_S390	(1)
-#define STRESS_OPCODE_SIZE	(48)
-#define STRESS_OPCODE_MASK	(0xffffffffffffULL)
-#endif
-
-/* Arch specific LoongArch64 */
-#if defined(__loongarch64) ||	\
-    defined(__loongarch__)
-#define STRESS_ARCH_LOONG64	(1)
-#define STRESS_OPCODE_SIZE	(32)
-#define STRESS_OPCODE_MASK	(0xffffffffUL)
-#endif
-
-/* Arch specific PPC64 */
-#if defined(__PPC64__) || defined(__ppc64__)
-#define STRESS_ARCH_PPC64	(1)
-#define STRESS_OPCODE_SIZE	(32)
-#define STRESS_OPCODE_MASK	(0xffffffffUL)
-#endif
-
-/* Arch specific PPC (32 bit ) */
-#if (defined(__PPC__) || defined(__ppc__)) &&	\
-    !defined(STRESS_ARCH_PPC64)
-#define STRESS_ARCH_PPC		(1)
-#define STRESS_OPCODE_SIZE	(32)
-#define STRESS_OPCODE_MASK	(0xffffffffUL)
-#endif
-
-/* Arch specific M68K */
-#if defined(__m68k__) ||	\
-    defined(__mc68000__) ||	\
-    defined(__mc68010__) ||	\
-    defined(__mc68020__)
-#define STRESS_ARCH_M68K	(1)
-#define STRESS_OPCODE_SIZE	(16)
-#define STRESS_OPCODE_MASK	(0xffffUL)
-#endif
-
-/* Arch specific SPARC */
-#if defined(__sparc) ||		\
-    defined(__sparc__) ||	\
-    defined(__sparc_v9__)
-#define STRESS_ARCH_SPARC	(1)
-#define STRESS_OPCODE_SIZE	(32)
-#define STRESS_OPCODE_MASK	(0xffffffffUL)
-#endif
-
-/* Arch specific SH4 */
-#if defined(__SH4__)
-#define STRESS_ARCH_SH4		(1)
-#define STRESS_OPCODE_SIZE	(16)
-#define STRESS_OPCODE_MASK	(0xffffUL)
-#endif
-
-/* Arch specific ALPHA */
-#if defined(__alpha) ||		\
-    defined(__alpha__)
-#define STRESS_ARCH_ALPHA	(1)
-#define STRESS_OPCODE_SIZE	(32)
-#define STRESS_OPCODE_MASK	(0xffffffffUL)
-#endif
-
 /* Arch specific HPPA  */
 #if defined(__hppa) ||		\
     defined(__hppa__)
@@ -153,6 +76,24 @@
 #define STRESS_ARCH_KVX		(1)
 #endif
 
+/* Arch specific LoongArch64 */
+#if defined(__loongarch64) ||	\
+    defined(__loongarch__)
+#define STRESS_ARCH_LOONG64	(1)
+#define STRESS_OPCODE_SIZE	(32)
+#define STRESS_OPCODE_MASK	(0xffffffffUL)
+#endif
+
+/* Arch specific M68K */
+#if defined(__m68k__) ||	\
+    defined(__mc68000__) ||	\
+    defined(__mc68010__) ||	\
+    defined(__mc68020__)
+#define STRESS_ARCH_M68K	(1)
+#define STRESS_OPCODE_SIZE	(16)
+#define STRESS_OPCODE_MASK	(0xffffUL)
+#endif
+
 /* Arch specific MIPS */
 #if defined(__mips) || 		\
     defined(__mips__) ||	\
@@ -160,6 +101,65 @@
 #define STRESS_ARCH_MIPS	(1)
 #define STRESS_OPCODE_SIZE	(32)
 #define STRESS_OPCODE_MASK	(0xffffffffUL)
+#endif
+
+/* Arch specific PPC (32 bit ) */
+#if (defined(__PPC__) || defined(__ppc__)) &&	\
+    !defined(STRESS_ARCH_PPC64)
+#define STRESS_ARCH_PPC		(1)
+#define STRESS_OPCODE_SIZE	(32)
+#define STRESS_OPCODE_MASK	(0xffffffffUL)
+#endif
+
+/* Arch specific PPC64 */
+#if defined(__PPC64__) || defined(__ppc64__)
+#define STRESS_ARCH_PPC64	(1)
+#define STRESS_OPCODE_SIZE	(32)
+#define STRESS_OPCODE_MASK	(0xffffffffUL)
+#endif
+
+/* Arch specific RISC-V */
+#if defined(__riscv) || \
+    defined(__riscv__)
+#define STRESS_ARCH_RISCV	(1)
+#define STRESS_OPCODE_SIZE	(32)
+#define STRESS_OPCODE_MASK	(0xffffffffUL)
+#endif
+
+/* Arch specific, IBM S390 */
+#if defined(__s390__)
+#define STRESS_ARCH_S390	(1)
+#define STRESS_OPCODE_SIZE	(48)
+#define STRESS_OPCODE_MASK	(0xffffffffffffULL)
+#endif
+
+/* Arch specific SH4 */
+#if defined(__SH4__)
+#define STRESS_ARCH_SH4		(1)
+#define STRESS_OPCODE_SIZE	(16)
+#define STRESS_OPCODE_MASK	(0xffffUL)
+#endif
+
+/* Arch specific SPARC */
+#if defined(__sparc) ||		\
+    defined(__sparc__) ||	\
+    defined(__sparc_v9__)
+#define STRESS_ARCH_SPARC	(1)
+#define STRESS_OPCODE_SIZE	(32)
+#define STRESS_OPCODE_MASK	(0xffffffffUL)
+#endif
+
+/* Arch specific, x86-64 */
+#if defined(__x86_64__) || defined(__x86_64) || \
+    defined(__amd64__) || defined(__amd64)
+#define STRESS_ARCH_X86		(1)
+#define STRESS_ARCH_X86_64	(1)
+#endif
+
+/* Arch specific, x86-32 (i386 et al) */
+#if defined(__i386__)   || defined(__i386)
+#define STRESS_ARCH_X86		(1)
+#define STRESS_ARCH_X86_32	(1)
 #endif
 
 #endif
