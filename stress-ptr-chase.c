e0c109e05d6b7dddbf22dd9f8c4eb3ca577799be
Colin Ian King <coking@nvidia.com>
2025-10-21 16:39:59 +0100

    stress-ptr-chase: report geometric mean of ptr-chase rates for each stressor
    When -v is enabled, add debug to report the per stressor ptr-chase
    rates. This allows per stressor rates to be compared to see if memory
    bandwidth is well balanced
    
    Signed-off-by: Colin Ian King <coking@nvidia.com>

--- stress-ptr-chase.c
+++ stress-ptr-chase.c
@@ -166,6 +166,9 @@ static int stress_ptr_chase(stress_args_t *args)
 	metric = (counter > 0) ? (duration * STRESS_DBL_NANOSECOND) / (double)counter: 0.0;
 	stress_metrics_set(args, 0, "nanosec per pointer", metric, STRESS_METRIC_HARMONIC_MEAN);
 
+	if (metric > 0.0)
+		pr_dbg("%s: %.2f pointers chased per second\n", args->name, 1.0E9 / metric);
+
 	rc = EXIT_SUCCESS;
 
 	stress_set_proc_state(args->name, STRESS_STATE_DEINIT);
