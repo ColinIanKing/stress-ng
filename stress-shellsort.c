c3809a1398e505924d23ddf502c4972913fd99f7
Colin Ian King <coking@nvidia.com>
2025-10-22 12:20:02 +0100

    stress-shellsort: report shellsort comparisons rate for each stressor
    When -v is enabled, add debug to report the per stressor shellsort
    comparisons rate for each stressor. This allows per stressor rates to
    be compared to see if memory bandwidth is well balanced.
    
    Signed-off-by: Colin Ian King <coking@nvidia.com>

--- stress-shellsort.c
+++ stress-shellsort.c
@@ -235,6 +235,8 @@ tidy:
 	stress_metrics_set(args, 1, "shellsort comparisons per item",
 		count / sorted, STRESS_METRIC_HARMONIC_MEAN);
 
+	pr_dbg("%s: %.2f shellsort comparisons per sec\n", args->name, rate);
+
 	(void)munmap((void *)data, data_size);
 
 	return rc;
