f5e046e7a1d7e381d5a8d6524b187bc46006aa46
Colin Ian King <coking@nvidia.com>
2025-10-26 15:22:34 +0000

    stress-expmath: reorder type keywords to long double complex
    Signed-off-by: Colin Ian King <coking@nvidia.com>

--- stress-expmath.c
+++ stress-expmath.c
@@ -113,7 +113,7 @@ static bool OPTIMIZE3 TARGET_CLONES stress_expmath_cexpl(stress_args_t *args)
 {
 	register long double complex sum = 0.0;
 	register int i;
-	static long complex double result = -1.0;
+	static long double complex result = -1.0;
 	static bool first_run = true;
 
 PRAGMA_UNROLL_N(8)
