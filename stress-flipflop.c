903dde8283eaf5b0e5a7c3184454ce6828def77f
Colin Ian King <coking@nvidia.com>
2025-10-27 10:08:28 +0000

    stress-flipflop: add missing space in copyright message
    Signed-off-by: Colin Ian King <coking@nvidia.com>

--- stress-flipflop.c
+++ stress-flipflop.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2024-2025Tejun Heo
- * Copyright (C) 2024-2025Colin Ian King
+ * Copyright (C) 2024-2025 Tejun Heo
+ * Copyright (C) 2024-2025 Colin Ian King
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
