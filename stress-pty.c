ColinIanKing/stress-ng

commit e4fa5ce0a20f691650cf66bebf376170a4c5a2f4

stress-pty: fix typo TCOOFLUSH -> TCIOFLUSH
Signed-off-by: Colin Ian King <coking@nvidia.com>

stress-pty.c
@@ -190,7 +190,7 @@ static int stress_pty(stress_args_t *args)
 #endif
 #if defined(TCIOFLUSH)
 				if (UNLIKELY(tcflush(ptys[i].follower, TCIOFLUSH) < 0)) {
-					pr_fail("%s: tcflush TCOOFLUSH on follower pty failed, errno=%d (%s)\n",
+					pr_fail("%s: tcflush TCIOFLUSH on follower pty failed, errno=%d (%s)\n",
 						args->name, errno, strerror(errno));
 					rc = EXIT_FAILURE;
 				}

commit 02b52b4995e199fbbfbc274e6a1eda0907c958c0

stress-pty: fix build issue due to misplaced #endif
Fixes: e4fa5ce0a20f ("stress-pty: fix typo TCOOFLUSH -> TCIOFLUSH")

Closes: https://github.com/ColinIanKing/stress-ng/issues/579

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-pty.c
@@ -525,8 +525,8 @@ static int stress_pty(stress_args_t *args)
 					(void)shim_sched_yield();
 				}
 			}
-		}
 #endif
+		}
 
 #if defined(HAVE_PATHCONF)
 #if defined(_PC_MAX_CANON)