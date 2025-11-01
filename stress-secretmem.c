c915fe7c052f345601870ca4c1244905aaa7fa63
Colin Ian King <coking@nvidia.com>
2025-11-01 15:39:36 +0000

    stress-secremem: add bitmap to track secretmem page mappings/unmappings
    Signed-off-by: Colin Ian King <coking@nvidia.com>

--- stress-secretmem.c
+++ stress-secretmem.c
@@ -22,7 +22,8 @@
 #include "core-madvise.h"
 #include "core-out-of-memory.h"
 
-#define MMAP_MAX	(256*1024)
+#define MAPPINGS_MAX	(32 * 1024)
+#define SECRETMEM_PAGES	((size_t)(MAPPINGS_MAX * 3))
 
 static const stress_help_t help[] = {
 	{ NULL,	"secretmem N",		"start N workers that use secretmem mappings" },
@@ -32,6 +33,16 @@ static const stress_help_t help[] = {
 
 #if defined(__NR_memfd_secret) &&	\
     defined(__linux__)
+
+/* page bitmaps */
+#define PAGE1	(0x01)
+#define PAGE2	(0x02)
+#define PAGE3	(0x04)
+
+typedef struct {
+	uint8_t	*addr;
+	uint8_t bitmap;
+} secretmem_mapping_t;
 
 static int stress_secretmem_supported(const char *name)
 {
@@ -64,27 +75,36 @@ static int stress_secretmem_supported(const char *name)
  *   stress_secretmem_unmap()
  *      unmap first and last page of 3 page mapping
  */
-static bool stress_secretmem_unmap(
-	uint8_t **mappings,
+static void stress_secretmem_unmap(
+	secretmem_mapping_t *mappings,
 	const ssize_t n,
 	const size_t page_size)
 {
 	ssize_t i;
-	bool retry = false;
 	const size_t page_size2 = page_size << 1;
 
 	for (i = 0; i < n; i++) {
-		if (mappings[i]) {
-			if (LIKELY((stress_munmap_force((void *)mappings[i], page_size) == 0) &&
-				   (stress_munmap_force((void *)(mappings[i] + page_size2), page_size) == 0))) {
-				mappings[i] = NULL;
-			} else {
-				/* munmap failed, e.g. ENOMEM, so flag it */
-				retry = true;
+		if (mappings[i].addr) {
+			if ((mappings[i].bitmap & PAGE1) &&
+			    (stress_munmap_force((void *)mappings[i].addr, page_size) == 0)) {
+				mappings[i].bitmap &= ~PAGE1;
+			}
+			if ((mappings[i].bitmap & PAGE3) &&
+			    (stress_munmap_force((void *)mappings[i].addr + page_size2, page_size) == 0)) {
+				mappings[i].bitmap &= ~PAGE3;
 			}
 		}
 	}
-	return retry;
+	for (i = 0; i < n; i++) {
+		if (mappings[i].addr) {
+			if ((mappings[i].bitmap & PAGE2) &&
+			    (stress_munmap_force((void *)mappings[i].addr, page_size) == 0)) {
+				mappings[i].bitmap &= ~PAGE2;
+			}
+		}
+		if (mappings[i].bitmap == 0)
+			mappings[i].addr = NULL;
+	}
 }
 
 /*
@@ -95,15 +115,17 @@ static int stress_secretmem_child(stress_args_t *args, void *context)
 {
 	const size_t page_size = args->page_size;
 	const size_t page_size3 = page_size * 3;
-	uint8_t **mappings;
+	secretmem_mapping_t *mappings;
 	int fd;
+	size_t i;
+	const size_t secretmem_size = SECRETMEM_PAGES * args->page_size;
 
 	(void)context;
 
-	mappings = (uint8_t **)calloc(MMAP_MAX, sizeof(*mappings));
+	mappings = (secretmem_mapping_t *)calloc(MAPPINGS_MAX, sizeof(*mappings));
 	if (UNLIKELY(!mappings)) {
 		pr_inf_skip("%s: failed to allocate %zu bytes%s, skipping stressor\n",
-			args->name, (size_t)MMAP_MAX * sizeof(*mappings),
+			args->name, (size_t)MAPPINGS_MAX * sizeof(*mappings),
 			stress_get_memfree_str());
 		return EXIT_NO_RESOURCE;
 	}
@@ -115,70 +137,65 @@ static int stress_secretmem_child(stress_args_t *args, void *context)
 		free(mappings);
 		return EXIT_NO_RESOURCE;
 	}
+	if (UNLIKELY(ftruncate(fd, secretmem_size) != 0)) {
+		pr_inf_skip("%s: failed to ftruncate %zu secretmem bytes\n",
+			args->name, secretmem_size);
+		free(mappings);
+		(void)close(fd);
+		return EXIT_NO_RESOURCE;
+	}
+
+	for (i = 0; i < MAPPINGS_MAX; i++) {
+		mappings[i].addr = NULL;
+		mappings[i].bitmap = 0;
+	}
 
 	stress_set_proc_state(args->name, STRESS_STATE_SYNC_WAIT);
 	stress_sync_start_wait(args);
 	stress_set_proc_state(args->name, STRESS_STATE_RUN);
 
 	do {
-		ssize_t n;
-		uint8_t *redo_unmapping = NULL;
-		off_t sz = 0;
+		size_t n;
+		off_t offset = 0;
 
-		for (n = 0; LIKELY(stress_continue_flag() && (n < MMAP_MAX)); n++) {
-			const off_t offset = sz;
-
+		for (n = 0; LIKELY(stress_continue_flag() && (n < MAPPINGS_MAX)); n++, offset += page_size3) {
 			if (UNLIKELY(!stress_continue(args)))
-				break;
-
-			sz += page_size3;
-
-			/* expand secret memory size */
-			if (UNLIKELY(ftruncate(fd, sz) != 0))
 				break;
 
 			if (UNLIKELY((g_opt_flags & OPT_FLAGS_OOM_AVOID) && stress_low_memory(page_size3)))
 				break;
 
-			mappings[n] = (uint8_t *)mmap(NULL, page_size3,
+			if (mappings[n].addr || mappings[n].bitmap)
+				continue;
+
+			mappings[n].addr = (uint8_t *)mmap(NULL, page_size3,
 				PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset);
-			if (UNLIKELY(mappings[n] == MAP_FAILED)) {
-				mappings[n] = NULL;
-				break;
+			if (UNLIKELY(mappings[n].addr == MAP_FAILED)) {
+				mappings[n].addr = NULL;
+				mappings[n].bitmap = 0;
+				continue;
+			} else {
+				/* bitmap for 3 pages mapped */
+				mappings[n].bitmap = PAGE1 | PAGE2 | PAGE3;
 			}
 
-			(void)stress_madvise_mergeable(mappings[n], page_size3);
+			(void)stress_madvise_mergeable(mappings[n].addr, page_size3);
 			/*
 			 *  touch pages, this will trigger OOM SIGKILL
 			 *  when we run low on secretmem pages
 			 */
-			(void)shim_memset((void *)mappings[n], 0xff, page_size3);
+			(void)shim_memset((void *)mappings[n].addr, 0xff, page_size3);
 
 			/*
 			 *  Make an hole in the 3 page mapping on middle page
 			 */
-			if (UNLIKELY(stress_munmap_force((void *)(mappings[n] + page_size), page_size) < 0)) {
-				/* Failed?, remember to retry later */
-				redo_unmapping = mappings[n];
-				break;
+			if (stress_munmap_force((void *)(mappings[n].addr + page_size), page_size) == 0) {
+				mappings[n].bitmap &= ~PAGE2; /* 2nd page removed */
 			}
 			stress_bogo_inc(args);
 		}
 
-		if (stress_secretmem_unmap(mappings, n, page_size)) {
-			/*
-			 *  Since munmap above may fail with ENOMEM, retry any
-			 *  failed unmappings once more...
-			 */
-			(void)stress_secretmem_unmap(mappings, n, page_size);
-		}
-
-		/*
-		 *  ..and now redo an unmapping that failed earlier
-		 */
-		if (redo_unmapping)
-			(void)stress_munmap_force(redo_unmapping, page_size3);
-
+		stress_secretmem_unmap(mappings, n, page_size);
 	} while (stress_continue(args));
 
 	stress_set_proc_state(args->name, STRESS_STATE_DEINIT);
