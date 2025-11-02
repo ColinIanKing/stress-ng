ColinIanKing/stress-ng

commit fad9070622662e7aa9ab304db14431787309113b

stress-secretmem: fix fd leak in stress_secretmem_supported
Signed-off-by: Colin Ian King <coking@nvidia.com>

stress-secretmem.c
@@ -53,8 +53,10 @@ static int stress_secretmem_supported(const char *name)
 				name, errno, strerror(errno));
 			break;
 		}
+		(void)close(fd);
 		return -1;
 	}
+	(void)close(fd);
 	return 0;
 }
 

stress-seccomp.c
@@ -226,7 +226,7 @@ static int stress_seccomp_set_huge_filter(stress_args_t *args)
 
 	(void)shim_memset(&huge_prog, 0, sizeof(huge_prog));
 	if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) < 0) {
-		pr_fail("%s: prctl PR_SET_NEW_PRIVS failed, errno=%d (%s)\n",
+		pr_fail("%s: prctl PR_SET_NO_NEW_PRIVS failed, errno=%d (%s)\n",
 			args->name, errno, strerror(errno));
 		return -1;
 	}

commit afb1d4433f4f9ad038b3a6b840a16c26d85d8bd8

stress-secretmem: fix close on unopened fd
Fixes: fad907062266 ("stress-secretmem: fix fd leak in stress_secretmem_supported")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-secretmem.c
@@ -64,7 +64,6 @@ static int stress_secretmem_supported(const char *name)
 				name, errno, strerror(errno));
 			break;
 		}
-		(void)close(fd);
 		return -1;
 	}
 	(void)close(fd);