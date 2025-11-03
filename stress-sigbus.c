e9642d305617d145ee1ba5259d4fca750b855bd3
Colin Ian King <coking@nvidia.com>
2025-11-03 08:36:46 +0000

    stress-sigbus: remove double space in assignment
    Signed-off-by: Colin Ian King <coking@nvidia.com>

--- stress-sigbus.c
+++ stress-sigbus.c
@@ -110,8 +110,8 @@ static int stress_sigbus(stress_args_t *args)
 	if (ret < 0)
 		return stress_exit_status(-ret);
 	(void)stress_temp_filename_args(args, filename, sizeof(filename), stress_mwc32());
-	if ((fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR)) < 0) {
-		rc  = stress_exit_status(errno);
+	if ((fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR)) < 1) {
+		rc = stress_exit_status(errno);
 		pr_fail("%s: open %s failed, errno=%d (%s)\n",
 			args->name, filename, errno, strerror(errno));
 		goto tidy_dir;
