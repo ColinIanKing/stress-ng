ColinIanKing/stress-ng

commit def7aafcafeb07d8406f82cfdc4afddbbf49b569

stress-str: use test_strcpy_y type to declare test_strcpy
Signed-off-by: Colin Ian King <coking@nvidia.com>

stress-str.c
@@ -230,7 +230,7 @@ static size_t stress_strcpy(stress_args_t *args, stress_str_args_t *info)
 {
 	typedef char * (*test_strcpy_t)(char *dest, const char *src);
 
-	char * (*test_strcpy)(char *dest, const char *src) = (test_strcpy_t)info->libc_func;
+	char test_strcpy_t test_strcpy = (test_strcpy_t)info->libc_func;
 	const char *str1 = info->str1;
 	const char *str2 = info->str2;
 	char *strdst = info->strdst;

commit ec1958fe895025837c29b1b5e0143ea3979b9032

stress-str: fix build issue, remove char from typedef
Fixes: def7aafcafeb ("stress-str: use test_strcpy_y type to declare
test_strcpy")

Closes: https://github.com/ColinIanKing/stress-ng/issues/582

Signed-off-by: Colin Ian King <coking@nvidia.com>

stress-str.c
@@ -230,7 +230,7 @@ static size_t stress_strcpy(stress_args_t *args, stress_str_args_t *info)
 {
 	typedef char * (*test_strcpy_t)(char *dest, const char *src);
 
-	char test_strcpy_t test_strcpy = (test_strcpy_t)info->libc_func;
+	test_strcpy_t test_strcpy = (test_strcpy_t)info->libc_func;
 	const char *str1 = info->str1;
 	const char *str2 = info->str2;
 	char *strdst = info->strdst;