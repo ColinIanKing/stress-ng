ColinIanKing/stress-ng

commit ad4b8410b09595d0020a0ee995efa9efce60e99f

stress-memrate: swap stow and stod names, stow = word, stod = double word
Signed-off-by: Colin Ian King <coking@nvidia.com>

stress-memrate.c
@@ -874,12 +874,12 @@ static const stress_memrate_info_t memrate_info[] = {
 	{ "write64stoq", MR_WR,	stress_memrate_write_stos64,	stress_memrate_write_stos_rate64 },
 #endif
 #if defined(HAVE_ASM_X86_REP_STOSD) &&	\
-    !defined(__ILP32__)
-	{ "write32stow",MR_WR,	stress_memrate_write_stos32,	stress_memrate_write_stos_rate32 },
+    !defined(__ILP2__)
+	{ "write32stod",MR_WR,	stress_memrate_write_stos32,	stress_memrate_write_stos_rate32 },
 #endif
 #if defined(HAVE_ASM_X86_REP_STOSW) &&	\
     !defined(__ILP32__)
-	{ "write16stod",MR_WR,	stress_memrate_write_stos16,	stress_memrate_write_stos_rate16 },
+	{ "write16stow",MR_WR,	stress_memrate_write_stos16,	stress_memrate_write_stos_rate16 },
 #endif
 #if defined(HAVE_ASM_X86_REP_STOSB) &&	\
     !defined(__ILP32__)

commit cf9c91a51cad954c7d5795b5391b1c828a2e7c77

stress-memrate: fix macro check typo, __ILP2__ should be __ILP32__
Fixes: ad4b8410b095 ("stress-memrate: swap stow and stod names, stow = word, stod = double word")

Signed-off-by: Colin Ian King <coking@nvidia.com>

stress-memrate.c
@@ -874,7 +874,7 @@ static const stress_memrate_info_t memrate_info[] = {
 	{ "write64stoq", MR_WR,	stress_memrate_write_stos64,	stress_memrate_write_stos_rate64 },
 #endif
 #if defined(HAVE_ASM_X86_REP_STOSD) &&	\
-    !defined(__ILP2__)
+    !defined(__ILP32__)
 	{ "write32stod",MR_WR,	stress_memrate_write_stos32,	stress_memrate_write_stos_rate32 },
 #endif
 #if defined(HAVE_ASM_X86_REP_STOSW) &&	\