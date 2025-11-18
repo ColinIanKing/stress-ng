5e1978f4d2b124f236d2e1d5d7e4e1134711b2eb
Colin Ian King <coking@nvidia.com>
2025-11-18 15:12:07 +0000

    stress-sigpipe: use ssize_t for write return value
    Signed-off-by: Colin Ian King <coking@nvidia.com>

--- stress-sigpipe.c
+++ stress-sigpipe.c
@@ -67,7 +67,7 @@ static int stress_sigpipe(stress_args_t *args)
 	stress_set_proc_state(args->name, STRESS_STATE_RUN);
 
 	do {
-		register int ret;
+		register ssize_t ret;
 
 		/* cause SIGPIPE if pipe closed */
 		ret = write(pipefds[1], &data, sizeof(data));
