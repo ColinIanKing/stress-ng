6561885b90c3bca1624e89dd2d6384c595eaa509
Colin Ian King <colin.i.king@gmail.com>
2025-11-28 22:23:56 +0000

    stress-heapsort: fix division by zero in comparisons metrics
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-heapsort.c
+++ stress-heapsort.c
@@ -326,8 +326,9 @@ tidy:
 	rate = (duration > 0.0) ? count / duration : 0.0;
 	stress_metrics_set(args, 0, "heapsort comparisons per sec",
 		rate, STRESS_METRIC_HARMONIC_MEAN);
+	rate = (sorted > 0.0) ? count / sorted : 0.0;
 	stress_metrics_set(args, 1, "heapsort comparisons per item",
-		count / sorted, STRESS_METRIC_HARMONIC_MEAN);
+		rate, STRESS_METRIC_HARMONIC_MEAN);
 
 	pr_dbg("%s: %.2f heapsort comparisons per sec\n", args->name, rate);
 
