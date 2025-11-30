692b4c43ee7f9f3b302e508fd039268869e18ccf
Colin Ian King <colin.i.king@gmail.com>
2025-11-30 19:20:17 +0000

    stress-oom-pipe: clean up some implicit cast warnings
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-oom-pipe.c
+++ stress-oom-pipe.c
@@ -93,8 +93,8 @@ static int stress_oom_pipe_child(stress_args_t *args, void *ctxt)
 	size_t i;
 	int *fds = context->fds, *fd, pipes_open = 0;
 	const bool aggressive = (g_opt_flags & OPT_FLAGS_AGGRESSIVE);
-	uint32_t *rd_buffer = context->rd_buffer;
-	uint32_t *wr_buffer = context->wr_buffer;
+	uint32_t *rd_buffer = (uint32_t *)context->rd_buffer;
+	uint32_t *wr_buffer = (uint32_t *)context->wr_buffer;
 
 	stress_uint8rnd4((uint8_t *)wr_buffer, page_size);
 
