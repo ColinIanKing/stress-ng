3abb7a42b1acb920aef6c815118307f8a076a7f8
Colin Ian King <colin.i.king@gmail.com>
2025-11-30 11:42:01 +0000

    stress-peterson: add NORETURN attribute to non-return function
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-peterson.c
+++ stress-peterson.c
@@ -67,7 +67,7 @@ static inline void ALWAYS_INLINE peterson_mbarrier(void)
 #endif
 }
 
-static void stress_peterson_sigill_handler(int signum)
+static void NORETURN stress_peterson_sigill_handler(int signum)
 {
 	(void)signum;
 
