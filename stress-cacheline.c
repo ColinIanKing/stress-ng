3f76f8b468de31f5266e4f968e68894776f0851e
Colin Ian King <colin.i.king@gmail.com>
2025-12-21 22:45:11 +0000

    stress-*: update copyright year to 2025
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-cacheline.c
+++ stress-cacheline.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022      Colin Ian King.
+ * Copyright (C) 2022-2025 Colin Ian King.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
