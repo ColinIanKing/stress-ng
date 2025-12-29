ColinIanKing/stress-ng

commit bdac8c4ad9a2d42531e90e8e7770684aba067fe8

core-madvise, stress-madvise: don't allow MADV_GUARD_INSTALL
Don't allow guard pages to be installed as these will cause
accesses to SEGV. Add checks for this and revert back to
MADV_NORMAL if they ever occur.

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-madvise.c
@@ -123,9 +123,11 @@ const int madvise_options[] = {
 /* Linux 6.12 */
 #if defined(MADV_GUARD_INSTALL) &&	\
     defined(MADV_NORMAL)
-	MADV_GUARD_INSTALL,
+	/* This makes a page non writable, disable it to avoid SIGSEGVs */
+	/* MADV_GUARD_INSTALL, */
 #endif
 #if defined(MADV_GUARD_REMOVE)
+	/* Should always fail as MADV_GUARD_INSTALL is not used */
 	MADV_GUARD_REMOVE,
 #endif
 /* OpenBSD */
@@ -237,6 +239,25 @@ static const int madvise_random_options[] = {
 };
 #endif
 
+
+/*
+ *  stress_advice_check()
+ *	return MADV_NORMAL if advice should be ignored
+ *	otherwise retuen advice
+ */
+int stress_advice_check(const int advice)
+{
+	switch (madvise_options[advice]) {
+#if defined(MADV_GUARD_INSTALL)
+	case MADV_GUARD_INSTALL:
+		return MADV_NORMAL;
+#endif
+	default:
+		break;
+	}
+	return advice;
+}
+
 /*
  *  stress_madvise_randomize()
  *	apply random madvise setting to a memory region
@@ -246,8 +267,9 @@ int stress_madvise_randomize(void *addr, const size_t length)
 #if defined(HAVE_MADVISE)
 	if (g_opt_flags & OPT_FLAGS_MMAP_MADVISE) {
 		const int i = stress_mwc32modn((uint32_t)SIZEOF_ARRAY(madvise_random_options));
+		const int advice = stress_advice_check(madvise_random_options[i]);
 
-		return madvise(addr, length, madvise_random_options[i]);
+		return madvise(addr, length, advice);
 	}
 #else
 	UNEXPECTED

core-madvise.h
@@ -22,6 +22,7 @@
 extern const int madvise_options[];
 extern const size_t madvise_options_elements;
 
+extern int stress_advice_check(const int advice);
 extern int stress_madvise_randomize(void *addr, const size_t length);
 extern int stress_madvise_random(void *addr, const size_t length);
 extern int stress_madvise_mergeable(void *addr, const size_t length);

stress-madvise.c
@@ -116,27 +116,6 @@ static inline void stress_read_proc_maps(const char *maps)
 }
 
 /*
- *  stress_ignore_advice()
- *	return true if advice should not be used
- */
-static bool stress_ignore_advice(const int advice)
-{
-	switch (madvise_options[advice]) {
-#if defined(MADV_GUARD_INSTALL)
-	case MADV_GUARD_INSTALL:
-		return true;
-#endif
-#if defined(MADV_GUARD_REMOVE)
-	case MADV_GUARD_REMOVE:
-		return true;
-#endif
-	default:
-		break;
-	}
-	return false;
-}
-
-/*
  *  stress_random_advise()
  *	get a random advise option
  */
@@ -146,7 +125,7 @@ static int stress_random_advise(
 	const size_t size,
 	const bool hwpoison)
 {
-	const int advise = madvise_options[stress_mwcsizemodn(madvise_options_elements)];
+	int advice = madvise_options[stress_mwcsizemodn(madvise_options_elements)];
 #if defined(MADV_HWPOISON) || defined(MADV_SOFT_OFFLINE)
 	static int hwpoison_count = 0;
 #if defined(MADV_NORMAL)
@@ -156,11 +135,10 @@ static int stress_random_advise(
 #endif
 #endif
 
-	if (stress_ignore_advice(advise))
-		return madv_normal;
+	advice = stress_advice_check(advice);
 
 #if defined(MADV_HWPOISON)
-	if (advise == MADV_HWPOISON) {
+	if (advice == MADV_HWPOISON) {
 		if (hwpoison) {
 			const size_t page_size = args->page_size;
 			const size_t vec_size = (size + page_size - 1) / page_size;
@@ -228,7 +206,7 @@ static int stress_random_advise(
 #endif
 
 #if defined(MADV_SOFT_OFFLINE)
-	if (advise == MADV_SOFT_OFFLINE) {
+	if (advice == MADV_SOFT_OFFLINE) {
 		static int soft_offline_count = 0;
 
 		/* ..and minimize number of soft offline pages */
@@ -238,7 +216,7 @@ static int stress_random_advise(
 		soft_offline_count++;
 	}
 #endif
-	return advise;
+	return advice;
 }
 
 /*
@@ -266,16 +244,16 @@ static void *stress_madvise_pages(void *arg)
 
 	for (n = 0; n < sz; n += page_size) {
 		void *ptr = (void *)(((uint8_t *)buf) + n);
-		const int advise = stress_random_advise(args, ptr, page_size, ctxt->hwpoison);
+		const int advice = stress_random_advise(args, ptr, page_size, ctxt->hwpoison);
 
-		(void)shim_madvise(ptr, page_size, advise);
+		(void)shim_madvise(ptr, page_size, advice);
 		if (stress_mwc8() < 16)
 			stress_read_proc_smaps(ctxt->smaps);
 		if (stress_mwc8() < 16)
 			stress_read_proc_maps(ctxt->maps);
 #if defined(MADV_GUARD_INSTALL) && defined(MADV_NORMAL)
 		/* avoid segfaults by setting back to normal */
-		if (advise == MADV_GUARD_INSTALL)
+		if (advice == MADV_GUARD_INSTALL)
 			(void)shim_madvise(ptr, page_size, MADV_NORMAL);
 #endif
 		(void)shim_msync(ptr, page_size, MS_ASYNC);
@@ -283,12 +261,12 @@ static void *stress_madvise_pages(void *arg)
 	for (n = 0; n < sz; n += page_size) {
 		const size_t m = (size_t)(stress_mwc64modn((uint64_t)sz) & ~(page_size - 1));
 		void *ptr = (void *)(((uint8_t *)buf) + m);
-		const int advise = stress_random_advise(args, ptr, page_size, ctxt->hwpoison);
+		const int advice = stress_random_advise(args, ptr, page_size, ctxt->hwpoison);
 
-		(void)shim_madvise(ptr, page_size, advise);
+		(void)shim_madvise(ptr, page_size, advice);
 #if defined(MADV_GUARD_INSTALL) && defined(MADV_NORMAL)
 		/* avoid segfaults by setting back to normal */
-		if (advise == MADV_GUARD_INSTALL)
+		if (advice == MADV_GUARD_INSTALL)
 			(void)shim_madvise(ptr, page_size, MADV_NORMAL);
 #endif
 		(void)shim_msync(ptr, page_size, MS_ASYNC);
@@ -619,7 +597,7 @@ madv_free_out:
 		 * Some systems allow zero sized page zero madvise
 		 * to see if that madvice is implemented, so try this
 		 */
-		(void)madvise(0, 0, stress_ignore_advice(advice) ? MADV_NORMAL : madvise_options[advice]);
+		(void)madvise(0, 0, stress_advice_check(madvise_options[advice]));
 		advice++;
 		advice = (advice >= madvise_options_elements) ? 0: advice;
 

commit adff6ee189bee5725ca5f3d58e6ffb6798a01783

core-madvise: fix incorrect use of madvise_options array
The advice is the actual advice and not an index into the
madvise_options array, so use of the array is wrong. Fix
this.

Fixes: bdac8c4ad9a2 ("core-madvise, stress-madvise: don't allow MADV_GUARD_INSTALL")
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-madvise.c
@@ -247,7 +247,7 @@ static const int madvise_random_options[] = {
  */
 int stress_advice_check(const int advice)
 {
-	switch (madvise_options[advice]) {
+	switch (advice) {
 #if defined(MADV_GUARD_INSTALL)
 	case MADV_GUARD_INSTALL:
 		return MADV_NORMAL;