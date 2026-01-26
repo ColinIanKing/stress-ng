ColinIanKing/stress-ng

commit 85755c69b6e49d2171750e33dd159bf8efa12df7

core-signal: add siglongjmp helpers
Refactor code, add siglongjmp helper code to reduce repeated
idioms for handling siglongjmps

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-sigfpe.c
@@ -65,25 +65,23 @@ static volatile siginfo_t siginfo;
  *	SIGFPE handler
  */
 #if defined(STRESS_CHECK_SIGINFO)
-static void NORETURN MLOCKED_TEXT stress_fpehandler(int num, siginfo_t *info, void *ucontext)
+static void NORETURN MLOCKED_TEXT stress_fpehandler(int signum, siginfo_t *info, void *ucontext)
 {
 	(void)ucontext;
 
-	signum = num;
+	signum = signum;
 	(void)feclearexcept(FE_ALL_EXCEPT);
 	siginfo = *info;
 
-	siglongjmp(jmp_env, 1);		/* Ugly, bounce back */
-	stress_no_return();
+	stress_signal_longjmp(signum, jmp_env, 1);
 }
 #else
-static void NORETURN MLOCKED_TEXT stress_fpehandler(int num)
+static void NORETURN MLOCKED_TEXT stress_fpehandler(int signum)
 {
-	signum = num;
+	signum = signum;
 	(void)feclearexcept(FE_ALL_EXCEPT);
 
-	siglongjmp(jmp_env, 1);		/* Ugly, bounce back */
-	stress_no_return();
+	stress_signal_longjmp(signum, jmp_env, 1);
 }
 #endif
 

core-signal.c
@@ -718,3 +718,35 @@ void stress_signal_catch_sigsegv(void)
 {
 	stress_signal_catch_sig(SIGSEGV, stress_signal_catch_sigsegv_handler);
 }
+
+/*
+ *  stress_signal_longjmp()
+ *	perform siglongjm
+ */
+void NORETURN stress_signal_longjmp(
+	const int signum,
+	sigjmp_buf jmp_env,
+	const int val)
+{
+        (void)signum;
+
+        siglongjmp(jmp_env, val);
+        stress_no_return();
+}
+
+/*
+ *  stress_signal_longjmp()
+ *	perform siglongjmp, set do_jmp flag if non-null
+ */
+void stress_signal_longjmp_flag(
+	const int signum,
+	sigjmp_buf jmp_env,
+	const int val,
+	volatile bool *do_jmp)
+{
+	if (!*do_jmp)
+		return;
+	*do_jmp = false;
+	stress_signal_longjmp(signum, jmp_env, val);
+
+}

core-signal.h
@@ -38,5 +38,8 @@ extern NORETURN void stress_signal_exit_handler(int signum);
 extern void stress_signal_ignore_handler(int sig);
 extern void stress_signal_catch_sigill(void);
 extern void stress_signal_catch_sigsegv(void);
+extern NORETURN void stress_signal_longjmp(const int signum, sigjmp_buf jmp_env, const int val);
+extern void stress_signal_longjmp_flag(const int signum, sigjmp_buf jmp_env, const int val,
+	volatile bool *do_jmp);
 
 #endif

stress-af-alg.c
@@ -50,7 +50,7 @@ static const stress_opt_t opts[] = {
     defined(AF_ALG)
 
 static volatile bool do_jmp = true;
-static sigjmp_buf jmpbuf;
+static sigjmp_buf jmp_env;
 
 #if !defined(SOL_ALG)
 #define SOL_ALG				(279)
@@ -137,11 +137,8 @@ static void MLOCKED_TEXT stress_af_alg_alarm_handler(int signum)
 	 * If we've not stopped after 5 seconds then an af-alg
 	 * got stuck, so force  jmp to terminate path
 	 */
-	if (UNLIKELY(do_jmp && count++ > 5)) {
-		do_jmp = false;
-		siglongjmp(jmpbuf, 1);
-		stress_no_return();
-	}
+	if (UNLIKELY(do_jmp && count++ > 5))
+		stress_signal_longjmp_flag(signum, jmp_env, 1, &do_jmp);
 }
 
 /*
@@ -939,7 +936,7 @@ static int stress_af_alg(stress_args_t *args)
 	stress_sync_start_wait(args);
 	stress_set_proc_state(args->name, STRESS_STATE_RUN);
 
-	if (sigsetjmp(jmpbuf, 1) != 0)
+	if (sigsetjmp(jmp_env, 1) != 0)
 		goto deinit;
 
 	if (stress_signal_handler(args->name, SIGALRM, stress_af_alg_alarm_handler, NULL) < 0) {

stress-bad-altstack.c
@@ -53,7 +53,7 @@ static void *zero_stack;
 #if defined(O_TMPFILE)
 static void *bus_stack;
 #endif
-static sigjmp_buf jmpbuf;
+static sigjmp_buf jmp_env;
 static size_t stress_minsigstksz;
 
 STRESS_PRAGMA_PUSH
@@ -83,7 +83,6 @@ static void NORETURN MLOCKED_TEXT stress_bad_altstack_signal_handler(int signum)
 	}
 	 */
 
-	(void)signum;
 	(void)munmap(stack, stress_minsigstksz);
 	(void)shim_memset(data, 0xff, sizeof(data));
 	stress_uint8_put(data[0]);
@@ -102,8 +101,7 @@ static void NORETURN MLOCKED_TEXT stress_bad_altstack_signal_handler(int signum)
 	 *  generated a fault inside the stack of the
 	 *  signal handler, so jmp back and re-try
 	 */
-	siglongjmp(jmpbuf, 1);
-	stress_no_return();
+	stress_signal_longjmp(signum, jmp_env, 1);
 }
 
 static int stress_bad_altstack_child(stress_args_t *args)
@@ -122,7 +120,7 @@ static int stress_bad_altstack_child(stress_args_t *args)
 	struct rlimit rlim;
 #endif
 
-	if (sigsetjmp(jmpbuf, 1) != 0) {
+	if (sigsetjmp(jmp_env, 1) != 0) {
 		/*
 		 *  We land here if we get a segfault
 		 *  but not a segfault in the sighandler

stress-bad-ioctl.c
@@ -224,10 +224,7 @@ done:
 
 static void NORETURN MLOCKED_TEXT stress_segv_handler(int signum)
 {
-	(void)signum;
-
-	siglongjmp(jmp_env, 1);
-	stress_no_return();
+	stress_signal_longjmp(signum, jmp_env, 1);
 }
 
 /*

stress-bigheap.c
@@ -100,27 +100,22 @@ static const char *stress_bigheap_phase(void)
  */
 #if defined(SA_SIGINFO)
 static void NORETURN MLOCKED_TEXT stress_bigheap_segvhandler(
-	int num,
+	int signum,
 	siginfo_t *info,
 	void *ucontext)
 {
-	(void)num;
 	(void)ucontext;
 
 	fault_addr = info->si_addr;
 	signo = info->si_signo;
 	sigcode = info->si_code;
 
-	siglongjmp(jmp_env, 1);		/* Ugly, bounce back */
-	stress_no_return();
+	stress_signal_longjmp(signum, jmp_env, 1);
 }
 #else
 static void NORETURN MLOCKED_TEXT stress_bigheap_segvhandler(int signum)
 {
-	(void)signum;
-
-	siglongjmp(jmp_env, 1);		/* Ugly, bounce back */
-	stress_no_return();
+	stress_signal_longjmp(signum, jmp_env, 1);
 }
 #endif
 

stress-bitonicsort.c
@@ -52,13 +52,7 @@ static const stress_opt_t opts[] = {
  */
 static void MLOCKED_TEXT stress_bitonicsort_handler(int signum)
 {
-	(void)signum;
-
-	if (do_jmp) {
-		do_jmp = false;
-		siglongjmp(jmp_env, 1);		/* Ugly, bounce back */
-		stress_no_return();
-	}
+	stress_signal_longjmp_flag(signum, jmp_env, 1, &do_jmp);
 }
 #endif
 

commit 157929f68b2247b09e5e25bd116e1105495e1fa7

stress-sigfpe: fix name shadowing of signum
Fixes: 85755c69b6e4 ("core-signal: add siglongjmp helpers")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-sigfpe.c
@@ -55,7 +55,7 @@ static const stress_help_t help[] = {
 #define SNG_FLTDIV	(0x80000000)
 
 static sigjmp_buf jmp_env;
-static int signum;
+static int sigfpe_signum;
 #if defined(STRESS_CHECK_SIGINFO)
 static volatile siginfo_t siginfo;
 #endif
@@ -69,7 +69,7 @@ static void NORETURN MLOCKED_TEXT stress_fpehandler(int signum, siginfo_t *info,
 {
 	(void)ucontext;
 
-	signum = signum;
+	sigfpe_signum = signum;
 	(void)feclearexcept(FE_ALL_EXCEPT);
 	siginfo = *info;
 
@@ -78,7 +78,7 @@ static void NORETURN MLOCKED_TEXT stress_fpehandler(int signum, siginfo_t *info,
 #else
 static void NORETURN MLOCKED_TEXT stress_fpehandler(int signum)
 {
-	signum = signum;
+	sigfpe_signum = signum;
 	(void)feclearexcept(FE_ALL_EXCEPT);
 
 	stress_signal_longjmp(signum, jmp_env, 1);
@@ -330,7 +330,7 @@ static int stress_sigfpe(stress_args_t *args)
 
 #if defined(STRESS_CHECK_SIGINFO)
 			if (verify) {
-				if (signum == SIGFPE) {
+				if (sigfpe_signum == SIGFPE) {
 					if (UNLIKELY((siginfo.si_code >= 0) &&
 						     (siginfo.si_code != expected_err_code))) {
 						pr_fail("%s: got SIGFPE error %d (%s), expecting %d (%s)\n",
@@ -340,7 +340,7 @@ static int stress_sigfpe(stress_args_t *args)
 						rc = EXIT_FAILURE;
 						break;
 					}
-				} else if (signum == SIGILL) {
+				} else if (sigfpe_signum == SIGILL) {
 					static bool reported = false;
 
 					if (!reported) {