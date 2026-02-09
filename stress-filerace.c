ed1d7e0541467525d8b030678197381b27c97b54
Colin Ian King <colin.i.king@gmail.com>
2026-02-09 19:09:44 +0000

    stress-filerace: minimize/maximize --filerace-procs option
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-filerace.c
+++ stress-filerace.c
@@ -1598,7 +1598,12 @@ static int stress_filerace(stress_args_t *args)
 	size_t i, children = 0;
 	size_t filerace_procs = DEFAULT_FILERACE_PROCS;
 
-	(void)stress_get_setting("filerace-procs", &filerace_procs);
+	if (!stress_get_setting("filerace-procs", &filerace_procs)) {
+		if (g_opt_flags & OPT_FLAGS_MAXIMIZE)
+			filerace_procs = MAX_FILERACE_PROCS;
+		if (g_opt_flags & OPT_FLAGS_MINIMIZE)
+			filerace_procs = MIN_FILERACE_PROCS;
+	}
 
 	t_start = time(NULL);
 #if defined(SIGIO)
