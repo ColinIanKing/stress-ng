e6cd990ee63fdba86c876ba1cf02e486a995470d
Colin Ian King <colin.i.king@gmail.com>
2026-03-15 22:36:21 +0000

    stress-netlink-task: remove from scheduler class
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-netlink-task.c
+++ stress-netlink-task.c
@@ -355,14 +355,14 @@ static int stress_netlink_task(stress_args_t *args)
 const stressor_info_t stress_netlink_task_info = {
 	.stressor = stress_netlink_task,
 	.supported = stress_netlink_task_supported,
-	.classifier = CLASS_SCHEDULER | CLASS_OS,
+	.classifier = CLASS_OS,
 	.verify = VERIFY_ALWAYS,
 	.help = help
 };
 #else
 const stressor_info_t stress_netlink_task_info = {
 	.stressor = stress_unimplemented,
-	.classifier = CLASS_SCHEDULER | CLASS_OS,
+	.classifier = CLASS_OS,
 	.verify = VERIFY_ALWAYS,
 	.help = help,
 	.unimplemented_reason = "built without linux/connector.h, linux/netlink.h, linux/cn_proc.h, linux/taskstats.h or linux/genetlink.h support"
