ColinIanKing/stress-ng

commit 624a19b79c461dde0537937f957c9c2f8a9d4029

stress-clock: add CLOCK_AUX clock
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-clock.c
@@ -80,6 +80,9 @@ static const stress_clock_info_t clocks[] = {
 #if defined(CLOCK_TAI)
 	CLOCK_INFO(CLOCK_TAI)
 #endif
+#if defined(CLOCK_AUX)
+	CLOCK_INFO(CLOCK_AUX)
+#endif
 };
 
 #if defined(HAVE_CLOCK_NANOSLEEP)

commit f9d3fb0acb8fb0f4b8a49994bc4b646f510a1d3e

stress-clock: add missing commas at the end of all clock array elements
If both CLOCK_TAI and CLOCK_AUX are defined, the comma between those two
array elements will be missing, leading to a compiler error.

Add the missing comma to both elements, so that on the next clock
addition this will not break again.

Fixes: 624a19b79c46 ("stress-clock: add CLOCK_AUX clock")
Signed-off-by: Thomas Weißschuh (Schneider Electric) <thomas.weissschuh@linutronix.de>

stress-clock.c
@@ -75,10 +75,10 @@ static const stress_clock_info_t clocks[] = {
 	CLOCK_INFO(CLOCK_THREAD_CPUTIME_ID),
 #endif
 #if defined(CLOCK_TAI)
-	CLOCK_INFO(CLOCK_TAI)
+	CLOCK_INFO(CLOCK_TAI),
 #endif
 #if defined(CLOCK_AUX)
-	CLOCK_INFO(CLOCK_AUX)
+	CLOCK_INFO(CLOCK_AUX),
 #endif
 };
 