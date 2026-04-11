31d4d387a88b4a2efe28676e0369a2d78365f0f2
Colin Ian King <colin.i.king@gmail.com>
2026-04-11 13:03:36 +0100

    stress-io: add some writes before file syncs, add fsync and fdatasync
    Add more sync'ing activity
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-io.c
+++ stress-io.c
@@ -29,35 +29,80 @@ static const stress_help_t help[] = {
 };
 
 /*
+ *  stess_io_write()
+ *	write a 32 bit random value to bytes  0..3 of file
+ */
+static void stess_io_write(const int fd)
+{
+	uint32_t data = stress_mwc32();
+
+	VOID_RET(off_t, lseek(fd, 0, SEEK_SET));
+	VOID_RET(ssize_t, write(fd, &data, sizeof(data)));
+}
+
+/*
  *  stress on sync()
  *	stress system by IO sync calls
  */
 static int stress_io(stress_args_t *args)
 {
 	int rc = EXIT_SUCCESS;
+	int ret;
 #if defined(HAVE_SYNCFS)
-	int i, fd, n_mnts;
+	int i;
+	int fd_dir;
+	int fd_tmp;
+	const int fd_bad = stress_fs_bad_fd_get();
+	int n_mnts;
 	char *mnts[MAX_MNTS];
+	char filename[PATH_MAX];
 	int fds[MAX_MNTS];
-	const int bad_fd = stress_fs_bad_fd_get();
 
 	if (stress_instance_zero(args))
 		pr_inf("%s: this is a legacy I/O sync stressor, consider using iomix instead\n", args->name);
+
+	ret = stress_fs_temp_dir_make_args(args);
+	if (ret < 0) {
+		rc = stress_exit_status((int)-ret);
+		return rc;
+	}
+
+	(void)stress_fs_temp_filename_args(args, filename, sizeof(filename), stress_mwc32());
+	if ((fd_tmp = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR)) < 0) {
+		rc = stress_exit_status(errno);
+		pr_fail("%s: open %s failed, errno=%d (%s)\n",
+			args->name, filename, errno, strerror(errno));
+                goto tidy_dir;
+
+	}
+	(void)shim_unlink(filename);
 
 	n_mnts = stress_mount_get(mnts, MAX_MNTS);
 	for (i = 0; i < n_mnts; i++)
 		fds[i] = openat(AT_FDCWD, mnts[i], O_RDONLY | O_NONBLOCK | O_DIRECTORY);
 
-	fd = openat(AT_FDCWD, ".", O_RDONLY | O_NONBLOCK | O_DIRECTORY);
+	fd_dir = openat(AT_FDCWD, ".", O_RDONLY | O_NONBLOCK | O_DIRECTORY);
 #endif
 	stress_proc_state_set(args->name, STRESS_STATE_SYNC_WAIT);
 	stress_sync_start_wait(args);
 	stress_proc_state_set(args->name, STRESS_STATE_RUN);
 
 	do {
+		stess_io_write(fd_tmp);
+		if (stress_mwc1()) {
+			shim_fsync(fd_tmp);
+			shim_fdatasync(fd_tmp);
+		} else {
+			shim_fdatasync(fd_tmp);
+			shim_fsync(fd_tmp);
+		}
+
+		stess_io_write(fd_tmp);
 		shim_sync();
 #if defined(HAVE_SYNCFS)
-		if (UNLIKELY((fd != -1) && (syncfs(fd) < 0))) {
+
+		stess_io_write(fd_tmp);
+		if (UNLIKELY((fd_dir != -1) && (syncfs(fd_dir) < 0))) {
 			if (UNLIKELY(errno == ENOSYS))
 				goto bogo_inc;
 			pr_fail("%s: syncfs failed, errno=%d (%s)\n",
@@ -87,9 +132,9 @@ static int stress_io(stress_args_t *args)
 		/*
 		 *  exercise with an invalid fd
 		 */
-		if (UNLIKELY(syncfs(bad_fd) == 0)) {
+		if (UNLIKELY(syncfs(fd_bad) == 0)) {
 			pr_fail("%s: syncfs on invalid fd %d succeeded\n",
-				args->name, bad_fd);
+				args->name, fd_bad);
 			rc = EXIT_FAILURE;
 			goto tidy;
 		}
@@ -105,14 +150,18 @@ tidy:
 #endif
 	stress_proc_state_set(args->name, STRESS_STATE_DEINIT);
 #if defined(HAVE_SYNCFS)
-	if (fd != -1)
-		(void)close(fd);
+	if (fd_dir != -1)
+		(void)close(fd_dir);
 
 	stress_fs_close_fds(fds, n_mnts);
 	stress_mount_free(mnts, n_mnts);
 #else
 	UNEXPECTED
 #endif
+
+	(void)close(fd_tmp);
+tidy_dir:
+	(void)stress_fs_temp_dir_rm_args(args);
 
 	return rc;
 }
