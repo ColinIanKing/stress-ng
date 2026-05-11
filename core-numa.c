ColinIanKing/stress-ng

commit 6400dc836f15c016106c0283e57272311d814c28

stress-ng: move "mbind" option into main_opts
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-numa.c
@@ -393,16 +393,19 @@ static long int stress_parse_node(const char *const str)
  *
  * Returns: 0 - OK
  */
-int stress_set_mbind(const char *arg)
+int stress_set_mbind(void)
 {
-	char *str, *ptr, *token;
+	char *str, *ptr, *token, *mbind;
 	long int max_node;
 	unsigned long int *nodemask;
 	const size_t nodemask_bits = sizeof(*nodemask) * 8;
 	size_t nodemask_sz;
 
+	if (!stress_setting_get("mbind", &mbind))
+		return 0;
+
 	if (stress_numa_count_mem_nodes(&max_node) < 1) {
-		(void)fprintf(stderr, "no NUMA nodes found, ignoring --mbind setting '%s'\n", arg);
+		(void)fprintf(stderr, "no NUMA nodes found, ignoring --mbind setting '%s'\n", mbind);
 		return 0;
 	}
 
@@ -413,9 +416,9 @@ int stress_set_mbind(const char *arg)
 		_exit(EXIT_FAILURE);
 	}
 
-	str = stress_const_optdup(arg);
+	str = stress_const_optdup(mbind);
 	if (!str) {
-		(void)fprintf(stderr, "out of memory duplicating argument '%s'\n", arg);
+		(void)fprintf(stderr, "out of memory duplicating argument '%s'\n", mbind);
 		free(nodemask);
 		_exit(EXIT_FAILURE);
 	}

core-numa.h
@@ -92,7 +92,7 @@ extern long int stress_numa_mask_nodes_get(stress_numa_mask_t *numa_mask);
 extern long stress_numa_next_node(const long int node,
 	stress_numa_mask_t *numa_nodes);
 extern long int stress_numa_nodes(void);
-extern int stress_set_mbind(const char *arg);
+extern int stress_set_mbind(void);
 extern stress_numa_mask_t *stress_numa_mask_alloc(void);
 extern void stress_numa_mask_and_node_alloc(stress_args_t *args,
 	stress_numa_mask_t **numa_nodes, stress_numa_mask_t **numa_mask,

stress-ng.c
@@ -3410,6 +3410,7 @@ static const stress_opt_t main_opts[] = {
 	{ OPT_iostat,         "iostat",          TYPE_ID_INT32, 1, 3600, NULL },
 	{ OPT_job,	      "job",             TYPE_ID_STR, 0, 0, NULL },
 	{ OPT_log_file,       "log-file",        TYPE_ID_STR, 0, 0, NULL },
+	{ OPT_mbind,          "mbind",           TYPE_ID_STR, 0, 0, NULL },
 	{ OPT_no_madvise,     "no-madvise",      TYPE_ID_BOOL, 0, 1, NULL },
 	{ OPT_pause,          "pause",           TYPE_ID_UINT, 0, INT_MAX, NULL },
 	{ OPT_quiet,          "quiet",           TYPE_ID_BOOL, 0, 1, NULL },
@@ -3552,11 +3553,6 @@ next_opt:
 				"cannot determine maximum file descriptor limit");
 			stress_check_range(optarg, u64, 8, max_fds);
 			stress_setting_global_set("max-fd", TYPE_ID_UINT64, &u64);
-			break;
-		case OPT_mbind:
-			if (stress_set_mbind(optarg) < 0)
-				exit(EXIT_FAILURE);
-			stress_setting_global_set("mbind", TYPE_ID_STR, (void *)optarg);
 			break;
 		case OPT_oom_avoid_bytes:
 			{
@@ -4124,6 +4120,12 @@ int main(int argc, char **argv, char **envp)
 		g_pr_log_flags &= ~(PR_LOG_FLAGS_ALL);
 	if (stress_setting_get("seed", &seed)) {
 		g_opt_flags |= OPT_FLAGS_SEED;
+	}
+
+	/* --mbind */
+	if (stress_set_mbind() < 0) {
+		ret = EXIT_FAILURE;
+		goto exit_stressors_free;
 	}
 
 	/* Load in job file options */

commit 4573a06993dca26044b76030876ed54911b57b49

core-numa: fix stress_set_mbind for non-Linux systems
Fixes: 6400dc836f15 ("stress-ng: move "mbind" option into main_opts")

Closes: https://github.com/ColinIanKing/stress-ng/issues/619

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-numa.c
@@ -543,9 +543,12 @@ long int CONST stress_numa_nodes(void)
 	return 1;
 }
 
-int stress_set_mbind(const char *arg)
+int stress_set_mbind(void)
 {
-	(void)arg;
+	char *mbind;
+
+	if (!stress_setting_get("mbind", &mbind))
+		return 0;
 
 	(void)fprintf(stderr, "%s: setting NUMA memory policy binding not supported\n", option);
 	_exit(EXIT_FAILURE);