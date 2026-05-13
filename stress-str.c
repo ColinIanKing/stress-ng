ColinIanKing/stress-ng

commit fe8d323e7ebc37f5bb7f1e90c92c89776087591f

stress-str: fix potential string overflow on str*cmp calls
Signed-off-by: Colin Ian King <coking@nvidia.com>

stress-str.c
@@ -126,11 +126,11 @@ static size_t stress_strncasecmp(stress_args_t *args, stress_str_args_t *info)
 		STRCHK(info, 0 != test_strncasecmp(str2, str1, len2));
 		STRCHK(info, 0 != test_strncasecmp(str1, str2, len1));
 
-		STRCHK(info, 0 != test_strncasecmp(str1 + i, str1, len1));
-		STRCHK(info, 0 != test_strncasecmp(str1, str1 + i, len1));
-		STRCHK(info, 0 == test_strncasecmp(str1 + i, str1 + i, len1));
+		STRCHK(info, 0 != test_strncasecmp(str1 + i, str1, len1 - i));
+		STRCHK(info, 0 != test_strncasecmp(str1, str1 + i, len1 - i));
+		STRCHK(info, 0 == test_strncasecmp(str1 + i, str1 + i, len1 - i));
 
-		STRCHK(info, 0 != test_strncasecmp(str1 + i, str2, len1));
+		STRCHK(info, 0 != test_strncasecmp(str1 + i, str2, len1 - i));
 		STRCHK(info, 0 != test_strncasecmp(str2, str1 + i, len2));
 	}
 	stress_bogo_add(args, 9);
@@ -452,11 +452,11 @@ static size_t stress_strncmp(stress_args_t *args, stress_str_args_t *info)
 		STRCHK(info, 0 != test_strncmp(str2, str1, len2));
 		STRCHK(info, 0 != test_strncmp(str1, str2, len1));
 
-		STRCHK(info, 0 != test_strncmp(str1 + i, str1, len1));
-		STRCHK(info, 0 != test_strncmp(str1, str1 + i, len1));
-		STRCHK(info, 0 == test_strncmp(str1 + i, str1 + i, len1));
+		STRCHK(info, 0 != test_strncmp(str1 + i, str1, len1 - i));
+		STRCHK(info, 0 != test_strncmp(str1, str1 + i, len1 - i));
+		STRCHK(info, 0 == test_strncmp(str1 + i, str1 + i, len1 - i));
 
-		STRCHK(info, 0 != test_strncmp(str1 + i, str2, len2));
+		STRCHK(info, 0 != test_strncmp(str1 + i, str2, len2 - i));
 		STRCHK(info, 0 != test_strncmp(str2, str1 + i, len2));
 	}
 	stress_bogo_add(args, 9);

commit 26a5b5433e6ea9330ed992a138ddc28b4787e9f9

stress-str: fix index underflow on strncmp
The size parameter is incorrect, it should be str1 - i. Kudos to monokuma
for the analysis and the suggessted fix.

https://github.com/ColinIanKing/stress-ng/issues/620#issuecomment-4429099343

Fixes: fe8d323e7ebc ("stress-str: fix potential string overflow on str*cmp calls")
       etc..
Closes: https://github.com/ColinIanKing/stress-ng/issues/620

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-str.c
@@ -498,7 +498,7 @@ static size_t stress_strncmp(stress_args_t *args, stress_str_args_t *info)
 		STRCHK(info, 0 != test_strncmp(str1, str1 + i, len1 - i));
 		STRCHK(info, 0 == test_strncmp(str1 + i, str1 + i, len1 - i));
 
-		STRCHK(info, 0 != test_strncmp(str1 + i, str2, len2 - i));
+		STRCHK(info, 0 != test_strncmp(str1 + i, str2, len1 - i));
 		STRCHK(info, 0 != test_strncmp(str2, str1 + i, len2));
 	}
 	stress_bogo_add(args, 9);