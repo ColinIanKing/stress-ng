ColinIanKing/stress-ng

commit 88a9d74549b09bf76795b7d2b1f72e7494aede3c

stress-ng: move "max-fd" option into main_opts
Add a callback option parser function stress_fs_max_fd to parse
--max-fd option. Add stress_fs_max_file_rlimit and rework
stress_fs_max_file_limit_get to improve maximum fd limit detection.

Use the stress_fs_max_fd for open-max stressor option too

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-open.c
@@ -19,6 +19,7 @@
  */
 #include "stress-ng.h"
 #include "core-builtin.h"
+#include "core-filesystem.h"
 #include "core-killpid.h"
 
 #if defined(HAVE_LINUX_OPENAT2_H)
@@ -147,48 +148,9 @@ static const int open_flags[] = {
 #endif
 };
 
-static size_t stress_get_max_fds(void)
-{
-	const size_t max_size = (size_t)-1;
-	size_t max_fds = 0;
-
-#if defined(RLIMIT_NOFILE)
-	struct rlimit rlim;
-
-	if (getrlimit(RLIMIT_NOFILE, &rlim) == 0) {
-		struct rlimit new_rlim = rlim;
-
-		new_rlim.rlim_cur = new_rlim.rlim_max;
-		if (setrlimit(RLIMIT_NOFILE, &new_rlim) == 0) {
-			max_fds = stress_fs_max_file_limit_get();
-			(void)setrlimit(RLIMIT_NOFILE, &rlim);
-		}
-	}
-#endif
-
-	if (max_fds == 0)
-		max_fds = stress_fs_max_file_limit_get();
-	if (max_fds > max_size)
-		max_fds = max_size;
-
-	return max_fds;
-}
-
-static void stress_open_max(const char *opt_name, const char *opt_arg, stress_type_id_t *type_id, void *value)
-{
-	size_t *open_max = (size_t *)value;
-	const size_t max_fds = stress_get_max_fds();
-
-	(void)opt_name;
-
-	*type_id = TYPE_ID_SIZE_T;
-	*open_max = (size_t)stress_get_uint64_percent(opt_arg, 1, (uint64_t)max_fds, NULL,
-                        "cannot determine maximum number of file descriptors");
-}
-
 static const stress_opt_t opts[] = {
 	{ OPT_open_fd,  "open-fd",  TYPE_ID_BOOL, 0, 1, NULL },
-	{ OPT_open_max,	"open-max", TYPE_ID_CALLBACK, 0, 1, (void *)stress_open_max },
+	{ OPT_open_max,	"open-max", TYPE_ID_CALLBACK, 0, 1, (void *)stress_fs_max_fd },
 	END_OPT,
 };
 
@@ -1076,8 +1038,8 @@ static int stress_open(stress_args_t *args)
 {
 	int *fds, ret;
 	char path[PATH_MAX], temp_dir[PATH_MAX];
-	const size_t max_size = (size_t)-1;
-	size_t open_max = stress_fs_max_file_limit_get();
+	const uint64_t max_size = (uint64_t)-1LL;
+	uint64_t open_max = stress_fs_max_file_limit_get();
 	size_t i, sz;
 	pid_t pid = -1;
 	const pid_t mypid = getpid();
@@ -1113,13 +1075,13 @@ static int stress_open(stress_args_t *args)
 	if (open_max < 1)
 		open_max = 1;
 
-	sz = open_max * sizeof(*fds);
+	sz = (size_t)open_max * sizeof(*fds);
 	fds = (int *)mmap(NULL, sz, PROT_READ | PROT_WRITE,
 			MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 	if (fds == MAP_FAILED) {
 		/* shrink */
 		open_max = 1024 * 1024;
-		sz = open_max * sizeof(*fds);
+		sz = (size_t)open_max * sizeof(*fds);
 		fds = (int *)mmap(NULL, sz, PROT_READ | PROT_WRITE,
 				MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 		if (fds == MAP_FAILED) {

core-filesystem.c
@@ -791,44 +791,131 @@ ssize_t stress_fs_file_read(
 }
 
 /*
+ * stress_fs_max_file_rlimit()
+ *	binary chop to find largest rlimit NOFILE
+ */
+static uint64_t stress_fs_max_file_rlimit(void)
+{
+#if defined(RLIMIT_NOFILE)
+	struct rlimit rlim_orig;
+	struct rlimit rlim;
+	rlim_t min = 64;
+	rlim_t max = RLIM_INFINITY / 2;
+	rlim_t cur;
+	rlim_t prev_cur = 0;
+	int i;
+
+#if defined(__linux__)
+	{
+		uint64_t val;
+		char buf[64];
+
+		if (stress_fs_file_read("/proc/sys/fs/file-max", buf, sizeof(buf)) > 0) {
+			errno = 0;
+			if ((sscanf(buf, "%" SCNu64, &val) == 1) && (errno == 0))
+				max = (rlim_t)val;
+		}
+	}
+#endif
+	if (getrlimit(RLIMIT_NOFILE, &rlim_orig) < 0)
+		return 0ULL;
+
+	for (i = 0; i <= 64; i++) {
+		cur = min + (max - min) / 2;
+
+		/* Try rlim cur AND max in case we can push both up */
+		rlim.rlim_cur = cur;
+		rlim.rlim_max = cur;
+		if (setrlimit(RLIMIT_NOFILE, &rlim) == 0) {
+			min = cur;
+		} else {
+			/* Try just limi cur */
+			rlim.rlim_cur = cur;
+			rlim.rlim_max = rlim_orig.rlim_max;
+			if (setrlimit(RLIMIT_NOFILE, &rlim) == 0) {
+				min = cur;
+			} else {
+				max = cur;
+			}
+		}
+		/* No change, we got the best max setting */
+		if (cur == prev_cur)
+			return cur;
+		prev_cur = cur;
+	}
+#endif
+	return 0ULL;
+}
+
+/*
  *  stress_fs_max_file_limit_get()
  *	get max number of files that the current
  *	process can open not counting the files that
  *	may already been opened.
  */
-size_t stress_fs_max_file_limit_get(void)
+uint64_t stress_fs_max_file_limit_get(void)
 {
+	uint64_t max_fd = 0;
+	uint64_t rlimit_fd;
 #if defined(HAVE_GETDTABLESIZE)
 	int tablesize;
 #endif
-#if defined(RLIMIT_NOFILE)
-	struct rlimit rlim;
-#endif
-	size_t max_rlim = SIZE_MAX;
-	size_t max_sysconf;
 
-#if defined(HAVE_GETDTABLESIZE)
-	/* try the simple way first */
-	tablesize = getdtablesize();
-	if (tablesize > 0)
-		return (size_t)tablesize;
-#endif
-#if defined(RLIMIT_NOFILE)
-	if (!getrlimit(RLIMIT_NOFILE, &rlim))
-		max_rlim = (size_t)rlim.rlim_cur;
-#endif
+	rlimit_fd = stress_fs_max_file_rlimit();
+	if (rlimit_fd > max_fd)
+		max_fd = rlimit_fd;
+
 #if defined(_SC_OPEN_MAX)
 	{
 		const long int open_max = sysconf(_SC_OPEN_MAX);
 
-		max_sysconf = (open_max > 0) ? (size_t)open_max : SIZE_MAX;
+		if ((open_max > 0) && ((uint64_t)open_max > max_fd))
+			max_fd = (uint64_t)open_max;
 	}
 #else
-	max_sysconf = SIZE_MAX;
-	UNEXPECTED
+	/* got nothing, try SIZE_MAX */
+	if (max_fd == 0)
+		max_fd = SIZE_MAX;
 #endif
-	/* return the lowest of these two */
-	return STRESS_MINIMUM(max_rlim, max_sysconf);
+
+#if defined(HAVE_GETDTABLESIZE)
+	/* constrain down to tablesize */
+	tablesize = getdtablesize();
+	if ((tablesize > 0) && ((uint64_t)tablesize < max_fd))
+		max_fd = (uint64_t)tablesize;
+#endif
+
+	return max_fd;
+}
+
+/*
+ *  stress_fs_max_fd()
+ *	parse maximum file descriptor limit option
+ */
+void stress_fs_max_fd(
+	const char *opt_name,
+	const char *opt_arg,
+	stress_type_id_t *type_id,
+	void *value)
+{
+        uint64_t *open_max = (uint64_t *)value;
+        const uint64_t max_fds = stress_fs_max_file_limit_get();
+
+        (void)opt_name;
+
+        *type_id = TYPE_ID_UINT64;
+        *open_max = (size_t)stress_get_uint64_percent(opt_arg, 1, max_fds, NULL,
+                        "cannot determine maximum number of file descriptors");
+	if (*open_max < 4) {
+		*open_max = 16;
+		pr_inf("setting %s value %s too small, defaulting to lower limit %" PRIu64 "\n",
+			opt_name, opt_arg, *open_max);
+	}
+	if (*open_max > max_fds) {
+		*open_max = max_fds;
+		pr_inf("setting %s value %s too large, defaulting to upper limit %" PRIu64 "\n",
+			opt_name, opt_arg, *open_max);
+	}
 }
 
 /*

core-filesystem.h
@@ -54,7 +54,9 @@ extern ssize_t stress_fs_file_write(const char *path, const char *buf,
 extern ssize_t stress_fs_discard(const char *path);
 extern WARN_UNUSED ssize_t stress_fs_file_read(const char *path, char *buf,
 	const size_t buf_len);
-extern WARN_UNUSED size_t stress_fs_max_file_limit_get(void);
+extern WARN_UNUSED uint64_t stress_fs_max_file_limit_get(void);
+extern void stress_fs_max_fd(const char *opt_name, const char *opt_arg,
+	stress_type_id_t *type_id, void *value);
 extern WARN_UNUSED size_t stress_fs_file_limit_get(void);
 extern WARN_UNUSED int stress_fs_bad_fd_get(void);
 extern WARN_UNUSED bool stress_fs_pipe_check(const int fd);

core-limit.c
@@ -117,8 +117,8 @@ void stress_limit_max_set(void)
 
 		(void)stress_setting_get("max-fd", &max_fd);
 		if (max_fd != 0) {
-			rlim.rlim_cur = (rlim_t)(max_fd + 1);
-			rlim.rlim_max = (rlim_t)(max_fd + 1);
+			rlim.rlim_cur = (rlim_t)max_fd;
+			rlim.rlim_max = (rlim_t)max_fd;
 			(void)setrlimit(RLIMIT_NOFILE, &rlim);
 		}
 	}

core-setting.c
@@ -277,10 +277,10 @@ static int stress_setting_generic_set(
 	case TYPE_ID_INT32:
 		setting->u.int32 = *(const int32_t *)value;
 		break;
-	case TYPE_ID_UINT64_BYTES_FS_PERCENT:
 	case TYPE_ID_UINT64:
 	case TYPE_ID_UINT64_BYTES:
 	case TYPE_ID_UINT64_BYTES_FS:
+	case TYPE_ID_UINT64_BYTES_FS_PERCENT:
 	case TYPE_ID_UINT64_BYTES_VM:
 	case TYPE_ID_UINT64_TIME:
 		setting->u.uint64 = *(const uint64_t *)value;

core-setting.h
@@ -29,16 +29,16 @@ typedef enum {
 	TYPE_ID_UINT32,			/* uint32_t */
 	TYPE_ID_INT32,			/* int32_t */
 	TYPE_ID_UINT64,			/* uint64_t */
-	TYPE_ID_UINT64_BYTES_FS_PERCENT,/* uint64_t in % units, file system */
 	TYPE_ID_UINT64_BYTES,		/* uint64_t in bytes units, generic */
 	TYPE_ID_UINT64_BYTES_FS,	/* uint64_t in bytes units, file system */
+	TYPE_ID_UINT64_BYTES_FS_PERCENT,/* uint64_t in % units, file system */
 	TYPE_ID_UINT64_BYTES_VM,	/* uint64_t in bytes units, memory */
 	TYPE_ID_UINT64_TIME,		/* uint64_t in seconds */
 	TYPE_ID_INT64,			/* int64_t */
 	TYPE_ID_SIZE_T,			/* size_t */
-	TYPE_ID_SIZE_T_BYTES_FS_PERCENT,/* size_t in % units, file system */
 	TYPE_ID_SIZE_T_BYTES,		/* size_t in bytes units, generic */
 	TYPE_ID_SIZE_T_BYTES_FS,	/* size_t in bytes units, file system */
+	TYPE_ID_SIZE_T_BYTES_FS_PERCENT,/* size_t in % units, file system */
 	TYPE_ID_SIZE_T_BYTES_VM,	/* size_t in bytes units, memory */
 	TYPE_ID_SSIZE_T,		/* ssize_t */
 	TYPE_ID_UINT,			/* unsigned int */

stress-ng.c
@@ -3423,6 +3423,7 @@ static const stress_opt_t main_opts[] = {
 	{ OPT_limit_data,     "limit-data",      TYPE_ID_UINT64_BYTES, 1 * MB, RLIM_INFINITY, NULL },
 	{ OPT_limit_stack,    "limit-stack",     TYPE_ID_UINT64_BYTES, 1 * MB, RLIM_INFINITY, NULL },
 	{ OPT_log_file,       "log-file",        TYPE_ID_STR, 0, 0, NULL },
+	{ OPT_max_fd,         "max-fd",          TYPE_ID_CALLBACK, 16, 0xffffffffffffffffULL, stress_fs_max_fd },
 	{ OPT_mbind,          "mbind",           TYPE_ID_STR, 0, 0, NULL },
 	{ OPT_no_madvise,     "no-madvise",      TYPE_ID_BOOL, 0, 1, NULL },
 	{ OPT_oom_avoid_bytes,"oom-avoid-bytes", TYPE_ID_SIZE_T_BYTES_VM, 4096, 0xffffffffffffffffULL, NULL },
@@ -3458,7 +3459,6 @@ int stress_opts_parse(int argc, char **argv, const bool jobmode)
 
 	for (;;) {
 		int32_t i32;
-		uint64_t u64, max_fds;
 		int c, option_index, ret;
 		size_t i;
 
@@ -3536,13 +3536,6 @@ next_opt:
 			exit(EXIT_SUCCESS);
 		case OPT_help:
 			stress_usage();
-			break;
-		case OPT_max_fd:
-			max_fds = (uint64_t)stress_fs_file_limit_get();
-			u64 = stress_get_uint64_percent(optarg, 1, max_fds, NULL,
-				"cannot determine maximum file descriptor limit");
-			stress_check_range(optarg, u64, 8, max_fds);
-			stress_setting_global_set("max-fd", TYPE_ID_UINT64, &u64);
 			break;
 		case OPT_query:
 			if (!jobmode)

commit fedfcc1d41d422d59829d3b8ec6f6fa894ba92f9

stress-open: use PRIu64 to print uint64_t open_max
Fixes: 88a9d74549b0 ("stress-ng: move "max-fd" option into main_opts")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-open.c
@@ -1085,7 +1085,7 @@ static int stress_open(stress_args_t *args)
 		fds = (int *)mmap(NULL, sz, PROT_READ | PROT_WRITE,
 				MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 		if (fds == MAP_FAILED) {
-			pr_inf_skip("%s: cannot mmap %zu file descriptors%s, "
+			pr_inf_skip("%s: cannot mmap %" PRIu64 " file descriptors%s, "
 				"errno=%d (%s), skipping stressor\n",
 				args->name, open_max, stress_memory_free_get(),
 				errno, strerror(errno));
@@ -1093,7 +1093,7 @@ static int stress_open(stress_args_t *args)
 		}
 	}
 	if (!args->instance)
-		pr_inf("%s: using a maximum of %zu file descriptors\n", args->name, open_max);
+		pr_inf("%s: using a maximum of %" PRIu64 " file descriptors\n", args->name, open_max);
 	stress_memory_anon_name_set(fds, sz, "fds");
 
 	if (open_fd) {