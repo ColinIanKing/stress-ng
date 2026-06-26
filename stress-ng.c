ColinIanKing/stress-ng

commit 387953e4a339c125bdce0eef93eaa0b663a8b4d0

stress-ng: rename str to newstr to avoid name shadowing
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-ng.c
@@ -3607,7 +3607,7 @@ static int stress_exercises_get(
 				size_t len = 1;
 				size_t n;
 				size_t i;
-				char *str;
+				char *newstr;
 				char **array;
 
 				/* string long enough for the text */
@@ -3634,8 +3634,8 @@ static int stress_exercises_get(
 
 				shim_qsort(array, n, sizeof(*array), stress_sort_cmp_str);
 
-				str = calloc(len, sizeof(*str));
-				if (!str) {
+				newstr = calloc(len, sizeof(*str));
+				if (!newstr) {
 					free(array);
 					continue;
 				}
@@ -3643,9 +3643,9 @@ static int stress_exercises_get(
 					shim_strlcat(str, " ", len);
 					shim_strlcat(str, array[i], len);
 				}
-				pr_inf("  %s:%s\n", stressor->name, str);
+				pr_inf("  %s:%s\n", stressor->name, newstr);
 
-				free(str);
+				free(newstr);
 				free(array);
 			}
 		}

commit 1408f3ac67a64109a41bae0cf44c5bbf21382ccd

stress-ng: fix typo in str operations
Should be operating on newstr and not str, doh.

Fixes: 387953e4a339 ("stress-ng: rename str to newstr to avoid name shadowing")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-ng.c
@@ -3640,8 +3640,8 @@ static int stress_exercises_get(
 					continue;
 				}
 				for (i = 0; i < n; i++) {
-					shim_strlcat(str, " ", len);
-					shim_strlcat(str, array[i], len);
+					shim_strlcat(newstr, " ", len);
+					shim_strlcat(newstr, array[i], len);
 				}
 				pr_inf("  %s:%s\n", stressor->name, newstr);
 