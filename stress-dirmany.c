d4497b1987520407f1210a5bea1222e58424365e
Colin Ian King <colin.i.king@gmail.com>
2026-06-28 18:19:32 +0100

    stress-dirmany: use min size as the memcpy minimum
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-dirmany.c
+++ stress-dirmany.c
@@ -38,7 +38,9 @@ static void stress_dirmany_filename(
 	const size_t filename_len,
 	const uint64_t n)
 {
-	if (pathname_len + filename_len + 18 < filename_sz) {
+	const size_t minlen = STRESS_MINIMUM(filename_sz, filename_len);
+
+	if (pathname_len + filename_len + 18 < minlen) {
 		char *ptr = filename;
 
 		(void)shim_memcpy(ptr, pathname, pathname_len);
