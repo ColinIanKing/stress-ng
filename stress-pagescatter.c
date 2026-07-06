ColinIanKing/stress-ng

commit 292871b7ffa9c4845f3206aec89995ebcf235553

stress-pagescatter: widen # pages field to 10 chars for 2^30
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-pagescatter.c
@@ -477,7 +477,8 @@ static int stress_pagescatter(stress_args_t *args)
 
 	stress_proc_state_set(args->name, STRESS_STATE_DEINIT);
 
-	shim_strscpy(buffer, "# pages", sizeof(buffer));
+	/* # pages field is 10 chars wide for 2^30 pages */
+	(void)snprintf(buffer, sizeof(buffer), "%14s", "# pages");
 	for (i = 0; i < SCATTER_MAX; i++) {
 		(void)snprintf(tmp, sizeof(tmp), " %10s", scatter_types[i]);
 		(void)shim_strlcat(buffer, tmp, sizeof(buffer));
@@ -490,7 +491,7 @@ static int stress_pagescatter(stress_args_t *args)
 		for (i = 0; i <= info->order; i++) {
 			size_t j;
 
-			(void)snprintf(buffer, sizeof(buffer), "%7d", 1U << i);
+			(void)snprintf(buffer, sizeof(buffer), "%10d", 1U << i);
 			for (j = 0; j < SCATTER_MAX; j++) {
 				if (info->rate[i].duration[j] > 0.0) {
 					double rate = info->rate[i].duration[j] > 0.0 ?

commit 2b89ae748e09667fbe2d023d996b3aac37c8d68e

stress-pagescatter: actually make # pages field 10 chars wide
Fixes: 292871b7ffa9 ("stress-pagescatter: widen # pages field to 10 chars for 2^30")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-pagescatter.c
@@ -480,7 +480,7 @@ static int stress_pagescatter(stress_args_t *args)
 	stress_proc_state_set(args->name, STRESS_STATE_DEINIT);
 
 	/* # pages field is 10 chars wide for 2^30 pages */
-	(void)snprintf(buffer, sizeof(buffer), "%14s", "# pages");
+	(void)snprintf(buffer, sizeof(buffer), "%10s", "# pages");
 	for (i = 0; i < SCATTER_MAX; i++) {
 		(void)snprintf(tmp, sizeof(tmp), " %10s", scatter_types[i]);
 		(void)shim_strlcat(buffer, tmp, sizeof(buffer));