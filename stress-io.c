ColinIanKing/stress-ng

commit e5363ea9d6a1e4f0ffac8389408800d8dc3fab6c

stress-io: clean up warnings when HAVE_SYNCFS is not defined
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-io.c
@@ -29,6 +29,7 @@ static const stress_help_t help[] = {
 	{ NULL,	 NULL,		NULL }
 };
 
+#if defined(HAVE_SYNCFS)
 /*
  *  stess_io_write()
  *	write a 32 bit random value to bytes  0..3 of file
@@ -40,6 +41,7 @@ static void stess_io_write(const int fd)
 	VOID_RET(off_t, lseek(fd, 0, SEEK_SET));
 	VOID_RET(ssize_t, write(fd, &data, sizeof(data)));
 }
+#endif
 
 /*
  *  stress on sync()
@@ -48,8 +50,8 @@ static void stess_io_write(const int fd)
 static int stress_io(stress_args_t *args)
 {
 	int rc = EXIT_SUCCESS;
-	int ret;
 #if defined(HAVE_SYNCFS)
+	int ret;
 	int i;
 	int fd_dir;
 	int fd_tmp;
@@ -91,7 +93,7 @@ static int stress_io(stress_args_t *args)
 	stress_proc_state_set(args->name, STRESS_STATE_RUN);
 
 	do {
-#if defined (HAVE_SYNCFS)
+#if defined(HAVE_SYNCFS)
 		stess_io_write(fd_tmp);
 		if (stress_mwc1()) {
 			shim_fsync(fd_tmp);
@@ -165,7 +167,9 @@ tidy:
 	UNEXPECTED
 #endif
 
+#if defined(HAVE_SYNCFS)
 tidy_dir:
+#endif
 	(void)stress_fs_temp_dir_rm_args(args);
 
 	return rc;

commit e47bbb9d8162eb584227fa312cc1f493dc6caee2

stress-io: ensure directory is removed on tidy_dir exit path
Kudos to Christian Franke for spotting this and suggesting the fix.

Fixes: e5363ea9d6a1 ("stress-io: clean up warnings when HAVE_SYNCFS is not defined")
Closes: https://github.com/ColinIanKing/stress-ng/issues/641

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-io.c
@@ -169,8 +169,8 @@ tidy:
 
 #if defined(HAVE_SYNCFS)
 tidy_dir:
-#endif
 	(void)stress_fs_temp_dir_rm_args(args);
+#endif
 
 	return rc;
 }