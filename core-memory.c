ColinIanKing/stress-ng

commit 8e39cafba5873ef54820dd73690b9a6158954238

core-memory: put memory info into stress_memory_info_t
Put memory info into a single struct rather than passing several
variables by reference. Deprecate stress_memory_limits_get.

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-memory.c
@@ -95,28 +95,38 @@ size_t stress_memory_page_size_get(void)
  *  stress_memory_info_get()
  *	wrapper for linux sysinfo
  */
-int stress_memory_info_get(
-	size_t *freemem,
-	size_t *totalmem,
-	size_t *freeswap,
-	size_t *totalswap)
+int stress_memory_info_get(stress_memory_info_t *info)
 {
-	if (UNLIKELY(!freemem || !totalmem || !freeswap || !totalswap))
+	if (UNLIKELY(!info))
 		return -1;
+
+	(void)shim_memset(info, 0, sizeof(*info));
 #if defined(HAVE_SYS_SYSINFO_H) &&	\
     defined(HAVE_SYSINFO) &&		\
     !defined(__fiwix)
 	{
-		struct sysinfo info;
+		struct sysinfo s_info;
 
-		(void)shim_memset(&info, 0, sizeof(info));
+		(void)shim_memset(&s_info, 0, sizeof(s_info));
+		if (LIKELY(sysinfo(&s_info) == 0)) {
+#if defined(__linux__)
+			char buf[64];
+#endif
 
-		if (LIKELY(sysinfo(&info) == 0)) {
-			*freemem = info.freeram * info.mem_unit;
-			*totalmem = info.totalram * info.mem_unit;
-			*freeswap = info.freeswap * info.mem_unit;
-			*totalswap = info.totalswap * info.mem_unit;
-			return 0;
+			info->freemem = s_info.freeram * s_info.mem_unit;
+			info->totalmem = s_info.totalram * s_info.mem_unit;
+			info->freeswap = s_info.freeswap * s_info.mem_unit;
+			info->totalswap = s_info.totalswap * s_info.mem_unit;
+#if defined(__linux__)
+			if (stress_fs_file_read("/proc/sys/kernel/shmall", buf, sizeof(buf)) > 0) {
+				size_t shmall = 0;
+
+				if ((sscanf(buf, "%zu", &shmall) == 1) && (errno == 0))
+					info->shmall = shmall;
+			}
+#else
+			info->shmall = 0;
+#endif
 		}
 	}
 #endif
@@ -132,15 +142,16 @@ int stress_memory_info_get(
 
 		*totalswap = (vm_swap_total >= max_size_t) ? max_size_t : (size_t)vm_swap_total;
 #endif
-		*freemem = page_size * stress_bsd_getsysctl_uint32("vm.stats.vm.v_free_count");
-		*totalmem = page_size *
+		info->freemem = page_size * stress_bsd_getsysctl_uint32("vm.stats.vm.v_free_count");
+		info->totalmem = page_size *
 			(stress_bsd_getsysctl_uint32("vm.stats.vm.v_active_count") +
 			 stress_bsd_getsysctl_uint32("vm.stats.vm.v_inactive_count") +
 			 stress_bsd_getsysctl_uint32("vm.stats.vm.v_laundry_count") +
 			 stress_bsd_getsysctl_uint32("vm.stats.vm.v_wire_count") +
 			 stress_bsd_getsysctl_uint32("vm.stats.vm.v_free_count"));
-		*freeswap = 0;
-		*totalswap = 0;
+		info->freeswap = 0;
+		info->totalswap = 0;
+		info->shmall = 0;
 		return 0;
 	}
 #endif
@@ -150,10 +161,11 @@ int stress_memory_info_get(
 		struct uvmexp_sysctl u;
 
 		if (stress_bsd_getsysctl("vm.uvmexp2", &u, sizeof(u)) == 0) {
-			*freemem = (size_t)u.free * u.pagesize;
-			*totalmem = (size_t)u.npages * u.pagesize;
-			*totalswap = (size_t)u.swpages * u.pagesize;
-			*freeswap = *totalswap - (size_t)u.swpginuse * u.pagesize;
+			info->freemem = (size_t)u.free * u.pagesize;
+			info->totalmem = (size_t)u.npages * u.pagesize;
+			info->totalswap = (size_t)u.swpages * u.pagesize;
+			info->freeswap = *totalswap - (size_t)u.swpginuse * u.pagesize;
+			info->shmall = 0;
 			return 0;
 		}
 	}
@@ -172,50 +184,20 @@ int stress_memory_info_get(
 		(void)shim_memset(&vm_stat, 0, sizeof(vm_stat));
 		ret = host_statistics64(host, HOST_VM_INFO64, (host_info64_t)&vm_stat, &count);
 		if (ret >= 0) {
-			*freemem = page_size * vm_stat.free_count;
-			*totalmem = page_size * (vm_stat.active_count +
+			info->freemem = page_size * vm_stat.free_count;
+			info->totalmem = page_size * (vm_stat.active_count +
 						 vm_stat.inactive_count +
 						 vm_stat.wire_count +
 						 vm_stat.zero_fill_count);
+			info->totalswap = 0;
+			info->freeswap = 0;
+			info->shmall = 0;
 			return 0;
 		}
 
 	}
 #endif
-	*freemem = 0;
-	*totalmem = 0;
-	*freeswap = 0;
-	*totalswap = 0;
-
 	return -1;
-}
-
-/*
- *  stress_memory_limits_get()
- *	get SHMALL and memory in system
- *	these are set to zero on failure
- */
-void stress_memory_limits_get(
-	size_t *shmall,
-	size_t *freemem,
-	size_t *totalmem,
-	size_t *freeswap,
-	size_t *totalswap)
-{
-#if defined(__linux__)
-	char buf[64];
-#endif
-	if (UNLIKELY(!shmall || !freemem || !totalmem || !freeswap || !totalswap))
-		return;
-
-	(void)stress_memory_info_get(freemem, totalmem, freeswap, totalswap);
-#if defined(__linux__)
-	if (LIKELY(stress_fs_file_read("/proc/sys/kernel/shmall", buf, sizeof(buf)) > 0)) {
-		if (sscanf(buf, "%zu", shmall) == 1)
-			return;
-	}
-#endif
-	*shmall = 0;
 }
 
 /*
@@ -227,23 +209,23 @@ void stress_memory_limits_get(
  */
 char *stress_memory_free_get(void)
 {
-        size_t freemem = 0;
-	size_t totalmem = 0;
-	size_t freeswap = 0;
-	size_t totalswap = 0;
+	stress_memory_info_t info;
 	char freemem_str[32];
 	char freeswap_str[32];
 	static char buf[96];
 
 	(void)shim_memset(buf, 0, sizeof(buf));
-	if (stress_memory_info_get(&freemem, &totalmem, &freeswap, &totalswap) < 0)
+	if (stress_memory_info_get(&info) < 0)
 		return buf;
 
-	if ((freemem == 0) && (totalmem == 0) && (freeswap == 0) && (totalswap == 0))
+	if ((info.freemem == 0) &&
+	    (info.totalmem == 0) &&
+	    (info.freeswap == 0) &&
+	    (info.totalswap == 0))
 		return buf;
 
-	(void)stress_uint64_to_str(freemem_str, sizeof(freemem_str), (uint64_t)freemem, 0, true);
-	(void)stress_uint64_to_str(freeswap_str, sizeof(freeswap_str), (uint64_t)freeswap, 0, true);
+	(void)stress_uint64_to_str(freemem_str, sizeof(freemem_str), (uint64_t)info.freemem, 0, true);
+	(void)stress_uint64_to_str(freeswap_str, sizeof(freeswap_str), (uint64_t)info.freeswap, 0, true);
 	(void)snprintf(buf, sizeof(buf), " (%s mem free, %s swap free)", freemem_str, freeswap_str);
 	return buf;
 }
@@ -281,16 +263,13 @@ void stress_memory_ksm_merge(const int flag)
  */
 bool stress_memory_low_check(const size_t requested)
 {
+	stress_memory_info_t info;
 	static size_t prev_freemem = 0;
 	static size_t prev_freeswap = 0;
-	size_t freemem;
-	size_t totalmem;
-	size_t freeswap;
-	size_t totalswap;
 	static double threshold = -1.0;
 	bool low_memory = false;
 
-	if (stress_memory_info_get(&freemem, &totalmem, &freeswap, &totalswap) == 0) {
+	if (stress_memory_info_get(&info) == 0) {
 		/*
 		 *  Threshold not set, then get
 		 */
@@ -298,7 +277,7 @@ bool stress_memory_low_check(const size_t requested)
 			size_t bytes = 0;
 
 			if (stress_setting_get("oom-avoid-bytes", &bytes)) {
-				threshold = 100.0 * (double)bytes / (double)freemem;
+				threshold = 100.0 * (double)bytes / (double)info.freemem;
 			} else {
 				/* Not specified, then default to 2.5% */
 				threshold = 2.5;
@@ -311,38 +290,38 @@ bool stress_memory_low_check(const size_t requested)
 		if ((prev_freemem + prev_freeswap) > 0) {
 			ssize_t delta;
 
-			delta = (ssize_t)prev_freemem - (ssize_t)freemem;
+			delta = (ssize_t)prev_freemem - (ssize_t)info.freemem;
 			delta = (delta * 2) + requested;
 			/* memory shrinking quickly? */
-			if (delta  > (ssize_t)freemem) {
+			if (delta  > (ssize_t)info.freemem) {
 				low_memory = true;
 				goto update;
 			}
 			/* swap shrinking quickly? */
-			delta = (ssize_t)prev_freeswap - (ssize_t)freeswap;
-			if (delta > (ssize_t)freeswap / 8) {
+			delta = (ssize_t)prev_freeswap - (ssize_t)info.freeswap;
+			if (delta > (ssize_t)info.freeswap / 8) {
 				low_memory = true;
 				goto update;
 			}
 		}
 		/* Not enough for allocation and slop? */
-		if (freemem < ((4 * MB) + requested)) {
+		if (info.freemem < ((4 * MB) + requested)) {
 			low_memory = true;
 			goto update;
 		}
 		/* Less than threshold left? */
-		if (((double)(freemem - requested) * 100.0 / (double)totalmem) < threshold) {
+		if (((double)(info.freemem - requested) * 100.0 / (double)info.totalmem) < threshold) {
 			low_memory = true;
 			goto update;
 		}
 		/* Any swap enabled with free memory we are too low? */
-		if ((totalswap > 0) && (freeswap + freemem < (requested + (2 * MB)))) {
+		if ((info.totalswap > 0) && (info.freeswap + info.freemem < (requested + (2 * MB)))) {
 			low_memory = true;
 			goto update;
 		}
 update:
-		prev_freemem = freemem;
-		prev_freeswap = freeswap;
+		prev_freemem = info.freemem;
+		prev_freeswap = info.freeswap;
 
 		/* low memory? drop caches and automatically enable ksm memory merging */
 		if (low_memory) {

core-config-check.c
@@ -116,11 +116,7 @@ static int stress_config_read(const char *path, uint64_t *value)
  */
 void stress_config_check(void)
 {
-	size_t shmall = 0;
-	size_t freemem = 0;
-	size_t totalmem = 0;
-	size_t freeswap = 0;
-	size_t totalswap = 0;
+	stress_memory_info_t info;
 	size_t freetotal;
 
 #if defined(__linux__)
@@ -186,10 +182,10 @@ void stress_config_check(void)
 	}
 #endif
 
-	stress_memory_limits_get(&shmall, &freemem, &totalmem, &freeswap, &totalswap);
-	freetotal = freemem + freeswap;
+	stress_memory_info_get(&info);
+	freetotal = info.freemem + info.freeswap;
 	if (!(g_opt_flags & OPT_FLAGS_OOM_AVOID) &&
-	    (((freemem > 0) && (freemem < (size_t)(256 * MB))) ||
+	    (((info.freemem > 0) && (info.freemem < (size_t)(256 * MB))) ||
 	    ((freetotal > 0) && (freetotal < (size_t)(512 * MB))))) {
 		pr_inf("note: system has only %zu MB of free memory and swap, "
 			"recommend using --oom-avoid\n", freetotal / (size_t)MB);

core-helper.c
@@ -740,10 +740,7 @@ void stress_runinfo(void)
 	char *real_path_ret;
 	const char *temp_path = stress_fs_temp_path_get();
 	const char *fs_type = stress_fs_type_get(temp_path);
-	size_t freemem;
-	size_t totalmem;
-	size_t freeswap;
-	size_t totalswap;
+	stress_memory_info_t info;
 #if defined(HAVE_UNAME) &&	\
     defined(HAVE_SYS_UTSNAME_H)
 	struct utsname uts;
@@ -776,14 +773,14 @@ void stress_runinfo(void)
 		stress_libc_version_get(),
 		stress_endian_str());
 #endif
-	if (stress_memory_info_get(&freemem, &totalmem, &freeswap, &totalswap) == 0) {
+	if (stress_memory_info_get(&info) == 0) {
 		char ram_t[32];
 		char ram_f[32];
 		char ram_s[32];
 
-		stress_uint64_to_str(ram_t, sizeof(ram_t), (uint64_t)totalmem, 1, false);
-		stress_uint64_to_str(ram_f, sizeof(ram_f), (uint64_t)freemem, 1, false);
-		stress_uint64_to_str(ram_s, sizeof(ram_s), (uint64_t)freeswap, 1, false);
+		stress_uint64_to_str(ram_t, sizeof(ram_t), (uint64_t)info.totalmem, 1, false);
+		stress_uint64_to_str(ram_f, sizeof(ram_f), (uint64_t)info.freemem, 1, false);
+		stress_uint64_to_str(ram_s, sizeof(ram_s), (uint64_t)info.freeswap, 1, false);
 		pr_dbg("RAM total: %s, RAM free: %s, swap free: %s\n", ram_t, ram_f, ram_s);
 	}
 	real_path_ret = realpath(temp_path, real_path);

core-memory.h
@@ -21,11 +21,16 @@
 
 #include "stress-ng.h"
 
+typedef struct stress_memory_info {
+	size_t freemem;
+	size_t totalmem;
+	size_t freeswap;
+	size_t totalswap;
+	size_t shmall;
+} stress_memory_info_t;
+
 extern size_t stress_memory_page_size_get(void);
-extern int stress_memory_info_get(size_t *freemem, size_t *totalmem,
-        size_t *freeswap, size_t *totalswap);
-extern void stress_memory_limits_get(size_t *shmall, size_t *freemem,
-	size_t *totalmem, size_t *freeswap, size_t *totalswap);
+extern int stress_memory_info_get(stress_memory_info_t *info);
 extern WARN_UNUSED char *stress_memory_free_get(void);
 extern void stress_memory_ksm_merge(const int flag);
 extern WARN_UNUSED bool stress_memory_low_check(const size_t requested);

core-resources.c
@@ -157,17 +157,13 @@ size_t stress_resources_allocate(
 	const size_t min_mem_free,
 	const bool do_fork)
 {
+	stress_memory_info_t info;
 #if defined(RLIMIT_MEMLOCK)
 	struct rlimit rlim;
 #endif
 	size_t mlock_size;
 	size_t i;
 	size_t n = 0;
-	size_t shmall;
-	size_t freemem;
-	size_t totalmem;
-	size_t freeswap;
-	size_t totalswap;
 	const pid_t pid = getpid();
 #if defined(HAVE_PIDFD_OPEN)
 	const pid_t ppid = getppid();
@@ -208,8 +204,8 @@ size_t stress_resources_allocate(
       defined(HAVE_MQUEUE_H))
 	(void)args;
 #endif
-	stress_memory_limits_get(&shmall, &freemem, &totalmem, &freeswap, &totalswap);
-	if ((freemem > 0) && (freemem < min_mem_free))
+	stress_memory_info_get(&info);
+	if ((info.freemem > 0) && (info.freemem < min_mem_free))
 		return 0;
 
 	(void)shim_memset(resources, 0, sizeof(*resources) * num_resources);
@@ -325,8 +321,8 @@ size_t stress_resources_allocate(
 		if (UNLIKELY(!stress_continue_flag()))
 			break;
 
-		stress_memory_limits_get(&shmall, &freemem, &totalmem, &freeswap, &totalswap);
-		if (UNLIKELY((freemem > 0) && (freemem < min_mem_free)))
+		stress_memory_info_get(&info);
+		if (UNLIKELY((info.freemem > 0) && (info.freemem < min_mem_free)))
 			break;
 
 		if (UNLIKELY((stress_mwc8() & 0xf) == 0)) {

stress-forkheavy.c
@@ -146,21 +146,17 @@ static void stress_forkheavy_free(void)
 
 static int stress_forkheavy_child(stress_args_t *args, void *context)
 {
+	stress_memory_info_t info;
 	const stress_forkheavy_args_t *forkheavy_args = (stress_forkheavy_args_t *)context;
 	stress_metrics_t *metrics = forkheavy_args->metrics;
 	uint32_t forkheavy_allocs = DEFAULT_FORKHEAVY_ALLOCS;
 	uint32_t forkheavy_procs = DEFAULT_FORKHEAVY_PROCS;
 	bool forkheavy_mlock = false;
 	size_t num_resources;
-	size_t shmall;
-	size_t freemem;
-	size_t totalmem;
-	size_t freeswap;
-	size_t totalswap;
 	size_t min_mem_free;
 
-	stress_memory_limits_get(&shmall, &freemem, &totalmem, &freeswap, &totalswap);
-	min_mem_free = (freemem / 100) * 2;
+	stress_memory_info_get(&info);
+	min_mem_free = (info.freemem / 100) * 2;
 	if (min_mem_free < MIN_MEM_FREE)
 		min_mem_free = MIN_MEM_FREE;
 

stress-mlock.c
@@ -297,17 +297,13 @@ static void stress_mlock_misc(stress_args_t *args, const size_t page_size, const
 
 static int stress_mlock_child(stress_args_t *args, void *context)
 {
+	stress_memory_info_t info;
 	size_t i;
 	size_t n;
 	uint8_t **mappings;
 	const size_t page_size = args->page_size;
 	size_t max = stress_mlock_max_lockable(), mappings_max;
 	size_t mappings_len = max * sizeof(*mappings);
-	size_t shmall;
-	size_t freemem;
-	size_t totalmem;
-	size_t freeswap;
-	size_t totalswap;
 	double mlock_duration = 0.0;
 	double mlock_count = 0.0;
 	double munlock_duration = 0.0;
@@ -320,7 +316,7 @@ static int stress_mlock_child(stress_args_t *args, void *context)
 	const size_t mappings_per_page = page_size / sizeof(*mappings);
 	const bool oom_avoid = !!(g_opt_flags & OPT_FLAGS_OOM_AVOID);
 
-	stress_memory_limits_get(&shmall, &freemem, &totalmem, &freeswap, &totalswap);
+	stress_memory_info_get(&info);
 
 	(void)context;
 
@@ -337,8 +333,8 @@ static int stress_mlock_child(stress_args_t *args, void *context)
 	 *  pages mappable, so allocate page mappings pointers that
 	 *  won't exceed this upper limit.
 	 */
-	if (mappings_len > totalmem / mappings_per_page)
-		mappings_len = totalmem / mappings_per_page;
+	if (mappings_len > info.totalmem / mappings_per_page)
+		mappings_len = info.totalmem / mappings_per_page;
 	mappings_len &= ~(page_size - 1);
 	if (mappings_len < page_size)
 		mappings_len = page_size;

stress-mlockmany.c
@@ -145,17 +145,13 @@ static int stress_mlockmany_child(stress_args_t *args, void *context)
 	stress_proc_state_set(args->name, STRESS_STATE_RUN);
 
 	do {
+		stress_memory_info_t info;
 		unsigned int n;
-		size_t shmall;
-		size_t freemem;
-		size_t totalmem;
-		size_t freeswap;
-		size_t totalswap;
 		size_t last_freeswap;
-		size_t last_totalswap;
 
 		stress_sync_init_pids(s_pids, mlockmany_procs);
-		stress_memory_limits_get(&shmall, &freemem, &totalmem, &last_freeswap, &last_totalswap);
+		stress_memory_info_get(&info);
+		last_freeswap = info.freeswap;
 
 		for (n = 0; LIKELY(stress_continue(args) && (n < mlockmany_procs)); n++) {
 			pid_t pid;
@@ -166,15 +162,15 @@ static int stress_mlockmany_child(stress_args_t *args, void *context)
 				break;
 			}
 
-			stress_memory_limits_get(&shmall, &freemem, &totalmem, &freeswap, &totalswap);
+			stress_memory_info_get(&info);
 
 			/* We detected swap being used, bail out */
-			if (last_freeswap > freeswap)
+			if (last_freeswap > info.freeswap)
 				break;
 
 			/* Keep track of expanding free swap space */
-			if (freeswap > last_freeswap)
-				last_freeswap = freeswap;
+			if (info.freeswap > last_freeswap)
+				last_freeswap = info.freeswap;
 
 			pid = fork();
 			if (pid == 0) {
@@ -198,9 +194,9 @@ static int stress_mlockmany_child(stress_args_t *args, void *context)
 				/* unlock all mlocked memory */
 				shim_munlockall();
 
-				stress_memory_limits_get(&shmall, &freemem, &totalmem, &freeswap, &totalswap);
+				stress_memory_info_get(&info);
 				/* We detected swap being used, bail out */
-				if (last_freeswap > freeswap)
+				if (last_freeswap > info.freeswap)
 					_exit(0);
 
 				while (mmap_size > args->page_size) {

commit 1a3deadcd4f0ee80a1a9485370a31f1af634be87

core-memory: return 0 when memory info is correctly fetched
Should be returning 0 when all OK

Fixes: 8e39cafba587 ("core-memory: put memory info into stress_memory_info_t")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

core-memory.c
@@ -128,6 +128,7 @@ int stress_memory_info_get(stress_memory_info_t *info)
 			info->shmall = 0;
 #endif
 		}
+		return 0;
 	}
 #endif
 #if defined(__FreeBSD__)