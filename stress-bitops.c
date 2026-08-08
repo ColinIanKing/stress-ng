ColinIanKing/stress-ng

commit f616045865387605fde83aab8ba8446e128fe62b

stress-bitops: use builtin to generate optimal log2(n)
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-bitops.c
@@ -707,7 +707,7 @@ static int OPTIMIZE3 stress_bitops_log2(const char *name, uint32_t *count)
 		static const int8_t bitposition[32] ALIGN64 = {
 			 0,  9,  1, 10, 13, 21,  2, 29,
 			11, 14, 16, 18, 22, 25,  3, 30,
-			 8, 12, 20, 28, 15, 17, 24, 7,
+			 8, 12, 20, 28, 15, 17, 24,  7,
 			19, 27, 23,  6, 26,  5,  4, 31
 		};
 		uint32_t ln2_1;
@@ -715,11 +715,16 @@ static int OPTIMIZE3 stress_bitops_log2(const char *name, uint32_t *count)
 		uint32_t tmp;
 		uint32_t shift;
 
+#if defined(HAVE_BUILTIN_CLZ)
+		ln2_1 = (8 * sizeof(v)) - __builtin_clz(v) - 1;
+		sum += ln2_1;
+#else
 		tmp = v;
 		ln2_1 = 0;
 		while (tmp >>= 1)
 			ln2_1++;
 		sum += ln2_1;
+#endif
 
 		tmp = v;
 		ln2_2 = 0;

commit f72525256c143abe06c99c222da727b51e8a1057

stress-bitops: fix log2(0) case
The log2(0) case is inconsistent, fix this by checking
for this corner case.

Fixes: f61604586538 ("stress-bitops: use builtin to generate optimal log2(n)")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-bitops.c
@@ -716,7 +716,7 @@ static int OPTIMIZE3 stress_bitops_log2(const char *name, uint32_t *count)
 		uint32_t shift;
 
 #if defined(HAVE_BUILTIN_CLZ)
-		ln2_1 = (8 * sizeof(v)) - __builtin_clz(v) - 1;
+		ln2_1 = (UNLIKELY(v == 0)) ? 0 : (8 * sizeof(v)) - __builtin_clz(v) - 1;
 		sum += ln2_1;
 #else
 		tmp = v;