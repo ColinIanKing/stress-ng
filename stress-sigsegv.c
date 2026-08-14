ColinIanKing/stress-ng

commit 7288c64d09eba0a3770190f2f28dcf3bb0432117

stress-sigsegv: stress-sigsegv: set max mask shift of 46 bits for NetBSD and OpenBSD
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-sigsegv.c
@@ -60,7 +60,9 @@ static volatile int signo;
 static volatile int code;
 #endif
 
-#if defined(__FreeBSD__)
+#if defined(__FreeBSD__) ||	\
+    defined(__NetNBSD__) ||	\
+    defined(__OpenBSD__)
 #define MAX_MASK_SHIFT	(46)
 #else
 #define MAX_MASK_SHIFT	(50)

commit 1c827c1d9a4ddee37d55243762fd8fa4dbd8249d

stress-sigsegv: Fix typo, __NetNBSD__ -> __NetBSD__
Fix NetBSD build check for address space sizing.
Fixes: commit 7288c64d09eb ("stress-sigsegv: stress-sigsegv: set max mask shift of 46 bits for NetBSD and OpenBSD")

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-sigsegv.c
@@ -61,7 +61,7 @@ static volatile int code;
 #endif
 
 #if defined(__FreeBSD__) ||	\
-    defined(__NetNBSD__) ||	\
+    defined(__NetBSD__) ||	\
     defined(__OpenBSD__) ||	\
     defined(__sun__)
 #define MAX_MASK_SHIFT	(46)