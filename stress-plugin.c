43c95782e68b02b89fcf1680a085bc71f53c3e42
Colin Ian King <colin.i.king@gmail.com>
2026-08-14 21:41:06 +0100

    stress-plugin: voidify return from fprintf
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-plugin.c
+++ stress-plugin.c
@@ -167,7 +167,7 @@ static void stress_plugin_so(const char *opt_name, const char *opt_arg, stress_t
 
 	stress_plugin_so_dl = dlopen(opt_arg, RTLD_LAZY | RTLD_GLOBAL);
 	if (!stress_plugin_so_dl) {
-		fprintf(stderr, "option %s: cannot load shared object file '%s' "
+		(void)fprintf(stderr, "option %s: cannot load shared object file '%s' "
 			"(please specify full path to .so file)\n", opt_name, opt_arg);
 		longjmp(g_error_env, 1);
 		stress_no_return();
@@ -175,7 +175,7 @@ static void stress_plugin_so(const char *opt_name, const char *opt_arg, stress_t
 
 	dlinfo(stress_plugin_so_dl, RTLD_DI_LINKMAP, &map);
 	if (!map) {
-		fprintf(stderr, "plugin-so: cannot find linkmap in file '%s'\n", opt_arg);
+		(void)fprintf(stderr, "plugin-so: cannot find linkmap in file '%s'\n", opt_arg);
 		longjmp(g_error_env, 1);
 		stress_no_return();
 	}
@@ -197,17 +197,17 @@ static void stress_plugin_so(const char *opt_name, const char *opt_arg, stress_t
 	}
 
 	if (!symtab) {
-		fprintf(stderr, "plugin-so: cannot find symbol table in file '%s'\n", opt_arg);
+		(void)fprintf(stderr, "plugin-so: cannot find symbol table in file '%s'\n", opt_arg);
 		longjmp(g_error_env, 1);
 		stress_no_return();
 	}
 	if (!strtab) {
-		fprintf(stderr, "plugin-so: cannot find string table in file '%s'\n", opt_arg);
+		(void)fprintf(stderr, "plugin-so: cannot find string table in file '%s'\n", opt_arg);
 		longjmp(g_error_env, 1);
 		stress_no_return();
 	}
 	if (!symentries) {
-		fprintf(stderr, "plugin-so: cannot find symbol table entry count in file '%s'\n", opt_arg);
+		(void)fprintf(stderr, "plugin-so: cannot find symbol table entry count in file '%s'\n", opt_arg);
 		longjmp(g_error_env, 1);
 		stress_no_return();
 	}
@@ -223,14 +223,14 @@ static void stress_plugin_so(const char *opt_name, const char *opt_arg, stress_t
 		}
 	}
 	if (!n_funcs) {
-		fprintf(stderr, "plugin-so: cannot find any function symbols in file '%s'\n", opt_arg);
+		(void)fprintf(stderr, "plugin-so: cannot find any function symbols in file '%s'\n", opt_arg);
 		longjmp(g_error_env, 1);
 		stress_no_return();
 	}
 
 	stress_plugin_methods = (stress_plugin_method_info_t *)calloc(n_funcs + 1, sizeof(*stress_plugin_methods));
 	if (!stress_plugin_methods) {
-		fprintf(stderr, "plugin-so: allocate %zu plugin methods failed%s\n",
+		(void)fprintf(stderr, "plugin-so: allocate %zu plugin methods failed%s\n",
 			n_funcs, stress_memory_free_get());
 		longjmp(g_error_env, 1);
 		stress_no_return();
@@ -250,7 +250,7 @@ static void stress_plugin_so(const char *opt_name, const char *opt_arg, stress_t
 				stress_plugin_methods[n_funcs].name = str + 7;
 				stress_plugin_methods[n_funcs].func = (stress_plugin_func)dlsym(stress_plugin_so_dl, str);
 				if (!stress_plugin_methods[n_funcs].func) {
-					fprintf(stderr, "plugin-so: cannot get address of function %s()\n", str);
+					(void)fprintf(stderr, "plugin-so: cannot get address of function %s()\n", str);
 					longjmp(g_error_env, 1);
 					stress_no_return();
 				}
@@ -444,7 +444,7 @@ static void stress_plugin_so(const char *opt_name, const char *opt_arg, stress_t
 	*type_id = TYPE_ID_STR;
 	*(char **)value = stress_const_optdup(opt_arg);
 
-	fprintf(stderr, "option %s '%s' not supported on unimplemented stressor\n", opt_name, opt_arg);
+	(void)fprintf(stderr, "option %s '%s' not supported on unimplemented stressor\n", opt_name, opt_arg);
 }
 
 static const stress_opt_t opts[] = {
