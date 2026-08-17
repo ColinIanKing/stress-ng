ColinIanKing/stress-ng

commit 6e2787ed6fe811ed75ed0e9bc61c0eab6d7839df

Add --sync-start option
It's useful to be able to spawn stressor instances and wait until
they are in a ready-to-run state and then synchronously start them
all together in the run stressing state.

Add a new --sync-start option to allow this. This change adds
some helper functions that wait for the sync start by SIGSTOP'ing
the processes re-starting them using SIGCONT.

For stressors that spawn their own processes there are helpers
to mmap and unmap a shared memory stress_pid_t pids array and
to link the pids together, and to start them in a synchronous
manner using the list of pids.

Closes https://github.com/ColinIanKing/stress-ng/issues/397

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-sem-sysv.c
@@ -523,24 +523,29 @@ timed_out:
  *  semaphore_sysv_spawn()
  *	spawn a process
  */
-static pid_t semaphore_sysv_spawn(stress_args_t *args)
+static pid_t semaphore_sysv_spawn(
+	stress_args_t *args,
+	stress_pid_t **s_pids_head,
+	stress_pid_t *s_pid)
 {
-	pid_t pid;
-
 again:
-	pid = fork();
-	if (pid < 0) {
+	s_pid->pid = fork();
+	if (s_pid->pid < 0) {
 		if (stress_redo_fork(args, errno))
 			goto again;
 		return -1;
-	}
-	if (pid == 0) {
+	} else if (s_pid->pid == 0) {
+		s_pid->pid = getpid();
+		stress_sync_start_wait_s_pid(s_pid);
+
 		stress_parent_died_alarm();
 		(void)sched_settings_apply(true);
 
 		_exit(stress_semaphore_sysv_thrash(args));
+	} else {
+		stress_sync_start_s_pid_list_add(s_pids_head, s_pid);
 	}
-	return pid;
+	return s_pid->pid;
 }
 
 /*
@@ -549,7 +554,7 @@ again:
  */
 static int stress_sem_sysv(stress_args_t *args)
 {
-	pid_t pids[MAX_SEM_SYSV_PROCS];
+	stress_pid_t *s_pids, *s_pids_head = NULL;
 	uint64_t i;
 	uint64_t semaphore_sysv_procs = DEFAULT_SEM_SYSV_PROCS;
 	int rc = EXIT_SUCCESS;
@@ -569,25 +574,35 @@ static int stress_sem_sysv(stress_args_t *args)
 			semaphore_sysv_procs = MIN_SEM_SYSV_PROCS;
 	}
 
-
 	if (stress_sighandler(args->name, SIGCHLD, stress_sighandler_nop, NULL) < 0)
 		return EXIT_NO_RESOURCE;
 
-	stress_set_proc_state(args->name, STRESS_STATE_RUN);
+	s_pids = stress_s_pids_mmap(MAX_SEM_SYSV_PROCS);
+	if (s_pids == MAP_FAILED) {
+		pr_inf_skip("%s: failed to mmap %d PIDs, skipping stressor\n", args->name, MAX_SEM_SYSV_PROCS);
+		return EXIT_NO_RESOURCE;
+	}
 
-	(void)shim_memset(pids, 0, sizeof(pids));
 	for (i = 0; i < semaphore_sysv_procs; i++) {
-		pids[i] = semaphore_sysv_spawn(args);
-		if (!stress_continue_flag() || pids[i] < 0)
+		stress_sync_start_init(&s_pids[i]);
+
+		if (semaphore_sysv_spawn(args, &s_pids_head, &s_pids[i]) < 0)
+			goto reap;
+		if (!stress_continue_flag())
 			goto reap;
 	}
+
+	stress_set_proc_state(args->name, STRESS_STATE_RUN);
+	stress_sync_start_wait(args);
+	stress_sync_start_cont_list(s_pids_head);
 
 	/* Wait for termination */
 	while (stress_continue(args))
 		pause();
 reap:
 	stress_set_proc_state(args->name, STRESS_STATE_DEINIT);
-	stress_kill_and_wait_many(args, pids, semaphore_sysv_procs, SIGALRM, true);
+	stress_kill_and_wait_many(args, s_pids, semaphore_sysv_procs, SIGALRM, true);
+	stress_s_pids_munmap(s_pids, MAX_SEM_SYSV_PROCS);
 
 	return rc;
 }

commit 779f0a0743cc0ca6f493480f7b800960f81fa311

stress-sem-sysv: fix race in shared s_pids->pid assignment
The assignment of s_pids->pid is being performed at the end of the
fork return, the parent assignes it the childs pid where as the child
assigns it zero.

There is a small race window where a child may take a while to start
while the main controlling stressor parent is tidying up doing a
kill and wait for the child PIDS. During the reap/wait loop the
child assigns 0 to the s_pid->pid. This causes the kill/wait to
not successfully kill (and/or) wait for the child to die, the
parent stressor then returns to be reaped itself without, meanwhile
the child maybe incrementing the  bogo-op counter. This causes

stress-ng: fail:  [21453] sem-sysv instance 3 corrupted bogo-ops counter, 751 vs 750
stress-ng: fail:  [21453] sem-sysv instance 3 hash error in bogo-ops counter and run flag, 2210998880 vs 47247250
stress-ng: fail:  [21453] metrics-check: stressor metrics corrupted, data is compromised

Fixes: 6e2787ed6fe8 ("Add --sync-start option")
Closes: https://github.com/ColinIanKing/stress-ng/pull/657

Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

stress-sem-sysv.c
@@ -529,12 +529,14 @@ static pid_t semaphore_sysv_spawn(
 	stress_pid_t *s_pid,
 	const bool semaphore_sysv_setall)
 {
-	s_pid->pid = stress_retry_fork(args, 0);
-	if (s_pid->pid < 0) {
+	pid_t pid;
+
+	pid = stress_retry_fork(args, 0);
+	if (pid < 0) {
 		return -1;
-	} else if (s_pid->pid == 0) {
-		stress_proc_state_set(args->name, STRESS_STATE_SYNC_WAIT);
+	} else if (pid == 0) {
 		s_pid->pid = getpid();
+		stress_proc_state_set(args->name, STRESS_STATE_SYNC_WAIT);
 		stress_sync_start_wait_s_pid(s_pid);
 		stress_proc_state_set(args->name, STRESS_STATE_RUN);
 		stress_make_it_fail_set();
@@ -544,6 +546,7 @@ static pid_t semaphore_sysv_spawn(
 
 		_exit(stress_semaphore_sysv_thrash(args, semaphore_sysv_setall));
 	} else {
+		s_pid->pid = pid;
 		stress_sync_start_s_pid_list_add(s_pids_head, s_pid);
 	}
 	return s_pid->pid;