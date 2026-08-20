aba5bdbdf82aabebb099daf5c008bb6d693dcf91
Colin Ian King <colin.i.king@gmail.com>
2026-08-20 21:53:59 +0100

    stress-clock: rework nested if statements
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-clock.c
+++ stress-clock.c
@@ -316,14 +316,12 @@ static int stress_clock(stress_args_t *args)
 				/* Ensuring clock_settime cannot succeed without privilege */
 				if (!is_root) {
 					ret = shim_clock_settime(clocks[i].id, &t1);
-					if (ret < 0) {
-						if (UNLIKELY(((errno != EPERM) && (errno != EINVAL)))) {
-							/* This is an error, report it! */
-							pr_fail("%s: clock_settime failed, did not have privilege to "
-								"set time, expected EPERM or EINVAL, instead got errno=%d (%s)\n",
-								args->name, errno, strerror(errno));
-							rc = EXIT_FAILURE;
-						}
+					if ((ret < 0) && UNLIKELY(((errno != EPERM) && (errno != EINVAL)))) {
+						/* This is an error, report it! */
+						pr_fail("%s: clock_settime failed, did not have privilege to "
+							"set time, expected EPERM or EINVAL, instead got errno=%d (%s)\n",
+							args->name, errno, strerror(errno));
+						rc = EXIT_FAILURE;
 					}
 				}
 
