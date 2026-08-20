2652596f56bc361c8dd069df121a6068e8a6a1f7
Colin Ian King <colin.i.king@gmail.com>
2026-08-20 21:49:46 +0100

    stress-chown: rework nested if statements
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-chown.c
+++ stress-chown.c
@@ -185,13 +185,11 @@ static int stress_chown(stress_args_t *args)
 	 *  Allow for multiple workers to chown the *same* file
 	 */
 	stress_fs_temp_dir(pathname, sizeof(pathname), args->name, ppid, 0);
-	if (mkdir(pathname, S_IRWXU) < 0) {
-		if (errno != EEXIST) {
-			rc = stress_exit_status(errno);
-			pr_fail("%s: mkdir '%s' failed, errno=%d (%s)\n",
-				args->name, pathname, errno, strerror(errno));
-			return rc;
-		}
+	if ((mkdir(pathname, S_IRWXU) < 0) && (errno != EEXIST)) {
+		rc = stress_exit_status(errno);
+		pr_fail("%s: mkdir '%s' failed, errno=%d (%s)\n",
+			args->name, pathname, errno, strerror(errno));
+		return rc;
 	}
 	(void)stress_fs_temp_filename(filename, sizeof(filename),
 		args->name, ppid, 0, 0);
