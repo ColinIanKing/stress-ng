fdd4501dcf1886745d6c8c0c8b76c87a7635200a
Colin Ian King <colin.i.king@gmail.com>
2026-08-28 16:01:37 +0100

    stress-bpf: re-cycle cached bpf instructions
    save and re-use cached bpf instructions 50% of the time
    so that we waste less time generating invalid bpf code.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- stress-bpf.c
+++ stress-bpf.c
@@ -29,7 +29,68 @@
 #define MAX_BPF_PROG_SIZE	(1024* 1024)
 #define DEFAULT_BPF_PROG_SIZE	(2048)
 
+/* number of unique bpf instruction to cache */
+#define MAX_UNIQUE_INSNS	(8192)
+
+/* prime size hash table of unique bpf instructions */
+//#define BPF_HASH_TABLE_SIZE	(65537)
+#define BPF_HASH_TABLE_SIZE	(31)
+
+/* number of bpf opcodes */
 #define MAX_BPF_OPCODES		(256)
+
+/*
+ *  hash table entry of a valid bpf instruction
+ */
+typedef struct stress_bpf_insn_node {
+	struct stress_bpf_insn_node *next;
+	struct bpf_insn insn;
+} stress_bpf_insn_node_t;
+
+static stress_bpf_insn_node_t **stress_bpf_insn_hash_table;
+static stress_bpf_insn_node_t *unique_insns;
+static size_t unique_insns_count;
+
+/*
+ *  stress_bpf_hash_insn()
+ *	bpf instruction -> hash index
+ */
+static inline size_t ALWAYS_INLINE stress_bpf_hash_insn(struct bpf_insn *insn)
+{
+	register size_t hash = insn->code ^
+	       (((size_t)insn->dst_reg) << 8) ^
+	       (((size_t)insn->src_reg) << 12) ^
+	       (((size_t)insn->off << 16)) ^
+	       (((size_t)insn->imm << 24));
+
+	return hash % (size_t)BPF_HASH_TABLE_SIZE;
+}
+
+static void OPTIMIZE3 stress_bpf_insn_add(struct bpf_insn *insn)
+{
+	register stress_bpf_insn_node_t *insn_node;
+	register size_t idx;
+
+	/* any free unqiue instructions in table? */
+	if (UNLIKELY(unique_insns_count >= MAX_UNIQUE_INSNS))
+		return;
+
+	idx = stress_bpf_hash_insn(insn);
+
+	for (insn_node = stress_bpf_insn_hash_table[idx]; insn_node; insn_node = insn_node->next) {
+		if (shim_memcmp(insn, &insn_node->insn, sizeof(struct bpf_insn)) == 0)
+			break;
+	}
+	if (insn_node)
+		return;	/* don't add duplicates */
+
+	insn_node = &unique_insns[unique_insns_count];
+	unique_insns_count++;
+	insn_node->insn = *insn;
+	insn_node->next = stress_bpf_insn_hash_table[idx];
+	stress_bpf_insn_hash_table[idx] = insn_node;
+	return;
+}
 
 static const stress_help_t help[] = {
 	{ NULL, "bpf N",	"start N workers exercising random BPF instructions" },
@@ -124,15 +185,25 @@ static int OPTIMIZE3 stress_bpf_push_op(
 
 	for (i = 0; i < MAX_BPF_OPCODES; i++) {
 		int fd;
+		bool get_cached;
 
 		if (!stress_continue(args))
 			return 0;
 
-		insns[len].code = opcode;
-		insns[len].dst_reg = 0;
-		insns[len].src_reg = stress_mwc8modn(11);
-		insns[len].off = 0;
-		insns[len].imm = 0;
+		get_cached = ((unique_insns_count > 0) && stress_mwc8() > 128);
+		if (get_cached) {
+			/* fetch an exisiting cached instruction */
+			register const size_t idx = stress_mwcsizemodn(unique_insns_count);
+
+			insns[len] = unique_insns[idx].insn;
+		} else {
+			/* try and used a new one */
+			insns[len].code = opcode;
+			insns[len].dst_reg = 0;
+			insns[len].src_reg = stress_mwc8modn(11);
+			insns[len].off = 0;
+			insns[len].imm = 0;
+		}
 
 		insns[len + 1].code = BPF_JMP | BPF_EXIT;
 		insns[len + 1].dst_reg = 0;
@@ -141,6 +212,7 @@ static int OPTIMIZE3 stress_bpf_push_op(
 		insns[len + 1].imm = 0;
 
 		fd = bpf_prog_load(BPF_PROG_TYPE_KPROBE, insns, prog_len, version);
+		stress_bogo_inc(args);
 		if (fd == -1) {
 			if (errno == ENOSYS) {
 				pr_inf_skip("%s: bpf() system call not supported, errno=%d (%s), "
@@ -152,11 +224,13 @@ static int OPTIMIZE3 stress_bpf_push_op(
 			if (bpf_size_max < prog_len)
 				bpf_size_max = prog_len;
 			(void)close(fd);
-
 			bpf_success++;
+
+			/* add new instruction if it wasn't already cached */
+			if (!get_cached)
+				stress_bpf_insn_add(&insns[len]);
 			stress_bpf_push_op(args, insns, insns_max, version, len + 1, opcode + 1);
 		}
-		stress_bogo_inc(args);
 		opcode = (opcode + 1) & 0xff;
 	}
 	insns[len] = saved;
@@ -194,9 +268,13 @@ static int stress_bpf(stress_args_t *args)
 	int rc = EXIT_SUCCESS;
 	double percent;
 	int version;
+	struct bpf_insn *insns;
 	size_t insns_max = DEFAULT_BPF_PROG_SIZE;
 	size_t insns_sz;
-	struct bpf_insn *insns;
+	const size_t unique_insns_sz = sizeof(*unique_insns) * MAX_UNIQUE_INSNS;
+	const size_t hash_table_sz = sizeof(stress_bpf_insn_node_t *) * BPF_HASH_TABLE_SIZE;
+
+	unique_insns_count = 0;
 
 	version = stress_bpf_kernel_version_binary();
 	if (version < 0) {
@@ -213,13 +291,36 @@ static int stress_bpf(stress_args_t *args)
 	}
 	insns_sz = sizeof(*insns) * insns_max;
 
-	insns = stress_mmap_populate(NULL, insns_sz, PROT_READ | PROT_WRITE,
+	insns = (struct bpf_insn *)stress_mmap_populate(NULL,
+				insns_sz, PROT_READ | PROT_WRITE,
 				MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 	if (insns == MAP_FAILED) {
 		pr_inf_skip("%s: failed to mmap %zu bytes for BPF instructions, "
 			"errno=%d (%s), skipping stressor\n",
 			args->name, insns_sz, errno, strerror(errno));
 		return EXIT_NO_RESOURCE;
+	}
+
+	unique_insns = (stress_bpf_insn_node_t *)stress_mmap_populate(NULL,
+				unique_insns_sz, PROT_READ | PROT_WRITE,
+				MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	if (unique_insns == MAP_FAILED) {
+		pr_inf_skip("%s: failed to mmap %zu bytes for unique BPF instructions, "
+			"errno=%d (%s), skipping stressor\n",
+			args->name, unique_insns_sz, errno, strerror(errno));
+		rc = EXIT_NO_RESOURCE;
+		goto unmap_insns;
+	}
+
+	stress_bpf_insn_hash_table = (stress_bpf_insn_node_t **)stress_mmap_populate(NULL,
+				hash_table_sz, PROT_READ | PROT_WRITE,
+				MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	if (stress_bpf_insn_hash_table == MAP_FAILED) {
+		pr_inf_skip("%s: failed to mmap %zu bytes for hash table, "
+			"errno=%d (%s), skipping stressor\n",
+			args->name, unique_insns_sz, errno, strerror(errno));
+		rc = EXIT_NO_RESOURCE;
+		goto unmap_unique_insns;
 	}
 
 	stress_proc_state_set(args->name, STRESS_STATE_SYNC_WAIT);
@@ -239,12 +340,18 @@ static int stress_bpf(stress_args_t *args)
 
 	stress_proc_state_set(args->name, STRESS_STATE_DEINIT);
 
-	(void)munmap((void *)insns, insns_sz);
 
-	percent = (bpf_fail > 0) ? (double)bpf_success * 100.0 / (double)bpf_fail : 0.0;
+	percent = (bpf_fail > 0) ? (double)bpf_success * 100.0 / (double)(bpf_fail + bpf_success) : 0.0;
 
-	stress_metrics_set(args, "% BPF program success rate", percent, STRESS_METRIC_GEOMETRIC_MEAN);
+	stress_metrics_set(args, "% BPF program success rate (should be around 50%)", percent, STRESS_METRIC_GEOMETRIC_MEAN);
 	stress_metrics_set(args, "maxiumum BPF code instructions", (double)bpf_size_max, STRESS_METRIC_MAXIMUM);
+	stress_metrics_set(args, "unique BPF instructions used", (double)unique_insns_count, STRESS_METRIC_MAXIMUM);
+
+	(void)munmap((void *)stress_bpf_insn_hash_table, hash_table_sz);
+unmap_unique_insns:
+	(void)munmap((void *)unique_insns, unique_insns_sz);
+unmap_insns:
+	(void)munmap((void *)insns, insns_sz);
 
 	return rc;
 }
